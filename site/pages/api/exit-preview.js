cdedcdfca94195d2e443c9b8b5ebf7294ce7169b
Jared Palmer <jared@jaredpalmer.com>
2021-11-29 20:15:21 -0500

    Remove old site

--- site/pages/api/exit-preview.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/vercel/next.js/tree/canary/examples/cms-contentful/pages/api
-export default async function exit(_, res) {
-  // Exit the current user from "Preview Mode". This function accepts no args.
-  res.clearPreviewData()
-
-  // Redirect the user back to the index page.
-  res.writeHead(307, { Location: '/' })
-  res.end()
-}
