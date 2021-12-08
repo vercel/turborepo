c1e7f6b02a08b540fba2472a5571272784b6ea17
Jared Palmer <jared@jaredpalmer.com>
2021-12-08 09:20:23 -0800

    Tweak turbo.sh

--- turbow.sh
+++ turbow.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 cd cli && go build ./cmd/turbo && cd ..;
-./cli/turbo $@
+
+UNAME=$(uname)
+
+if [ "$UNAME" == "Linux" ] ; then
+	./cli/turbo $@
+elif [ "$UNAME" == "Darwin" ] ; then
+	./cli/turbo $@
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+	./cli/turbo.exe $@
+fi
