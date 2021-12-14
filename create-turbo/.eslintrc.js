4b1478c184d3907a70899f501d3c03d1a69c8863
seanyusa <seanyusa@gmail.com>
2021-12-13 20:34:17 -0600

    lint for test files

--- create-turbo/.eslintrc.js
+++ create-turbo/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  env: { node: true },
+  env: { node: true, jest: true },
   extends: ["eslint:recommended"],
   rules: {
     "no-empty": ["error", { allowEmptyCatch: true }],
