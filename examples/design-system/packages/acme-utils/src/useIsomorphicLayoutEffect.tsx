3f3904ce49797e71e47334321dea2c9890687a9a
Jared Palmer <jared@jaredpalmer.com>
2021-12-14 12:01:42 -0500

    Add design system example

---
+++ examples/design-system/packages/acme-utils/src/useIsomorphicLayoutEffect.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+/**
+ * On the server, React emits a warning when calling `useLayoutEffect`.
+ * This is because neither `useLayoutEffect` nor `useEffect` run on the server.
+ * We use this safe version which suppresses the warning by replacing it with a noop on the server.
+ *
+ * See: https://reactjs.org/docs/hooks-reference.html#uselayouteffect
+ */
+const useIsomorphicLayoutEffect = Boolean(globalThis?.document)
+  ? React.useLayoutEffect
+  : () => {};
+
+export { useIsomorphicLayoutEffect };
