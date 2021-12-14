d6f38fa66e566dc92b77394b7ed75b8277dddc93
seanyusa <seanyusa@gmail.com>
2021-12-13 21:11:10 -0600

    eslint import resolver

--- create-turbo/.eslintrc.js
+++ create-turbo/.eslintrc.js
@@ -2,8 +2,25 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   env: { node: true, jest: true },
-  extends: ["eslint:recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  plugins: ["import"],
   rules: {
     "no-empty": ["error", { allowEmptyCatch: true }],
+    "import/no-named-as-default-member": "off",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+    },
   },
 };
