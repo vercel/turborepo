2362d4777d961e74c507f00c329d908cf024b09d
Ayoub Baali <ba3li.ay@gmail.com>
2021-12-15 14:20:42 +0100

    disable yarn if not installed only fixes #285

--- create-turbo/src/shouldUseYarn.ts
+++ create-turbo/src/shouldUseYarn.ts
@@ -3,12 +3,14 @@ import { execSync } from "child_process";
 export function shouldUseYarn(): boolean {
   try {
     const userAgent = process.env.npm_config_user_agent;
-    if (userAgent) {
-      return Boolean(userAgent && userAgent.startsWith("yarn"));
+    if (userAgent && userAgent.startsWith("yarn")) {
+      return true;
     }
+    
     execSync("yarnpkg --version", { stdio: "ignore" });
     return true;
   } catch (e) {
+
     return false;
   }
 }
