e58de98b376b14e41f219add6e166ce015184062
Jared Palmer <jared@jaredpalmer.com>
2021-12-17 08:35:04 -0500

    Tweak html graph and automatically open in browser if tty

---
+++ cli/internal/util/browser/open.go
@@ -0,0 +1,27 @@
+package browser
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"runtime"
+)
+
+func OpenBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
