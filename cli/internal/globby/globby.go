b0f4473a9ba418d531753cfe02ef4e0a1bc3d628
Jared Palmer <jared@jaredpalmer.com>
2021-12-16 23:48:39 -0500

    Rename to baseDir

--- cli/internal/globby/globby.go
+++ cli/internal/globby/globby.go
@@ -9,22 +9,22 @@ import (
 	"github.com/bmatcuk/doublestar/v4"
 )
 
-func GlobFiles(ws_path string, includePatterns []string, excludePatterns []string) []string {
+func GlobFiles(basePath string, includePatterns []string, excludePatterns []string) []string {
 	var include []string
 	var exclude []string
 	var result []string
 
 	for _, p := range includePatterns {
-		include = append(include, filepath.Join(ws_path, p))
+		include = append(include, filepath.Join(basePath, p))
 	}
 
 	for _, p := range excludePatterns {
-		exclude = append(exclude, filepath.Join(ws_path, p))
+		exclude = append(exclude, filepath.Join(basePath, p))
 	}
 
 	includePattern := "{" + strings.Join(include, ",") + "}"
 	excludePattern := "{" + strings.Join(exclude, ",") + "}"
-	_ = fs.Walk(ws_path, func(p string, isDir bool) error {
+	_ = fs.Walk(basePath, func(p string, isDir bool) error {
 		if val, _ := doublestar.PathMatch(excludePattern, p); val {
 			if isDir {
 				return filepath.SkipDir
