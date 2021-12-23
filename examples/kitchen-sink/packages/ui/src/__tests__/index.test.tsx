6e2f4a7869b0d8ba205516c6dfde2065e236124f
Thomas Knickman <tknickman@gmail.com>
2021-12-23 10:28:29 -0500

    examples: Inject react runtime for ui package (#371)
    Co-authored-by: Jared Palmer <jared@jaredpalmer.com>

--- examples/kitchen-sink/packages/ui/src/__tests__/index.test.tsx
+++ examples/kitchen-sink/packages/ui/src/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { CounterButton } from "../index";
+import { CounterButton } from "../CounterButton";
 
 describe("CounterButton", () => {
   it("renders without crashing", () => {
