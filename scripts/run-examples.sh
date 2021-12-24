e87c5816f99f6d182c1feb92749d60a218742316
Jared Palmer <jared@jaredpalmer.com>
2021-12-24 13:02:14 -0500

    Add examples checking script

--- scripts/run-examples.sh
+++ scripts/run-examples.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-
+echo "=> Running examples..."
+echo "=> Building turbo from source..."
+cd cli && CGO_ENABLED=0 go build ./cmd/turbo/... && cd ..;
+export TURBO_BINARY_PATH=$(pwd)/cli/turbo
+echo "=> Binary path: TURBO_BINARY_PATH=$TURBO_BINARY_PATH"
+echo "=> Moving our own eslint settings out of the way..."
+echo "=> Actually running examples for real..."
 if [ -f ".eslintrc.js" ]; then
   mv .eslintrc.js .eslintrc.js.bak
 fi
@@ -17,10 +23,14 @@ function cleanup {
     rm -rf packages/*/.turbo      
     rm -rf *.log
     rm -rf yarn.lock
+    rm -rf package-lock.json   
+    rm -rf pnpm-lock.yaml
     rm -rf .git
+    
 }
 
 function setup_git { 
+   echo "=> Setting up git..."
    git init .
    git add . 
    git commit -m "Initial commit"      
@@ -28,93 +38,94 @@ function setup_git {
 
 for folder in examples/* ; do
   if [ -f "$folder/package.json" ]; then
+    cd $folder 
+    echo "======================================================="
+    echo "=> checking $folder "
+    echo "======================================================="
     if [ "$folder" != "examples/with-pnpm" ]; then
-      cd $folder 
+      
       cleanup
       setup_git
       
-      echo "-------------------------------------------------------"
-      echo "$folder: yarn install"
-      echo "-------------------------------------------------------"
+      echo "======================================================="
+      echo "=> $folder: yarn install"
+      echo "======================================================="
       yarn install
       
-      echo "-------------------------------------------------------"
-      echo "$folder: yarn build lint"
-      echo "-------------------------------------------------------"
+      echo "======================================================="
+      echo "=> $folder: yarn build lint"
+      echo "======================================================="
       yarn build lint
       
-      echo "-------------------------------------------------------"
-      echo "$folder: yarn build again"
-      echo "-------------------------------------------------------"
+      echo "======================================================="
+      echo "=> $folder: yarn build again"
+      echo "======================================================="
       yarn build lint
                 
-      echo "-------------------------------------------------------"    
-      echo "$folder: SUCCESSFUL"
-      echo "-------------------------------------------------------"    
+      echo "======================================================="    
+      echo "=> $folder: yarn SUCCESSFUL"
+      echo "======================================================="    
       
       cleanup
       setup_git
 
-      echo "-------------------------------------------------------"
-      echo "$folder: npm install"
-      echo "-------------------------------------------------------"
+      echo "======================================================="
+      echo "=> $folder: npm install"
+      echo "======================================================="
       npm install
       
-      echo "-------------------------------------------------------"
-      echo "$folder: npm build lint"
-      echo "-------------------------------------------------------"
+      echo "======================================================="
+      echo "=> $folder: npm build lint"
+      echo "======================================================="
       npm run build lint
       
-      echo "-------------------------------------------------------"
-      echo "$folder: npm build lint again"
-      echo "-------------------------------------------------------"
+      echo "======================================================="
+      echo "=> $folder: npm build lint again"
+      echo "======================================================="
       npm run build lint
                 
-      echo "-------------------------------------------------------"    
-      echo "$folder: SUCCESSFUL"
-      echo "-------------------------------------------------------"    
-      
-      cleanup
-      cd ../..
+      echo "======================================================="    
+      echo "=> $folder: npm SUCCESSFUL"
+      echo "======================================================="    
     fi
+
+    if [ "$folder" == "examples/with-pnpm" ]; then
+      cleanup
+      setup_git      
+      echo "======================================================="
+      echo "=> $folder: pnpm install"
+      echo "======================================================="
+      pnpm install
+      
+      echo "======================================================="
+      echo "=> $folder: pnpm build lint"
+      echo "======================================================="
+      pnpm run build lint
+      
+      echo "======================================================="
+      echo "=> $folder: pnpm build lint again"
+      echo "======================================================="
+      pnpm run build lint
+                
+      echo "======================================================="    
+      echo "=> $folder: pnpm SUCCESSFUL"
+      echo "======================================================="               
+    fi
+    
+
+    cleanup
+
+    cd ../..
   fi
 done;
 
-# for folder in examples/* ; do
-#   if [[ -f "$folder/package.json" ]]; then
-#     if [[ -f $folder != "with-pnpm" ]]; then
-#       cd $folder 
-#       rm -rf node_modules
-#       rm -rf **/node_modules/**
-#       rm -rf **/.next
-#       rm -rf **/dist
-#       rm -rf **/build
-#       rm -rf **/.turbo
-#       rm -rf yarn.lock
-#       rm -rf .git
-#       git init .
-#       git add . 
-#       git commit -m "Initial commit"
-#       echo "-------------------------------------------------------"
-#       echo "$folder: yarn install"
-#       yarn install
-#       echo "$folder: yarn build lint"
-#       yarn build lint
-#       echo "$folder: yarn build again"
-#       yarn build lint
-#       echo "$folder: successful"
-#       echo "-------------------------------------------------------"    
-#       cd ../..
-#     fi
-#   fi
-# done;
 
-# if [ -f ".eslintrc.js.bak" ]; then
-#   mv .eslintrc.js.bak .eslintrc.js 
-# fi
+if [ -f ".eslintrc.js.bak" ]; then
+  mv .eslintrc.js.bak .eslintrc.js 
+fi
 
-# if [[ ! -z $(git status -s) ]];then
-#   echo "Detected changes"
-#   git status
-#   exit 1
-# fi
+if [[ ! -z $(git status -s) ]];then
+  echo "Detected changes"
+  git status
+  exit 1
+fi
