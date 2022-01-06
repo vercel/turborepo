48f384326e082d42a7d14141d2d8463fe18e7ceb
Jared Palmer <jared@jaredpalmer.com>
2022-01-06 11:42:00 -0500

    Fix safari

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -6,6 +6,14 @@ import Prism from "prism-react-renderer/prism";
 (typeof global !== "undefined" ? global : window).Prism = Prism;
 require("prismjs/components/prism-docker");
 
+// Some of the NPM packages we included, use following methods without polyfills.
+// So, we need to include these polyfills.
+// (This is only needed for Safari)
+if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
+  window.requestIdleCallback = (fn) => setTimeout(fn, 1);
+  window.cancelIdleCallback = (e) => clearTimeout(e);
+}
+
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
