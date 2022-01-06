248688bf53012ca15910862fdaf484b1347c9bf9
Jared Palmer <jared@jaredpalmer.com>
2022-01-06 11:48:52 -0500

    docs: Fix gap on open mobile nav with banner (#507)

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -6,6 +6,12 @@ import Prism from "prism-react-renderer/prism";
 (typeof global !== "undefined" ? global : window).Prism = Prism;
 require("prismjs/components/prism-docker");
 
+// Shim requestIdleCallback in Safari
+if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
+  window.requestIdleCallback = (fn) => setTimeout(fn, 1);
+  window.cancelIdleCallback = (e) => clearTimeout(e);
+}
+
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
