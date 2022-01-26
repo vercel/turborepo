9f3c68a07937eade36cfc65e9bba19aa09170756
Jared Palmer <jared@jaredpalmer.com>
2022-01-26 12:47:21 -0500

    Fully revert fs.Walk symlink behavior

--- cli/internal/fs/copy_file.go
+++ cli/internal/fs/copy_file.go
@@ -71,21 +71,17 @@ func Walk(rootPath string, callback func(name string, isDir bool) error) error {
 // WalkMode is like Walk but the callback receives an additional type specifying the file mode type.
 // N.B. This only includes the bits of the mode that determine the mode type, not the permissions.
 func WalkMode(rootPath string, callback func(name string, isDir bool, mode os.FileMode) error) error {
+	// Compatibility with filepath.Walk which allows passing a file as the root argument.
+	if info, err := os.Lstat(rootPath); err != nil {
+		return err
+	} else if !info.IsDir() {
+		return callback(rootPath, false, info.Mode())
+	}
 	return godirwalk.Walk(rootPath, &godirwalk.Options{
 		Callback: func(name string, info *godirwalk.Dirent) error {
-			if isDirLike, err := info.IsDirOrSymlinkToDir(); err == nil {
-				return callback(name, isDirLike, info.ModeType())
-			} else {
-				// Skip running callback on "dead" symlink (symlink to directory that doesn't exist)
-				if err, ok := err.(*os.PathError); !ok {
-					return err
-				}
-				return nil
-			}
+			return callback(name, info.IsDir(), info.ModeType())
 		},
-		Unsorted:            true,
-		AllowNonDirectory:   true,
-		FollowSymbolicLinks: false,
+		Unsorted: true,
 	})
 }
 
