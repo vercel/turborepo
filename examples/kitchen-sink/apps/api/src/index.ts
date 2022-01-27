bc68f73e8e19160b9005589b9494a2c6df4e2c0d
Greg Soltis <greg.soltis@vercel.com>
2022-01-27 13:27:38 -0800

    Newer versions of OSX use port 5000 by default for Airplay

--- examples/kitchen-sink/apps/api/src/index.ts
+++ examples/kitchen-sink/apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import { createServer } from "./server";
 import { log } from "logger";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
