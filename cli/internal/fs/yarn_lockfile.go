be83bce82e34788352d6fdb4f407bbe1be4833b0
Joe Duchnowski <joe@enerflo.io>
2022-01-30 00:12:49 -0700

    Support yarn 3 lockfile entries

--- cli/internal/fs/yarn_lockfile.go
+++ cli/internal/fs/yarn_lockfile.go
@@ -2,13 +2,17 @@ package fs
 
 type LockfileEntry struct {
 	// resolved version for the particular entry based on the provided semver revision
-	Version   string `yaml:"version"`
-	Resolved  string `yaml:"resolved"`
-	Integrity string `yaml:"integrity"`
+	Version    string `yaml:"version"`
+	Resolved   string `yaml:"resolved,omitempty"`
+	Integrity  string `yaml:"integrity,omitempty"`
+	Resolution string `yaml:"resolution,omitempty"`
 	// the list of unresolved modules and revisions (e.g. type-detect : ^4.0.0)
 	Dependencies map[string]string `yaml:"dependencies,omitempty"`
 	// the list of unresolved modules and revisions (e.g. type-detect : ^4.0.0)
 	OptionalDependencies map[string]string `yaml:"optionalDependencies,omitempty"`
+	Checksum             string            `yaml:"checksum,omitempty"`
+	LanguageName         string            `yaml:"languageName,omitempty"`
+	LinkType             string            `yaml:"linkType,omitempty"`
 }
 
 type YarnLockfile map[string]*LockfileEntry
