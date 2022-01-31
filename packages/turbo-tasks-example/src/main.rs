410a3c7c7f6e819c99242a44c4c5bc300ef4fdc4
Tobias Koppers <tobias.koppers@googlemail.com>
2022-01-31 09:39:40 +0100

    basic visualization

--- packages/turbo-tasks-example/src/main.rs
+++ packages/turbo-tasks-example/src/main.rs
@@ -1,12 +1,15 @@
 #![feature(once_cell)]
 
-use std::{env::current_dir, thread::sleep, time::Duration};
+use std::{env::current_dir, fs, thread::sleep, time::Duration};
 
 use async_std::task::block_on;
 use math::add;
-use turbo_tasks::TurboTasks;
+use turbo_tasks::{
+    viz::{GraphViz, Visualizable},
+    Task, TurboTasks,
+};
 use turbo_tasks_fs::{
-    read, DiskFileSystemRef, FileSystemPathRef, FileSystemRef, PathInFileSystemRef,
+    read, DiskFileSystemRef, FileContentRef, FileSystemPathRef, FileSystemRef, PathInFileSystemRef,
 };
 
 use crate::{
@@ -23,40 +26,65 @@ fn main() {
     let tt = TurboTasks::new();
     let task = tt.spawn_root_task(Box::new(|| {
         Box::pin(async {
-            let a = I32ValueRef::new(42);
-            let b = I32ValueRef::new(2);
-            let c = I32ValueRef::new(7);
-            let r = random().await;
-            let x = add(a, b.clone());
-            let y = add(b, c);
-            let (x, y) = (x.await, y.await);
-            let z = add(x.clone(), y.clone());
-            let rz = add(r, y);
-            let z = z.await;
-            let rz = rz.await;
-            log(x, LoggingOptionsRef::new("value of x".to_string())).await;
-            log(z, LoggingOptionsRef::new("value of z".to_string())).await;
-            log(
-                rz.clone(),
-                LoggingOptionsRef::new("value of rz".to_string()),
-            )
-            .await;
+            make_math().await;
 
-            let disk_fs = DiskFileSystemRef::new(
-                "project".to_string(),
-                current_dir().unwrap().to_str().unwrap().to_string(),
-            );
-            // TODO add casts to Smart Pointers
-            let fs = FileSystemRef::from_node(disk_fs.into()).unwrap();
-            let file_ref =
-                FileSystemPathRef::new(fs, PathInFileSystemRef::new("Cargo.toml".to_string()));
-            let content = read(file_ref).await;
-            println!("Size of Cargo.toml: {}", content.get().buffer.len());
+            let content = ls().await;
             content.into()
         })
     }));
     // println!("{:#?}", task);
     block_on(task.wait_output());
+    let mut graph_viz = GraphViz::new();
+    task.visualize(&mut graph_viz);
+    fs::write("graph.html", GraphViz::wrap_html(&graph_viz.to_string())).unwrap();
     // println!("{:#?}", task);
     sleep(Duration::from_secs(30));
+}
+
+#[turbo_tasks::function]
+async fn make_math() -> I32ValueRef {
+    let a = I32ValueRef::new(42);
+    let b = I32ValueRef::new(2);
+    let c = I32ValueRef::new(7);
+    let r = random().await;
+    let x = add(a, b.clone());
+    let y = add(b, c);
+    let (x, y) = (x.await, y.await);
+    let z = add(x.clone(), y.clone());
+    let rz = add(r, y);
+    let z = z.await;
+    let rz = rz.await;
+    log(x, LoggingOptionsRef::new("value of x".to_string())).await;
+    log(z, LoggingOptionsRef::new("value of z".to_string())).await;
+    log(
+        rz.clone(),
+        LoggingOptionsRef::new("value of rz".to_string()),
+    )
+    .await;
+    rz
+}
+
+#[turbo_tasks::function]
+async fn ls() -> FileContentRef {
+    let disk_fs = DiskFileSystemRef::new(
+        "project".to_string(),
+        current_dir().unwrap().to_str().unwrap().to_string(),
+    );
+    // TODO add casts to Smart Pointers
+    let fs = FileSystemRef::from_node(disk_fs.into()).unwrap();
+    let path = PathInFileSystemRef::new("Cargo.toml".to_string());
+    let file_ref = FileSystemPathRef::new(fs, path.clone());
+    let content = read(file_ref).await;
+    print_size(path, content.clone()).await
+}
+
+#[turbo_tasks::function]
+async fn print_size(path: PathInFileSystemRef, content: FileContentRef) -> FileContentRef {
+    println!(
+        "Size of {}: {}",
+        path.get().path,
+        content.get().buffer.len()
+    );
+    Task::side_effect();
+    content
 }
