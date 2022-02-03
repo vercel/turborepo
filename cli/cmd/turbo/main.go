8e8a4c6873c3544479fa286ed5e5e5a96e68bacc
Greg Soltis <greg.soltis@vercel.com>
2022-02-03 13:21:18 -0800

    Improve child process management (#663)

--- cli/cmd/turbo/main.go
+++ cli/cmd/turbo/main.go
@@ -8,12 +8,14 @@ import (
 	"time"
 	"turbo/internal/config"
 	"turbo/internal/login"
+	"turbo/internal/process"
 	prune "turbo/internal/prune"
 	"turbo/internal/run"
 	uiPkg "turbo/internal/ui"
 	"turbo/internal/util"
 
 	"github.com/fatih/color"
+	hclog "github.com/hashicorp/go-hclog"
 	"github.com/mitchellh/cli"
 )
 
@@ -38,6 +40,7 @@ func main() {
 		}
 	}
 	args = args[:argsEnd]
+
 	c := cli.NewCLI("turbo", turboVersion)
 
 	util.InitPrintf()
@@ -57,15 +60,25 @@ func main() {
 	c.HelpWriter = os.Stdout
 	c.ErrorWriter = os.Stderr
 	// Parse and validate cmd line flags and env vars
+	// Note that cf can be nil
 	cf, err := config.ParseAndValidate(c.Args, ui, turboVersion)
 	if err != nil {
 		ui.Error(fmt.Sprintf("%s %s", uiPkg.ERROR_PREFIX, color.RedString(err.Error())))
 		os.Exit(1)
 	}
+
+	var logger hclog.Logger
+	if cf != nil {
+		logger = cf.Logger
+	} else {
+		logger = hclog.Default()
+	}
+	processes := process.NewManager(logger.Named("processes"))
+	signalCh := watchSignals(func() { processes.Close() })
 	c.HiddenCommands = []string{"graph"}
 	c.Commands = map[string]cli.CommandFactory{
 		"run": func() (cli.Command, error) {
-			return &run.RunCommand{Config: cf, Ui: ui},
+			return &run.RunCommand{Config: cf, Ui: ui, Processes: processes},
 				nil
 		},
 		"prune": func() (cli.Command, error) {
@@ -87,7 +100,9 @@ func main() {
 
 	// Capture the defer statements below so the "done" message comes last
 	exitCode := 1
+	doneCh := make(chan struct{})
 	func() {
+		defer func() { close(doneCh) }()
 		// To view a CPU trace, use "go tool trace [file]". Note that the trace
 		// viewer doesn't work under Windows Subsystem for Linux for some reason.
 		if traceFile != "" {
@@ -157,5 +172,12 @@ func main() {
 			}
 		}
 	}()
+	// Wait for either our command to finish, in which case we need to clean up,
+	// or to receive a signal, in which case the signal handler above does the cleanup
+	select {
+	case <-doneCh:
+		processes.Close()
+	case <-signalCh:
+	}
 	os.Exit(exitCode)
 }
