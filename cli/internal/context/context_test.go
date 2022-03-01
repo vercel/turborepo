d176f03444251de69f77d7dd2a05052410a65a3c
Adam J. Hines <ahines@factset.com>
2022-03-01 11:03:00 -0700

    fix(dep-graph): allow mix of internal and external deps with same name
    When turbo currently tries to build the dependency graph, it doesn't consider the specified
    version/range of the dependency, it just assumes that if that package name exists in the
    workspace it must be an internal dependency. Because of this the cycles are detected and
    the process hangs.
    
    This PR adds some additional logic to ensure that dependencies are only considered
    internal if the local/workspace package's version matches the semver range specified for
    the dependency.
    
    Fixes: #796

--- cli/internal/context/context_test.go
+++ cli/internal/context/context_test.go
@@ -114,3 +114,82 @@ func Test_getHashableTurboEnvVarsFromOs(t *testing.T) {
 		t.Errorf("getHashableTurboEnvVarsFromOs() env pairs got = %v, want %v", gotPairs, wantPairs)
 	}
 }
+
+func Test_isWorkspaceReference(t *testing.T) {
+	tests := []struct {
+		name              string
+		packageVersion    string
+		dependencyVersion string
+		want              bool
+	}{
+		{
+			name:              "handles exact match",
+			packageVersion:    "1.2.3",
+			dependencyVersion: "1.2.3",
+			want:              true,
+		},
+		{
+			name:              "handles semver range satisfied",
+			packageVersion:    "1.2.3",
+			dependencyVersion: "^1.0.0",
+			want:              true,
+		},
+		{
+			name:              "handles semver range not-satisfied",
+			packageVersion:    "2.3.4",
+			dependencyVersion: "^1.0.0",
+			want:              false,
+		},
+		{
+			name:              "handles workspace protocol with version",
+			packageVersion:    "1.2.3",
+			dependencyVersion: "workspace:1.2.3",
+			want:              true,
+		},
+		{
+			name:              "handles workspace protocol with relative path",
+			packageVersion:    "1.2.3",
+			dependencyVersion: "workspace:../other-package/",
+			want:              true,
+		},
+		{
+			name:              "handles npm protocol with satisfied semver range",
+			packageVersion:    "1.2.3",
+			dependencyVersion: "npm:^1.2.3",
+			want:              true, // default in yarn is to use the workspace version unless `enableTransparentWorkspaces: true`. This isn't currently being checked.
+		},
+		{
+			name:              "handles npm protocol with non-satisfied semver range",
+			packageVersion:    "2.3.4",
+			dependencyVersion: "npm:^1.2.3",
+			want:              false,
+		},
+		{
+			name:              "handles pre-release versions",
+			packageVersion:    "1.2.3",
+			dependencyVersion: "1.2.2-alpha-1234abcd.0",
+			want:              false,
+		},
+		{
+			name:              "handles non-semver package version",
+			packageVersion:    "sometag",
+			dependencyVersion: "1.2.3",
+			want:              true, // for backwards compatability with the code before versions were verified
+		},
+		{
+			name:              "handles non-semver package version",
+			packageVersion:    "1.2.3",
+			dependencyVersion: "sometag",
+			want:              true, // for backwards compatability with the code before versions were verified
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isWorkspaceReference(tt.packageVersion, tt.dependencyVersion)
+			if got != tt.want {
+				t.Errorf("isWorkspaceReference() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
