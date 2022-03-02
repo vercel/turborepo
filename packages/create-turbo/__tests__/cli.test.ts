fd171519ec02a69c9afafc1bc5d9d1b481fba721
Jared Palmer <jared@jaredpalmer.com>
2022-03-02 12:48:37 -0500

    Move to core monorepo to pnpm (#810)

--- packages/create-turbo/__tests__/cli.test.ts
+++ packages/create-turbo/__tests__/cli.test.ts
@@ -18,13 +18,16 @@ const keys = {
 };
 
 const createTurbo = path.resolve(__dirname, "../dist/index.js");
-
+const testDir = path.join(__dirname, "../my-turborepo");
 const DEFAULT_JEST_TIMEOUT = 5000;
 
 describe("create-turbo cli", () => {
   beforeAll(() => {
     jest.setTimeout(DEFAULT_JEST_TIMEOUT * 3);
-    fs.rmSync(path.join(__dirname, "../my-turborepo"), { recursive: true });
+    if (fs.existsSync(testDir)) {
+      fs.rmSync(testDir, { recursive: true });
+    }
+
     if (!fs.existsSync(createTurbo)) {
       // TODO: Consider running the build here instead of throwing
       throw new Error(
@@ -35,7 +38,9 @@ describe("create-turbo cli", () => {
 
   afterAll(() => {
     jest.setTimeout(DEFAULT_JEST_TIMEOUT);
-    fs.rmSync(path.join(__dirname, "../my-turborepo"), { recursive: true });
+    if (fs.existsSync(testDir)) {
+      fs.rmSync(testDir, { recursive: true });
+    }
   });
 
   it("guides the user through the process", (done) => {
