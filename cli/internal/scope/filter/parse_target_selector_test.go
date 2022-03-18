f1bdd66c76cdfdd42ff0ee8159799c8046b94eeb
Greg Soltis <greg.soltis@vercel.com>
2022-03-18 11:07:03 -0700

    Implement filter parsing and package selection

---
+++ cli/internal/scope/filter/parse_target_selector_test.go
@@ -0,0 +1,313 @@
+package filter
+
+import (
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestParseTargetSelector(t *testing.T) {
+	type args struct {
+		rawSelector string
+		prefix      string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    TargetSelector
+		wantErr bool
+	}{
+		{
+			"foo",
+			args{"foo", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "foo",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"foo...",
+			args{"foo...", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: true,
+				includeDependents:   false,
+				namePattern:         "foo",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"...foo",
+			args{"...foo", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   true,
+				namePattern:         "foo",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"...foo...",
+			args{"...foo...", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: true,
+				includeDependents:   true,
+				namePattern:         "foo",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"foo^...",
+			args{"foo^...", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         true,
+				includeDependencies: true,
+				includeDependents:   false,
+				namePattern:         "foo",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"...^foo",
+			args{"...^foo", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         true,
+				includeDependencies: false,
+				includeDependents:   true,
+				namePattern:         "foo",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"./foo",
+			args{"./foo", "./"},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "",
+				parentDir:           "foo",
+			},
+			false,
+		},
+		{
+			"../foo",
+			args{"../foo", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "",
+				parentDir:           filepath.FromSlash("../foo"),
+			},
+			false,
+		},
+		{
+			"...{./foo}",
+			args{"...{./foo}", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   true,
+				namePattern:         "",
+				parentDir:           "foo",
+			},
+			false,
+		},
+		{
+			".",
+			args{".", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "",
+				parentDir:           ".",
+			},
+			false,
+		},
+		{
+			"..",
+			args{"..", "."},
+			TargetSelector{
+				diff:                "",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "",
+				parentDir:           "..",
+			},
+			false,
+		},
+		{
+			"[master]",
+			args{"[master]", "."},
+			TargetSelector{
+				diff:                "master",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"{foo}[master]",
+			args{"{foo}[master]", "."},
+			TargetSelector{
+				diff:                "master",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "",
+				parentDir:           "foo",
+			},
+			false,
+		},
+		{
+			"pattern{foo}[master]",
+			args{"pattern{foo}[master]", "."},
+			TargetSelector{
+				diff:                "master",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   false,
+				namePattern:         "pattern",
+				parentDir:           "foo",
+			},
+			false,
+		},
+		{
+			"[master]...",
+			args{"[master]...", "."},
+			TargetSelector{
+				diff:                "master",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: true,
+				includeDependents:   false,
+				namePattern:         "",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"...[master]",
+			args{"...[master]", "."},
+			TargetSelector{
+				diff:                "master",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: false,
+				includeDependents:   true,
+				namePattern:         "",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"...[master]...",
+			args{"...[master]...", "."},
+			TargetSelector{
+				diff:                "master",
+				exclude:             false,
+				excludeSelf:         false,
+				includeDependencies: true,
+				includeDependents:   true,
+				namePattern:         "",
+				parentDir:           "",
+			},
+			false,
+		},
+		{
+			"foo...[master]",
+			args{"foo...[master]", "."},
+			TargetSelector{
+				diff:              "master",
+				namePattern:       "foo",
+				matchDependencies: true,
+			},
+			false,
+		},
+		{
+			"foo...[master]...",
+			args{"foo...[master]...", "."},
+			TargetSelector{
+				diff:                "master",
+				namePattern:         "foo",
+				matchDependencies:   true,
+				includeDependencies: true,
+			},
+			false,
+		},
+		{
+			"{foo}...[master]",
+			args{"{foo}...[master]", "."},
+			TargetSelector{
+				diff:              "master",
+				parentDir:         "foo",
+				matchDependencies: true,
+			},
+			false,
+		},
+		{
+			"......[master]",
+			args{"......[master]", "."},
+			TargetSelector{},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseTargetSelector(tt.args.rawSelector, tt.args.prefix)
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("ParseTargetSelector() error = %#v, wantErr %#v", err, tt.wantErr)
+				}
+			} else {
+				// copy the raw selector from the args into what we want. This value is used
+				// for reporting errors in the case of a malformed selector
+				tt.want.raw = tt.args.rawSelector
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("ParseTargetSelector() = %#v, want %#v", got, tt.want)
+				}
+			}
+		})
+	}
+}
