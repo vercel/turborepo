b5a6f586d9f0bf8d0954c5f49db3d22419f9054e
Greg Soltis <greg.soltis@vercel.com>
2022-03-23 15:27:30 -0700

    Stop removing unreferenced packages before task graph construction (#918)
    Fixes #912 
    
    In this PR:
     * Stop removing unreferenced packages from the package dependency graph before building the task graph.
     * Remove the implied `--include-dependencies` if `--scope` and `--since` are both set. It should no longer be necessary.
     * Remove the test for `--scope` and `--since` implying `--include-dependencies`
     * Add a new scheduler test that mimics the way `run` now behaves.
     * Add a debugging profile for doing a dry run against the kitchen sink example.
    
    Future work includes being able to write a test in `run_test.go` to get the whole end-to-end graph build, but we don't quite have the infrastructure for that yet (also required for follow-on work to #911) 
    
    Note that this is technically a breaking change. Previously, if `a` depends on `b`, and the `"test"` task depends on `"^build"` the following would be true:
     * `turbo run test --scope=a` would test `a` but not build `b`
     * `turbo run test --scope=a --include-dependencies` would build `b` and test `a` *AND* `b`.
    
    With this change, `turbo run test --scope=a` will now build `b` and test `a`. The addition of tasks running for unselected packages is new. While this seems like more work being done, in practice I expect users to be able to move from the second example above to the first, ultimately resulting in less work being done: `b#test` will no longer need to run to get `a#test` to run.

--- cli/internal/run/run_test.go
+++ cli/internal/run/run_test.go
@@ -142,24 +142,6 @@ func TestParseConfig(t *testing.T) {
 			},
 		},
 		{
-			"since and scope imply including dependencies for backwards compatibility",
-			[]string{"foo", "--scope=bar", "--since=some-ref"},
-			&RunOptions{
-				includeDependents:   true,
-				stream:              true,
-				bail:                true,
-				concurrency:         10,
-				includeDependencies: true,
-				cache:               true,
-				cwd:                 defaultCwd,
-				cacheFolder:         defaultCacheFolder,
-				scope:               []string{"bar"},
-				since:               "some-ref",
-				cacheHitLogsMode:    FullLogs,
-				cacheMissLogsMode:   FullLogs,
-			},
-		},
-		{
 			"can specify filter patterns",
 			[]string{"foo", "--filter=bar", "--filter=...[main]"},
 			&RunOptions{
