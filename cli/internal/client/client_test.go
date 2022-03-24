f3cdad9cd629ebe10a2fdec57d7f7fc0a73d2a6e
Gaspar Garcia <gaspar@vercel.com>
2022-03-24 11:15:16 -0700

    fix client_test

--- cli/internal/client/client_test.go
+++ cli/internal/client/client_test.go
@@ -1,14 +1,12 @@
 package client
 
 import (
-	"crypto/md5"
-	"encoding/base64"
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
-	"strings"
 	"testing"
 
 	"github.com/google/uuid"
@@ -67,16 +65,14 @@ func Test_sendToServer(t *testing.T) {
 }
 
 func Test_PutArtifact(t *testing.T) {
-	ch := make(chan string, 2)
+	ch := make(chan []byte, 1)
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		defer req.Body.Close()
 		b, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			t.Errorf("failed to read request %v", err)
 		}
-		ch <- string(b)
-		contentMd5 := req.Header.Get("Content-MD5")
-		ch <- contentMd5
+		ch <- b
 		w.WriteHeader(200)
 		w.Write([]byte{})
 	}))
@@ -85,21 +81,13 @@ func Test_PutArtifact(t *testing.T) {
 	// Set up test expected values
 	apiClient := NewClient(ts.URL+"/hash", hclog.Default(), "v1", "", "my-team-slug", 1)
 	apiClient.SetToken("my-token")
-	expectedArtifactBody := "My string artifact"
-	artifactReader := strings.NewReader(expectedArtifactBody)
-	md5Sum := md5.Sum([]byte(expectedArtifactBody))
-	expectedMd5 := base64.StdEncoding.EncodeToString(md5Sum[:])
+	expectedArtifactBody := []byte("My string artifact")
 
 	// Test Put Artifact
-	apiClient.PutArtifact("hash", 500, artifactReader)
+	apiClient.PutArtifact("hash", expectedArtifactBody, 500, "")
 	testBody := <-ch
-	if expectedArtifactBody != testBody {
+	if !bytes.Equal(expectedArtifactBody, testBody) {
 		t.Errorf("Handler read '%v', wants '%v'", testBody, expectedArtifactBody)
-	}
-
-	testMd5 := <-ch
-	if expectedMd5 != testMd5 {
-		t.Errorf("Handler read trailer '%v', wants '%v'", testMd5, expectedMd5)
 	}
 
 }
