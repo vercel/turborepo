ee1aa64739ce4c335223cf7a1fd766b2fc388bea
Shu Ding <g@shud.in>
2022-03-25 15:06:29 +0100

    Upgrade docs theme (#893)
    Co-authored-by: Jared Palmer <jared@jaredpalmer.com>

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
-import { Avatar } from "./components/Avatar";
+import { Footer } from "./components/Footer";
+
 const Logo = ({ height, ...props }) => (
   <svg
     height={height}
@@ -118,66 +119,20 @@ const Logo = ({ height, ...props }) => (
   </svg>
 );
 
-const Vercel = ({ height = 20 }) => (
-  <svg height={height} viewBox="0 0 283 64" fill="none">
-    <path
-      fill="currentColor"
-      d="M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z"
-    />
-  </svg>
-);
-
-const team = {
-  jaredpalmer: {
-    name: "Jared Palmer",
-    twitterUsername: "jaredpalmer",
-    picture: "/images/people/jaredpalmer_headshot.jpeg",
-  },
-  gaspargarcia_: {
-    name: "Gaspar Garcia",
-    twitterUsername: "gaspargarcia_",
-    picture: "/images/people/gaspargarcia_.jpeg",
-  },
-  becca__z: {
-    name: "Becca Z.",
-    twitterUsername: "becca__z",
-    picture: "/images/people/becca__z.jpeg",
-  },
-  gsoltis: {
-    name: "Greg Soltis",
-    twitterUsername: "gsoltis",
-    picture: "/images/people/gsoltis.jpeg",
-  },
-};
-
 const theme = {
   github: "https://github.com/vercel/turborepo",
   docsRepositoryBase:
     "https://github.com/vercel/turborepo/blob/main/docs/pages",
   titleSuffix: " | Turborepo",
   search: true,
-  unstable_stork: false,
+  unstable_flexsearch: true,
   unstable_staticImage: true,
   floatTOC: true,
   font: false,
-  enterpriseLink:
-    "https://vercel.com/contact/turborepo?utm_source=turborepo.org&utm_medium=referral&utm_campaign=header-enterpriseLink", // @TODO
   projectChatLink: "https://turborepo.org/discord",
   feedbackLink: "Question? Give us feedback →",
-  authors: function Authors({ authors }) {
-    return (
-      <div className="grid max-w-screen-md gap-4 px-6 sm:grid-cols-2 md:grid-cols-4">
-        {authors.map((username) =>
-          !!team[username] ? (
-            <Avatar key={username} {...team[username]} />
-          ) : (
-            console.warning("no author found for", username) || null
-          )
-        )}
-      </div>
-    );
-  },
   banner: function Banner() {
+    // TODO
     return (
       <div className="px-6 py-2 text-sm text-center text-white bg-black dark:bg-white dark:text-black">
         <a
@@ -200,7 +155,8 @@ const theme = {
       </>
     );
   },
-  head: function Head({ title, meta, router }) {
+  head: function Head({ title, meta }) {
+    const router = useRouter();
     return (
       <>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -248,23 +204,14 @@ const theme = {
       </>
     );
   },
-  footerEditLink: ({ locale }) => {
+  footerEditLink: () => {
     return "Edit this page on GitHub";
   },
-  footerText: ({ locale }) => {
-    return (
-      <a
-        href="https://vercel.com?utm_source=turborepo.org&utm_medium=referral&utm_campaign=header-enterpriseLink"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="inline-flex items-center font-semibold text-current no-underline"
-      >
-        <span className="mr-1">Powered by</span>
-        <span>
-          <Vercel />
-        </span>
-      </a>
-    );
+  footerText: () => {
+    return <Footer />;
+  },
+  nextThemes: {
+    defaultTheme: "dark",
   },
 };
 export default theme;
