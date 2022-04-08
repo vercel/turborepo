e9a9249db781d6498a7c2ad057149ac1cb432302
Nathan Hammond <nathan.hammond@vercel.com>
2022-04-08 14:54:27 +0800

    Apply EditorConfig to all files
    This mostly applies a lot of line-ending fixes. A few other indentation issues were detected. All "material" changes can be seen in `git show --ignore-space-at-eol`

--- scripts/run-examples.sh
+++ scripts/run-examples.sh
@@ -19,97 +19,97 @@ if [ -f ".eslintrc.js" ]; then
 fi
 
 function cleanup {
-    rm -rf node_modules
-    rm -rf apps/*/node_modules
-    rm -rf apps/*/.next
-    rm -rf apps/*/.turbo
-    rm -rf packages/*/node_modules
-    rm -rf packages/*/.next
-    rm -rf packages/*/.turbo      
-    rm -rf *.log
-    rm -rf yarn.lock
-    rm -rf package-lock.json   
-    rm -rf pnpm-lock.yaml
+  rm -rf node_modules
+  rm -rf apps/*/node_modules
+  rm -rf apps/*/.next
+  rm -rf apps/*/.turbo
+  rm -rf packages/*/node_modules
+  rm -rf packages/*/.next
+  rm -rf packages/*/.turbo
+  rm -rf *.log
+  rm -rf yarn.lock
+  rm -rf package-lock.json
+  rm -rf pnpm-lock.yaml
 }
 
-function setup_git { 
-   echo "=> Setting up git..."
-   rm -rf .git
-   mkdir .git
-   touch .git/config
-   echo "[user]" >> .git/config
-   echo "  name = GitHub Actions" >> .git/config
-   echo "  email = actions@users.noreply.github.com" >> .git/config
-   echo "" >> .git/config
-   echo "[init]" >> .git/config
-   echo "  defaultBranch = main" >> .git/config
-   git init . -q
-   git add . 
-   git commit -m "Initial commit"      
+function setup_git {
+  echo "=> Setting up git..."
+  rm -rf .git
+  mkdir .git
+  touch .git/config
+  echo "[user]" >> .git/config
+  echo "  name = GitHub Actions" >> .git/config
+  echo "  email = actions@users.noreply.github.com" >> .git/config
+  echo "" >> .git/config
+  echo "[init]" >> .git/config
+  echo "  defaultBranch = main" >> .git/config
+  git init . -q
+  git add .
+  git commit -m "Initial commit"
 }
 
 for folder in examples/* ; do
   if [ -f "$folder/package.json" ]; then
-    cd $folder 
+    cd $folder
     echo "======================================================="
     echo "=> checking $folder "
     echo "======================================================="
-    
+
     if [ "$folder" != "examples/with-pnpm" ]; then
-            
+
       cleanup
       setup_git
-      
-      cat package.json | jq '.packageManager = "npm@8.1.2"' | sponge package.json      
+
+      cat package.json | jq '.packageManager = "npm@8.1.2"' | sponge package.json
       if [ "$TURBO_TAG" == "canary" ]; then
-         cat package.json | jq '.devDependencies.turbo = "canary"' | sponge package.json
+        cat package.json | jq '.devDependencies.turbo = "canary"' | sponge package.json
       fi
-      
+
       echo "======================================================="
       echo "=> $folder: npm install"
       echo "======================================================="
       npm install --force
-      
+
       echo "======================================================="
       echo "=> $folder: npm build lint"
       echo "======================================================="
       npm run build lint
-      
+
       echo "======================================================="
       echo "=> $folder: npm build lint again"
       echo "======================================================="
       npm run build lint
-                
-      echo "======================================================="    
+
+      echo "======================================================="
       echo "=> $folder: npm SUCCESSFUL"
-      echo "======================================================="    
+      echo "======================================================="
 
       cleanup
       setup_git
-      
-      cat package.json | jq '.packageManager = "yarn@1.22.17"' | sponge package.json      
+
+      cat package.json | jq '.packageManager = "yarn@1.22.17"' | sponge package.json
       if [ "$TURBO_TAG" == "canary" ]; then
-         cat package.json | jq '.devDependencies.turbo = "canary"' | sponge package.json
+        cat package.json | jq '.devDependencies.turbo = "canary"' | sponge package.json
       fi
-      
+
       echo "======================================================="
       echo "=> $folder: yarn install"
       echo "======================================================="
       yarn install
-      
+
       echo "======================================================="
       echo "=> $folder: yarn build lint"
       echo "======================================================="
       yarn build lint
-      
+
       echo "======================================================="
       echo "=> $folder: yarn build again"
       echo "======================================================="
       yarn build lint
-                
-      echo "======================================================="    
+
+      echo "======================================================="
       echo "=> $folder: yarn SUCCESSFUL"
-      echo "======================================================="    
+      echo "======================================================="
     fi
 
     if [ "$folder" == "examples/with-pnpm" ]; then
@@ -118,32 +118,32 @@ for folder in examples/* ; do
 
       cat package.json | jq '.packageManager = "pnpm@6.26.1"' | sponge package.json
       if [ "$TURBO_TAG" == "canary" ]; then
-         cat package.json | jq '.devDependencies.turbo = "canary"' | sponge package.json
+        cat package.json | jq '.devDependencies.turbo = "canary"' | sponge package.json
       fi
-    
+
       echo "======================================================="
       echo "=> $folder: pnpm install"
       echo "======================================================="
       pnpm install
-      
+
       echo "======================================================="
       echo "=> $folder: pnpm build lint"
       echo "======================================================="
       pnpm run build lint
-      
+
       echo "======================================================="
       echo "=> $folder: pnpm build lint again"
       echo "======================================================="
       pnpm run build lint
-                
-      echo "======================================================="    
+
+      echo "======================================================="
       echo "=> $folder: pnpm SUCCESSFUL"
-      echo "======================================================="               
+      echo "======================================================="
     fi
-    
+
     cat package.json | jq 'del(.packageManager)' | sponge package.json
     if [ "$TURBO_TAG" == "canary" ]; then
-       cat package.json | jq '.devDependencies.turbo = "latest"' | sponge package.json
+      cat package.json | jq '.devDependencies.turbo = "latest"' | sponge package.json
     fi
 
     cleanup
@@ -154,7 +154,7 @@ done;
 
 
 if [ -f ".eslintrc.js.bak" ]; then
-  mv .eslintrc.js.bak .eslintrc.js 
+  mv .eslintrc.js.bak .eslintrc.js
 fi
 
 if [[ ! -z $(git status -s) ]];then
