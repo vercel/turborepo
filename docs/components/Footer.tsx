37b6fbfff253da94f2961969fc14c5594edc1c6a
Jared Palmer <jared@jaredpalmer.com>
2022-04-11 14:35:32 -0400

    Revert "feat(docs): added Turborepo + Vercel CTA in footer (#1037)"
    This reverts commit c7393e5ec8057a40f63087899cd7b29f86354066.

--- docs/components/Footer.tsx
+++ docs/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { useState } from "react";
+import Cookies from "js-cookie";
 const Vercel = ({ height = 20 }) => (
   <svg height={height} viewBox="0 0 283 64" fill="none">
     <path
@@ -54,10 +55,6 @@ const navigation = {
     },
   ],
   company: [
-    {
-      name: "Try Turborepo + Vercel",
-      href: "https://vercel.com/contact/turborepo?utm_source=turborepo.org&utm_medium=referral&utm_campaign=footer-try-link",
-    },
     {
       name: "Open Source Software",
       href: "https://vercel.com/oss?utm_source=turborepo.org&utm_medium=referral&utm_campaign=footer-ossLink",
