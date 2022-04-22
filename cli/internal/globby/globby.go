45f3e412d4ead439a6ee52902a9a695a683377db
Nathan Hammond <nathan.hammond@vercel.com>
2022-04-22 23:59:49 +0800

    Implement globby.GlobFiles using doublestar.GlobWalk
    This change aims to improve the performance of our file walking by only walking the portions of the directory that we care about. In this iteration it aims to be 100% backwards compatible with the previous implementation, excepting one obvious bug, resulting in a boolean change to the tests.
    
    It also adopts a set instead of a range for storage as doublestar will happily walk a directory multiple times if you feed it a nonsensical glob pattern such as `**/**/**/*.*`.

--- cli/internal/globby/globby.go
+++ cli/internal/globby/globby.go
@@ -1,55 +1,141 @@
 package globby
 
 import (
-	"os"
+	"errors"
 	"path/filepath"
 	"strings"
+
+	iofs "io/fs"
 
 	"github.com/bmatcuk/doublestar/v4"
 	"github.com/spf13/afero"
 )
 
-var _osFS = afero.NewOsFs()
+var _aferoOsFs = afero.NewOsFs()
+var _aferoIOFS = afero.NewIOFS(_aferoOsFs)
 
-func GlobFiles(basePath string, includePatterns []string, excludePatterns []string) []string {
-	return globFilesFs(_osFS, basePath, includePatterns, excludePatterns)
+// set is a naive partial implementation of a string set.
+// TODO: remove after we can use generics.
+type set struct {
+	storageMap map[string]bool
 }
 
-func globFilesFs(fs afero.Fs, basePath string, includePatterns []string, excludePatterns []string) []string {
-	var include []string
-	var exclude []string
-	var result []string
+// Add a string to the set.
+func (s *set) Add(value string) {
+	s.storageMap[value] = true
+}
 
-	for _, p := range includePatterns {
-		include = append(include, filepath.Join(basePath, p))
+// Returns a range of the set's values.
+func (s *set) Values() []string {
+	setValues := make([]string, len(s.storageMap))
+
+	i := 0
+	for key := range s.storageMap {
+		setValues[i] = key
+		i++
 	}
 
-	for _, p := range excludePatterns {
-		exclude = append(exclude, filepath.Join(basePath, p))
+	return setValues
+}
+
+func GlobFiles(basePath string, includePatterns []string, excludePatterns []string) []string {
+	return globFilesFs(_aferoIOFS, basePath, includePatterns, excludePatterns)
+}
+
+// getRelativePath ensures that the the requested file path is a child of `from`.
+func getRelativePath(from string, to string) (path string, err error) {
+	relativePath, err := filepath.Rel(from, to)
+
+	if err != nil {
+		return "", err
 	}
 
-	includePattern := "{" + strings.Join(include, ",") + "}"
-	excludePattern := "{" + strings.Join(exclude, ",") + "}"
+	if strings.HasPrefix(relativePath, "..") {
+		return "", errors.New("the path you are attempting to specify is outside of the root")
+	}
 
-	_ = afero.Walk(fs, basePath, func(path string, info os.FileInfo, err error) error {
-		isDir := info.IsDir()
-		if val, _ := doublestar.PathMatch(excludePattern, path); val {
-			if isDir {
-				return filepath.SkipDir
-			}
+	return relativePath, nil
+}
+
+// globFilesFs searches the specified file system to ensure to enumerate all files to include.
+func globFilesFs(fs afero.IOFS, basePath string, includePatterns []string, excludePatterns []string) []string {
+	var processedIncludes []string
+	var processedExcludes []string
+	var result = &set{
+		storageMap: map[string]bool{},
+	}
+
+	for _, includePattern := range includePatterns {
+		includePath := filepath.Join(basePath, includePattern)
+		relativePath, err := getRelativePath(basePath, includePath)
+
+		if err == nil {
+			// Includes only operate on files.
+			processedIncludes = append(processedIncludes, relativePath)
+		}
+	}
+
+	for _, excludePattern := range excludePatterns {
+		excludePath := filepath.Join(basePath, excludePattern)
+		relativePath, err := getRelativePath(basePath, excludePath)
+
+		if err == nil {
+			// Excludes operate on entire folders.
+			processedExcludes = append(processedExcludes, filepath.Join(relativePath, "**"))
+		}
+	}
+
+	// We start from a naive includePattern
+	includePattern := ""
+	includeCount := len(processedIncludes)
+
+	// Do not use alternation if unnecessary.
+	if includeCount == 1 {
+		includePattern = filepath.Join(basePath, processedIncludes[0])
+	} else if includeCount > 1 {
+		// We start from a basePath prefix which allows doublestar to optimize the access.
+		includePattern = filepath.Join(basePath, "{"+strings.Join(processedIncludes, ",")+"}")
+	}
+
+	// We only create an exclude pattern if we have excludes.
+	var excludePattern string
+	excludeCount := len(processedExcludes)
+
+	// Do not use alternation if unnecessary.
+	if excludeCount == 1 {
+		excludePattern = filepath.Join(basePath, processedExcludes[0])
+	} else if excludeCount > 1 {
+		// We start from a basePath prefix which allows doublestar to optimize the access.
+		excludePattern = filepath.Join(basePath, "{"+strings.Join(processedExcludes, ",")+"}")
+	}
+
+	err := doublestar.GlobWalk(fs, includePattern, func(path string, dirEntry iofs.DirEntry) error {
+		// Root paths do not prepend the leading slash.
+		if !strings.HasPrefix(path, "/") {
+			path = filepath.Join(basePath, path)
+		}
+
+		if dirEntry.IsDir() {
 			return nil
 		}
 
-		if isDir {
+		if excludeCount == 0 {
+			result.Add(path)
 			return nil
 		}
 
-		if val, _ := doublestar.PathMatch(includePattern, path); val || len(includePatterns) == 0 {
-			result = append(result, path)
+		isExcluded, err := doublestar.Match(excludePattern, path)
+
+		if err == nil && !isExcluded {
+			result.Add(path)
 		}
 
 		return nil
 	})
 
-	return result
+	if err != nil {
+		return nil
+	}
+
+	return result.Values()
 }
