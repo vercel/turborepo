55f690827d229fcf29758d7bcde6c568a812b3b2
Greg Soltis <greg.soltis@vercel.com>
2022-04-29 13:44:00 -0600

    Implement status check for SSO login (#1139)
    Add a check to `/artifacts/status` to `turbo login --sso-team`, since it implicitly links the repo to a team.

---
+++ cli/internal/login/status.go
@@ -0,0 +1,32 @@
+package login
+
+import (
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/pkg/errors"
+	"github.com/vercel/turborepo/cli/internal/util"
+)
+
+var (
+	errOverage            = errors.New("usage limit")
+	errNeedCachingEnabled = errors.New("caching not enabled")
+	errTryAfterEnable     = errors.New("link after enabling caching")
+)
+
+func promptEnableCaching() (bool, error) {
+	shouldEnable := false
+	err := survey.AskOne(
+		&survey.Confirm{
+			Default: true,
+			Message: util.Sprintf("Remote Caching was previously disabled for this team. Would you like to enable it now?"),
+		},
+		&shouldEnable,
+		survey.WithValidator(survey.Required),
+		survey.WithIcons(func(icons *survey.IconSet) {
+			icons.Question.Format = "gray+hb"
+		}),
+	)
+	if err != nil {
+		return false, err
+	}
+	return shouldEnable, nil
+}
