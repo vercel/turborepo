4c181ee038a04835fabd0bd1e4f9a3fad44308b3
Jared Palmer <jared@jaredpalmer.com>
2022-05-03 09:37:22 -0400

    Rename eslint-config-acme to eslint-config-custom

--- examples/with-pnpm/.eslintrc.js
+++ examples/with-pnpm/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["acme"],
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  extends: ["custom"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
