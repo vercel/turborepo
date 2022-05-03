cdd6ebb91ac3d0ccf23c2c09e00092b28834e987
Nathan Hammond <nathan.hammond@vercel.com>
2022-05-04 07:30:07 +0800

    ESLint: one more spot change (#1180)
    Followup to #1172 (tweak all the ESLint things!) and #1174 (much better name) we have one spot that needs re-linking.

--- examples/with-changesets/.eslintrc.js
+++ examples/with-changesets/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["kitchen-sink"],
+  extends: ["custom"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
