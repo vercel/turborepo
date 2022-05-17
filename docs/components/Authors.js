a04a2ebaf8f88dc4de8126fc651284d947f5e843
Greg Soltis <greg.soltis@vercel.com>
2022-05-17 09:31:43 -0700

    Implement root tasks (#1040)
    Adds the ability to run tasks from the root `package.json` file.
    
    Implementation notes that will need to be documented:
     * Tasks run from the root are opt-in. That is, `turbo.json`*must* have an entry for `_root#<task>` to enable running that task at the root.
     * `_root` is available in filters, so `turbo run my-task --filter=_root` will run `_root#my-task`. 
     * If an entry exists in `turbo.json` for `_root#task`, then `turbo run task` will also run that task for the root.
     * There is some best-effort cycle detection in place. If the root `package.json` has `"build": "turbo run build"`, then even after opting in, `turbo run build --filter=_root` will produce an error. This cycle detection can be circumvented with aliases or other indirection used to run `turbo`.
     * For the purposes of changed files, any changed file in a repo that doesn't belong to another package is considered to belong to the root.
       * Note that this could cause frequent runs of root tasks if they are triggered from git changes. With `inputs` specified, they should hit cache if nothing relevant has changed.
    
    Additional changes:
     * `e2e` tests now include a root task
     * a package task is now sufficient to mark a particular task as runnable by `turbo`. e.g. if you defined `foo#my-task` but not `my-task` in `turbo.json`, previously running `turbo run my-task` would be an error, now it will run `foo#my-task`.
    
    Fixes #845

--- docs/components/Authors.js
+++ docs/components/Authors.js
@@ -25,10 +25,13 @@ const team = {
 
 export function Authors({ authors }) {
   return (
-    <div className="authors grid grid-cols-[repeat(var(--grid-cnt-small),minmax(0,1fr))] gap-4 py-8 border-b border-gray-400 md:grid-cols-[repeat(var(--grid-cnt),minmax(0,1fr))] border-opacity-20" style={{
-      '--grid-cnt': authors.length,
-      '--grid-cnt-small': authors.length > 1 ? 2 : 1,
-    }}>
+    <div
+      className="authors grid grid-cols-[repeat(var(--grid-cnt-small),minmax(0,1fr))] gap-4 py-8 border-b border-gray-400 md:grid-cols-[repeat(var(--grid-cnt),minmax(0,1fr))] border-opacity-20"
+      style={{
+        "--grid-cnt": authors.length,
+        "--grid-cnt-small": authors.length > 1 ? 2 : 1,
+      }}
+    >
       {authors.map((username) =>
         !!team[username] ? (
           <Avatar key={username} {...team[username]} />
