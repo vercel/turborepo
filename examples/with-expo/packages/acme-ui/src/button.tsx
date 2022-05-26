f08b996e006a6869f6e852f7e482dc73458dac29
Jesús Ferretti <jferrettiboke@gmail.com>
2022-05-26 23:34:29 +0200

    Add `@acme/ui` package

---
+++ examples/with-expo/packages/acme-ui/src/button.tsx
@@ -0,0 +1,50 @@
+import {
+  Pressable,
+  Text,
+  StyleSheet,
+  GestureResponderEvent,
+} from "react-native";
+
+export interface ButtonProps {
+  /** Explain `color` prop here. */
+  color: "primary" | "secondary";
+  /** Explain `size` prop here. */
+  size: "sm" | "md" | "lg";
+  /** Explain `text` prop here. */
+  text: string;
+  /** Explain `onPress` prop here. */
+  onPress?: ((event: GestureResponderEvent) => void) | null | undefined;
+}
+
+/** Explain `<Button />` component here. */
+export function Button({ color, size, text, onPress }: ButtonProps) {
+  return (
+    <Pressable
+      style={{ ...colorVariant[color], ...sizeVariant[size] }}
+      onPress={onPress}
+    >
+      <Text>{text}</Text>
+    </Pressable>
+  );
+}
+
+const colorVariant = StyleSheet.create({
+  primary: {
+    backgroundColor: "pink",
+  },
+  secondary: {
+    backgroundColor: "yellow",
+  },
+});
+
+const sizeVariant = StyleSheet.create({
+  sm: {
+    padding: 10,
+  },
+  md: {
+    padding: 20,
+  },
+  lg: {
+    padding: 30,
+  },
+});
