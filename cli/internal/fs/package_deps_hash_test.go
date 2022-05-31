dc2d0b384864130c8e6f77f3ff6143594a3afa25
Nathan Hammond <nathan.hammond@vercel.com>
2022-05-31 11:53:55 +0800

    Dynamically generate special character test

--- cli/internal/fs/package_deps_hash_test.go
+++ cli/internal/fs/package_deps_hash_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -38,6 +39,78 @@ func getFixture(id int) turbopath.AbsoluteSystemPath {
 	panic("fixtures not found!")
 }
 
+func TestSpecialCharacters(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		return
+	}
+
+	fixturePath := getFixture(1)
+	newlinePath := turbopath.RelativeSystemPath("new\nline")
+	quotePath := turbopath.RelativeSystemPath("\"quote\"")
+	newline := fixturePath.Join(newlinePath)
+	quote := fixturePath.Join(quotePath)
+
+	// Setup
+	one := os.WriteFile(newline.ToString(), []byte{}, 0644)
+	two := os.WriteFile(quote.ToString(), []byte{}, 0644)
+
+	// Cleanup
+	defer func() {
+		one := os.Remove(newline.ToString())
+		two := os.Remove(quote.ToString())
+
+		if one != nil || two != nil {
+			return
+		}
+	}()
+
+	// Setup error check
+	if one != nil || two != nil {
+		return
+	}
+
+	tests := []struct {
+		name        string
+		rootPath    turbopath.AbsoluteSystemPath
+		filesToHash []turbopath.RelativeUnixPath
+		want        map[turbopath.RelativeUnixPath]string
+		wantErr     bool
+	}{
+		{
+			name:     "Quotes",
+			rootPath: fixturePath,
+			filesToHash: []turbopath.RelativeUnixPath{
+				turbopath.RelativeUnixPath(quotePath),
+			},
+			want: map[turbopath.RelativeUnixPath]string{
+				quotePath.ToRelativeUnixPath(): "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+			},
+		},
+		{
+			name:     "Newlines",
+			rootPath: fixturePath,
+			filesToHash: []turbopath.RelativeUnixPath{
+				turbopath.RelativeUnixPath(newlinePath),
+			},
+			want: map[turbopath.RelativeUnixPath]string{
+				newlinePath.ToRelativeUnixPath(): "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := gitHashObject(tt.rootPath, tt.filesToHash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("gitHashObject() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("gitHashObject() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func Test_gitHashObject(t *testing.T) {
 	fixturePath := getFixture(1)
 	traversePath, err := getTraversePath(AbsolutePath(fixturePath))
@@ -57,18 +130,6 @@ func Test_gitHashObject(t *testing.T) {
 			rootPath:    fixturePath,
 			filesToHash: []turbopath.RelativeUnixPath{},
 			want:        map[turbopath.RelativeUnixPath]string{},
-		},
-		{
-			name:     "Special characters",
-			rootPath: fixturePath,
-			filesToHash: []turbopath.RelativeUnixPath{
-				turbopath.RelativeUnixPath("new\nline"),
-				turbopath.RelativeUnixPath("\"quote\""),
-			},
-			want: map[turbopath.RelativeUnixPath]string{
-				"new\nline": "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
-				"\"quote\"": "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
-			},
 		},
 		{
 			name:     "Absolute paths come back relative to rootPath",
