cac44918ff146158803676dca0564d3b7698cc52
Christopher Allford <christopher.allford@automattic.com>
2022-06-06 12:08:42 -0700

    Simplified TaskOutputMode Constants

--- cli/internal/util/task_output_mode.go
+++ cli/internal/util/task_output_mode.go
@@ -3,17 +3,16 @@ package util
 // TaskOutputMode defines the ways turbo can display task output during a run
 type TaskOutputMode string
 
-// FullTaskOutput will show all task output
-const FullTaskOutput TaskOutputMode = "full"
-
-// NoTaskOutput will hide all task output
-const NoTaskOutput TaskOutputMode = "none"
-
-// HashTaskOutput will display turbo-computed task hashes
-const HashTaskOutput TaskOutputMode = "hash-only"
-
-// NewTaskOutput will show all new task output and turbo-computed task hashes for cached output
-const NewTaskOutput TaskOutputMode = "new-only"
+const (
+	// FullTaskOutput will show all task output
+	FullTaskOutput TaskOutputMode = "full"
+	// NoTaskOutput will hide all task output
+	NoTaskOutput TaskOutputMode = "none"
+	// HashTaskOutput will display turbo-computed task hashes
+	HashTaskOutput TaskOutputMode = "hash-only"
+	// NewTaskOutput will show all new task output and turbo-computed task hashes for cached output
+	NewTaskOutput TaskOutputMode = "new-only"
+)
 
 // TaskOutputModes contains all of the valid task output modes
 var TaskOutputModes = []TaskOutputMode{
