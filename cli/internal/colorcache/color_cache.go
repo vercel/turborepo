vercel/turborepo

commit 0f2f7d5eb847193522f9ac03fcb50ba016e38c92

Wrap cache in abstraction layer for a run and for a package task (#1256)
 * Move `Cache` options into their own struct
 * Move per-run cache handling into `RunCache` instance, along with associated `Opts`
 * Move per-packagetask cache handling into `TaskCache` instance, created from a `RunCache`
 * `TaskCache` is responsible for log recording and replaying, as well as restoring/saving task outputs as appropriate

The `TaskCache` save and restore methods are intended to be the place to hook in an eventual rpc to no-op the operation if the relevant files haven't changed.

cli/internal/run/run.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -24,11 +23,11 @@ import (
 	"github.com/vercel/turborepo/cli/internal/context"
 	"github.com/vercel/turborepo/cli/internal/core"
 	"github.com/vercel/turborepo/cli/internal/fs"
-	"github.com/vercel/turborepo/cli/internal/globby"
 	"github.com/vercel/turborepo/cli/internal/logstreamer"
 	"github.com/vercel/turborepo/cli/internal/nodes"
 	"github.com/vercel/turborepo/cli/internal/packagemanager"
 	"github.com/vercel/turborepo/cli/internal/process"
+	"github.com/vercel/turborepo/cli/internal/runcache"
 	"github.com/vercel/turborepo/cli/internal/scm"
 	"github.com/vercel/turborepo/cli/internal/scope"
 	"github.com/vercel/turborepo/cli/internal/taskhash"
@@ -68,14 +67,6 @@ type runSpec struct {
 	FilteredPkgs util.Set
 	Opts         *RunOptions
 }
-
-type LogsMode string
-
-const (
-	FullLogs LogsMode = "full"
-	HashLogs LogsMode = "hash"
-	NoLogs   LogsMode = "none"
-)
 
 func (rs *runSpec) ArgsForTask(task string) []string {
 	passThroughArgs := make([]string, 0, len(rs.Opts.passThroughArgs))
@@ -173,15 +164,13 @@ func (c *RunCommand) Run(args []string) int {
 		return 1
 	}
 
-	runOptions, err := parseRunArgs(args, c.Config.Cwd, c.Ui)
+	runOptions, err := parseRunArgs(args, c.Config, c.Ui)
 	if err != nil {
 		c.logError(c.Config.Logger, "", err)
 		return 1
 	}
 
-	c.Config.Cache.Dir = runOptions.cacheFolder
-
-	ctx, err := context.New(context.WithGraph(runOptions.cwd, c.Config))
+	ctx, err := context.New(context.WithGraph(runOptions.cwd, c.Config, runOptions.cacheOpts.Dir))
 	if err != nil {
 		c.logError(c.Config.Logger, "", err)
 		return 1
@@ -224,8 +213,7 @@ func (c *RunCommand) Run(args []string) int {
 		}
 	}
 	c.Config.Logger.Debug("global hash", "value", ctx.GlobalHash)
-	c.Config.Logger.Debug("local cache folder", "path", runOptions.cacheFolder)
-	fs.EnsureDir(runOptions.cacheFolder)
+	c.Config.Logger.Debug("local cache folder", "path", runOptions.cacheOpts.Dir)
 
 	// TODO: consolidate some of these arguments
 	g := &completeGraph{
@@ -293,7 +281,7 @@ func (c *RunCommand) runOperation(g *completeGraph, rs *runSpec, packageManager 
 		}
 		packagesInScope := rs.FilteredPkgs.UnsafeListOfStrings()
 		sort.Strings(packagesInScope)
-		if rs.Opts.dryRunJson {
+		if rs.Opts.dryRunJSON {
 			dryRun := &struct {
 				Packages []string     `json:"packages"`
 				Tasks    []hashedTask `json:"tasks"`
@@ -415,27 +403,16 @@ type RunOptions struct {
 	cwd string
 	// Whether to emit a perf profile
 	profile string
-	// Force task execution
-	forceExecution bool
-	// Cache results, false only if --no-cache is set, there is no flag to force caching
-	cache bool
-	// Cache folder
-	cacheFolder string
 	// Immediately exit on task failure
 	bail            bool
 	passThroughArgs []string
 	// Restrict execution to only the listed task names. Default false
-	only bool
-	// Task logs output modes (cached and not cached tasks):
-	// full - show all,
-	// hash - only show task hash,
-	// none - show nothing
-	cacheHitLogsMode  LogsMode
-	cacheMissLogsMode LogsMode
-	dryRun            bool
-	dryRunJson        bool
-	// Only use the Remote Cache and ignore the local cache
-	remoteOnly bool
+	only       bool
+	dryRun     bool
+	dryRunJSON bool
+
+	cacheOpts    cache.Opts
+	runcacheOpts runcache.Opts
 }
 
 func (ro *RunOptions) scopeOpts() *scope.Opts {
@@ -451,7 +428,7 @@ func (ro *RunOptions) scopeOpts() *scope.Opts {
 	}
 }
 
-func getDefaultRunOptions() *RunOptions {
+func getDefaultRunOptions(config *config.Config) *RunOptions {
 	return &RunOptions{
 		bail:                true,
 		includeDependents:   true,
@@ -459,32 +436,33 @@ func getDefaultRunOptions() *RunOptions {
 		concurrency:         10,
 		dotGraph:            "",
 		includeDependencies: false,
-		cache:               true,
 		profile:             "", // empty string does no tracing
-		forceExecution:      false,
 		only:                false,
-		cacheHitLogsMode:    FullLogs,
-		cacheMissLogsMode:   FullLogs,
-		remoteOnly:          false,
+
+		cacheOpts: cache.Opts{
+			Dir:     cache.DefaultLocation(config.Cwd),
+			Workers: config.Cache.Workers,
+		},
 	}
 }
 
-func parseRunArgs(args []string, cwd fs.AbsolutePath, output cli.Ui) (*RunOptions, error) {
-	var runOptions = getDefaultRunOptions()
+func parseRunArgs(args []string, config *config.Config, output cli.Ui) (*RunOptions, error) {
+	runOptions := getDefaultRunOptions(config)
 
 	if len(args) == 0 {
 		return nil, errors.Errorf("At least one task must be specified.")
 	}
 
-	runOptions.cwd = cwd.ToStringDuringMigration()
-	unresolvedCacheFolder := filepath.FromSlash("./node_modules/.cache/turbo")
+	runOptions.cwd = config.Cwd.ToStringDuringMigration()
+	var unresolvedCacheFolder string
+	// unresolvedCacheFolder := filepath.FromSlash("./node_modules/.cache/turbo")
 
 	if os.Getenv("TURBO_FORCE") == "true" {
-		runOptions.forceExecution = true
+		runOptions.runcacheOpts.SkipReads = true
 	}
 
 	if os.Getenv("TURBO_REMOTE_ONLY") == "true" {
-		runOptions.remoteOnly = true
+		runOptions.cacheOpts.SkipFilesystem = true
 	}
 
 	for argIndex, arg := range args {
@@ -526,7 +504,7 @@ func parseRunArgs(args []string, cwd fs.AbsolutePath, output cli.Ui) (*RunOption
 			case strings.HasPrefix(arg, "--no-deps"):
 				runOptions.includeDependents = false
 			case strings.HasPrefix(arg, "--no-cache"):
-				runOptions.cache = false
+				runOptions.runcacheOpts.SkipWrites = true
 			case strings.HasPrefix(arg, "--cacheFolder"):
 				output.Warn("[WARNING] The --cacheFolder flag has been deprecated and will be removed in future versions of turbo. Please use `--cache-dir` instead")
 				unresolvedCacheFolder = arg[len("--cacheFolder="):]
@@ -535,7 +513,7 @@ func parseRunArgs(args []string, cwd fs.AbsolutePath, output cli.Ui) (*RunOption
 			case strings.HasPrefix(arg, "--continue"):
 				runOptions.bail = false
 			case strings.HasPrefix(arg, "--force"):
-				runOptions.forceExecution = true
+				runOptions.runcacheOpts.SkipReads = true
 			case strings.HasPrefix(arg, "--stream"):
 				output.Warn("[WARNING] The --stream flag is unnecesary and has been deprecated. It will be removed in future versions of turbo.")
 			case strings.HasPrefix(arg, "--graph="): // this one must com before the next
@@ -565,32 +543,32 @@ func parseRunArgs(args []string, cwd fs.AbsolutePath, output cli.Ui) (*RunOption
 				outputLogsMode := arg[len("--output-logs="):]
 				switch outputLogsMode {
 				case "full":
-					runOptions.cacheMissLogsMode = FullLogs
-					runOptions.cacheHitLogsMode = FullLogs
+					runOptions.runcacheOpts.CacheMissLogsMode = runcache.FullLogs
+					runOptions.runcacheOpts.CacheHitLogsMode = runcache.FullLogs
 				case "none":
-					runOptions.cacheMissLogsMode = NoLogs
-					runOptions.cacheHitLogsMode = NoLogs
+					runOptions.runcacheOpts.CacheMissLogsMode = runcache.NoLogs
+					runOptions.runcacheOpts.CacheHitLogsMode = runcache.NoLogs
 				case "hash-only":
-					runOptions.cacheMissLogsMode = HashLogs
-					runOptions.cacheHitLogsMode = HashLogs
+					runOptions.runcacheOpts.CacheMissLogsMode = runcache.HashLogs
+					runOptions.runcacheOpts.CacheHitLogsMode = runcache.HashLogs
 				case "new-only":
-					runOptions.cacheMissLogsMode = FullLogs
-					runOptions.cacheHitLogsMode = HashLogs
+					runOptions.runcacheOpts.CacheMissLogsMode = runcache.FullLogs
+					runOptions.runcacheOpts.CacheHitLogsMode = runcache.HashLogs
 				default:
 					output.Warn(fmt.Sprintf("[WARNING] unknown value %v for --output-logs CLI flag. Falling back to full", outputLogsMode))
 				}
 			case strings.HasPrefix(arg, "--dry-run"):
 				runOptions.dryRun = true
 				if strings.HasPrefix(arg, "--dry-run=json") {
-					runOptions.dryRunJson = true
+					runOptions.dryRunJSON = true
 				}
 			case strings.HasPrefix(arg, "--dry"):
 				runOptions.dryRun = true
 				if strings.HasPrefix(arg, "--dry=json") {
-					runOptions.dryRunJson = true
+					runOptions.dryRunJSON = true
 				}
 			case strings.HasPrefix(arg, "--remote-only"):
-				runOptions.remoteOnly = true
+				runOptions.cacheOpts.SkipFilesystem = true
 			case strings.HasPrefix(arg, "--team"):
 			case strings.HasPrefix(arg, "--token"):
 			case strings.HasPrefix(arg, "--preflight"):
@@ -608,7 +586,12 @@ func parseRunArgs(args []string, cwd fs.AbsolutePath, output cli.Ui) (*RunOption
 	}
 
 	// We can only set this cache folder after we know actual cwd
-	runOptions.cacheFolder = filepath.Join(runOptions.cwd, unresolvedCacheFolder)
+	if unresolvedCacheFolder != "" {
+		runOptions.cacheOpts.Dir = fs.ResolveUnknownPath(config.Cwd, unresolvedCacheFolder)
+	}
+	if !config.IsLoggedIn() {
+		runOptions.cacheOpts.SkipRemote = true
+	}
 
 	return runOptions, nil
 }
@@ -652,7 +635,7 @@ func (c *RunCommand) executeTasks(g *completeGraph, rs *runSpec, engine *core.Sc
 	defer analyticsClient.CloseWithTimeout(50 * time.Millisecond)
 	// Theoretically this is overkill, but bias towards not spamming the console
 	once := &sync.Once{}
-	turboCache, err := cache.New(c.Config, rs.Opts.remoteOnly, analyticsClient, func(_cache cache.Cache, err error) {
+	turboCache, err := cache.New(rs.Opts.cacheOpts, c.Config, analyticsClient, func(_cache cache.Cache, err error) {
 		// Currently the HTTP Cache is the only one that can be disabled.
 		// With a cache system refactor, we might consider giving names to the caches so
 		// we can accurately report them here.
@@ -670,12 +653,14 @@ func (c *RunCommand) executeTasks(g *completeGraph, rs *runSpec, engine *core.Sc
 	}
 	defer turboCache.Shutdown()
 	runState := NewRunState(startAt, rs.Opts.profile)
+	runCache := runcache.New(turboCache, c.Config.Cwd, rs.Opts.runcacheOpts)
 	ec := &execContext{
 		colorCache:     NewColorCache(),
 		runState:       runState,
 		rs:             rs,
 		ui:             &cli.ConcurrentUi{Ui: c.Ui},
 		turboCache:     turboCache,
+		runCache:       runCache,
 		logger:         c.Config.Logger,
 		packageManager: packageManager,
 		processes:      c.Processes,
@@ -842,6 +827,7 @@ type execContext struct {
 	runState       *RunState
 	rs             *runSpec
 	ui             cli.Ui
+	runCache       *runcache.RunCache
 	turboCache     cache.Cache
 	logger         hclog.Logger
 	packageManager *packagemanager.PackageManager
@@ -879,9 +865,6 @@ func (e *execContext) exec(pt *nodes.PackageTask, deps dag.Set) error {
 		WarnPrefix:   actualPrefix,
 	}
 
-	logFileName := pt.RepoRelativeLogFile()
-	targetLogger.Debug("log file", "path", filepath.Join(e.rs.Opts.cwd, logFileName))
-
 	passThroughArgs := e.rs.ArgsForTask(pt.Task)
 	hash, err := e.taskHashes.CalculateTaskHash(pt, deps, passThroughArgs)
 	e.logger.Debug("task hash", "value", hash)
@@ -900,38 +883,14 @@ func (e *execContext) exec(pt *nodes.PackageTask, deps dag.Set) error {
 		return nil
 	}
 	// Cache ---------------------------------------------
-	var hit bool
-	// If we aren't forcing execution, and the task is not explicitly marked cache: false,
-	// then try to read from the cache first.
-	if !e.rs.Opts.forceExecution && pt.TaskDefinition.ShouldCache {
-		hit, _, _, err = e.turboCache.Fetch(e.rs.Opts.cwd, hash, nil)
-		if err != nil {
-			targetUi.Error(fmt.Sprintf("error fetching from cache: %s", err))
-		} else if hit {
-			switch e.rs.Opts.cacheHitLogsMode {
-			case HashLogs:
-				targetUi.Output(fmt.Sprintf("cache hit, suppressing output %s", ui.Dim(hash)))
-			case FullLogs:
-				if fs.FileExists(filepath.Join(e.rs.Opts.cwd, logFileName)) {
-					replayLogs(targetLogger, e.ui, e.rs.Opts, logFileName, hash)
-				}
-			default:
-				// NoLogs, do not output anything
-			}
-			targetLogger.Debug("done", "status", "complete", "duration", time.Since(cmdTime))
-			tracer(TargetCached, nil)
-
-			return nil
-		}
-		if e.rs.Opts.cacheHitLogsMode != NoLogs {
-			targetUi.Output(fmt.Sprintf("cache miss, executing %s", ui.Dim(hash)))
-		}
-	} else {
-		if e.rs.Opts.cacheHitLogsMode != NoLogs {
-			targetUi.Output(fmt.Sprintf("cache bypass, force executing %s", ui.Dim(hash)))
-		}
+	taskCache := e.runCache.TaskCache(pt, hash)
+	hit, err := taskCache.RestoreOutputs(targetUi, targetLogger)
+	if err != nil {
+		targetUi.Error(fmt.Sprintf("error fetching from cache: %s", err))
+	} else if hit {
+		tracer(TargetCached, nil)
+		return nil
 	}
-
 	// Setup command execution
 	argsactual := append([]string{"run"}, pt.Task)
 	argsactual = append(argsactual, passThroughArgs...)
@@ -944,37 +903,15 @@ func (e *execContext) exec(pt *nodes.PackageTask, deps dag.Set) error {
 	// Setup stdout/stderr
 	// If we are not caching anything, then we don't need to write logs to disk
 	// be careful about this conditional given the default of cache = true
-	var writer io.Writer
-	if !e.rs.Opts.cache || !pt.TaskDefinition.ShouldCache {
-		writer = os.Stdout
-	} else {
-		// Setup log file
-		if err := fs.EnsureDir(logFileName); err != nil {
-			tracer(TargetBuildFailed, err)
-			e.logError(targetLogger, actualPrefix, err)
-			if e.rs.Opts.bail {
-				os.Exit(1)
-			}
-		}
-		output, err := os.Create(logFileName)
-		if err != nil {
-			tracer(TargetBuildFailed, err)
-			e.logError(targetLogger, actualPrefix, err)
-			if e.rs.Opts.bail {
-				os.Exit(1)
-			}
-		}
-		defer output.Close()
-		bufWriter := bufio.NewWriter(output)
-		bufWriter.WriteString(fmt.Sprintf("%scache hit, replaying output %s\n", actualPrefix, ui.Dim(hash)))
-		defer bufWriter.Flush()
-		if e.rs.Opts.cacheMissLogsMode == NoLogs || e.rs.Opts.cacheMissLogsMode == HashLogs {
-			// only write to log file, not to stdout
-			writer = bufWriter
-		} else {
-			writer = io.MultiWriter(os.Stdout, bufWriter)
+	writer, err := taskCache.OutputWriter()
+	if err != nil {
+		tracer(TargetBuildFailed, err)
+		e.logError(targetLogger, actualPrefix, err)
+		if e.rs.Opts.bail {
+			os.Exit(1)
 		}
 	}
+	defer func() { _ = writer.Close() }()
 
 	logger := log.New(writer, "", 0)
 	// Setup a streamer that we'll pipe cmd.Stdout to
@@ -1006,35 +943,9 @@ func (e *execContext) exec(pt *nodes.PackageTask, deps dag.Set) error {
 	}
 
 	// Cache command outputs
-	if e.rs.Opts.cache && pt.TaskDefinition.ShouldCache {
-		outputs := pt.HashableOutputs()
-		targetLogger.Debug("caching output", "outputs", outputs)
-		ignore := []string{}
-
-		repoRelativeGlobs := make([]string, len(outputs))
-		for index, output := range outputs {
-			repoRelativeGlobs[index] = filepath.Join(pt.Pkg.Dir, output)
-		}
-
-		filesToBeCached, err := globby.GlobFiles(e.rs.Opts.cwd, repoRelativeGlobs, ignore)
-		if err != nil {
-			return err
-		}
-
-		relativePaths := make([]string, len(filesToBeCached))
-
-		for index, value := range filesToBeCached {
-			relativePath, err := filepath.Rel(e.rs.Opts.cwd, value)
-			if err != nil {
-				e.logError(targetLogger, "", fmt.Errorf("File path cannot be made relative: %w", err))
-				continue
-			}
-			relativePaths[index] = relativePath
-		}
-
-		if err := e.turboCache.Put(pt.Pkg.Dir, hash, int(time.Since(cmdTime).Milliseconds()), relativePaths); err != nil {
-			e.logError(targetLogger, "", fmt.Errorf("error caching output: %w", err))
-		}
+	duration := int(time.Since(cmdTime).Milliseconds())
+	if err = taskCache.SaveOutputs(targetLogger, targetUi, duration); err != nil {
+		e.logError(targetLogger, "", fmt.Errorf("error caching output: %w", err))
 	}
 
 	// Clean up tracing

cli/internal/runcache/runcache.go
@@ -1,0 +1,236 @@
+package runcache
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/fatih/color"
+	"github.com/hashicorp/go-hclog"
+	"github.com/mitchellh/cli"
+	"github.com/vercel/turborepo/cli/internal/cache"
+	"github.com/vercel/turborepo/cli/internal/fs"
+	"github.com/vercel/turborepo/cli/internal/globby"
+	"github.com/vercel/turborepo/cli/internal/nodes"
+	"github.com/vercel/turborepo/cli/internal/ui"
+)
+
+// LogsMode defines the ways turbo can output logs during a run for cached and not cached tasks
+type LogsMode int
+
+// LogReplayer is a function that is responsible for replaying the contents of a given log file
+type LogReplayer = func(logger hclog.Logger, output cli.Ui, logFile fs.AbsolutePath)
+
+const (
+	// FullLogs - show all,
+	FullLogs LogsMode = iota
+	// HashLogs - only show task hash
+	HashLogs
+	// NoLogs - show nothing
+	NoLogs
+)
+
+// Opts holds the configurable options for a RunCache instance
+type Opts struct {
+	SkipReads         bool
+	SkipWrites        bool
+	CacheHitLogsMode  LogsMode
+	CacheMissLogsMode LogsMode
+	LogReplayer       LogReplayer
+}
+
+// RunCache represents the interface to the cache for a single `turbo run`
+type RunCache struct {
+	cacheHitLogsMode  LogsMode
+	cacheMissLogsMode LogsMode
+	cache             cache.Cache
+	readsDisabled     bool
+	writesDisabled    bool
+	repoRoot          fs.AbsolutePath
+	logReplayer       LogReplayer
+}
+
+// New returns a new instance of RunCache, wrapping the given cache
+func New(cache cache.Cache, repoRoot fs.AbsolutePath, opts Opts) *RunCache {
+	rc := &RunCache{
+		cacheHitLogsMode:  opts.CacheHitLogsMode,
+		cacheMissLogsMode: opts.CacheMissLogsMode,
+		cache:             cache,
+		readsDisabled:     opts.SkipReads,
+		writesDisabled:    opts.SkipWrites,
+		repoRoot:          repoRoot,
+		logReplayer:       opts.LogReplayer,
+	}
+	if rc.logReplayer == nil {
+		rc.logReplayer = defaultLogReplayer
+	}
+	return rc
+}
+
+// TaskCache represents a single task's (package-task?) interface to the RunCache
+// and controls access to the task's outputs
+type TaskCache struct {
+	rc                *RunCache
+	repoRelativeGlobs []string
+	hash              string
+	pt                *nodes.PackageTask
+	cachingDisabled   bool
+	LogFileName       fs.AbsolutePath
+}
+
+// RestoreOutputs attempts to restore output for the corresponding task from the cache. Returns true
+// if successful.
+func (tc TaskCache) RestoreOutputs(terminal *cli.PrefixedUi, logger hclog.Logger) (bool, error) {
+	if tc.cachingDisabled || tc.rc.readsDisabled {
+		if tc.rc.cacheHitLogsMode != NoLogs {
+			terminal.Output(fmt.Sprintf("cache bypass, force executing %s", ui.Dim(tc.hash)))
+		}
+		return false, nil
+	}
+	// TODO(gsoltis): check if we need to restore goes here
+	// That will be an opportunity to prune down the set of outputs as well
+	hit, _, _, err := tc.rc.cache.Fetch(tc.rc.repoRoot.ToString(), tc.hash, tc.repoRelativeGlobs)
+	if err != nil {
+		return false, err
+	} else if !hit {
+		if tc.rc.cacheMissLogsMode != NoLogs {
+			terminal.Output(fmt.Sprintf("cache miss, executing %s", ui.Dim(tc.hash)))
+		}
+		return false, nil
+	}
+	switch tc.rc.cacheHitLogsMode {
+	case HashLogs:
+		terminal.Output(fmt.Sprintf("cache hit, suppressing output %s", ui.Dim(tc.hash)))
+	case FullLogs:
+		logger.Debug("log file", "path", tc.LogFileName)
+		if tc.LogFileName.FileExists() {
+			// The task label is baked into the log file, so we need to grab the underlying Ui
+			// instance in order to not duplicate it
+			tc.rc.logReplayer(logger, terminal.Ui, tc.LogFileName)
+		}
+	default:
+		// NoLogs, do not output anything
+	}
+	return true, nil
+}
+
+// nopWriteCloser is modeled after io.NopCloser, which is for Readers
+type nopWriteCloser struct {
+	io.Writer
+}
+
+func (nopWriteCloser) Close() error { return nil }
+
+type fileWriterCloser struct {
+	io.Writer
+	file  *os.File
+	bufio *bufio.Writer
+}
+
+func (fwc *fileWriterCloser) Close() error {
+	if err := fwc.bufio.Flush(); err != nil {
+		return err
+	}
+	return fwc.file.Close()
+}
+
+// OutputWriter creates a sink suitable for handling the output of the command associated
+// with this task.
+func (tc TaskCache) OutputWriter() (io.WriteCloser, error) {
+	if tc.cachingDisabled || tc.rc.writesDisabled {
+		return nopWriteCloser{os.Stdout}, nil
+	}
+	// Setup log file
+	if err := tc.LogFileName.EnsureDir(); err != nil {
+		return nil, err
+	}
+	output, err := tc.LogFileName.Create()
+	if err != nil {
+		return nil, err
+	}
+	bufWriter := bufio.NewWriter(output)
+	if _, err := bufWriter.WriteString(fmt.Sprintf("%s: cache hit, replaying output %s\n", tc.pt.OutputPrefix(), ui.Dim(tc.hash))); err != nil {
+		// We've already errored, we don't care if there's a further error closing the file we just
+		// failed to write to.
+		_ = output.Close()
+		return nil, err
+	}
+	fwc := &fileWriterCloser{
+		file:  output,
+		bufio: bufWriter,
+	}
+	if tc.rc.cacheMissLogsMode == NoLogs || tc.rc.cacheMissLogsMode == HashLogs {
+		// only write to log file, not to stdout
+		fwc.Writer = bufWriter
+	} else {
+		fwc.Writer = io.MultiWriter(os.Stdout, bufWriter)
+	}
+	return fwc, nil
+}
+
+var _emptyIgnore []string
+
+// SaveOutputs is responsible for saving the outputs of task to the cache, after the task has completed
+func (tc TaskCache) SaveOutputs(logger hclog.Logger, terminal cli.Ui, duration int) error {
+	if tc.cachingDisabled || tc.rc.writesDisabled {
+		return nil
+	}
+
+	logger.Debug("caching output", "outputs", tc.repoRelativeGlobs)
+
+	filesToBeCached, err := globby.GlobFiles(tc.rc.repoRoot.ToStringDuringMigration(), tc.repoRelativeGlobs, _emptyIgnore)
+	if err != nil {
+		return err
+	}
+
+	relativePaths := make([]string, len(filesToBeCached))
+
+	for index, value := range filesToBeCached {
+		relativePath, err := tc.rc.repoRoot.RelativePathString(value)
+		if err != nil {
+			logger.Error("error", err)
+			terminal.Error(fmt.Sprintf("%s%s", ui.ERROR_PREFIX, color.RedString(" %v", fmt.Errorf("File path cannot be made relative: %w", err))))
+			continue
+		}
+		relativePaths[index] = relativePath
+	}
+
+	return tc.rc.cache.Put(tc.pt.Pkg.Dir, tc.hash, duration, relativePaths)
+}
+
+// TaskCache returns a TaskCache instance, providing an interface to the underlying cache specific
+// to this run and the given PackageTask
+func (rc *RunCache) TaskCache(pt *nodes.PackageTask, hash string) TaskCache {
+	logFileName := rc.repoRoot.Join(pt.RepoRelativeLogFile())
+	hashableOutputs := pt.HashableOutputs()
+	repoRelativeGlobs := make([]string, len(hashableOutputs))
+	for index, output := range hashableOutputs {
+		repoRelativeGlobs[index] = filepath.Join(pt.Pkg.Dir, output)
+	}
+	return TaskCache{
+		rc:                rc,
+		repoRelativeGlobs: repoRelativeGlobs,
+		hash:              hash,
+		pt:                pt,
+		cachingDisabled:   !pt.TaskDefinition.ShouldCache,
+		LogFileName:       logFileName,
+	}
+}
+
+// defaultLogReplayer will try to replay logs back to the given Ui instance
+func defaultLogReplayer(logger hclog.Logger, output cli.Ui, logFileName fs.AbsolutePath) {
+	logger.Debug("start replaying logs")
+	f, err := logFileName.Open()
+	if err != nil {
+		output.Warn(fmt.Sprintf("error reading logs: %v", err))
+		logger.Error(fmt.Sprintf("error reading logs: %v", err.Error()))
+	}
+	defer func() { _ = f.Close() }()
+	scan := bufio.NewScanner(f)
+	for scan.Scan() {
+		output.Output(string(scan.Bytes())) //Writing to Stdout
+	}
+	logger.Debug("finish replaying logs")
+}

commit b2bd1bcf5f2b917cfb5adbbe1bee2e4357101421

fix(cli): add colors back for cache hit log line (#1346)
Fixes a bug introduced in [1.2.10](https://github.com/vercel/turborepo/releases/tag/v1.2.10) in which the first line (cache hit) is missing the correct color. 


Caused by https://github.com/vercel/turborepo/pull/1256

cli/internal/colorcache/color_cache.go
@@ -1,4 +1,4 @@
-package run
+package colorcache
 
 import (
 	"sync"
@@ -21,7 +21,8 @@ type ColorCache struct {
 	Cache      map[interface{}]colorFn
 }
 
-func NewColorCache() *ColorCache {
+// New creates an instance of ColorCache with helpers for adding colors to task outputs
+func New() *ColorCache {
 	return &ColorCache{
 		TermColors: getTerminalPackageColors(),
 		index:      0,

cli/internal/run/run.go
@@ -19,6 +19,7 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/vercel/turborepo/cli/internal/analytics"
 	"github.com/vercel/turborepo/cli/internal/cache"
+	"github.com/vercel/turborepo/cli/internal/colorcache"
 	"github.com/vercel/turborepo/cli/internal/config"
 	"github.com/vercel/turborepo/cli/internal/context"
 	"github.com/vercel/turborepo/cli/internal/core"
@@ -590,10 +591,11 @@ func (r *run) executeTasks(g *completeGraph, rs *runSpec, engine *core.Scheduler
 		}
 	}
 	defer turboCache.Shutdown()
+	colorCache := colorcache.New()
 	runState := NewRunState(startAt, rs.Opts.runOpts.profile)
-	runCache := runcache.New(turboCache, r.config.Cwd, rs.Opts.runcacheOpts)
+	runCache := runcache.New(turboCache, r.config.Cwd, rs.Opts.runcacheOpts, colorCache)
 	ec := &execContext{
-		colorCache:     NewColorCache(),
+		colorCache:     colorCache,
 		runState:       runState,
 		rs:             rs,
 		ui:             &cli.ConcurrentUi{Ui: r.ui},
@@ -736,7 +738,7 @@ func validateTasks(pipeline fs.Pipeline, tasks []string) error {
 }
 
 type execContext struct {
-	colorCache     *ColorCache
+	colorCache     *colorcache.ColorCache
 	runState       *RunState
 	rs             *runSpec
 	ui             cli.Ui
@@ -768,14 +770,14 @@ func (e *execContext) exec(pt *nodes.PackageTask, deps dag.Set) error {
 	tracer := e.runState.Run(pt.TaskID)
 
 	// Create a logger
-	pref := e.colorCache.PrefixColor(pt.PackageName)
-	actualPrefix := pref("%s: ", pt.OutputPrefix())
+	colorPrefixer := e.colorCache.PrefixColor(pt.PackageName)
+	prettyTaskPrefix := colorPrefixer("%s: ", pt.OutputPrefix())
 	targetUi := &cli.PrefixedUi{
 		Ui:           e.ui,
-		OutputPrefix: actualPrefix,
-		InfoPrefix:   actualPrefix,
-		ErrorPrefix:  actualPrefix,
-		WarnPrefix:   actualPrefix,
+		OutputPrefix: prettyTaskPrefix,
+		InfoPrefix:   prettyTaskPrefix,
+		ErrorPrefix:  prettyTaskPrefix,
+		WarnPrefix:   prettyTaskPrefix,
 	}
 
 	passThroughArgs := e.rs.ArgsForTask(pt.Task)
@@ -819,16 +821,16 @@ func (e *execContext) exec(pt *nodes.PackageTask, deps dag.Set) error {
 	writer, err := taskCache.OutputWriter()
 	if err != nil {
 		tracer(TargetBuildFailed, err)
-		e.logError(targetLogger, actualPrefix, err)
+		e.logError(targetLogger, prettyTaskPrefix, err)
 		if !e.rs.Opts.runOpts.continueOnError {
 			os.Exit(1)
 		}
 	}
 	logger := log.New(writer, "", 0)
 	// Setup a streamer that we'll pipe cmd.Stdout to
-	logStreamerOut := logstreamer.NewLogstreamer(logger, actualPrefix, false)
+	logStreamerOut := logstreamer.NewLogstreamer(logger, prettyTaskPrefix, false)
 	// Setup a streamer that we'll pipe cmd.Stderr to.
-	logStreamerErr := logstreamer.NewLogstreamer(logger, actualPrefix, false)
+	logStreamerErr := logstreamer.NewLogstreamer(logger, prettyTaskPrefix, false)
 	cmd.Stderr = logStreamerErr
 	cmd.Stdout = logStreamerOut
 	// Flush/Reset any error we recorded

cli/internal/runcache/runcache.go
@@ -12,6 +12,7 @@ import (
 	"github.com/mitchellh/cli"
 	"github.com/spf13/pflag"
 	"github.com/vercel/turborepo/cli/internal/cache"
+	"github.com/vercel/turborepo/cli/internal/colorcache"
 	"github.com/vercel/turborepo/cli/internal/fs"
 	"github.com/vercel/turborepo/cli/internal/globby"
 	"github.com/vercel/turborepo/cli/internal/nodes"
@@ -60,7 +61,7 @@ func AddFlags(opts *Opts, flags *pflag.FlagSet) {
 		Value:    &logsModeValue{opts: opts},
 	})
 	_ = flags.Bool("stream", true, "Unused")
-	if err := flags.MarkDeprecated("stream", "[WARNING] The --stream flag is unnecesary and has been deprecated. It will be removed in future versions of turbo."); err != nil {
+	if err := flags.MarkDeprecated("stream", "[WARNING] The --stream flag is unnecessary and has been deprecated. It will be removed in future versions of turbo."); err != nil {
 		// fail fast if we've misconfigured our flags
 		panic(err)
 	}
@@ -125,10 +126,11 @@ type RunCache struct {
 	writesDisabled    bool
 	repoRoot          fs.AbsolutePath
 	logReplayer       LogReplayer
+	colorCache        *colorcache.ColorCache
 }
 
 // New returns a new instance of RunCache, wrapping the given cache
-func New(cache cache.Cache, repoRoot fs.AbsolutePath, opts Opts) *RunCache {
+func New(cache cache.Cache, repoRoot fs.AbsolutePath, opts Opts, colorCache *colorcache.ColorCache) *RunCache {
 	rc := &RunCache{
 		cacheHitLogsMode:  opts.CacheHitLogsMode,
 		cacheMissLogsMode: opts.CacheMissLogsMode,
@@ -137,6 +139,7 @@ func New(cache cache.Cache, repoRoot fs.AbsolutePath, opts Opts) *RunCache {
 		writesDisabled:    opts.SkipWrites,
 		repoRoot:          repoRoot,
 		logReplayer:       opts.LogReplayer,
+		colorCache:        colorCache,
 	}
 	if rc.logReplayer == nil {
 		rc.logReplayer = defaultLogReplayer
@@ -225,8 +228,10 @@ func (tc TaskCache) OutputWriter() (io.WriteCloser, error) {
 	if err != nil {
 		return nil, err
 	}
+	colorPrefixer := tc.rc.colorCache.PrefixColor(tc.pt.PackageName)
+	prettyTaskPrefix := colorPrefixer(tc.pt.OutputPrefix())
 	bufWriter := bufio.NewWriter(output)
-	if _, err := bufWriter.WriteString(fmt.Sprintf("%s: cache hit, replaying output %s\n", tc.pt.OutputPrefix(), ui.Dim(tc.hash))); err != nil {
+	if _, err := bufWriter.WriteString(fmt.Sprintf("%s: cache hit, replaying output %s\n", prettyTaskPrefix, ui.Dim(tc.hash))); err != nil {
 		// We've already errored, we don't care if there's a further error closing the file we just
 		// failed to write to.
 		_ = output.Close()