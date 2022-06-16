3921b975069675d52ddea30ac792bd0380308698
Christopher Allford <christopher.allford@automattic.com>
2022-06-15 23:17:02 -0700

    Added Turbo Version Compatibility Validation

--- cli/internal/fs/package_json.go
+++ cli/internal/fs/package_json.go
@@ -10,6 +10,7 @@ import (
 type PackageJSON struct {
 	Name                   string            `json:"name,omitempty"`
 	Version                string            `json:"version,omitempty"`
+	Engines                map[string]string `json:"engines,omitempty"`
 	Scripts                map[string]string `json:"scripts,omitempty"`
 	Dependencies           map[string]string `json:"dependencies,omitempty"`
 	DevDependencies        map[string]string `json:"devDependencies,omitempty"`
