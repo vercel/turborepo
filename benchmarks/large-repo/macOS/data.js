15bbc3d575a9ea35879f62bfded7cba5f09a3fde
github-action-benchmark <github@users.noreply.github.com>
2022-06-18 00:44:06 +0000

    add macOS Benchmark (customSmallerIsBetter) benchmark result for 6e12d11e3aca1f063b87bbcba5aff654fa9a7b23

--- benchmarks/large-repo/macOS/data.js
+++ benchmarks/large-repo/macOS/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655426480073,
+  "lastUpdate": 1655513046010,
   "repoUrl": "https://github.com/vercel/turborepo",
   "entries": {
     "macOS Benchmark": [
@@ -1112,6 +1112,52 @@ window.BENCHMARK_DATA = {
             "value": 48385.2,
             "unit": "ms",
             "range": "10955"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jeff Astor",
+            "username": "Jastor11",
+            "email": "jeff@astor.io"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6e12d11e3aca1f063b87bbcba5aff654fa9a7b23",
+          "message": "Friendly spellcheck in README.md (#1416)",
+          "timestamp": "2022-06-17T16:08:29Z",
+          "url": "https://github.com/vercel/turborepo/commit/6e12d11e3aca1f063b87bbcba5aff654fa9a7b23"
+        },
+        "date": 1655513044818,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Clean Build",
+            "value": 212889,
+            "unit": "ms",
+            "range": "0"
+          },
+          {
+            "name": "Cached Build - no changes",
+            "value": 8277.4,
+            "unit": "ms",
+            "range": "1334"
+          },
+          {
+            "name": "Cached Build - source code change",
+            "value": 7610.8,
+            "unit": "ms",
+            "range": "1296"
+          },
+          {
+            "name": "Cached Build - dependency change",
+            "value": 48680.8,
+            "unit": "ms",
+            "range": "11024"
           }
         ]
       }
