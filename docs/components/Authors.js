300b50cf308e2cc9ef1b1d67309994ad5e0bc56a
Jared Palmer <jared@jaredpalmer.com>
2022-06-23 09:43:25 -0400

    Add tom and nathan to docs site

--- docs/components/Authors.js
+++ docs/components/Authors.js
@@ -21,17 +21,21 @@ const team = {
     twitterUsername: "gsoltis",
     picture: "/images/people/gsoltis.jpeg",
   },
+  nathanhammond: {
+    name: "Nathan Hammond",
+    twitterUsername: "nathanhammond",
+    picture: "/images/people/nathanhammond.jpeg",
+  },
+  tknickman: {
+    name: "Tom Knickman",
+    twitterUsername: "tknickman",
+    picture: "/images/people/tknickman.jpeg",
+  },
 };
 
 export function Authors({ authors }) {
   return (
-    <div
-      className="authors grid grid-cols-[repeat(var(--grid-cnt-small),minmax(0,1fr))] gap-4 py-8 border-b border-gray-400 md:grid-cols-[repeat(var(--grid-cnt),minmax(0,1fr))] border-opacity-20"
-      style={{
-        "--grid-cnt": authors.length,
-        "--grid-cnt-small": authors.length > 1 ? 2 : 1,
-      }}
-    >
+    <div className="authors justify-center mx-auto flex flex-wrap gap-7  py-8 border-b border-gray-400  border-opacity-20">
       {authors.map((username) =>
         !!team[username] ? (
           <Avatar key={username} {...team[username]} />
