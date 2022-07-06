1a6c178c570af39d013bf3891cd4f79009433ec0
Thomas Knickman <tom.knickman@vercel.com>
2022-07-06 14:56:02 -0400

    feat(examples): add react-native-web (#1486)
    Fixes https://github.com/vercel/turborepo/issues/1119
    
    Adds an example with:
    - `native`: a [react-native](https://reactnative.dev/) app built with [expo](https://docs.expo.dev/)
    - `web`: a [Next.js](https://nextjs.org) app with [react-native-web](https://necolas.github.io/react-native-web/)
    - `ui`: a stub [react-native](https://reactnative.dev/) component library shared by both `web` and `native` applications
    
    This uses expo with react native for a simpler native development experience.

---
+++ examples/with-react-native-web/apps/native/metro.config.js
@@ -0,0 +1,19 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(__dirname, "../..");
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages, and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+
+module.exports = config;
