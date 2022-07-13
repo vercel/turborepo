2f6e46f89578e829ec2b47162fd0e8e8c9ee14cf
tknickman <tom.knickman@vercel.com>
2022-07-13 15:23:49 -0400

    update tailwind ui docs

--- examples/with-tailwind/packages/tailwind-config/tailwind.config.js
+++ examples/with-tailwind/packages/tailwind-config/tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
   content: [
     // app content
     `src/**/*.{js,ts,jsx,tsx}`,
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
