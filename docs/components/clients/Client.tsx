268337fce95e188e89631a0c42cc4082e4fe134f
Jared Palmer <jared@jaredpalmer.com>
2022-07-27 09:21:41 -0700

    Use new future image component and priority prop

--- docs/components/clients/Client.tsx
+++ docs/components/clients/Client.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Image from "next/image";
+import Image from "next/future/image";
 
 interface ClientProps {
   name: string;
   image: string;
   className?: string;
   theme: string;
+  priority: boolean;
   style?: React.CSSProperties;
 }
 
 export const Client = React.memo<ClientProps>(
-  ({ name, image, style, theme, ...rest }) => (
+  ({ name, image, style, theme, priority, ...rest }) => (
     <span title={name} {...rest}>
       <Image
         src={image.replace(
@@ -18,10 +19,11 @@ export const Client = React.memo<ClientProps>(
           theme == "dark" ? "/logos/white" : "/logos/color"
         )}
         alt={name}
-        width={style?.width ?? style?.maxWidth ?? 100}
+        width={style?.width ?? 100}
         height={style?.height ?? 75}
-        loading="lazy"
         className="inline"
+        priority={priority}
+        style={{ width: "auto" }}
       />
     </span>
   )
