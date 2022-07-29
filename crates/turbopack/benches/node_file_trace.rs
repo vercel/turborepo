vercel/turborepo

commit dfc7822db83fc21cf1d9bf3eb3eac322a02da747

benchmark refactor (#177)
Groups the benchmarks in a nicer way for the final report and reduces nesting in the code

crates/turbopack/benches/node_file_trace.rs
@@ -1,7 +1,6 @@
 use std::{fs, path::PathBuf};
 
-use criterion::Criterion;
-use lazy_static::lazy_static;
+use criterion::{Bencher, BenchmarkId, Criterion};
 use regex::Regex;
 use turbo_tasks::{NothingVc, TurboTasks, Value};
 use turbo_tasks_fs::{DiskFileSystemVc, FileSystemPathVc, NullFileSystem, NullFileSystemVc};
@@ -18,129 +17,139 @@ use turbopack_core::{
 pub fn benchmark(c: &mut Criterion) {
     register();
 
-    let package_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let mut tests_root = package_root;
-    tests_root.push("tests");
-    let mut tests_dir = tests_root.clone();
-    tests_dir.push("node-file-trace");
-    tests_dir.push("integration");
+    let bench_filter = Regex::new(r"(empty|simple|dynamic-in-package|react|whatwg-url|axios|azure-cosmos|cowsay|env-var|fast-glob)\.js$").unwrap();
+
+    let tests_root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests");
+    let tests_dir = tests_root.join("node-file-trace/integration");
+
     let results = fs::read_dir(tests_dir).unwrap();
+
+    let mut group = c.benchmark_group("node-file-trace");
+    group.sample_size(50);
+
     for result in results {
-        let result = result.unwrap();
-        if result.file_type().unwrap().is_file() {
-            let name = result.file_name();
-            let name = name.to_string_lossy();
-            lazy_static! {
-                static ref BENCH_FILTER: Regex =
-                    Regex::new(r"(empty|simple|dynamic-in-package|react|whatwg-url|axios|azure-cosmos|cowsay|env-var|fast-glob)\.js$").unwrap();
-            }
-            if !BENCH_FILTER.is_match(name.as_ref()) {
+        let entry = result.unwrap();
+        if entry.file_type().unwrap().is_file() {
+            let name = entry.file_name().into_string().unwrap();
+            if !bench_filter.is_match(&name) {
                 continue;
             }
-            let input = format!("node-file-trace/integration/{name}");
+
+            let input = format!("integration/{name}");
             let tests_root = tests_root.to_string_lossy().to_string();
 
-            let mut group = c.benchmark_group(&input);
-            group.sample_size(10);
-            {
-                let tests_root = tests_root.clone();
-                let input = input.clone();
-                group.bench_function("emit_with_completion RebasedAsset", move |b| {
-                    let tests_root = tests_root.clone();
-                    let input = input.clone();
-                    let r = tokio::runtime::Builder::new_current_thread()
-                        .enable_all()
-                        .build()
-                        .unwrap();
-                    b.to_async(r).iter(move || {
-                        let tt = TurboTasks::new(MemoryBackend::new());
-                        let tests_root = tests_root.clone();
-                        let input = input.clone();
-                        async move {
-                            tt.run_once(async move {
-                                let input_fs =
-                                    DiskFileSystemVc::new("tests".to_string(), tests_root.clone());
-                                let input = FileSystemPathVc::new(input_fs.into(), &input);
+            let bench_input = BenchInput { tests_root, input };
 
-                                let input_dir = input.parent().parent();
-                                let output_fs: NullFileSystemVc = NullFileSystem.into();
-                                let output_dir = FileSystemPathVc::new(output_fs.into(), "");
-
-                                let source = SourceAssetVc::new(input);
-                                let context = ModuleAssetContextVc::new(
-                                    input_dir,
-                                    EnvironmentVc::new(
-                                        Value::new(ExecutionEnvironment::NodeJsLambda(
-                                            NodeJsEnvironment {
-                                                typescript_enabled: false,
-                                                compile_target: CompileTargetVc::current(),
-                                                node_version: 0,
-                                            }
-                                            .into(),
-                                        )),
-                                        Value::new(EnvironmentIntention::Server),
-                                    ),
-                                );
-                                let module = context.process(source.into());
-                                let rebased = RebasedAssetVc::new(module, input_dir, output_dir);
-
-                                emit_with_completion(rebased.into()).await?;
-
-                                Ok(())
-                            })
-                            .await
-                            .unwrap();
-                        }
-                    })
-                });
-            }
-            group.bench_function("emit module", move |b| {
-                let tests_root = tests_root.clone();
-                let input = input.clone();
-                let r = tokio::runtime::Builder::new_current_thread()
-                    .enable_all()
-                    .build()
-                    .unwrap();
-                b.to_async(r).iter(move || {
-                    let tt = TurboTasks::new(MemoryBackend::new());
-                    let tests_root = tests_root.clone();
-                    let input = input.clone();
-                    async move {
-                        let task = tt.spawn_once_task(async move {
-                            let input_fs =
-                                DiskFileSystemVc::new("tests".to_string(), tests_root.clone());
-                            let input = FileSystemPathVc::new(input_fs.into(), &input);
-
-                            let input_dir = input.parent().parent();
-                            let output_fs: NullFileSystemVc = NullFileSystem.into();
-                            let output_dir = FileSystemPathVc::new(output_fs.into(), "");
-
-                            let source = SourceAssetVc::new(input);
-                            let context = ModuleAssetContextVc::new(
-                                input_dir,
-                                EnvironmentVc::new(
-                                    Value::new(ExecutionEnvironment::NodeJsLambda(
-                                        NodeJsEnvironment {
-                                            typescript_enabled: false,
-                                            compile_target: CompileTargetVc::current(),
-                                            node_version: 0,
-                                        }
-                                        .into(),
-                                    )),
-                                    Value::new(EnvironmentIntention::Server),
-                                ),
-                            );
-                            let module = context.process(source.into());
-                            let rebased = RebasedAssetVc::new(module, input_dir, output_dir);
-
-                            emit(rebased.into());
-
-                            Ok(NothingVc::new().into())
-                        });
-                        tt.wait_task_completion(task, true).await.unwrap();
-                    }
-                })
-            });
+            group.bench_with_input(
+                BenchmarkId::new("emit_with_completion", &bench_input.input),
+                &bench_input,
+                bench_emit_with_completion,
+            );
+            group.bench_with_input(
+                BenchmarkId::new("emit", &bench_input.input),
+                &bench_input,
+                bench_emit,
+            );
         }
     }
+
+    group.finish();
+}
+
+struct BenchInput {
+    tests_root: String,
+    input: String,
+}
+
+fn bench_emit_with_completion(b: &mut Bencher, bench_input: &BenchInput) {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    b.to_async(rt).iter(move || {
+        let tt = TurboTasks::new(MemoryBackend::new());
+        let tests_root = bench_input.tests_root.clone();
+        let input = bench_input.input.clone();
+        async move {
+            tt.run_once(async move {
+                let input_fs = DiskFileSystemVc::new("tests".to_string(), tests_root.clone());
+                let input = FileSystemPathVc::new(input_fs.into(), &input);
+
+                let input_dir = input.parent().parent();
+                let output_fs: NullFileSystemVc = NullFileSystem.into();
+                let output_dir = FileSystemPathVc::new(output_fs.into(), "");
+
+                let source = SourceAssetVc::new(input);
+                let context = ModuleAssetContextVc::new(
+                    input_dir,
+                    EnvironmentVc::new(
+                        Value::new(ExecutionEnvironment::NodeJsLambda(
+                            NodeJsEnvironment {
+                                typescript_enabled: false,
+                                compile_target: CompileTargetVc::current(),
+                                node_version: 0,
+                            }
+                            .into(),
+                        )),
+                        Value::new(EnvironmentIntention::Server),
+                    ),
+                );
+                let module = context.process(source.into());
+                let rebased = RebasedAssetVc::new(module, input_dir, output_dir);
+
+                emit_with_completion(rebased.into()).await?;
+
+                Ok(())
+            })
+            .await
+            .unwrap();
+        }
+    })
+}
+
+fn bench_emit(b: &mut Bencher, bench_input: &BenchInput) {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    b.to_async(rt).iter(move || {
+        let tt = TurboTasks::new(MemoryBackend::new());
+        let tests_root = bench_input.tests_root.clone();
+        let input = bench_input.input.clone();
+        async move {
+            let task = tt.spawn_once_task(async move {
+                let input_fs = DiskFileSystemVc::new("tests".to_string(), tests_root.clone());
+                let input = FileSystemPathVc::new(input_fs.into(), &input);
+
+                let input_dir = input.parent().parent();
+                let output_fs: NullFileSystemVc = NullFileSystem.into();
+                let output_dir = FileSystemPathVc::new(output_fs.into(), "");
+
+                let source = SourceAssetVc::new(input);
+                let context = ModuleAssetContextVc::new(
+                    input_dir,
+                    EnvironmentVc::new(
+                        Value::new(ExecutionEnvironment::NodeJsLambda(
+                            NodeJsEnvironment {
+                                typescript_enabled: false,
+                                compile_target: CompileTargetVc::current(),
+                                node_version: 0,
+                            }
+                            .into(),
+                        )),
+                        Value::new(EnvironmentIntention::Server),
+                    ),
+                );
+                let module = context.process(source.into());
+                let rebased = RebasedAssetVc::new(module, input_dir, output_dir);
+
+                emit(rebased.into());
+
+                Ok(NothingVc::new().into())
+            });
+            tt.wait_task_completion(task, true).await.unwrap();
+        }
+    })
 }

crates/swc-ast-explorer/Cargo.toml
@@ -1,7 +1,12 @@
 [package]
-edition = "2021"
 name = "swc-ast-explorer"
 version = "0.1.0"
+edition = "2021"
+
+[[bin]]
+name = "swc-ast-explorer"
+path = "src/main.rs"
+bench = false
 
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 

crates/turbo-tasks/Cargo.toml
@@ -11,6 +11,7 @@ bench = false
 [features]
 assert_task_state = []
 tokio_tracing = ["tokio/tracing"]
+log_function_stats = []
 
 [dependencies]
 any_key = "0.1.1"

crates/turbo-tasks/src/native_function.rs
@@ -57,9 +57,11 @@ impl NativeFunction {
         match (self.bind_fn)(inputs) {
             Ok(native_fn) => Box::new(move || {
                 let r = native_fn();
-                let count = self.executed_count.fetch_add(1, Ordering::Relaxed);
-                if count > 0 && count % 100000 == 0 {
-                    println!("{} was executed {}k times", self.name, count / 1000);
+                if cfg!(feature = "log_function_stats") {
+                    let count = self.executed_count.fetch_add(1, Ordering::Relaxed);
+                    if count > 0 && count % 100000 == 0 {
+                        println!("{} was executed {}k times", self.name, count / 1000);
+                    }
                 }
                 r
             }),

crates/turbopack-ecmascript/benches/analyzer.rs
@@ -5,9 +5,9 @@ use std::{
     time::Duration,
 };
 
-use criterion::Criterion;
+use criterion::{Bencher, BenchmarkId, Criterion};
 use swc_common::{FilePathMapping, Mark, SourceMap, GLOBALS};
-use swc_ecma_ast::EsVersion;
+use swc_ecma_ast::{EsVersion, Program};
 use swc_ecma_parser::parse_file_as_program;
 use swc_ecma_transforms_base::resolver;
 use swc_ecma_visit::VisitMutWith;
@@ -18,30 +18,31 @@ use turbopack_core::{
     target::{Arch, CompileTarget, Endianness, Libc, Platform},
 };
 use turbopack_ecmascript::analyzer::{
-    graph::{create_graph, EvalContext},
+    graph::{create_graph, EvalContext, VarGraph},
     linker::{link, LinkCache},
     test_utils::visitor,
 };
 
 pub fn benchmark(c: &mut Criterion) {
     turbopack_ecmascript::register();
-    let mut tests_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    tests_dir.push("tests");
-    tests_dir.push("analyzer");
-    tests_dir.push("graph");
+
+    let tests_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/analyzer/graph");
     let results = fs::read_dir(tests_dir).unwrap();
+
+    let mut group = c.benchmark_group("analyzer");
+    group.warm_up_time(Duration::from_secs(1));
+    group.measurement_time(Duration::from_secs(3));
+
     for result in results {
-        let result = result.unwrap();
-        if result.file_type().unwrap().is_dir() {
-            let name = result.file_name();
-            let name = name.to_string_lossy();
-            let mut input = result.path();
-            input.push("input.js");
+        let entry = result.unwrap();
+        if entry.file_type().unwrap().is_dir() {
+            let name = entry.file_name().into_string().unwrap();
+            let input = entry.path().join("input.js");
 
             let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
             let fm = cm.load_file(&input).unwrap();
             GLOBALS.set(&swc_common::Globals::new(), || {
-                let mut m = parse_file_as_program(
+                let mut program = parse_file_as_program(
                     &fm,
                     Default::default(),
                     EsVersion::latest(),
@@ -52,61 +53,78 @@ pub fn benchmark(c: &mut Criterion) {
 
                 let unresolved_mark = Mark::new();
                 let top_level_mark = Mark::new();
-                m.visit_mut_with(&mut resolver(unresolved_mark, top_level_mark, false));
+                program.visit_mut_with(&mut resolver(unresolved_mark, top_level_mark, false));
 
-                let eval_context = EvalContext::new(&m, unresolved_mark);
+                let eval_context = EvalContext::new(&program, unresolved_mark);
+                let var_graph = create_graph(&program, &eval_context);
 
-                let var_graph = create_graph(&m, &eval_context);
+                let input = BenchInput {
+                    program,
+                    eval_context,
+                    var_graph,
+                };
 
-                let mut group = c.benchmark_group(&format!("analyzer/{name}"));
-                group.warm_up_time(Duration::from_secs(1));
-                group.measurement_time(Duration::from_secs(3));
-                group.bench_function("create_graph", move |b| {
-                    b.iter(|| create_graph(&m, &eval_context));
-                });
-                group.bench_function("link", move |b| {
-                    let r = tokio::runtime::Builder::new_current_thread()
-                        .build()
-                        .unwrap();
-                    b.to_async(r).iter(|| async {
-                        let cache = Mutex::new(LinkCache::new());
-                        for val in var_graph.values.values() {
-                            VcStorage::with(async {
-                                link(
-                                    &var_graph,
-                                    val.clone(),
-                                    &(|val| {
-                                        Box::pin(visitor(
-                                            val,
-                                            EnvironmentVc::new(
-                                                Value::new(ExecutionEnvironment::NodeJsLambda(
-                                                    NodeJsEnvironment {
-                                                        typescript_enabled: false,
-                                                        compile_target: CompileTarget {
-                                                            arch: Arch::Unknown,
-                                                            endianness: Endianness::Big,
-                                                            platform: Platform::Unknown,
-                                                            libc: Libc::Unknown,
-                                                        }
-                                                        .into(),
-                                                        node_version: 0,
-                                                    }
-                                                    .into(),
-                                                )),
-                                                Value::new(EnvironmentIntention::Server),
-                                            ),
-                                        ))
-                                    }),
-                                    &cache,
-                                )
-                                .await
-                            })
-                            .await
-                            .unwrap();
-                        }
-                    });
-                });
+                group.bench_with_input(
+                    BenchmarkId::new("create_graph", &name),
+                    &input,
+                    bench_create_graph,
+                );
+                group.bench_with_input(BenchmarkId::new("link", &name), &input, bench_link);
             });
         }
     }
+}
+
+struct BenchInput {
+    program: Program,
+    eval_context: EvalContext,
+    var_graph: VarGraph,
+}
+
+fn bench_create_graph(b: &mut Bencher, input: &BenchInput) {
+    b.iter(|| create_graph(&input.program, &input.eval_context));
+}
+
+fn bench_link(b: &mut Bencher, input: &BenchInput) {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .build()
+        .unwrap();
+
+    b.to_async(rt).iter(|| async {
+        let cache = Mutex::new(LinkCache::new());
+        for val in input.var_graph.values.values() {
+            VcStorage::with(async {
+                link(
+                    &input.var_graph,
+                    val.clone(),
+                    &(|val| {
+                        Box::pin(visitor(
+                            val,
+                            EnvironmentVc::new(
+                                Value::new(ExecutionEnvironment::NodeJsLambda(
+                                    NodeJsEnvironment {
+                                        typescript_enabled: false,
+                                        compile_target: CompileTarget {
+                                            arch: Arch::Unknown,
+                                            endianness: Endianness::Big,
+                                            platform: Platform::Unknown,
+                                            libc: Libc::Unknown,
+                                        }
+                                        .into(),
+                                        node_version: 0,
+                                    }
+                                    .into(),
+                                )),
+                                Value::new(EnvironmentIntention::Server),
+                            ),
+                        ))
+                    }),
+                    &cache,
+                )
+                .await
+            })
+            .await
+            .unwrap();
+        }
+    });
 }

crates/turbopack/Cargo.toml
@@ -47,7 +47,7 @@ version = "0.184.0"
 version = "0.4"
 
 [dev-dependencies]
-criterion = { version = "0.3.5", features = ["tokio"] }
+criterion = { version = "0.3.5", features = ["async_tokio"] }
 difference = "2.0"
 rstest = "0.12.0"
 rstest_reuse = "0.3.0"

commit cf2f949c98a8bbc3f21d580abd2592eb3c39dcbf

fix benchmarks (#182)
broke the path in #177

.github/workflows/bench_pr.yml
@@ -26,43 +26,9 @@ jobs:
         with:
           key: benchmark
 
-      - name: Run cargo build benches (main)
-        uses: actions-rs/cargo@v1
-        continue-on-error: true
+      - uses: boa-dev/criterion-compare-action@v3.2.2
         with:
-          command: build
-          args: --benches --release
-
-      - name: Run cargo bench (main)
-        uses: actions-rs/cargo@v1
-        continue-on-error: true
-        with:
-          command: bench
-          args: -- --save-baseline=main
-
-      - uses: actions/checkout@v3
-        with:
-          clean: false
-
-      - name: Run cargo build benches (PR)
-        uses: actions-rs/cargo@v1
-        continue-on-error: true
-        with:
-          command: build
-          args: --benches --release
-
-      - name: Run cargo bench (PR)
-        uses: actions-rs/cargo@v1
-        continue-on-error: true
-        with:
-          command: bench
-          args: -- --save-baseline=pr
-
-      - name: Show results
-        uses: actions-rs/cargo@v1
-        with:
-          command: bench
-          args: -- --load-baseline=pr --baseline=main
+          branchName: ${{ github.base_ref }}
 
       - name: Upload report
         if: always()

crates/turbopack/benches/node_file_trace.rs
@@ -22,11 +22,10 @@ pub fn benchmark(c: &mut Criterion) {
     let tests_root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests");
     let tests_dir = tests_root.join("node-file-trace/integration");
 
-    let results = fs::read_dir(tests_dir).unwrap();
-
     let mut group = c.benchmark_group("node-file-trace");
-    group.sample_size(50);
+    group.sample_size(10);
 
+    let results = fs::read_dir(tests_dir).unwrap();
     for result in results {
         let entry = result.unwrap();
         if entry.file_type().unwrap().is_file() {
@@ -35,7 +34,7 @@ pub fn benchmark(c: &mut Criterion) {
                 continue;
             }
 
-            let input = format!("integration/{name}");
+            let input = format!("node-file-trace/integration/{name}");
             let tests_root = tests_root.to_string_lossy().to_string();
 
             let bench_input = BenchInput { tests_root, input };