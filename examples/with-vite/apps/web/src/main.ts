73647a43187c4073ffb649039c1b96cd88b97653
Thomas Knickman <tom.knickman@vercel.com>
2022-08-05 11:07:01 -0400

    feat(examples): add vite (#1573)
    Vite example. 
    
    Fixes TURBO-205

---
+++ examples/with-vite/apps/web/src/main.ts
@@ -0,0 +1,20 @@
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import { Header, Counter, setupCounter } from "ui";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    ${Header({ title: "Web" })}
+    <div class="card">
+      ${Counter()}
+    </div>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
