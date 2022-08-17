da209fe05f1651be5a4fce92e05dd9e18f7ecacf
Thomas Knickman <tom.knickman@vercel.com>
2022-08-17 12:05:54 -0400

    chore(ci): test all other packages in CI (#1709)
    Test all packages in CI

--- packages/turbo-ignore/src/index.ts
+++ packages/turbo-ignore/src/index.ts
@@ -2,6 +2,7 @@
 
 import { exec } from "child_process";
 import { getTurboRoot, getScopeFromPath, getScopeFromArgs } from "turbo-utils";
+import { getComparison } from "./getComparison";
 
 console.log(
   "\u226B Using Turborepo to determine if this project is affected by the commit..."
@@ -99,21 +100,3 @@ exec(
     }
   }
 );
-
-export function getComparison(): {
-  ref: string;
-  type: "previousDeploy" | "headRelative";
-} | null {
-  if (process.env.VERCEL === "1") {
-    if (process.env.VERCEL_GIT_PREVIOUS_SHA) {
-      // use the commit SHA of the last successful deployment for this project / branch
-      return {
-        ref: process.env.VERCEL_GIT_PREVIOUS_SHA,
-        type: "previousDeploy",
-      };
-    } else {
-      return null;
-    }
-  }
-  return { ref: "HEAD^", type: "headRelative" };
-}
