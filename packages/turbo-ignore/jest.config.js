c7277e2c62c1e6f043802de3824a50fa993b01da
Thomas Knickman <tom.knickman@vercel.com>
2022-08-30 13:43:13 -0400

    chore(packages): typecheck and abstract ts config (#1807)

--- packages/turbo-ignore/jest.config.js
+++ packages/turbo-ignore/jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
+  modulePathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist"],
   transformIgnorePatterns: ["/node_modules/(?!(ansi-regex)/)"],
 };
