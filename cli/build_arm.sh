b9047b100e7e87baa36debd407a69674f7c0d2c7
Greg Soltis <greg.soltis@vercel.com>
2022-09-08 16:06:25 -0700

    Try snapshotting

--- cli/build_arm.sh
+++ cli/build_arm.sh
@@ -5,19 +5,6 @@ set -e
 IMG=ghcr.io/gsoltis/turbo-cross:v1.18.5-arm64
 #IMG=ghcr.io/gsoltis/goreleaser-cross:v1.18.5-arm64
 
-docker run \
-  --rm \
-  --privileged \
-  -e CGO_ENABLED=1 \
-  -e GORELEASER_KEY=${GORELEASER_KEY} \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $(dirname `pwd`):/go/src/turborepo \
-  -w /go/src/turborepo/cli \
-  --platform linux/arm64 \
-  --entrypoint /bin/bash \
-  -it \
-  $IMG
-
 # docker run \
 #   --rm \
 #   --privileged \
@@ -27,6 +14,19 @@ docker run \
 #   -v $(dirname `pwd`):/go/src/turborepo \
 #   -w /go/src/turborepo/cli \
 #   --platform linux/arm64 \
-#   $IMG \
-#   build --rm-dist -f arm-release.yml --snapshot --id turbo-linux-arm64
-# #  release  --rm-dist --snapshot -f arm-release.yml
+#   --entrypoint /bin/bash \
+#   -it \
+#   $IMG
+
+docker run \
+  --rm \
+  --privileged \
+  -e CGO_ENABLED=1 \
+  -e GORELEASER_KEY=${GORELEASER_KEY} \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $(dirname `pwd`):/go/src/turborepo \
+  -w /go/src/turborepo/cli \
+  --platform linux/arm64 \
+  $IMG \
+  build --rm-dist -f arm-release.yml --snapshot --id turbo-arm64
+#  release  --rm-dist --snapshot -f arm-release.yml
