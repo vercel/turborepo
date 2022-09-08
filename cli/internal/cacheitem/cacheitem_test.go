06e7a611ed397dfc15d3c860c1eaafdc09e1bbaa
Nathan Hammond <nathan.hammond@vercel.com>
2022-09-08 21:05:42 +0800

    first bit

---
+++ cli/internal/cacheitem/cacheitem_test.go
@@ -0,0 +1,30 @@
+package cacheitem
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/vercel/turborepo/cli/internal/turbopath"
+)
+
+func TestCreate(t *testing.T) {
+	archive, err := Create(turbopath.AbsoluteSystemPath("/Users/nathanhammond/out.tar.gz"))
+	defer func() { _ = archive.Close() }()
+
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
+	archive.AddFile("/Users/nathanhammond/.zprofile")
+}
+
+func TestOpen(t *testing.T) {
+	archive, err := Open(turbopath.AbsoluteSystemPath("/Users/nathanhammond/out.tar.gz"))
+	defer func() { _ = archive.Close() }()
+
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
+	archive.AddFile("/Users/nathanhammond/.zprofile")
+}
