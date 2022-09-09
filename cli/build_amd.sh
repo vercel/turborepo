49ae27260b9bafa9e6fc6313f5e7b0bb0639edb4
Greg Soltis <greg.soltis@vercel.com>
2022-09-09 10:43:35 -0700

    WIP on CGO

--- cli/build_amd.sh
+++ cli/build_amd.sh
@@ -2,18 +2,8 @@
 
 set -e
 
-# docker run \
-#   --rm \
-#   --privileged \
-#   -e CGO_ENABLED=1 \
-#   -e GORELEASER_KEY=${GORELEASER_KEY} \
-#   -v /var/run/docker.sock:/var/run/docker.sock \
-#   -v `pwd`:/go/src/turbo \
-#   -w /go/src/turbo \
-#   --platform linux/amd64 \
-#   --entrypoint /bin/bash \
-#   -it \
-#   ghcr.io/goreleaser/goreleaser-cross:v1.18
+IMG=ghcr.io/gsoltis/turbo-cross:v1.18.5-arm64
+#IMG=ghcr.io/gsoltis/goreleaser-cross:v1.18.5-arm64
 
 docker run \
   --rm \
@@ -21,8 +11,21 @@ docker run \
   -e CGO_ENABLED=1 \
   -e GORELEASER_KEY=${GORELEASER_KEY} \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v `pwd`:/go/src/turbo \
-  -w /go/src/turbo \
+  -v $(dirname `pwd`):/go/src/turborepo \
+  -w /go/src/turborepo/cli \
   --platform linux/amd64 \
-  ghcr.io/goreleaser/goreleaser-cross:v1.18 \
-  release  --rm-dist --snapshot -f amd-release.yml
+  --entrypoint /bin/bash \
+  -it \
+  ghcr.io/goreleaser/goreleaser-cross:v1.18
+
+# docker run \
+#   --rm \
+#   --privileged \
+#   -e CGO_ENABLED=1 \
+#   -e GORELEASER_KEY=${GORELEASER_KEY} \
+#   -v /var/run/docker.sock:/var/run/docker.sock \
+#   -v $(dirname `pwd`):/go/src/turborepo \
+#   -w /go/src/turbo \
+#   --platform linux/amd64 \
+#   ghcr.io/goreleaser/goreleaser-cross:v1.18 \
+#   release  --rm-dist --snapshot -f amd-release.yml
