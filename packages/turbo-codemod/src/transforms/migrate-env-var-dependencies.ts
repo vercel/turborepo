c56c0f6b230bbf81dcee080b973ed011ffd311e8
tknickman <tom.knickman@vercel.com>
2022-09-21 09:33:58 -0400

    feat(codemod): migrate turbo.json dependsOn

---
+++ packages/turbo-codemod/src/transforms/migrate-env-var-dependencies.ts
@@ -0,0 +1,152 @@
+import fs from "fs-extra";
+import path from "path";
+import { Flags, Pipeline, TurboConfig } from "../types";
+import chalk from "chalk";
+import { skip, ok, error } from "../logger";
+
+export function hasLegacyEnvVarDependencies(config: TurboConfig) {
+  const dependsOn = [
+    config.globalDependencies,
+    Object.values(config.pipeline ?? []).flatMap(
+      (pipeline) => pipeline.dependsOn ?? []
+    ),
+  ].flat();
+  const envVars = dependsOn.filter((dep) => dep?.startsWith("$"));
+  return { hasKeys: !!envVars.length, envVars };
+}
+
+export function migrateDependencies({
+  env,
+  deps,
+}: {
+  env?: string[];
+  deps?: string[];
+}) {
+  const envDeps: Set<string> = new Set(env);
+  const otherDeps: string[] = [];
+  deps?.forEach((dep) => {
+    if (dep?.startsWith("$")) {
+      envDeps.add(dep);
+    } else {
+      otherDeps.push(dep);
+    }
+  });
+  if (envDeps.size) {
+    return {
+      deps: otherDeps,
+      env: Array.from(envDeps),
+    };
+  } else {
+    return { env, deps };
+  }
+}
+
+export function migratePipeline(pipeline: Pipeline) {
+  const { deps: dependsOn, env } = migrateDependencies({
+    env: pipeline.env,
+    deps: pipeline.dependsOn,
+  });
+  const migratedPipeline = { ...pipeline };
+  if (dependsOn) {
+    migratedPipeline.dependsOn = dependsOn;
+  } else {
+    delete migratedPipeline.dependsOn;
+  }
+  if (env && env.length) {
+    migratedPipeline.env = env;
+  } else {
+    delete migratedPipeline.env;
+  }
+
+  return migratedPipeline;
+}
+
+export function migrateGlobal(config: TurboConfig) {
+  const { deps: globalDependencies, env } = migrateDependencies({
+    env: config.env,
+    deps: config.globalDependencies,
+  });
+  const migratedConfig = { ...config };
+  if (globalDependencies) {
+    migratedConfig.globalDependencies = globalDependencies;
+  } else {
+    delete migratedConfig.globalDependencies;
+  }
+  if (env && env.length) {
+    migratedConfig.env = env;
+  } else {
+    delete migratedConfig.env;
+  }
+
+  return migratedConfig;
+}
+
+export function migrateConfig(config: TurboConfig) {
+  let migratedConfig = migrateGlobal(config);
+  if (config.pipeline) {
+    Object.keys(config.pipeline).forEach((pipelineKey) => {
+      if (migratedConfig.pipeline && config.pipeline?.[pipelineKey]) {
+        const pipeline = migratedConfig.pipeline?.[pipelineKey];
+        migratedConfig.pipeline[pipelineKey] = {
+          ...pipeline,
+          ...migratePipeline(pipeline),
+        };
+      }
+    });
+  }
+  return migratedConfig;
+}
+
+export default function migrateEnvVarDependencies(
+  files: string[],
+  flags: Flags
+) {
+  if (files.length === 1) {
+    const dir = files[0];
+    const root = path.resolve(process.cwd(), dir);
+    console.log(
+      `Migrating environment variable dependencies from "globalDependencies" and "dependsOn" to "env" in "turbo.json"...`
+    );
+    const turboConfigPath = path.join(root, "turbo.json");
+    let modifiedCount = 0;
+    let skippedCount = 0;
+    let unmodifiedCount = 1;
+    if (!fs.existsSync(turboConfigPath)) {
+      error(`No turbo.json found at ${root}. Is the path correct?`);
+      process.exit(1);
+    }
+
+    const rootTurboJson = fs.readJsonSync(turboConfigPath);
+    if (hasLegacyEnvVarDependencies(rootTurboJson).hasKeys) {
+      if (flags.dry) {
+        if (flags.print) {
+          console.log(JSON.stringify(migrateConfig(rootTurboJson), null, 2));
+        }
+        skip("turbo.json", chalk.dim("(dry run)"));
+        skippedCount += 1;
+      } else {
+        if (flags.print) {
+          console.log(JSON.stringify(migrateConfig(rootTurboJson), null, 2));
+        }
+        ok("turbo.json");
+        fs.writeJsonSync(turboConfigPath, migrateConfig(rootTurboJson), {
+          spaces: 2,
+        });
+        modifiedCount += 1;
+        unmodifiedCount -= 1;
+      }
+    } else {
+      ok(
+        'no unmigrated environment variable dependencies found in "turbo.json"'
+      );
+      process.exit(0);
+    }
+
+    console.log("All done.");
+    console.log("Results:");
+    console.log(chalk.red(`0 errors`));
+    console.log(chalk.yellow(`${skippedCount} skipped`));
+    console.log(chalk.yellow(`${unmodifiedCount} unmodified`));
+    console.log(chalk.green(`${modifiedCount} modified`));
+  }
+}
--- packages/turbo-codemod/src/types.ts
+++ packages/turbo-codemod/src/types.ts
@@ -3,3 +3,18 @@ export interface Flags {
   force: boolean;
   print: boolean;
 }
+
+export interface Pipeline {
+  outputs?: Array<string>;
+  dependsOn?: Array<string>;
+  env?: Array<string>;
+  inputs?: Array<string>;
+  cache?: boolean;
+}
+
+export interface TurboConfig {
+  $schema?: string;
+  globalDependencies?: Array<string>;
+  env?: Array<string>;
+  pipeline?: Record<string, Pipeline>;
+}
