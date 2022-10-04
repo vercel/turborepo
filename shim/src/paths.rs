05da42733a07329d98f8185cea93d926285c9b3b
Nicholas Yang <nicholas.yang@vercel.com>
2022-10-04 14:29:50 -0400

    Revert "Removed changes to keep PR minimal"
    This reverts commit 515850c82c6977ecc72ddaa0ef541bbaf9b59b44.

--- shim/src/paths.rs
+++ shim/src/paths.rs
@@ -1,13 +1,61 @@
 use anyhow::Result;
+use globset::GlobSet;
 use std::ffi::OsString;
 use std::fs;
 use std::path::{Path, PathBuf};
+use walkdir::{DirEntry, IntoIter, WalkDir};
 
 pub type AbsolutePath = Path;
 
 #[derive(Debug, Clone, PartialEq)]
 struct RelativePath {
     segments: Vec<OsString>,
+}
+
+pub struct GlobWalker {
+    file_walker: IntoIter,
+    inclusions: GlobSet,
+    exclusions: GlobSet,
+}
+
+impl GlobWalker {
+    pub fn new(dir: impl AsRef<Path>, inclusions: GlobSet, exclusions: GlobSet) -> GlobWalker {
+        GlobWalker {
+            file_walker: WalkDir::new(dir).into_iter(),
+            inclusions,
+            exclusions,
+        }
+    }
+}
+
+impl Iterator for GlobWalker {
+    type Item = Result<DirEntry>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let entry = self.file_walker.next()?;
+            match entry {
+                Ok(entry) => {
+                    let matches = self.inclusions.is_match(entry.path());
+
+                    if matches {
+                        let is_excluded = self.exclusions.is_match(entry.path());
+
+                        if is_excluded {
+                            continue;
+                        } else {
+                            return Some(Ok(entry));
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+                Err(err) => {
+                    return Some(Err(err.into()));
+                }
+            }
+        }
+    }
 }
 
 /// An iterator for the ancestor path from a current directory to the root of the file system.
