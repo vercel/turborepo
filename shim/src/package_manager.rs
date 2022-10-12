8c472e7426ceb946dc6330bf11e0cdac63d4a6f2
Nicholas Yang <nicholas.yang@vercel.com>
2022-10-12 18:33:12 -0400

    feature(shim): Merge shim directory (#2210)
    This is a first attempt to merge the Rust shim into main. All of the code has been reviewed already, but you're welcome to take another look. The build changes will be merged later.
    
    Co-authored-by: Greg Soltis <418301+gsoltis@users.noreply.github.com>

---
+++ shim/src/package_manager.rs
@@ -0,0 +1,109 @@
+use anyhow::{anyhow, Result};
+use serde::Deserialize;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, Deserialize)]
+struct PnpmWorkspaces {
+    pub packages: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct PackageJsonWorkspaces {
+    pub workspaces: Vec<String>,
+}
+
+pub enum PackageManager {
+    #[allow(dead_code)]
+    Berry,
+    Npm,
+    Pnpm,
+    #[allow(dead_code)]
+    Pnpm6,
+    #[allow(dead_code)]
+    Yarn,
+}
+
+#[derive(Debug)]
+pub struct Globs {
+    #[allow(dead_code)]
+    inclusions: Vec<PathBuf>,
+    #[allow(dead_code)]
+    exclusions: Vec<PathBuf>,
+}
+
+impl PackageManager {
+    /// Returns a list of globs for the package workspace.
+    /// NOTE: We return a `Vec<PathBuf>` instead of a `GlobSet` because we
+    /// may need to iterate through these globs and a `GlobSet` doesn't allow that.
+    ///
+    /// # Arguments
+    ///
+    /// * `root_path`:
+    ///
+    /// returns: Result<Globs, Error>
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///
+    /// ```
+    pub fn get_workspace_globs(&self, root_path: &Path) -> Result<Globs> {
+        let globs = match self {
+            PackageManager::Pnpm | PackageManager::Pnpm6 => {
+                let workspace_yaml = fs::read_to_string(root_path.join("pnpm-workspace.yaml"))?;
+                let workspaces: PnpmWorkspaces = serde_yaml::from_str(&workspace_yaml)?;
+                if workspaces.packages.is_empty() {
+                    return Err(anyhow!("pnpm-workspace.yaml: no packages found. Turborepo requires pnpm workspaces and thus packages to be defined in the root pnpm-workspace.yaml"));
+                } else {
+                    workspaces.packages
+                }
+            }
+            PackageManager::Berry | PackageManager::Npm | PackageManager::Yarn => {
+                let package_json_text = fs::read_to_string(root_path.join("package.json"))?;
+                let package_json: PackageJsonWorkspaces = serde_json::from_str(&package_json_text)?;
+
+                if package_json.workspaces.is_empty() {
+                    return Err(anyhow!("package.json: no packages found. Turborepo requires packages to be defined in the root package.json"));
+                } else {
+                    package_json.workspaces
+                }
+            }
+        };
+
+        let mut inclusions = Vec::new();
+        let mut exclusions = Vec::new();
+
+        for glob in globs {
+            if let Some(exclusion) = glob.strip_prefix('!') {
+                exclusions.push(PathBuf::from(exclusion.to_string()));
+            } else {
+                inclusions.push(PathBuf::from(glob));
+            }
+        }
+
+        Ok(Globs {
+            inclusions,
+            exclusions,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn test_get_workspace_globs() {
+        let package_manager = PackageManager::Npm;
+        let globs = package_manager
+            .get_workspace_globs(&Path::new("../examples/basic"))
+            .unwrap();
+
+        assert_eq!(
+            globs.inclusions,
+            vec![PathBuf::from("apps/*"), PathBuf::from("packages/*")]
+        );
+    }
+}
---
+++ shim/tests/mod.rs
@@ -0,0 +1,43 @@
+use assert_cmd::Command;
+
+#[test]
+fn test_find_correct_turbo() {
+    let mut cmd = Command::cargo_bin("turbo").unwrap();
+    cmd.assert()
+        .append_context("turbo", "no arguments")
+        .append_context(
+            "expect",
+            "`turbo` with no arguments should exit with code 1",
+        )
+        .code(1);
+
+    let mut cmd = Command::cargo_bin("turbo").unwrap();
+    cmd.args(&["--cwd", "../examples/basic", "bin"])
+        .assert()
+        .append_context(
+            "turbo",
+            "bin command with cwd flag set to package with local turbo installed",
+        )
+        .append_context(
+            "expect",
+            "`turbo --cwd ../../examples/basic bin` should print out local turbo binary installed in examples/basic",
+        )
+        .success()
+        .stdout(predicates::str::ends_with(
+            "examples/basic/node_modules/.bin/turbo\n",
+        ));
+
+    let mut cmd = Command::cargo_bin("turbo").unwrap();
+    cmd.args(&["--cwd", "..", "bin"])
+        .assert()
+        .append_context(
+            "turbo",
+            "bin command with cwd flag set to package without local turbo installed",
+        )
+        .append_context(
+            "expect",
+            "`turbo --cwd .. bin` should print out current turbo binary",
+        )
+        .success()
+        .stdout(predicates::str::ends_with("target/debug/turbo\n"));
+}
