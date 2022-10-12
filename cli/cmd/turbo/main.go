fe3764bb62198b941018d97aad7ecdd92756624a
Greg Soltis <greg.soltis@vercel.com>
2022-10-12 11:52:38 -0700

    Add TODOs, start switching to prod workflows

--- cli/cmd/turbo/main.go
+++ cli/cmd/turbo/main.go
@@ -1,11 +1,7 @@
 package main
 
-// static int cgoCheck() {
-//     return 2;
-// }
 import "C"
 import (
-	"fmt"
 	"os"
 	"unsafe"
 
@@ -13,9 +9,6 @@ import (
 )
 
 func main() {
-	// TODO(gsoltis): remove after verification
-	cgoCheck := C.cgoCheck()
-	fmt.Printf("CGO Check: %v\n", int(cgoCheck))
 	os.Exit(cmd.RunWithArgs(os.Args[1:], turboVersion))
 }
 
@@ -26,6 +19,6 @@ func nativeRunWithArgs(argc C.int, argv **C.char) C.uint {
 	for i, arg := range unsafe.Slice(argv, arglen) {
 		args[i] = C.GoString(arg)
 	}
-	exitCode := cmd.RunWithArgs(args, "my-version")
+	exitCode := cmd.RunWithArgs(args, turboVersion)
 	return C.uint(exitCode)
 }
