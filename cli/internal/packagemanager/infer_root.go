ac486ba58bcd68f809c633502c3fc156f51631a8
Nathan Hammond <nathan.hammond@vercel.com>
2022-10-20 23:27:41 +0800

    Add tests for InferRoot.

--- cli/internal/packagemanager/infer_root.go
+++ cli/internal/packagemanager/infer_root.go
@@ -1,6 +1,8 @@
 package packagemanager
 
 import (
+	"path/filepath"
+
 	"github.com/vercel/turborepo/cli/internal/doublestar"
 	"github.com/vercel/turborepo/cli/internal/turbopath"
 )
@@ -45,7 +47,8 @@ func isOneOfTheWorkspaces(nearestPackageJsonDir turbopath.AbsoluteSystemPath, cu
 		}
 
 		for _, glob := range globs {
-			match, _ := doublestar.Match(glob, nearestPackageJsonDir.ToString())
+			globpattern := currentPackageJsonDir.UntypedJoin(filepath.FromSlash(glob)).ToString()
+			match, _ := doublestar.PathMatch(globpattern, nearestPackageJsonDir.ToString())
 			if match {
 				return true
 			}
@@ -59,69 +62,86 @@ func InferRoot(directory turbopath.AbsoluteSystemPath) (turbopath.AbsoluteSystem
 	// Go doesn't have iterators, so this is very not-elegant.
 
 	// Scenarios:
+	// 0. Has a turbo.json but doesn't have a peer package.json. directory + multi
 	// 1. Nearest turbo.json, check peer package.json/pnpm-workspace.yaml.
 	//    A. Has workspaces, multi package mode.
 	//    B. No workspaces, single package mode.
 	// 2. If no turbo.json find the closest package.json parent.
 	//    A. No parent package.json, default to current behavior.
-	//    B. Nearest package.json defines workspaces. Set root to that directory and multi to true.
+	//    B. Nearest package.json defines workspaces. Can't be in single-package mode, so we bail. (This could be changed in the future.)
 	// 3. Closest package.json does not define workspaces. Traverse toward the root looking for package.jsons.
 	//    A. No parent package.json with workspaces. nearestPackageJson + single
 	//    B. Stop at the first one that has workspaces.
-	//       i. If we are one of the workspaces, nextPackageJson + multi.
+	//       i. If we are one of the workspaces, directory + multi. (This could be changed in the future.)
 	//       ii. If we're not one of the workspaces, nearestPackageJson + single.
 
 	nearestTurbo, findTurboErr := directory.Findup("turbo.json")
 	if findTurboErr != nil {
 		// We didn't find a turbo.json. We're in situation 2 or 3.
+
+		// Unroll the first loop for Scenario 2
 		nearestPackageJson, nearestPackageJsonErr := directory.Findup("package.json")
 
 		// If we fail to find any package.json files we aren't in single package mode.
 		// We let things go through our existing failure paths.
+		// Scenario 2A.
 		if nearestPackageJsonErr != nil {
-			// Scenario 2A.
 			return directory, Multi
 		}
 
+		// If we find a package.json which has workspaces we aren't in single package mode.
+		// We let things go through our existing failure paths.
+		// Scenario 2B.
 		if candidateDirectoryHasWorkspaces(nearestPackageJson.Dir()) {
-			// Scenario 2B.
-			return nearestPackageJson.Dir(), Multi
-		} else {
-			// Scenario 3.
-			// Find the nearest package.json that has workspaces.
-			// If found _and_ the nearestPackageJson is one of the workspaces, thatPackageJson + multi.
-			// Else, nearestPackageJson + single
-			cursor := nearestPackageJson.UntypedJoin("..", "..")
-			for {
-				nextPackageJson, nextPackageJsonErr := cursor.Findup("package.json")
-				if nextPackageJsonErr != nil {
-					// We haven't found a parent defining workspaces.
-					// So we're single package mode at nearestPackageJson.
-					// Scenario 3A.
-					return nearestPackageJson.Dir(), Single
-				} else {
-					// Found a package.json file, see if it has workspaces.
-					// Workspaces are not allowed to be recursive, so we know what to
-					// return the moment we find something with workspaces.
-					if candidateDirectoryHasWorkspaces(nextPackageJson.Dir()) {
-						if isOneOfTheWorkspaces(nearestPackageJson.Dir(), nextPackageJson.Dir()) {
-							// If it has workspaces, and nearestPackageJson is one of them, we're multi
-							// Scenario 3BI.
-							return nextPackageJson.Dir(), Multi
-						} else {
-							// We found a parent with workspaces, but we're not one of them.
-							// We choose to operate in single package mode.
-							// Scenario 3BII
-							return nearestPackageJson.Dir(), Single
-						}
+			// In a future world we could maybe change this behavior.
+			// return nearestPackageJson.Dir(), Multi
+			return directory, Multi
+		}
+
+		// Scenario 3.
+		// Find the nearest package.json that has workspaces.
+		// If found _and_ the nearestPackageJson is one of the workspaces, thatPackageJson + multi.
+		// Else, nearestPackageJson + single
+		cursor := nearestPackageJson.Dir().UntypedJoin("..")
+		for {
+			nextPackageJson, nextPackageJsonErr := cursor.Findup("package.json")
+			if nextPackageJsonErr != nil {
+				// We haven't found a parent defining workspaces.
+				// So we're single package mode at nearestPackageJson.
+				// Scenario 3A.
+				return nearestPackageJson.Dir(), Single
+			} else {
+				// Found a package.json file, see if it has workspaces.
+				// Workspaces are not allowed to be recursive, so we know what to
+				// return the moment we find something with workspaces.
+				if candidateDirectoryHasWorkspaces(nextPackageJson.Dir()) {
+					if isOneOfTheWorkspaces(nearestPackageJson.Dir(), nextPackageJson.Dir()) {
+						// If it has workspaces, and nearestPackageJson is one of them, we're multi.
+						// We don't infer in this scenario.
+						// Scenario 3BI.
+						// TODO: return nextPackageJson.Dir(), Multi
+						return directory, Multi
 					} else {
-						// Loop around and see if we have another parent.
-						cursor = nextPackageJson.UntypedJoin("..", "..")
+						// We found a parent with workspaces, but we're not one of them.
+						// We choose to operate in single package mode.
+						// Scenario 3BII
+						return nearestPackageJson.Dir(), Single
 					}
+				} else {
+					// Loop around and see if we have another parent.
+					cursor = nextPackageJson.Dir().UntypedJoin("..")
 				}
 			}
 		}
 	} else {
+		// If there is no sibling package.json we do no inference.
+		siblingPackageJsonPath := nearestTurbo.Dir().UntypedJoin("package.json")
+		if !siblingPackageJsonPath.Exists() {
+			// We do no inference.
+			// Scenario 0
+			return directory, Multi
+		}
+
 		if candidateDirectoryHasWorkspaces(nearestTurbo.Dir()) {
 			// Scenario 1A.
 			return nearestTurbo.Dir(), Multi
