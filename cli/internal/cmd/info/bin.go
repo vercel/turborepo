5ea050847dd27606fc4b0086540887c5a9179e4a
Nicholas Yang <nicholas.yang@vercel.com>
2022-10-21 14:22:08 -0400

    Added back Go version of bin

---
+++ cli/internal/cmd/info/bin.go
@@ -0,0 +1,34 @@
+package info
+
+import (
+	"os"
+
+	"github.com/vercel/turborepo/cli/internal/cmdutil"
+
+	"github.com/spf13/cobra"
+)
+
+// BinCmd returns the Cobra bin command
+func BinCmd(helper *cmdutil.Helper) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "bin",
+		Short: "Get the path to the Turbo binary",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			base, err := helper.GetCmdBase(cmd.Flags())
+			if err != nil {
+				return err
+			}
+			path, err := os.Executable()
+			if err != nil {
+				base.LogError("could not get path to turbo binary: %w", err)
+				return err
+			}
+
+			base.UI.Output(path)
+
+			return nil
+		},
+	}
+
+	return cmd
+}
