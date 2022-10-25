e070988d1dba7607f4e744afca28c542a471ae71
Justin Ridgewell <justin@ridgewell.name>
2022-10-25 18:18:45 -0400

    Unify filter_for_client and env_for_js

--- crates/next-core/src/app_source.rs
+++ crates/next-core/src/app_source.rs
@@ -249,8 +249,8 @@ pub async fn create_app_source(
         externals,
     );
 
-    let server_runtime_entries =
-        vec![ProcessEnvAssetVc::new(project_root, env_for_js(env)).as_ecmascript_chunk_placeable()];
+    let server_runtime_entries = vec![ProcessEnvAssetVc::new(project_root, env_for_js(env, false))
+        .as_ecmascript_chunk_placeable()];
 
     let fallback_page = get_fallback_page(project_root, server_root, browserslist_query);
 
--- crates/next-core/src/env.rs
+++ crates/next-core/src/env.rs
@@ -7,30 +7,6 @@ use turbo_tasks_env::{
 use turbo_tasks_fs::FileSystemPathVc;
 use turbopack_env::{EmbeddableProcessEnvVc, TryDotenvProcessEnvVc};
 
-pub fn override_env() -> EnvMapVc {
-    let custom = indexmap! {
-        // We need to overload the __NEXT_IMAGE_OPTS to override the default remotePatterns field.
-        // This allows us to support loading from remote hostnames until we properly support reading
-        // the next.config.js file.
-        "__NEXT_IMAGE_OPTS".to_string() => json!({
-            "deviceSizes": [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-            "imageSizes": [16, 32, 48, 64, 96, 128, 256, 384],
-            "path": "/_next/image",
-            "loader": "default",
-            "domains": [],
-            "disableStaticImages": false,
-            "minimumCacheTTL": 60,
-            "formats": ["image/webp"],
-            "dangerouslyAllowSVG": false,
-            "contentSecurityPolicy": "script-src 'none'; frame-src 'none'; sandbox;",
-            "remotePatterns": [{ "hostname": "**" }],
-            "unoptimized": false,
-        }).to_string()
-    };
-
-    EnvMapVc::cell(custom)
-}
-
 /// Loads a series of dotenv files according to the precedence rules set by
 /// https://nextjs.org/docs/basic-features/environment-variables#environment-variable-load-order
 #[turbo_tasks::function]
@@ -61,15 +37,42 @@ pub async fn load_env(project_path: FileSystemPathVc) -> Result<ProcessEnvVc> {
     Ok(env)
 }
 
-/// Filters the env down to just the keys that are acceptable for serving to the
-/// client. In our case, keys that start with `NEXT_PUBLIC_`.
-pub fn filter_for_client(env: ProcessEnvVc) -> ProcessEnvVc {
-    FilterProcessEnvVc::new(env, "NEXT_PUBLIC_".to_string()).into()
-}
-
+/// Creates a ProcessEnvVc safe to use in JS, by stringifying and encoding as
+/// regular JS strings. Setting `client` to true will additionally filter the
+/// env to just the keys that are acceptable for the client to access.
+///
+/// For now, it also injects overridden values as if they were real JS code, eg
+/// an Object and not a String.
 #[turbo_tasks::function]
-pub fn env_for_js(env: ProcessEnvVc) -> ProcessEnvVc {
-    let embeddable = EmbeddableProcessEnvVc::new(env);
-    let overridden = CustomProcessEnvVc::new(embeddable.into(), override_env());
-    overridden.into()
+pub fn env_for_js(env: ProcessEnvVc, client: bool) -> ProcessEnvVc {
+    let env = if client {
+        FilterProcessEnvVc::new(env, "NEXT_PUBLIC_".to_string()).into()
+    } else {
+        env
+    };
+    let env = EmbeddableProcessEnvVc::new(env).into();
+
+    CustomProcessEnvVc::new(
+        env,
+        EnvMapVc::cell(indexmap! {
+            // We need to overload the __NEXT_IMAGE_OPTS to override the default remotePatterns field.
+            // This allows us to support loading from remote hostnames until we properly support reading
+            // the next.config.js file.
+            "__NEXT_IMAGE_OPTS".to_string() => json!({
+                "deviceSizes": [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+                "imageSizes": [16, 32, 48, 64, 96, 128, 256, 384],
+                "path": "/_next/image",
+                "loader": "default",
+                "domains": [],
+                "disableStaticImages": false,
+                "minimumCacheTTL": 60,
+                "formats": ["image/webp"],
+                "dangerouslyAllowSVG": false,
+                "contentSecurityPolicy": "script-src 'none'; frame-src 'none'; sandbox;",
+                "remotePatterns": [{ "hostname": "**" }],
+                "unoptimized": false,
+            }).to_string()
+        }),
+    )
+    .into()
 }
