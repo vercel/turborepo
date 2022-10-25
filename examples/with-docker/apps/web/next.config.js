575b68ebfea0db84699687648472b24c17f9049a
Mehul Kar <mehul.kar@vercel.com>
2022-10-25 05:09:05 -0400

    Update react & next in turborepo examples (#2317)
    Co-authored-by: Gaspar Garcia <gaspar@vercel.com>

--- examples/with-docker/apps/web/next.config.js
+++ examples/with-docker/apps/web/next.config.js
@@ -1,10 +1,10 @@
-const withTM = require("next-transpile-modules")(["ui"]);
 const path = require("path");
 
-module.exports = withTM({
+module.exports = {
   reactStrictMode: true,
   output: "standalone",
   experimental: {
     outputFileTracingRoot: path.join(__dirname, "../../"),
+    transpilePackages: ["ui"],
   },
-});
+};
