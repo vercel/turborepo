75d041d6179e12422ceefbe71f7c8b89ab4d7080
Justin Ridgewell <justin@ridgewell.name>
2022-10-25 19:04:50 -0400

    Disable HMR for snapshot tests
    The only effect is that we don't emit source maps for the individual chunk items, reducing the total number of output files considerably.

--- crates/turbopack/tests/snapshot.rs
+++ crates/turbopack/tests/snapshot.rs
@@ -164,7 +164,6 @@ async fn run(resource: &'static str) -> Result<()> {
         let asset_root_path = path.join("static");
         let chunking_context =
             DevChunkingContextVc::builder(project_root, path, chunk_root_path, asset_root_path)
-                .hot_module_replacment()
                 .build();
 
         let existing_dir = chunk_root_path.read_dir().await?;
