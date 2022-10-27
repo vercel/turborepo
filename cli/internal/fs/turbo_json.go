d2a7c3d0243c1e6eb11b39d7bf73a6a6be11249c
Chris Olszewski <chris.olszewski@vercel.com>
2022-10-27 13:40:02 -0700

    ensure task outputs are sorted prior to hashing

--- cli/internal/fs/turbo_json.go
+++ cli/internal/fs/turbo_json.go
@@ -121,6 +121,17 @@ type TaskOutputs struct {
 	Exclusions []string
 }
 
+// Sort contents of task outputs
+func (to TaskOutputs) Sort() TaskOutputs {
+	var inclusions []string
+	var exclusions []string
+	copy(inclusions, to.Inclusions)
+	copy(exclusions, to.Exclusions)
+	sort.Strings(inclusions)
+	sort.Strings(exclusions)
+	return TaskOutputs{Inclusions: inclusions, Exclusions: exclusions}
+}
+
 // ReadTurboConfig toggles between reading from package.json or the configFile to support early adopters.
 func ReadTurboConfig(rootPath turbopath.AbsoluteSystemPath, rootPackageJSON *PackageJSON) (*TurboJSON, error) {
 
