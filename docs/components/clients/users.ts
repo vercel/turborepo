b4fcc9340f705e5cd733e3239a70929aad73e819
Miro Koczka <usbecko@gmail.com>
2022-11-02 08:14:31 +0100

    docs: Add Supernova to showcase list (#2454)

--- docs/components/clients/users.ts
+++ docs/components/clients/users.ts
@@ -470,6 +470,15 @@ export const users: Array<TurboUser> = [
     pinned: true,
   },
   {
+    caption: "Supernova.io",
+    image: "/images/logos/supernova.svg",
+    infoLink: "https://www.supernova.io/",
+    pinned: true,
+    style: {
+      width: 150,
+    },
+  },
+  {
     caption: "BuildPass",
     image: "/images/logos/buildpass.svg",
     infoLink: "https://www.buildpass.com.au/",
