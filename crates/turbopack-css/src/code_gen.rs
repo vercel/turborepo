9ce751d141b5d37ff197116d0bc041577d8c810a
Will Binns-Smith <wbinnssmith@gmail.com>
2022-11-04 13:53:49 -0700

    Generate @imports for external css import references

--- crates/turbopack-css/src/code_gen.rs
+++ crates/turbopack-css/src/code_gen.rs
@@ -1,6 +1,8 @@
 use swc_core::css::visit::{AstParentKind, VisitMut};
 use turbopack_core::chunk::ChunkingContextVc;
 
+use crate::chunk::CssImport;
+
 /// impl of code generation inferred from a AssetReference.
 /// This is rust only and can't be implemented by non-rust plugins.
 #[turbo_tasks::value(
@@ -14,6 +16,8 @@ pub struct CodeGeneration {
     /// ast nodes matching the span will be visitor by the visitor
     #[turbo_tasks(debug_ignore, trace_ignore)]
     pub visitors: Vec<(Vec<AstParentKind>, Box<dyn VisitorFactory>)>,
+    #[turbo_tasks(debug_ignore, trace_ignore)]
+    pub imports: Vec<CssImport>,
 }
 
 pub trait VisitorFactory: Send + Sync {
