b5b51105c3ce7f7eb18db8f00b6aae3aad3c3e90
Mehul Kar <mehul.kar@vercel.com>
2022-11-15 13:37:42 -0800

    Add codemod to add default outputs when outputs is omitted

---
+++ packages/turbo-codemod/__tests__/add-default-ouptuts.test.ts
@@ -0,0 +1,93 @@
+import addDefaultOutputs from "../src/transforms/add-default-outputs";
+import path from "path";
+import fs from "fs-extra";
+
+const FIXTURES_DIR = path.join(__dirname, "__fixtures__");
+let restoreFn = () => {};
+const CREATED_DIRS: string[] = [];
+
+function useFixture(dir: string) {
+  let copiedDirPath = path.join(
+    __dirname,
+    "test-runtime",
+    `${path.basename(dir)}-${Date.now()}`
+  ); // should be __tests__
+
+  // copy the dir to another place
+  fs.copySync(dir, copiedDirPath);
+  // and store a reference so we can clean up later
+  CREATED_DIRS.push(copiedDirPath);
+
+  const cleanupFn = () => {
+    fs.rmSync(copiedDirPath, { recursive: true, force: true });
+  };
+
+  // return a function that can be called later to restore the original
+  return [copiedDirPath, cleanupFn];
+}
+
+describe("add-default-outputs", () => {
+  beforeEach(() => {
+    restoreFn = () => {};
+  });
+
+  afterEach(() => {
+    restoreFn();
+  });
+
+  // clean up all created dirs
+  afterAll(() => {
+    for (const dir of CREATED_DIRS) {
+      fs.rmSync(dir, { recursive: true, force: true });
+    }
+  });
+
+  test("basic run", () => {
+    const fixture = path.join(FIXTURES_DIR, "thingie");
+    const [testDir, cleanupFn] = useFixture(fixture);
+
+    const flags = {
+      dry: false,
+      force: false,
+      print: false,
+    };
+
+    /* @ts-ignore-next-line */
+    addDefaultOutputs([testDir], flags);
+
+    /* @ts-ignore-next-line */
+    const output = fs.readJSONSync(path.join(testDir, "turbo.json"));
+
+    expect(output.pipeline["build-one"].outputs).toStrictEqual(["foo"]);
+    expect(output.pipeline["build-two"].outputs).toStrictEqual([]);
+    expect(output.pipeline["build-three"].outputs).toStrictEqual([
+      "dist/**",
+      "build/**",
+    ]);
+
+    // @ts-ignore-next-line
+    cleanupFn();
+  });
+
+  test("dry run", () => {
+    const fixture = path.join(FIXTURES_DIR, "thingie");
+    const [testDir, cleanupFn] = useFixture(fixture);
+
+    // @ts-ignore
+    addDefaultOutputs([testDir], {
+      dry: true,
+      force: false,
+      print: false,
+    });
+
+    // @ts-ignore
+    const output = fs.readJSONSync(path.join(testDir, "turbo.json"));
+
+    expect(output.pipeline["build-one"].outputs).toStrictEqual(["foo"]);
+    expect(output.pipeline["build-two"].outputs).toStrictEqual([]);
+    expect(output.pipeline["build-three"].outputs).toStrictEqual(undefined);
+
+    // @ts-ignore-next-line
+    cleanupFn();
+  });
+});
---
+++ packages/turbo-codemod/src/transforms/add-default-outputs.ts
@@ -0,0 +1,53 @@
+import { Flags } from "../types";
+import path from "path";
+import fs from "fs-extra";
+import { error, ok, skip } from "../logger";
+
+const DEFAULT_OUTPUTS = ["dist/**", "build/**"];
+
+interface TaskDefinition {
+  outputs: [];
+}
+
+interface PipelineConfig {
+  [taskName: string]: TaskDefinition;
+}
+
+export default function addDefaultOutputs(files: string[], flags: Flags) {
+  // We should only get a directory as input
+  if (files.length !== 1) {
+    return;
+  }
+
+  // TODO: handle config in package.json turbo keys?
+
+  const dir = files[0];
+  const root = path.resolve(process.cwd(), dir);
+
+  console.log(`Adding default \`outputs\` key into tasks if it doesn't exist`);
+
+  const turboConfigPath = path.join(root, "turbo.json");
+  if (!fs.existsSync(turboConfigPath)) {
+    error(`No turbo.json found at ${root}. Is the path correct?`);
+    process.exit(1);
+  }
+
+  const rootTurboJson: PipelineConfig = fs.readJsonSync(turboConfigPath);
+
+  for (const [taskName, taskDef] of Object.entries(rootTurboJson.pipeline)) {
+    if (!taskDef.outputs) {
+      ok(`Updating outputs for ${taskName}`);
+      taskDef.outputs = DEFAULT_OUTPUTS;
+    } else {
+      skip(`Skipping "${taskName}", it already has an outputs key defined`);
+    }
+  }
+
+  if (!flags.dry) {
+    fs.writeJsonSync(turboConfigPath, rootTurboJson, {
+      spaces: 2,
+    });
+  }
+
+  ok("Done");
+}
