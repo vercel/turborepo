80fe75d346a5e44b545023f2442727ae3ed8f74b
Joshua Inglis <jt.inglis@outlook.com>
2022-11-25 11:48:16 +1000

    add better test names

--- packages/eslint-plugin-turbo/__tests__/lib/findTurboConfig.test.ts
+++ packages/eslint-plugin-turbo/__tests__/lib/findTurboConfig.test.ts
@@ -1,31 +1,33 @@
 import path from "path";
 import findTurboConfig from "../../lib/utils/findTurboConfig";
 
-test("Should parse valid turbo.json", () => {
-  const cwd = path.resolve(__dirname, "./fixtures/workspace");
-  expect(findTurboConfig({ cwd })).toEqual({
-    $schema: "https://turbo.build/schema.json",
-    pipeline: {
-      build: {
-        dependsOn: ["^build"],
+describe("findTurboConfig", () => {
+  it("it parses valid turbo.json file", () => {
+    const cwd = path.resolve(__dirname, "./fixtures/workspace");
+    expect(findTurboConfig({ cwd })).toEqual({
+      $schema: "https://turbo.build/schema.json",
+      pipeline: {
+        build: {
+          dependsOn: ["^build"],
+        },
+        test: {
+          dependsOn: ["build"],
+          outputs: [],
+          inputs: [
+            "src/**/*.tsx",
+            "src/**/*.ts",
+            "test/**/*.ts",
+            "test/**/*.tsx",
+          ],
+        },
+        lint: {
+          outputs: [],
+        },
+        deploy: {
+          dependsOn: ["build", "test", "lint"],
+          outputs: [],
+        },
       },
-      test: {
-        dependsOn: ["build"],
-        outputs: [],
-        inputs: [
-          "src/**/*.tsx",
-          "src/**/*.ts",
-          "test/**/*.ts",
-          "test/**/*.tsx",
-        ],
-      },
-      lint: {
-        outputs: [],
-      },
-      deploy: {
-        dependsOn: ["build", "test", "lint"],
-        outputs: [],
-      },
-    },
+    });
   });
 });
