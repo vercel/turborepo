Rafat97/turbo

Use next-font transform

crates/turbopack/src/module_options/module_options_context.rs
@@ -7,6 +7,7 @@ use super::ModuleRule;
 #[derive(Default, Clone)]
 pub struct ModuleOptionsContext {
     pub enable_emotion: bool,
+    pub enable_next_font: bool,
     pub enable_react_refresh: bool,
     pub enable_styled_components: bool,
     pub enable_styled_jsx: bool,

vercel/turborepo

crates/turbopack/src/module_options/module_options_context.rs
@@ -7,6 +7,7 @@ use super::ModuleRule;
 #[derive(Default, Clone)]
 pub struct ModuleOptionsContext {
     pub enable_emotion: bool,
+    pub enable_nextjs_font: bool,
     pub enable_react_refresh: bool,
     pub enable_styled_components: bool,
     pub enable_styled_jsx: bool,