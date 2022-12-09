5bb7f464b6a686161190efa48a5817704068bcf1
sokra <sokra@users.noreply.github.com>
2022-12-09 08:47:36 +0000

    Update docs (3b0089c45bde339bd86583444caf1a227acc9ab7)

--- docs/docs.rs/implementors/turbopack_core/source_map/trait.GenerateSourceMap.js
+++ docs/docs.rs/implementors/turbopack_core/source_map/trait.GenerateSourceMap.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {
 "turbopack_core":[],
+"turbopack_css":[["impl GenerateSourceMap for <a class=\"struct\" href=\"turbopack_css/chunk/struct.CssChunkVc.html\" title=\"struct turbopack_css::chunk::CssChunkVc\">CssChunkVc</a>"]],
 "turbopack_ecmascript":[["impl GenerateSourceMap for <a class=\"struct\" href=\"turbopack_ecmascript/chunk/struct.EcmascriptChunkContentVc.html\" title=\"struct turbopack_ecmascript::chunk::EcmascriptChunkContentVc\">EcmascriptChunkContentVc</a>"],["impl GenerateSourceMap for <a class=\"struct\" href=\"turbopack_ecmascript/chunk/struct.EcmascriptChunkVc.html\" title=\"struct turbopack_ecmascript::chunk::EcmascriptChunkVc\">EcmascriptChunkVc</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
