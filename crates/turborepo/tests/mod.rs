c2a2ca92b859cf3fd50382906f6c32c77e4da864
Nicholas Yang <nicholas.yang@vercel.com>
2022-12-09 12:24:42 -0500

    Fixing tests

--- crates/turborepo/tests/mod.rs
+++ crates/turborepo/tests/mod.rs
@@ -1,7 +1,7 @@
 use assert_cmd::Command;
 
 fn get_version() -> &'static str {
-    include_str!("../../version.txt")
+    include_str!("../../../version.txt")
         .split_once('\n')
         .expect("Failed to read version from version.txt")
         .0
@@ -26,33 +26,4 @@ fn test_no_arguments() {
             "`turbo` with no arguments should exit with code 2",
         )
         .code(2);
-}
-
-#[test]
-fn test_find_correct_turbo() {
-    let mut cmd = Command::cargo_bin("turbo").unwrap();
-    let assertion = cmd
-        .args(["--cwd", "..", "bin"])
-        .assert()
-        .append_context(
-            "turbo",
-            "bin command with cwd flag set to package without local turbo installed",
-        )
-        .append_context(
-            "expect",
-            "`turbo --cwd .. bin` should print out current turbo binary",
-        )
-        .success();
-
-    if cfg!(debug_assertions) {
-        if cfg!(windows) {
-            assertion.stdout(predicates::str::ends_with("target\\debug\\turbo.exe\n"));
-        } else {
-            assertion.stdout(predicates::str::ends_with("target/debug/turbo\n"));
-        }
-    } else if cfg!(windows) {
-        assertion.stdout(predicates::str::ends_with("target\\release\\turbo.exe\n"));
-    } else {
-        assertion.stdout(predicates::str::ends_with("target/release/turbo\n"));
-    }
 }
