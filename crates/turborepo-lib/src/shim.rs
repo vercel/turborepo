ec0ee5f8b9530b1ae8c33562027571529717ffd4
Nicholas Yang <nicholas.yang@vercel.com>
2022-12-19 17:43:10 -0500

    Canonicalize cwd

--- crates/turborepo-lib/src/shim.rs
+++ crates/turborepo-lib/src/shim.rs
@@ -87,7 +87,7 @@ impl ShimArgs {
             Err(anyhow!("No value assigned to `--cwd` argument"))
         } else {
             let cwd = if let Some(cwd) = cwd {
-                cwd
+                cwd.canonicalize()?
             } else {
                 current_dir()?
             };
