94bbc182028f08581b18e9e31f8cd52a022e0d40
Justin Ridgewell <justin@ridgewell.name>
2022-12-19 16:12:13 -0500

    Update snapshots

--- crates/turbopack-tests/tests/snapshot/node/node_protocol_external/output/79fb1_turbopack-tests_tests_snapshot_node_node_protocol_external_input_index_6a3d80.js
+++ crates/turbopack-tests/tests/snapshot/node/node_protocol_external/output/79fb1_turbopack-tests_tests_snapshot_node_node_protocol_external_input_index_6a3d80.js
@@ -204,8 +204,7 @@ var __TURBOPACK__external__node$3a$fs__ = __turbopack_external_require__("node:f
       // an external module we don't provide a shim for (e.g. querystring, url).
       // For now, we fail semi-silently, but in the future this should be a
       // compilation error.
-      console.error(`Failed to load external module ${id}: ${err}`);
-      return undefined;
+      throw new Error(`Failed to load external module ${id}: ${err}`);
     }
     if (!esm || raw.__esModule) {
       return raw;
