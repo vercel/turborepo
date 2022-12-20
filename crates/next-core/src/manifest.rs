3e9dbcf8543092be55b94422881aad9616196ca5
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2022-12-20 16:06:53 +0100

    Fix tests

--- crates/next-core/src/manifest.rs
+++ crates/next-core/src/manifest.rs
@@ -105,10 +105,14 @@ impl ContentSource for DevManifestContentSource {
 }
 
 /// Strips the base path from the given path. The base path must start with a
-/// slash.
+/// slash or be the empty string.
 ///
 /// Returns `None` if the path does not start with the base path.
 fn strip_base_path<'a, 'b>(path: &'a str, base_path: &'b str) -> Result<Option<&'a str>> {
+    if base_path.is_empty() {
+        return Ok(Some(path));
+    }
+
     let base_path = base_path
         .strip_prefix('/')
         .ok_or_else(|| anyhow::anyhow!("base path must start with a slash, got {}", base_path))?;
