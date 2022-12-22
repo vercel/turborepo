ad8b804a09b1b71113eedad2eb2b8d60eb45bdf8
tknickman <tknickman@users.noreply.github.com>
2022-12-22 20:48:03 +0000

    Update docs (59edbb74252311131c0cf393b43890e93df3931f)

--- docs/docs.rs/implementors/turbopack/transition/trait.Transition.js
+++ docs/docs.rs/implementors/turbopack/transition/trait.Transition.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"next_core":[["impl <a class=\"trait\" href=\"turbopack/transition/trait.Transition.html\" title=\"trait turbopack::transition::Transition\">Transition</a> for <a class=\"struct\" href=\"next_core/next_client/struct.NextClientTransitionVc.html\" title=\"struct next_core::next_client::NextClientTransitionVc\">NextClientTransitionVc</a>"]],
+"next_core":[["impl Transition for <a class=\"struct\" href=\"next_core/next_client/struct.NextClientTransitionVc.html\" title=\"struct next_core::next_client::NextClientTransitionVc\">NextClientTransitionVc</a>"]],
 "turbopack":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
