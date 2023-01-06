251d73271c063dfd76a7f140ce3fb0a0bd46a747
Nicholas Yang <nicholas.yang@vercel.com>
2023-01-06 15:21:55 -0500

    refactor(turborepo): Two binaries (#3189)
    Because of golang/go#13492, we cannot use Go as a shared library that we
    link to Rust. Instead we are now building the Go code as a separate
    binary that is executed by the Rust one.
    
    Co-authored-by: Greg Soltis <greg.soltis@vercel.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    Co-authored-by: Greg Soltis <greg@goldfiglabs.com>
    Co-authored-by: Thomas Knickman <tom.knickman@vercel.com>

--- crates/turborepo/src/main.rs
+++ crates/turborepo/src/main.rs
@@ -1,33 +1,28 @@
-mod ffi {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
-
-use std::{ffi::CString, process};
+use std::{env::current_exe, process, process::Stdio};
 
 use anyhow::Result;
 use turborepo_lib::{Args, Payload};
 
-use crate::ffi::{nativeRunWithArgs, GoString};
+fn run_go_binary(args: Args) -> Result<i32> {
+    let mut go_binary_path = current_exe()?;
+    go_binary_path.pop();
+    #[cfg(windows)]
+    go_binary_path.push("go-turbo.exe");
+    #[cfg(not(windows))]
+    go_binary_path.push("go-turbo");
 
-impl TryInto<GoString> for Args {
-    type Error = anyhow::Error;
+    let serialized_args = serde_json::to_string(&args)?;
 
-    fn try_into(self) -> std::result::Result<GoString, Self::Error> {
-        let json = serde_json::to_string(&self)?;
-        let cstring = CString::new(json)?;
-        let n = cstring.as_bytes().len() as isize;
+    let mut command = process::Command::new(go_binary_path)
+        .arg(serialized_args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Failed to execute turbo.");
 
-        Ok(GoString {
-            p: cstring.into_raw(),
-            n,
-        })
-    }
-}
+    let exit_code = command.wait()?.code().unwrap_or(2);
 
-fn native_run(args: Args) -> Result<i32> {
-    let serialized_args = args.try_into()?;
-    let exit_code = unsafe { nativeRunWithArgs(serialized_args) };
-    Ok(exit_code.try_into()?)
+    Ok(exit_code)
 }
 
 // This function should not expanded. Please add any logic to
@@ -35,7 +30,7 @@ fn native_run(args: Args) -> Result<i32> {
 fn main() -> Result<()> {
     let exit_code = match turborepo_lib::main()? {
         Payload::Rust(res) => res.unwrap_or(1),
-        Payload::Go(state) => native_run(*state)?,
+        Payload::Go(state) => run_go_binary(*state)?,
     };
 
     process::exit(exit_code)
