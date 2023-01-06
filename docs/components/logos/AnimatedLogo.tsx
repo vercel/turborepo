7b2b0a6b4684f959139a9b61f348852ec6912aff
Sam Becker <sam@sambecker.com>
2023-01-05 21:03:51 -0600

    Prevent logo animation on first load

--- docs/components/logos/AnimatedLogo.tsx
+++ docs/components/logos/AnimatedLogo.tsx
@@ -1,26 +1,34 @@
+import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useTurboSite } from "../SiteSwitcher";
 import PackSymbol from "./PackSymbol";
 import RepoSymbol from "./RepoSymbol";
 import TurboWordmark from "./TurboWordmark";
+import { useEffect } from "react";
 
 const VERTICAL_OFFSET = 23;
 const ANIMATION_SPEED = 0.4;
 
-const animatedSymbol = (children: JSX.Element, key: string) =>
+const AnimatedLogo = () => {
+  const symbol = useTurboSite();
+
+  const [hasLoadedOnce, setHasLoadedOnce] = useState(false);
+
+  useEffect(() => setHasLoadedOnce(true), []);
+
+  const animatedSymbol = (children: JSX.Element, key: string) =>
   <motion.div
     key={key}
     style={{ position: "absolute", top: 0, left: 0 }}
-    initial={{ opacity: 0, y: VERTICAL_OFFSET}}
+    initial={hasLoadedOnce
+      ? { opacity: 0, y: VERTICAL_OFFSET}
+      : false}
     animate={{ opacity: 1, y: 0 }}
     exit={{ opacity: 0, y: -VERTICAL_OFFSET }}
     transition={{ duration: ANIMATION_SPEED, ease: "easeOut" }}
   >
     {children}
   </motion.div>
-
-const AnimatedLogo = () => {
-  const symbol = useTurboSite();
 
   return (
       <div className="flex items-center gap-2">
