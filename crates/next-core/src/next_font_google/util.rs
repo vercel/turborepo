ebe28a4fda2540e3ad92fb03213ec26772c09b56
Will Binns-Smith <wbinnssmith@gmail.com>
2023-01-09 13:23:15 -0800

    Add tests for non-variable fonts

--- crates/next-core/src/next_font_google/util.rs
+++ crates/next-core/src/next_font_google/util.rs
@@ -367,6 +367,44 @@ mod tests {
     }
 
     #[test]
+    fn test_no_variable() -> Result<()> {
+        let data: FontData = serde_json::from_str(
+            r#"
+            {
+                "Hind": {
+                    "weights": [
+                        "300",
+                        "400",
+                        "500",
+                        "600",
+                        "700"
+                    ],
+                    "styles": [
+                        "normal"
+                    ]
+                }
+            }
+  "#,
+        )?;
+
+        assert_eq!(
+            get_font_axes(
+                &data,
+                "Hind",
+                &FontWeights::Fixed(indexset! {500}),
+                &indexset! {},
+                &None
+            )?,
+            FontAxes {
+                wght: indexset! {"500".to_owned()},
+                ital: indexset! {},
+                variable_axes: None
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
     fn test_stylesheet_url_no_axes() -> Result<()> {
         assert_eq!(
             get_stylesheet_url(
@@ -433,7 +471,7 @@ mod tests {
     }
 
     #[test]
-    fn test_variable_font_without_wgth_axis() -> Result<()> {
+    fn test_stylesheet_url_variable_font_without_wgth_axis() -> Result<()> {
         assert_eq!(
             get_stylesheet_url(
                 GOOGLE_FONTS_STYLESHEET_URL,
@@ -449,6 +487,25 @@ mod tests {
                 "optional"
             )?,
             "https://fonts.googleapis.com/css2?family=Nabla:EDPT,EHLT@0..200,0..24&display=optional"
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_stylesheet_url_no_variable() -> Result<()> {
+        assert_eq!(
+            get_stylesheet_url(
+                GOOGLE_FONTS_STYLESHEET_URL,
+                "Hind",
+                &FontAxes {
+                    wght: indexset! {"500".to_owned()},
+                    ital: indexset! {},
+                    variable_axes: None
+                },
+                "optional"
+            )?,
+            "https://fonts.googleapis.com/css2?family=Hind:wght@500&display=optional"
         );
 
         Ok(())
