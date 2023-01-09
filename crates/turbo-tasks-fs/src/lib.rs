d920957985a71e62be5fc20626f00d16a7247ee6
Tobias Koppers <tobias.koppers@googlemail.com>
2023-01-09 16:50:56 +0100

    fix inconsistent sys_path leading to watching issue on root dir

--- crates/turbo-tasks-fs/src/lib.rs
+++ crates/turbo-tasks-fs/src/lib.rs
@@ -264,8 +264,13 @@ impl DiskFileSystem {
     }
 
     pub async fn to_sys_path(&self, fs_path: FileSystemPathVc) -> Result<PathBuf> {
-        let path = Path::new(&self.root).join(&*unix_to_sys(&fs_path.await?.path));
-        Ok(path)
+        let path = Path::new(&self.root);
+        if fs_path.await?.path.is_empty() {
+            Ok(path.to_path_buf())
+        } else {
+            let path = path.join(&*unix_to_sys(&fs_path.await?.path));
+            Ok(path)
+        }
     }
 }
 
