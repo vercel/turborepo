2662956ede10309d76a4ef648192edbad8d47f25
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-01-11 13:35:15 +0100

    Add Turbo tests for the next/dynamic transform

--- crates/next-transform-dynamic/tests/fixture.rs
+++ crates/next-transform-dynamic/tests/fixture.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use next_transform_dynamic::{next_dynamic, NextDynamicMode};
 use swc_core::{
@@ -17,57 +17,78 @@ fn syntax() -> Syntax {
     })
 }
 
-#[fixture("tests/fixture/**/input.js")]
-fn next_dynamic_fixture(input: PathBuf) {
-    let output_dev = input.parent().unwrap().join("output-dev.js");
-    let output_prod = input.parent().unwrap().join("output-prod.js");
-    let output_server = input.parent().unwrap().join("output-server.js");
-    test_fixture(
-        syntax(),
-        &|_tr| {
-            next_dynamic(
-                true,
-                false,
-                false,
-                NextDynamicMode::Webpack,
-                FileName::Real(PathBuf::from("/some-project/src/some-file.js")),
-                Some("/some-project/src".into()),
-            )
-        },
+#[fixture("tests/webpack/fixture/**/input.js")]
+fn next_dynamic_webpack_fixture(input: PathBuf) {
+    next_dynamic_fixture(
         &input,
-        &output_dev,
-        Default::default(),
+        "output-dev.js",
+        true,
+        false,
+        false,
+        NextDynamicMode::Webpack,
     );
-    test_fixture(
-        syntax(),
-        &|_tr| {
-            next_dynamic(
-                false,
-                false,
-                false,
-                NextDynamicMode::Webpack,
-                FileName::Real(PathBuf::from("/some-project/src/some-file.js")),
-                Some("/some-project/src".into()),
-            )
-        },
+    next_dynamic_fixture(
         &input,
-        &output_prod,
-        Default::default(),
+        "output-prod.js",
+        false,
+        false,
+        false,
+        NextDynamicMode::Webpack,
     );
+    next_dynamic_fixture(
+        &input,
+        "output-server.js",
+        false,
+        true,
+        false,
+        NextDynamicMode::Webpack,
+    );
+}
+
+#[fixture("tests/turbo/fixture/**/input.js")]
+fn next_dynamic_turbo_fixture(input: PathBuf) {
+    // TODO(alexkirsz) Also test production once implemented.
+    next_dynamic_fixture(
+        &input,
+        "output-dev-client.js",
+        true,
+        false,
+        false,
+        NextDynamicMode::Turbo,
+    );
+    next_dynamic_fixture(
+        &input,
+        "output-dev-server.js",
+        true,
+        true,
+        false,
+        NextDynamicMode::Turbo,
+    );
+}
+
+fn next_dynamic_fixture(
+    input: &Path,
+    output: &str,
+    is_development: bool,
+    is_server: bool,
+    is_server_components: bool,
+    mode: NextDynamicMode,
+) {
+    let output = input.parent().unwrap().join(output);
     test_fixture(
         syntax(),
         &|_tr| {
             next_dynamic(
-                false,
-                true,
-                false,
-                NextDynamicMode::Webpack,
+                is_development,
+                is_server,
+                is_server_components,
+                mode,
                 FileName::Real(PathBuf::from("/some-project/src/some-file.js")),
                 Some("/some-project/src".into()),
             )
         },
         &input,
-        &output_server,
+        &output,
         Default::default(),
     );
 }
