2ce8ecc82b2d5ac09c34e1927c03fb68eae2a096
Chris Olszewski <chris.olszewski@vercel.com>
2023-01-12 15:52:37 -0800

    add unit tests for config

--- crates/turborepo-lib/src/config.rs
+++ crates/turborepo-lib/src/config.rs
@@ -16,18 +16,18 @@ pub fn default_user_config_path() -> Option<PathBuf> {
     config_dir().map(|p| p.join("turborepo").join("config.json"))
 }
 
+#[allow(dead_code)]
 pub fn data_dir() -> Option<PathBuf> {
     dirs_next::data_dir().map(|p| p.join("turborepo"))
 }
 
-// we probably need a wrapper struct that holds the file config so we don't
-// write env/cli values to file
-
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
+// Inner struct that matches the config file schema
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq, Default)]
 struct UserConfigInner {
     token: Option<String>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct UserConfig {
     disk_config: UserConfigInner,
     config: UserConfigInner,
@@ -35,11 +35,15 @@ pub struct UserConfig {
 }
 
 impl UserConfig {
+    /// Loads the user config from the given path,
     pub fn load(path: &Path, token: Option<&str>) -> Result<Self> {
+        // We load just the disk config to make sure we don't write a config
+        // value that comes from a flag or environment variable.
         let raw_disk_config = Config::builder()
             .add_source(
                 config::File::with_name(path.to_string_lossy().as_ref())
-                    .format(config::FileFormat::Json),
+                    .format(config::FileFormat::Json)
+                    .required(false),
             )
             .build()?;
 
@@ -59,11 +63,12 @@ impl UserConfig {
         })
     }
 
+    #[allow(dead_code)]
     pub fn token(&self) -> Option<&str> {
         self.config.token.as_deref()
     }
 
-    #[allow(dead_code)]
+    /// Set token and sync the changes to disk
     pub fn set_token(&mut self, token: Option<String>) -> Result<()> {
         self.disk_config.token = token.clone();
         self.config.token = token;
@@ -71,10 +76,57 @@ impl UserConfig {
     }
 
     fn write_to_disk(&self) -> Result<()> {
-        // TODO make sure that containing dir is created
+        if let Some(parent_dir) = self.path.parent() {
+            std::fs::create_dir_all(parent_dir)?;
+        }
         let config_file = std::fs::File::create(&self.path)?;
         serde_json::to_writer_pretty(&config_file, &self.disk_config)?;
         config_file.sync_all()?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{env, io::Write};
+
+    use tempfile::{NamedTempFile, TempDir};
+
+    use super::*;
+
+    #[test]
+    fn test_handles_non_existent_path() -> Result<()> {
+        let config_dir = TempDir::new()?;
+        let mut config_path = config_dir.path().to_path_buf();
+        config_path.push("turbo");
+        config_path.push("config.json");
+        let mut config = UserConfig::load(&config_path, None)?;
+        assert_eq!(config.token(), None);
+        config.set_token(Some("foo".to_string()))?;
+        let new_config = UserConfig::load(&config_path, None)?;
+        assert_eq!(new_config.token(), Some("foo"));
+        Ok(())
+    }
+
+    #[test]
+    fn test_disk_value_preserved() -> Result<()> {
+        let mut config_file = NamedTempFile::new()?;
+        writeln!(&mut config_file, "{{\"token\": \"foo\"}}")?;
+        let config = UserConfig::load(config_file.path(), Some("bar"))?;
+        assert_eq!(config.token(), Some("bar"));
+        config.write_to_disk()?;
+        let new_config = UserConfig::load(config_file.path(), None)?;
+        assert_eq!(new_config.token(), Some("foo"));
+        Ok(())
+    }
+
+    #[test]
+    fn test_env_var_trumps_disk() -> Result<()> {
+        let mut config_file = NamedTempFile::new()?;
+        writeln!(&mut config_file, "{{\"token\": \"foo\"}}")?;
+        env::set_var("TURBO_TOKEN", "bar");
+        let config = UserConfig::load(config_file.path(), None)?;
+        assert_eq!(config.token(), Some("bar"));
         Ok(())
     }
 }
