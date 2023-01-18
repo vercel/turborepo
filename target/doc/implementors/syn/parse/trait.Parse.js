6a284b0ef2999ef6fb96cba429c71ca390a68a8f
sokra <sokra@users.noreply.github.com>
2023-01-18 16:05:12 +0000

    Update docs (6301b709a4deb7552746c8bbf16f62d67b8a1513)

--- target/doc/implementors/syn/parse/trait.Parse.js
+++ target/doc/implementors/syn/parse/trait.Parse.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"turbo_tasks_macros_shared":[["impl <a class=\"trait\" href=\"https://docs.rs/syn/1.0.105/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"turbo_tasks_macros_shared/struct.ValueTraitArguments.html\" title=\"struct turbo_tasks_macros_shared::ValueTraitArguments\">ValueTraitArguments</a>"]]
+"turbo_tasks_macros_shared":[["impl <a class=\"trait\" href=\"https://docs.rs/syn/1.0.107/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"turbo_tasks_macros_shared/struct.ValueTraitArguments.html\" title=\"struct turbo_tasks_macros_shared::ValueTraitArguments\">ValueTraitArguments</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
