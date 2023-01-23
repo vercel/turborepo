3b48b77a7f345fc6388f22381e142afabdeac34d
Mehul Kar <mehul.kar@vercel.com>
2023-01-22 18:01:53 -0800

    reduce diff

--- cli/internal/core/engine.go
+++ cli/internal/core/engine.go
@@ -145,28 +145,23 @@ func (e *Engine) generateTaskGraph(pkgs []string, taskNames []string, tasksOnly 
 		taskID := traversalQueue[0]
 		traversalQueue = traversalQueue[1:]
 
-		packageName, taskName := util.GetPackageTaskFromId(taskID)
-		if packageName == util.RootPkgName && !e.rootEnabledTasks.Includes(taskName) {
+		pkg, taskName := util.GetPackageTaskFromId(taskID)
+		if pkg == util.RootPkgName && !e.rootEnabledTasks.Includes(taskName) {
 			return fmt.Errorf("%v needs an entry in turbo.json before it can be depended on because it is a task run from the root package", taskID)
 		}
 
-		// Skip this iteration of the loop if we've already seen this taskID
-		if visited.Includes(taskID) {
-			continue
-		}
-
-		pkg, ok := e.completeGraph.WorkspaceInfos[packageName]
+		pkgJSON, ok := e.completeGraph.WorkspaceInfos[pkg]
 
 		if !ok {
-			// This should be unlikely to happen. If we have a packageName
+			// This should be unlikely to happen. If we have a pkg
 			// it should be in WorkspaceInfos. If we're hitting this error
 			// something has gone wrong earlier when building WorkspaceInfos
-			return fmt.Errorf("Failed to look up workspace %s", packageName)
+			return fmt.Errorf("Failed to look up workspace %s", pkg)
 		}
 
 		taskDefinition, err := e.GetResolvedTaskDefinition(
 			&e.completeGraph.Pipeline,
-			pkg,
+			pkgJSON,
 			taskID,
 			taskName,
 		)
@@ -175,13 +170,17 @@ func (e *Engine) generateTaskGraph(pkgs []string, taskNames []string, tasksOnly 
 			return err
 		}
 
+		// Skip this iteration of the loop if we've already seen this taskID
+		if visited.Includes(taskID) {
+			continue
+		}
+
 		visited.Add(taskID)
 
 		// Put this taskDefinition into the Graph so we can look it up later during execution.
 		e.completeGraph.TaskDefinitions[taskID] = taskDefinition
 
 		topoDeps := util.SetFromStrings(taskDefinition.TopologicalDependencies)
-
 		deps := make(util.Set)
 		isPackageTask := util.IsPackageTask(taskName)
 
@@ -222,7 +221,7 @@ func (e *Engine) generateTaskGraph(pkgs []string, taskNames []string, tasksOnly 
 
 		// hasTopoDeps will be true if the task depends on any tasks from dependency packages
 		// E.g. `dev: { dependsOn: [^dev] }`
-		hasTopoDeps := topoDeps.Len() > 0 && e.TopologicGraph.DownEdges(packageName).Len() > 0
+		hasTopoDeps := topoDeps.Len() > 0 && e.TopologicGraph.DownEdges(pkg).Len() > 0
 
 		// hasDeps will be true if the task depends on any tasks from its own package
 		// E.g. `build: { dependsOn: [dev] }`
@@ -237,7 +236,7 @@ func (e *Engine) generateTaskGraph(pkgs []string, taskNames []string, tasksOnly 
 		}
 
 		if hasTopoDeps {
-			depPkgs := e.TopologicGraph.DownEdges(packageName)
+			depPkgs := e.TopologicGraph.DownEdges(pkg)
 			for _, from := range topoDeps.UnsafeListOfStrings() {
 				// add task dep from all the package deps within repo
 				for depPkg := range depPkgs {
@@ -252,7 +251,7 @@ func (e *Engine) generateTaskGraph(pkgs []string, taskNames []string, tasksOnly 
 
 		if hasDeps {
 			for _, from := range deps.UnsafeListOfStrings() {
-				fromTaskID := util.GetTaskId(packageName, from)
+				fromTaskID := util.GetTaskId(pkg, from)
 				e.TaskGraph.Add(fromTaskID)
 				e.TaskGraph.Add(toTaskID)
 				e.TaskGraph.Connect(dag.BasicEdge(toTaskID, fromTaskID))
