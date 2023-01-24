b44babb94c65cbd3e4b302968f7fc8a274a77a00
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-01-24 11:40:27 +0100

    Handle 404 page when getStaticProps.notFound = true

---
+++ crates/next-core/src/next_route_matcher/prefix_suffix.rs
@@ -0,0 +1,65 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use turbo_tasks::primitives::BoolVc;
+use turbopack_node::route_matcher::{ParamsVc, RouteMatcher};
+
+/// A composite route matcher that matches a path if it has a given prefix and
+/// suffix.
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub struct PrefixSuffixMatcher<T>
+where
+    T: RouteMatcher,
+{
+    prefix: String,
+    suffix: String,
+    inner: T,
+}
+
+impl<T> PrefixSuffixMatcher<T>
+where
+    T: RouteMatcher,
+{
+    /// Creates a new [PrefixSuffixMatcher].
+    pub fn new(prefix: String, suffix: String, inner: T) -> Self {
+        Self {
+            prefix,
+            suffix,
+            inner,
+        }
+    }
+
+    fn strip_prefix_and_suffix<'a, 'b>(&'a self, path: &'b str) -> Result<Option<&'b str>> {
+        let path = match path.strip_prefix(self.prefix.as_str()) {
+            Some(path) => path,
+            None => return Ok(None),
+        };
+
+        let path = match path.strip_suffix(self.suffix.as_str()) {
+            Some(path) => path,
+            None => return Ok(None),
+        };
+
+        Ok(Some(path))
+    }
+}
+
+impl<T> RouteMatcher for PrefixSuffixMatcher<T>
+where
+    T: RouteMatcher,
+{
+    fn matches(&self, path: &str) -> BoolVc {
+        if let Some(path) = self.strip_prefix_and_suffix(path).unwrap() {
+            self.inner.matches(path)
+        } else {
+            BoolVc::cell(false)
+        }
+    }
+
+    fn params(&self, path: &str) -> ParamsVc {
+        if let Some(path) = self.strip_prefix_and_suffix(path).unwrap() {
+            self.inner.params(path)
+        } else {
+            ParamsVc::cell(None)
+        }
+    }
+}
--- crates/turbopack-ecmascript/src/references/esm/module_id.rs
+++ crates/turbopack-ecmascript/src/references/esm/module_id.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
-use swc_core::ecma::ast::Expr;
+use swc_core::{
+    common::DUMMY_SP,
+    ecma::ast::{Expr, Lit, Null},
+};
 use turbo_tasks::{primitives::StringVc, ValueToString, ValueToStringVc};
 use turbopack_core::{
     chunk::{
@@ -73,6 +76,17 @@ impl CodeGenerateable for EsmModuleIdAssetReference {
                         ModuleId::String(s) => s.clone().into(),
                         ModuleId::Number(n) => (*n as f64).into(),
                     })
+                }),
+            );
+        } else {
+            // If the referenced asset can't be found, replace the expression with null.
+            // This can happen if the referenced asset is an external, or doesn't resolve
+            // to anything.
+            visitors.push(
+                create_visitor!(self.ast_path.await?, visit_mut_expr(expr: &mut Expr) {
+                    *expr = Expr::Lit(Lit::Null(Null {
+                        span: DUMMY_SP
+                    }));
                 }),
             );
         }
