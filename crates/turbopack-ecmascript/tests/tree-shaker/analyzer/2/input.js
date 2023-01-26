01acd9df0cddb2b032e32820c3c301f48b90dde0
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-26 13:54:10 +0900

    Make input more testable

--- crates/turbopack-ecmascript/tests/tree-shaker/analyzer/2/input.js
+++ crates/turbopack-ecmascript/tests/tree-shaker/analyzer/2/input.js
@@ -1,3 +1,6 @@
+export function external1() {
+  return internal() + foobar;
+}
 import { upper } from "module";
 export let foobar = "foo";
 export const foo = foobar;
@@ -10,9 +13,7 @@ foobarCopy += "Unused";
 function internal() {
   return upper(foobar);
 }
-export function external1() {
-  return internal() + foobar;
-}
 export function external2() {
   foobar += ".";
 }
+import "other";
