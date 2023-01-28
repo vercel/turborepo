vercel/turborepo

commit 2ec033026c03244dd3cd3b385d5206d5ca52bd44

Handle 404 page when getStaticProps.notFound = true or the route is not found (#3448)
This specifically supports the case where `getStaticProps.notFound =
true` or a matching route is not found, but not the case where
`getStaticPaths.fallback = false` and the route is not enumerated;

This separate case will be implemented in another PR.

crates/next-core/src/page_source.rs
@@ -8,7 +8,9 @@ use turbo_tasks::{
     Value,
 };
 use turbo_tasks_env::ProcessEnvVc;
-use turbo_tasks_fs::{DirectoryContent, DirectoryEntry, FileSystemEntryType, FileSystemPathVc};
+use turbo_tasks_fs::{
+    DirectoryContent, DirectoryEntry, FileContent, FileSystemEntryType, FileSystemPathVc,
+};
 use turbopack::{transition::TransitionsByNameVc, ModuleAssetContextVc};
 use turbopack_core::{
     asset::AssetVc,
@@ -38,11 +40,12 @@ use turbopack_node::{
     render::{
         node_api_source::create_node_api_source, rendered_source::create_node_rendered_source,
     },
+    route_matcher::RouteMatcherVc,
     NodeEntry, NodeEntryVc, NodeRenderingEntry, NodeRenderingEntryVc,
 };
 
 use crate::{
-    embed_js::{next_js_file, wrap_with_next_js_fs},
+    embed_js::{attached_next_js_package_path, next_asset, next_js_file, wrap_with_next_js_fs},
     env::env_for_js,
     fallback::get_fallback_page,
     next_client::{
@@ -59,12 +62,15 @@ use crate::{
         context::{get_edge_environment, get_edge_resolve_options_context},
         transition::NextEdgeTransition,
     },
+    next_route_matcher::{
+        NextExactMatcherVc, NextFallbackMatcherVc, NextParamsMatcherVc,
+        NextPrefixSuffixParamsMatcherVc,
+    },
     next_server::context::{
         get_server_environment, get_server_module_options_context,
         get_server_resolve_options_context, ServerContextType,
     },
     page_loader::create_page_loader,
-    params_matcher::NextParamsMatcherVc,
     util::{parse_config_from_source, pathname_for_path, NextRuntime},
 };
 
@@ -224,17 +230,45 @@ pub async fn create_page_source(
         next_config,
     );
 
+    let server_runtime_entries = EcmascriptChunkPlaceablesVc::cell(server_runtime_entries);
+    let page_extensions = next_config.page_extensions();
+    let force_not_found_source = create_not_found_page_source(
+        project_path,
+        server_context,
+        client_context,
+        pages_dir,
+        page_extensions,
+        server_runtime_entries,
+        fallback_page,
+        server_root,
+        output_path,
+        SpecificityVc::exact(),
+        NextExactMatcherVc::new(StringVc::cell("_next/404".to_string())).into(),
+    );
+    let fallback_not_found_source = create_not_found_page_source(
+        project_path,
+        server_context,
+        client_context,
+        pages_dir,
+        page_extensions,
+        server_runtime_entries,
+        fallback_page,
+        server_root,
+        output_path,
+        SpecificityVc::not_found(),
+        NextFallbackMatcherVc::new().into(),
+    );
     let page_source = create_page_source_for_directory(
         project_path,
         server_context,
         server_data_context,
         client_context,
         pages_dir,
-        next_config.page_extensions(),
+        page_extensions,
         SpecificityVc::exact(),
         0,
         pages_dir,
-        EcmascriptChunkPlaceablesVc::cell(server_runtime_entries),
+        server_runtime_entries,
         fallback_page,
         server_root,
         server_root,
@@ -246,7 +280,13 @@ pub async fn create_page_source(
         AssetGraphContentSourceVc::new_eager(server_root, fallback_page.as_asset());
 
     Ok(CombinedContentSource {
-        sources: vec![page_source.into(), fallback_source.into()],
+        sources: vec![
+            // Match _next/404 first to ensure rewrites work properly.
+            force_not_found_source,
+            page_source.into(),
+            fallback_source.into(),
+            fallback_not_found_source,
+        ],
     }
     .cell()
     .into())
@@ -261,7 +301,7 @@ async fn create_page_source_for_file(
     client_context: AssetContextVc,
     pages_dir: FileSystemPathVc,
     specificity: SpecificityVc,
-    page_file: FileSystemPathVc,
+    page_asset: AssetVc,
     runtime_entries: EcmascriptChunkPlaceablesVc,
     fallback_page: DevHtmlAssetVc,
     server_root: FileSystemPathVc,
@@ -270,13 +310,12 @@ async fn create_page_source_for_file(
     intermediate_output_path: FileSystemPathVc,
     output_root: FileSystemPathVc,
 ) -> Result<ContentSourceVc> {
-    let source_asset = SourceAssetVc::new(page_file).into();
     let entry_asset = server_context.process(
-        source_asset,
+        page_asset,
         Value::new(ReferenceType::Entry(EntryReferenceSubType::Page)),
     );
     let data_asset = server_data_context.process(
-        source_asset,
+        page_asset,
         Value::new(ReferenceType::Entry(EntryReferenceSubType::Page)),
     );
 
@@ -314,7 +353,7 @@ async fn create_page_source_for_file(
     );
 
     let pathname = pathname_for_path(server_root, server_path, true);
-    let params_matcher = NextParamsMatcherVc::new(pathname, None, None);
+    let route_matcher = NextParamsMatcherVc::new(pathname);
 
     let page_config = parse_config_from_source(entry_asset);
 
@@ -328,7 +367,7 @@ async fn create_page_source_for_file(
             specificity,
             server_root,
             pathname,
-            params_matcher.into(),
+            route_matcher.into(),
             SsrEntry {
                 context: server_context,
                 entry_asset,
@@ -342,11 +381,8 @@ async fn create_page_source_for_file(
             runtime_entries,
         )
     } else {
-        let data_params_matcher = NextParamsMatcherVc::new(
-            pathname,
-            Some(StringVc::cell("_next/data/development/".to_string())),
-            Some(StringVc::cell(".json".to_string())),
-        );
+        let data_route_matcher =
+            NextPrefixSuffixParamsMatcherVc::new(pathname, "_next/data/development/", ".json");
 
         let ssr_entry = SsrEntry {
             context: server_context,
@@ -374,8 +410,8 @@ async fn create_page_source_for_file(
             create_node_rendered_source(
                 specificity,
                 server_root,
+                route_matcher.into(),
                 pathname,
-                params_matcher.into(),
                 ssr_entry,
                 runtime_entries,
                 fallback_page,
@@ -383,8 +419,8 @@ async fn create_page_source_for_file(
             create_node_rendered_source(
                 specificity,
                 server_root,
+                data_route_matcher.into(),
                 pathname,
-                data_params_matcher.into(),
                 ssr_data_entry,
                 runtime_entries,
                 fallback_page,
@@ -399,6 +435,110 @@ async fn create_page_source_for_file(
         ])
         .into()
     })
+}
+
+async fn get_not_found_page(
+    pages_dir: FileSystemPathVc,
+    page_extensions: StringsVc,
+) -> Result<Option<AssetVc>> {
+    for ext in page_extensions.await?.iter() {
+        let not_found_path = pages_dir.join(&format!("404.{ext}"));
+        let content = not_found_path.read();
+        if let FileContent::Content(_) = &*content.await? {
+            return Ok(Some(SourceAssetVc::new(not_found_path).into()));
+        }
+    }
+    Ok(None)
+}
+
+/// Handles a single page file in the pages directory
+#[turbo_tasks::function]
+async fn create_not_found_page_source(
+    context_path: FileSystemPathVc,
+    server_context: AssetContextVc,
+    client_context: AssetContextVc,
+    pages_dir: FileSystemPathVc,
+    page_extensions: StringsVc,
+    runtime_entries: EcmascriptChunkPlaceablesVc,
+    fallback_page: DevHtmlAssetVc,
+    server_root: FileSystemPathVc,
+    intermediate_output_path: FileSystemPathVc,
+    specificity: SpecificityVc,
+    route_matcher: RouteMatcherVc,
+) -> Result<ContentSourceVc> {
+    let server_chunking_context = DevChunkingContextVc::builder(
+        context_path,
+        intermediate_output_path,
+        intermediate_output_path.join("chunks"),
+        get_client_assets_path(
+            server_root,
+            Value::new(ClientContextType::Pages { pages_dir }),
+        ),
+        server_context.environment(),
+    )
+    .build();
+
+    let client_chunking_context = get_client_chunking_context(
+        context_path,
+        server_root,
+        client_context.environment(),
+        Value::new(ClientContextType::Pages { pages_dir }),
+    );
+
+    let (page_asset, pathname) =
+        if let Some(not_found_page_asset) = get_not_found_page(pages_dir, page_extensions).await? {
+            // If a 404 page is defined, the pathname should be 404.
+            (not_found_page_asset, StringVc::cell("404".to_string()))
+        } else {
+            (
+                // The error page asset must be within the context path so it can depend on the
+                // Next.js module.
+                next_asset(
+                    attached_next_js_package_path(context_path).join("entry/error.tsx"),
+                    "entry/error.tsx",
+                ),
+                // If no 404 page is defined, the pathname should be _error.
+                StringVc::cell("_error".to_string()),
+            )
+        };
+
+    let entry_asset = server_context.process(
+        page_asset,
+        Value::new(ReferenceType::Entry(EntryReferenceSubType::Page)),
+    );
+
+    let ssr_entry = SsrEntry {
+        context: server_context,
+        entry_asset,
+        ty: SsrType::Html,
+        chunking_context: server_chunking_context,
+        intermediate_output_path,
+        output_root: intermediate_output_path,
+    }
+    .cell()
+    .into();
+
+    let page_loader = create_page_loader(
+        server_root,
+        client_context,
+        client_chunking_context,
+        entry_asset,
+        pathname,
+    );
+
+    Ok(CombinedContentSourceVc::new(vec![
+        create_node_rendered_source(
+            specificity,
+            server_root,
+            route_matcher,
+            pathname,
+            ssr_entry,
+            runtime_entries,
+            fallback_page,
+        ),
+        page_loader,
+    ])
+    .into())
 }
 
 /// Handles a directory in the pages directory (or the pages directory itself).
@@ -443,26 +583,15 @@ async fn create_page_source_for_directory(
                             .iter()
                             .any(|allowed| allowed == extension)
                         {
-                            let (dev_server_path, intermediate_output_path, specificity) =
-                                if basename == "index" {
-                                    (
-                                        server_path.join("index.html"),
-                                        intermediate_output_path,
-                                        specificity,
-                                    )
-                                } else if basename == "404" {
-                                    (
-                                        server_path.join("[...].html"),
-                                        intermediate_output_path.join(basename),
-                                        specificity.with_fallback(position),
-                                    )
-                                } else {
-                                    (
-                                        server_path.join(basename).join("index.html"),
-                                        intermediate_output_path.join(basename),
-                                        specificity,
-                                    )
-                                };
+                            let (dev_server_path, intermediate_output_path) = if basename == "index"
+                            {
+                                (server_path.join("index.html"), intermediate_output_path)
+                            } else {
+                                (
+                                    server_path.join(basename).join("index.html"),
+                                    intermediate_output_path.join(basename),
+                                )
+                            };
                             sources.push((
                                 name,
                                 create_page_source_for_file(
@@ -472,7 +601,7 @@ async fn create_page_source_for_directory(
                                     client_context,
                                     pages_dir,
                                     specificity,
-                                    *file,
+                                    SourceAssetVc::new(*file).into(),
                                     runtime_entries,
                                     fallback_page,
                                     server_root,

crates/next-core/js/src/entry/app-renderer.tsx
@@ -51,9 +51,13 @@ type IpcIncomingMessage = {
 };
 
 type IpcOutgoingMessage = {
-  type: "result";
-  result: string | { body: string; contentType?: string };
+  type: "response";
+  statusCode: number;
+  contentType: string;
+  body: string;
 };
+
+const MIME_TEXT_HTML_UTF8 = "text/html; charset=utf-8";
 
 (async () => {
   while (true) {
@@ -71,15 +75,16 @@ type IpcOutgoingMessage = {
       }
     }
 
-    const html = await runOperation(renderData);
+    const result = await runOperation(renderData);
 
-    if (html == null) {
+    if (result == null) {
       throw new Error("no html returned");
     }
 
     ipc.send({
-      type: "result",
-      result: html,
+      type: "response",
+      statusCode: 200,
+      ...result,
     });
   }
 })().catch((err) => {
@@ -255,7 +260,7 @@ async function runOperation(renderData: RenderData) {
     body = result.toUnchunkedString();
   }
   return {
-    contentType: result.contentType(),
+    contentType: result.contentType() ?? MIME_TEXT_HTML_UTF8,
     body,
   };
 }

crates/next-core/js/src/entry/error.tsx
@@ -1,0 +1,4 @@
+import Error, { getStaticProps } from "@vercel/turbopack-next/internal/_error";
+
+export default Error;
+export { getStaticProps };

crates/next-core/js/src/entry/server-renderer.tsx
@@ -6,6 +6,7 @@ import App from "@vercel/turbopack-next/pages/_app";
 import Document from "@vercel/turbopack-next/pages/_document";
 
 import Component, * as otherExports from ".";
+
 ("TURBOPACK { transition: next-client }");
 import chunkGroup from ".";
 

crates/next-core/js/src/internal/http.ts
@@ -8,6 +8,7 @@ import type { IncomingMessage, ServerResponse } from "node:http";
 export class ServerResponseShim {
   headersSent = false;
   #headers: Map<string, number | string | ReadonlyArray<string>> = new Map();
+  #statusCode: number = 200;
   req: IncomingMessage;
 
   constructor(req: IncomingMessage) {
@@ -40,11 +41,11 @@ export class ServerResponseShim {
   }
 
   get statusCode() {
-    throw new Error("statusCode is not implemented");
+    return this.#statusCode;
   }
 
   set statusCode(code) {
-    throw new Error("set statusCode is not implemented");
+    this.#statusCode = code;
   }
 
   get statusMessage() {

crates/next-core/js/src/internal/page-server-handler.tsx
@@ -23,10 +23,17 @@ type IpcIncomingMessage = {
   data: RenderData;
 };
 
-type IpcOutgoingMessage = {
-  type: "result";
-  result: string | { body: string; contentType?: string };
-};
+type IpcOutgoingMessage =
+  | {
+      type: "response";
+      statusCode: number;
+      contentType: string;
+      body: string;
+    }
+  | { type: "rewrite"; path: string };
+
+const MIME_APPLICATION_JAVASCRIPT = "application/javascript";
+const MIME_TEXT_HTML_UTF8 = "text/html; charset=utf-8";
 
 export default function startHandler({
   isDataReq,
@@ -59,35 +66,9 @@ export default function startHandler({
         }
       }
 
-      let res = await runOperation(renderData);
+      const res = await runOperation(renderData);
 
-      if (isDataReq) {
-        if (res === undefined) {
-          // Page data is only returned if the page had getXxyProps.
-          res = {};
-        }
-        ipc.send({
-          type: "result",
-          result: {
-            contentType: "application/json",
-            body: JSON.stringify(res),
-          },
-        });
-      } else {
-        if (res == null) {
-          throw new Error("no render result returned");
-        }
-        if (typeof res !== "string") {
-          throw new Error("Non-string render result returned");
-        }
-        ipc.send({
-          type: "result",
-          result: {
-            contentType: undefined,
-            body: res,
-          },
-        });
-      }
+      ipc.send(res);
     }
   })().catch((err) => {
     ipc.sendError(err);
@@ -95,7 +76,7 @@ export default function startHandler({
 
   async function runOperation(
     renderData: RenderData
-  ): Promise<Object | string | null> {
+  ): Promise<IpcOutgoingMessage> {
     // TODO(alexkirsz) This is missing *a lot* of data, but it's enough to get a
     // basic render working.
 
@@ -188,6 +169,19 @@ export default function startHandler({
       headers: renderData.headers,
     } as any;
     const res: ServerResponse = new ServerResponseShim(req) as any;
+
+    // Both _error and 404 should receive a 404 status code.
+    const statusCode =
+      renderData.path === "/404"
+        ? 404
+        : renderData.path === "/_error"
+        ? 404
+        : 200;
+
+    // Setting the status code on the response object is necessary for
+    // `Error.getInitialProps` to detect the status code.
+    res.statusCode = statusCode;
+
     const query = { ...renderData.query, ...renderData.params };
 
     const renderResult = await renderToHTML(
@@ -203,23 +197,54 @@ export default function startHandler({
       renderOpts
     );
 
+    // This is set when `getStaticProps` returns `notFound: true`.
+    const isNotFound = (renderOpts as any).isNotFound;
+
+    if (isNotFound) {
+      if (isDataReq) {
+        return {
+          type: "response",
+          statusCode,
+          body: '{"notFound":true}',
+          contentType: MIME_APPLICATION_JAVASCRIPT,
+        };
+      }
+
+      return {
+        type: "rewrite",
+        // _next/404 is a Turbopack-internal route that will always redirect to
+        // the 404 page.
+        path: "_next/404",
+      };
+    }
+
     if (isDataReq) {
       // TODO(from next.js): change this to a different passing mechanism
       const pageData = (renderOpts as any).pageData;
-      return pageData;
+      return {
+        type: "response",
+        statusCode,
+        contentType: MIME_APPLICATION_JAVASCRIPT,
+        // Page data is only returned if the page had getXxyProps.
+        body: JSON.stringify(pageData === undefined ? {} : pageData),
+      };
     }
 
     if (!renderResult) {
-      return null;
+      throw new Error("no render result returned");
     }
 
     const body = renderResult.toUnchunkedString();
     // TODO: handle these
     // const sprRevalidate = (renderOpts as any).revalidate;
-    // const isNotFound = (renderOpts as any).isNotFound;
     // const isRedirect = (renderOpts as any).isRedirect;
 
-    return body;
+    return {
+      type: "response",
+      statusCode,
+      contentType: renderResult.contentType() ?? MIME_TEXT_HTML_UTF8,
+      body,
+    };
   }
 }
 

crates/next-core/src/app_source.rs
@@ -60,11 +60,11 @@ use crate::{
         ssr_client_module_transition::NextSSRClientModuleTransition,
     },
     next_config::NextConfigVc,
+    next_route_matcher::NextParamsMatcherVc,
     next_server::context::{
         get_server_environment, get_server_module_options_context,
         get_server_resolve_options_context, ServerContextType,
     },
-    params_matcher::NextParamsMatcherVc,
     util::pathname_for_path,
 };
 
@@ -409,13 +409,13 @@ async fn create_app_source_for_directory(
         layouts = LayoutSegmentsVc::cell(list);
         if let Some(page_path) = page {
             let pathname = pathname_for_path(server_root, target, false);
-            let params_matcher = NextParamsMatcherVc::new(pathname, None, None);
+            let params_matcher = NextParamsMatcherVc::new(pathname);
 
             sources.push(create_node_rendered_source(
                 specificity,
                 server_root,
-                pathname,
                 params_matcher.into(),
+                pathname,
                 AppRenderer {
                     context_ssr,
                     context,

crates/next-core/src/lib.rs
@@ -16,11 +16,11 @@ mod next_edge;
 mod next_font_google;
 pub mod next_image;
 mod next_import_map;
+mod next_route_matcher;
 pub mod next_server;
 pub mod next_shared;
 mod page_loader;
 mod page_source;
-mod params_matcher;
 pub mod react_refresh;
 pub mod router;
 pub mod router_source;

commit 00803d6d1418c37f25ad8e9784254b20eaa56872

Fix infinite loop caused by error page rendering (#3525)
The new error paths introduced in #3448 both write their outputs to
`.next/server/pages/`, which conflicts with:
- The main pages source
- Each other

The infinite loop is very fun:
1. We need a `NodeJsPoolVc` to render pages
2. To get a `NodeJsPoolVc`, you need to write your files onto disk
    - So a pool is dependent on the contents of those files
3. When we render an error page, we need to write those files to disk
4. The error page shares the same file entrypoint as the main page
source

So, to render an error, we write the entrypoint, which is shared with
main source. This alone is pretty bad, because it will invalidate our
page source's node pool (and kill those processes). But, the loop is
triggered by a more subtle bug:

When we write a file, we read it to see if the contents have changed.
Writing creates a dependency on the read! So when the error page is
written to disk, it invalidated the read we preformed when we wrote the
main page. That invalidated the main page (and it's node pool), and so
we rendered again. That wrote the main page's code to disk, invalidating
the read of the error page performed when we wrote the error page. ♾️

(I'll be opening more PRs…)

crates/next-core/src/page_source.rs
@@ -241,7 +241,7 @@ pub async fn create_page_source(
         server_runtime_entries,
         fallback_page,
         server_root,
-        output_path,
+        output_path.join("force_not_found"),
         SpecificityVc::exact(),
         NextExactMatcherVc::new(StringVc::cell("_next/404".to_string())).into(),
     );
@@ -254,7 +254,7 @@ pub async fn create_page_source(
         server_runtime_entries,
         fallback_page,
         server_root,
-        output_path,
+        output_path.join("fallback_not_found"),
         SpecificityVc::not_found(),
         NextFallbackMatcherVc::new().into(),
     );