a323279c3af18e2a2c776f2e656c4a365d4bdcb2
ForsakenHarmony <ForsakenHarmony@users.noreply.github.com>
2023-02-01 16:48:38 +0000

    Update docs (bc9e7c6a2bad12e1e98957a997f29722134fe1ca)

---
+++ target/doc/turbopack_core/resolve/plugin/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["ResolvePluginCondition","A condition which determines if the hooks of a resolve plugin gets called."],["ResolvePluginConditionVc","Vc for [`ResolvePluginCondition`]"],["ResolvePluginVc",""]],"trait":[["ResolvePlugin",""]],"type":[["ResolvePluginConditionReadRef","see [turbo_tasks::ReadRef]"]]};
