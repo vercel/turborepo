f036deef712db5d57465972044b6c4cd1f35ddc9
sokra <sokra@users.noreply.github.com>
2023-02-03 19:50:29 +0000

    Update docs (2626223541b53e6511e23cc177e0f5f8f6ac8cf5)

---
+++ target/doc/implementors/stable_deref_trait/trait.StableDeref.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"turbo_tasks":[["impl&lt;T, U&gt; StableDeref for <a class=\"struct\" href=\"turbo_tasks/struct.ReadRef.html\" title=\"struct turbo_tasks::ReadRef\">ReadRef</a>&lt;T, U&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
