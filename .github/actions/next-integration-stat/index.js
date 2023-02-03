cfd3dedeac3b2278fc0b492b80d7699c20f47f53
Will Binns-Smith <wbinnssmith@gmail.com>
2023-02-03 14:12:20 -0800

    Send Next.js Integration test results to Slack (#3591)
    This uses Slack's GitHub action [0] to send the contents of the summary
    markdown file to a custom workflow for posting in Slack.
    
    Since the markdown file includes newlines and quotes, this uses `jq` to
    escape it and format a new json file that the action reads.
    
    Test Plan: Manually run the workflow against this branch
    
    [0] https://github.com/slackapi/slack-github-action
    
    ---------
    
    Co-authored-by: OJ Kwon <1210596+kwonoj@users.noreply.github.com>

--- .github/actions/next-integration-stat/index.js
+++ .github/actions/next-integration-stat/index.js
@@ -16450,39 +16450,48 @@
           .map((t) => `\t- ${t}`)
           .join(" \n")}`;
       }
-      // Store plain textbased summary to share into Slack channel
-      // Note: Likely we'll need to polish this summary to make it more readable.
+      // Store a json payload to share via slackapi/slack-github-action into Slack channel
       if (shouldShareTestSummaryToSlack) {
-        let textSummary = `*Next.js integration test status with Turbopack*
-
-    *Base: ${baseResults.ref} / ${shortBaseNextJsVersion}*
-    Test suites: :red_circle: ${baseTestFailedSuiteCount} / :green_circle: ${baseTestPassedSuiteCount} (Total: ${baseTestTotalSuiteCount})
-    Test cases : :red_circle: ${baseTestFailedCaseCount} / :green_circle: ${baseTestPassedCaseCount} (Total: ${baseTestTotalCaseCount})
-
-    *Current: ${sha} / ${shortCurrentNextJsVersion}*
-    Test suites: :red_circle: ${currentTestFailedSuiteCount} / :green_circle: ${currentTestPassedSuiteCount} (Total: ${currentTestTotalSuiteCount})
-    Test cases : :red_circle: ${currentTestFailedCaseCount} / :green_circle: ${currentTestPassedCaseCount} (Total: ${currentTestTotalCaseCount})
-
-    `;
+        let resultsSummary = "";
         if (suiteCountDiff === 0) {
-          textSummary += "No changes in suite count.";
+          resultsSummary += "No changes in suite count.";
         } else if (suiteCountDiff > 0) {
-          textSummary += `↓ ${suiteCountDiff} suites are fixed`;
+          resultsSummary += `↓ ${suiteCountDiff} suites are fixed`;
         } else if (suiteCountDiff < 0) {
-          textSummary += `↑ ${suiteCountDiff} suites are newly failed`;
+          resultsSummary += `↑ ${suiteCountDiff} suites are newly failed`;
         }
         if (caseCountDiff === 0) {
-          textSummary += "No changes in test cases count.";
+          resultsSummary += "No changes in test cases count.";
         } else if (caseCountDiff > 0) {
-          textSummary += `↓ ${caseCountDiff} test cases are fixed`;
+          resultsSummary += `↓ ${caseCountDiff} test cases are fixed`;
         } else if (caseCountDiff < 0) {
-          textSummary += `↑ ${caseCountDiff} test cases are newly failed`;
+          resultsSummary += `↑ ${caseCountDiff} test cases are newly failed`;
         }
-        console.log(
-          "Storing text summary to ./test-summary.md to report into Slack channel.",
-          textSummary
+        const slackPayloadJson = JSON.stringify(
+          {
+            title: "Next.js integration test status with Turbopack",
+            // Derived from https://github.com/orgs/community/discussions/25470#discussioncomment-4720013
+            actionUrl: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+            shaUrl: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/commit/${sha}`,
+            baseResultsRef: baseResults.ref,
+            shortBaseNextJsVersion,
+            // We're limited to 20 variables in Slack workflows, so combine these as text.
+            baseTestSuiteText: `:red_circle: ${baseTestFailedSuiteCount} / :large_green_circle: ${baseTestPassedSuiteCount} (Total: ${baseTestTotalSuiteCount})`,
+            baseTestCaseText: `:red_circle: ${baseTestFailedCaseCount} / :large_green_circle: ${baseTestPassedCaseCount} (Total: ${baseTestTotalCaseCount})`,
+            sha,
+            shortCurrentNextJsVersion,
+            currentTestSuiteText: `:red_circle: ${currentTestFailedSuiteCount} / :large_green_circle: ${currentTestPassedSuiteCount} (Total: ${currentTestTotalSuiteCount})`,
+            currentTestCaseText: `:red_circle: ${currentTestFailedCaseCount} / :large_green_circle: ${currentTestPassedCaseCount} (Total: ${currentTestTotalCaseCount})`,
+            resultsSummary,
+          },
+          null,
+          2
         );
-        fs.writeFileSync("./test-summary.md", textSummary);
+        console.log(
+          "Storing slack payload to ./slack-paylod.json to report into Slack channel.",
+          slackPayloadJson
+        );
+        fs.writeFileSync("./slack-payload.json", slackPayloadJson);
       }
       return ret;
     }
@@ -16499,7 +16508,9 @@
         } = yield getInputs();
         // determine if we want to report summary into slack channel.
         // As a first step, we'll only report summary when the test is run against release-to-release. (no main branch regressions yet)
-        const shouldReportSlack = !prNumber && !shouldDiffWithMain;
+        const shouldReportSlack =
+          process.env.NEXT_TURBO_FORCE_SLACK_UPDATE === "true" ||
+          (!prNumber && !shouldDiffWithMain);
         // Collect current PR's failed test results
         const failedJobResults = yield getFailedJobResults(octokit, token, sha);
         // Get the base to compare against
