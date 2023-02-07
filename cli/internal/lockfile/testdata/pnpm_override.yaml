vercel/turborepo

commit c9c78463e00fa108ca962fcc5e13d9f01a1f7c64

fix: mismatched pnpm lockfile version resolution (#3611)
Fixes #3564

I created a regression when adding support for pnpm `lockfileVersion: 6`
that caused us to not include some indirect dependencies of a workspace.
A package would be missing only if a workspace had a direct dependency
on the package that was also included as an indirect dependency where
the version ranges didn't overlap. See the new test fixture for a basic
version of how this could occur.

Huge thanks to @shawnmcknight for providing a reproduction!

cli/internal/lockfile/pnpm_lockfile.go
@@ -426,15 +426,13 @@ func (p *PnpmLockfile) resolveSpecifier(workspacePath turbopath.AnchoredUnixPath
 	if err != nil {
 		return "", false, err
 	}
-	if !ok {
+	// Verify that the specifier in the importer matches the one given
+	if !ok || resolution.Specifier != specifier {
 		// Check if the specifier is already a resolved version
 		if _, ok := p.Packages[p.formatKey(name, specifier)]; ok {
 			return specifier, true, nil
 		}
 		return "", false, fmt.Errorf("Unable to find resolved version for %s@%s in %s", name, specifier, workspacePath)
-	}
-	if resolution.Specifier != specifier {
-		return "", false, nil
 	}
 	return resolution.Version, true, nil
 }

cli/internal/lockfile/pnpm_lockfile_test.go
@@ -277,3 +277,19 @@ func Test_LockfilePeer(t *testing.T) {
 	assert.DeepEqual(t, pkg.Version, "13.0.4(react-dom@18.2.0)(react@18.2.0)")
 	assert.DeepEqual(t, pkg.Key, "/next@13.0.4(react-dom@18.2.0)(react@18.2.0)")
 }
+
+func Test_LockfileTopLevelOverride(t *testing.T) {
+	contents, err := getFixture(t, "pnpm-top-level-dupe.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+	lockfile, err := DecodePnpmLockfile(contents)
+	assert.NilError(t, err, "decode lockfile")
+
+	pkg, err := lockfile.ResolvePackage(turbopath.AnchoredUnixPath("packages/a"), "ci-info", "3.7.1")
+	assert.NilError(t, err, "resolve package")
+
+	assert.Assert(t, pkg.Found)
+	assert.DeepEqual(t, pkg.Key, "/ci-info/3.7.1")
+	assert.DeepEqual(t, pkg.Version, "3.7.1")
+}

cli/internal/lockfile/testdata/pnpm-top-level-dupe.yaml
@@ -1,0 +1,36 @@
+lockfileVersion: 5.4
+
+importers:
+  packages/a:
+    specifiers:
+      ci-info: ^2.0.0
+      is-ci: ^3.0.1
+    dependencies:
+      ci-info: 2.0.0
+      is-ci: 3.0.1
+
+packages:
+  /ci-info/2.0.0:
+    resolution:
+      {
+        integrity: sha512-5tK7EtrZ0N+OLFMthtqOj4fI2Jeb88C4CAZPu25LDVUgXJ0A3Js4PMGqrn0JU1W0Mh1/Z8wZzYPxqUrXeBboCQ==,
+      }
+    dev: false
+
+  /ci-info/3.7.1:
+    resolution:
+      {
+        integrity: sha512-4jYS4MOAaCIStSRwiuxc4B8MYhIe676yO1sYGzARnjXkWpmzZMMYxY6zu8WYWDhSuth5zhrQ1rhNSibyyvv4/w==,
+      }
+    engines: { node: ">=8" }
+    dev: false
+
+  /is-ci/3.0.1:
+    resolution:
+      {
+        integrity: sha512-ZYvCgrefwqoQ6yTyYUbQu64HsITZ3NfKX1lzaEYdkTDcfKzzCI/wthRRYKkdjHKFVgNiXKAKm65Zo1pk2as/QQ==,
+      }
+    hasBin: true
+    dependencies:
+      ci-info: 3.7.1
+    dev: false

commit 1493cc233a4e934c1562d812becb7487b823ecb9

fix: No longer error on pnpm lockfile specifier mismatch (#3656)
Fixes #3638

I added a regression in #3611 where we would error if there was a
specifier mismatch and the specifier wasn't totally resolved. This was
an unintended change to the previous behavior would indicate it didn't
find a package resolution.

Tested against [provided
reproduction](https://github.com/sushiswap/sushiswap)

cli/internal/lockfile/pnpm_lockfile.go
@@ -439,14 +439,33 @@ func (p *PnpmLockfile) resolveSpecifier(workspacePath turbopath.AnchoredUnixPath
 		return "", false, err
 	}
 	// Verify that the specifier in the importer matches the one given
-	if !ok || resolution.Specifier != specifier {
+	if !ok {
 		// Check if the specifier is already a resolved version
 		if _, ok := p.Packages[p.formatKey(name, specifier)]; ok {
 			return specifier, true, nil
 		}
 		return "", false, fmt.Errorf("Unable to find resolved version for %s@%s in %s", name, specifier, workspacePath)
 	}
+	overrideSpecifier := p.applyOverrides(name, specifier)
+	if resolution.Specifier != overrideSpecifier {
+		if _, ok := p.Packages[p.formatKey(name, overrideSpecifier)]; ok {
+			return overrideSpecifier, true, nil
+		}
+		return "", false, nil
+	}
 	return resolution.Version, true, nil
+}
+
+// Apply pnpm overrides to specifier, see https://pnpm.io/package_json#pnpmoverrides
+// Note this is barebones support and will only supports global overrides
+// future work will support semver ranges and selector filtering.
+func (p *PnpmLockfile) applyOverrides(name string, specifier string) string {
+	if len(p.Overrides) > 0 {
+		if new, ok := p.Overrides[name]; ok {
+			return new
+		}
+	}
+	return specifier
 }
 
 func formatPnpmKey(name string, version string) string {

cli/internal/lockfile/pnpm_lockfile_test.go
@@ -289,3 +289,22 @@ func Test_LockfileTopLevelOverride(t *testing.T) {
 	assert.DeepEqual(t, pkg.Key, "/ci-info/3.7.1")
 	assert.DeepEqual(t, pkg.Version, "3.7.1")
 }
+
+func Test_PnpmOverride(t *testing.T) {
+	contents, err := getFixture(t, "pnpm_override.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+	lockfile, err := DecodePnpmLockfile(contents)
+	assert.NilError(t, err, "decode lockfile")
+
+	pkg, err := lockfile.ResolvePackage(
+		turbopath.AnchoredUnixPath("config/hardhat"),
+		"@nomiclabs/hardhat-ethers",
+		"npm:hardhat-deploy-ethers@0.3.0-beta.13",
+	)
+	assert.NilError(t, err, "failure to find package")
+	assert.Assert(t, pkg.Found)
+	assert.DeepEqual(t, pkg.Key, "/hardhat-deploy-ethers/0.3.0-beta.13_yab2ug5tvye2kp6e24l5x3z7uy")
+	assert.DeepEqual(t, pkg.Version, "/hardhat-deploy-ethers/0.3.0-beta.13_yab2ug5tvye2kp6e24l5x3z7uy")
+}

cli/internal/lockfile/testdata/pnpm_override.yaml
@@ -1,0 +1,24 @@
+lockfileVersion: 5.4
+
+overrides:
+  "@nomiclabs/hardhat-ethers": npm:hardhat-deploy-ethers@^0.3.0-beta.13
+
+importers:
+  config/hardhat:
+    specifiers:
+      "@nomiclabs/hardhat-ethers": npm:hardhat-deploy-ethers@^0.3.0-beta.13
+    dependencies:
+      "@nomiclabs/hardhat-ethers": /hardhat-deploy-ethers/0.3.0-beta.13_yab2ug5tvye2kp6e24l5x3z7uy
+
+packages:
+  /hardhat-deploy-ethers/0.3.0-beta.13_yab2ug5tvye2kp6e24l5x3z7uy:
+    resolution:
+      {
+        integrity: sha512-PdWVcKB9coqWV1L7JTpfXRCI91Cgwsm7KLmBcwZ8f0COSm1xtABHZTyz3fvF6p42cTnz1VM0QnfDvMFlIRkSNw==,
+      }
+    peerDependencies:
+      ethers: ^5.0.0
+      hardhat: ^2.0.0
+    dependencies:
+      ethers: 5.7.2
+      hardhat: 2.12.4_typescript@4.9.4