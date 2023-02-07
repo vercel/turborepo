794f639523631f2b9cb8d523bb6c3622b1554a26
Justin Ridgewell <justin@ridgewell.name>
2023-02-07 18:23:36 -0500

    Isolate router to its own output dir

--- crates/next-core/src/router.rs
+++ crates/next-core/src/router.rs
@@ -207,7 +207,7 @@ pub async fn route(
         project_root,
         project_root,
         context,
-        intermediate_output_path,
+        intermediate_output_path.join("router"),
         Some(extra_configs),
         vec![
             JsonValueVc::cell(request),
