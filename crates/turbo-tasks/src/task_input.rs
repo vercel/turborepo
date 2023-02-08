7084c5f4b19c5d1def6eadd2c7a48a7688b5314a
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-02-08 10:25:59 +0100

    Allow tuples as TaskInputs

--- crates/turbo-tasks/src/task_input.rs
+++ crates/turbo-tasks/src/task_input.rs
@@ -860,3 +860,53 @@ impl TryFrom<&TaskInput> for RawVc {
         }
     }
 }
+
+macro_rules! tuple_impls {
+    ( $( $name:ident )+ ) => {
+        impl<$($name: Into<TaskInput>),+> From<($($name,)+)> for TaskInput {
+            #[allow(non_snake_case)]
+            fn from(s: ($($name,)+)) -> Self {
+                let ($($name,)+) = s;
+                let ($($name,)+) = ($($name.into(),)+);
+                TaskInput::List(vec![ $($name,)+ ])
+            }
+        }
+
+        impl<'a, $($name: FromTaskInput<'a, Error = anyhow::Error>,)+> FromTaskInput<'a> for ($($name,)+) {
+            type Error = anyhow::Error;
+
+            #[allow(non_snake_case)]
+            fn try_from(value: &'a TaskInput) -> Result<Self, Self::Error> {
+                match value {
+                    TaskInput::List(value) => {
+                        let mut i = 0;
+                        $(
+                            let $name = value.get(i).ok_or_else(|| anyhow!("missing tuple element"))?;
+                            let $name = FromTaskInput::try_from($name)?;
+                            #[allow(unused_assignments)]
+                            {
+                                i += 1;
+                            }
+                        )+
+                        Ok(($($name,)+))
+                    },
+                    _ => Err(anyhow!("invalid task input type, expected list")),
+                }
+            }
+        }
+    };
+
+}
+
+tuple_impls! { A }
+tuple_impls! { A B }
+tuple_impls! { A B C }
+tuple_impls! { A B C D }
+tuple_impls! { A B C D E }
+tuple_impls! { A B C D E F }
+tuple_impls! { A B C D E F G }
+tuple_impls! { A B C D E F G H }
+tuple_impls! { A B C D E F G H I }
+tuple_impls! { A B C D E F G H I J }
+tuple_impls! { A B C D E F G H I J K }
+tuple_impls! { A B C D E F G H I J K L }
