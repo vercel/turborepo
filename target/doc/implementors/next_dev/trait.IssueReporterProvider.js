e731ff32b246c27accb4202e0352eb257e804a0b
wbinnssmith <wbinnssmith@users.noreply.github.com>
2023-02-13 16:27:41 +0000

    Update docs (dae3e2155dcc5dc8e240825da4fd091296486b18)

---
+++ target/doc/implementors/next_dev/trait.IssueReporterProvider.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"next_dev":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ target/doc/implementors/turbopack_core/issue/trait.IssueReporter.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"turbopack_cli_utils":[["impl IssueReporter for <a class=\"struct\" href=\"turbopack_cli_utils/issue/struct.ConsoleUiVc.html\" title=\"struct turbopack_cli_utils::issue::ConsoleUiVc\">ConsoleUiVc</a>"]],
+"turbopack_core":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
