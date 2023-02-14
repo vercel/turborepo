894c15bcecfa1600da8acd6af4a8178011e2458e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-14 15:20:42 +0900

    Remove module evel check

--- crates/turbopack-ecmascript/src/tree_shake/graph.rs
+++ crates/turbopack-ecmascript/src/tree_shake/graph.rs
@@ -267,7 +267,7 @@ impl DepGraph {
 
                 let is_only_dep = count <= 1;
 
-                if (is_module_eval || is_only_dep) && done.insert(dep_ix) {
+                if is_only_dep && done.insert(dep_ix) {
                     changed = true;
 
                     let dep_id = graph.graph_ix.get_index(dep_ix as _).unwrap().clone();
