3c8ed95d9a1b07f361190f7ab49699cc0ee36b0c
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-15 12:41:17 +0900

    load

--- crates/turbopack-ecmascript/src/tree_shake/merge.rs
+++ crates/turbopack-ecmascript/src/tree_shake/merge.rs
@@ -1,4 +1,9 @@
-pub trait Load {}
+use anyhow::Error;
+use swc_core::ecma::ast::Module;
+
+pub trait Load {
+    fn load(&mut self, uri: &str) -> Result<Option<Module>, Error>;
+}
 
 pub struct Merger<L> {
     loader: L,
