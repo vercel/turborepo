e00c7657f6a468ef51f63c125864443d83b280cf
Nicholas Yang <nicholas.yang@vercel.com>
2023-02-16 17:26:33 -0500

    Fix

--- cli/internal/ffi/ffi.go
+++ cli/internal/ffi/ffi.go
@@ -2,8 +2,8 @@ package ffi
 
 // #include "bindings.h"
 //
-// #cgo darwin LDFLAGS: -L${SRCDIR} -lturborepo_ffi -lz
-// #cgo linux LDFLAGS: -L${SRCDIR} -lturborepo_ffi -lz -liconv
+// #cgo darwin LDFLAGS: -L${SRCDIR} -lturborepo_ffi -lz -liconv
+// #cgo linux LDFLAGS: -L${SRCDIR} -lturborepo_ffi -lz
 // #cgo windows LDFLAGS: -lole32 -lbcrypt -lws2_32 -luserenv
 import "C"
 
