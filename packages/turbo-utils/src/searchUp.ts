30060f386c17b25bbe41021652ffc8fc9a731adf
Thomas Knickman <tom.knickman@vercel.com>
2023-02-17 14:06:27 -0500

    feat(eslint): support turbo.json workspace configs (#3846)
    Support turbo.json workspace configs. 
    
    Still to do:
    - [x] Support workspace specific linting
    
    There exists one edge case here, if a user forgets to add an "extend" to workspace config, we _can_ treat it as root. I punted on this one for now since it's not this packages domain to solve that (at the moment)

--- packages/turbo-utils/src/searchUp.ts
+++ packages/turbo-utils/src/searchUp.ts
@@ -4,17 +4,31 @@ import path from "path";
 function searchUp({
   target,
   cwd,
+  contentCheck,
 }: {
   target: string;
   cwd: string;
+  contentCheck?: (content: string) => boolean;
 }): string | null {
   const root = path.parse(cwd).root;
 
   let found = false;
   while (!found && cwd !== root) {
-    if (fs.existsSync(path.join(cwd, target))) {
-      found = true;
-      break;
+    if (contentCheck) {
+      try {
+        const content = fs.readFileSync(path.join(cwd, target)).toString();
+        if (contentCheck(content)) {
+          found = true;
+          break;
+        }
+      } catch {
+        // keep looking
+      }
+    } else {
+      if (fs.existsSync(path.join(cwd, target))) {
+        found = true;
+        break;
+      }
     }
 
     cwd = path.dirname(cwd);
