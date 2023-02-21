5e92929a5134a820858145be5433384c79309173
Alberto Vilches <albertothedev@protonmail.com>
2023-02-21 07:56:49 +0100

    refactor: remove `react/jsx-key` ESLint rule (#3880)
    It was added in vercel/turbo#779 as a temporary fix but forgotten about.

--- examples/with-tailwind/packages/eslint-config-custom/index.js
+++ examples/with-tailwind/packages/eslint-config-custom/index.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
   },
   parserOptions: {
     babelOptions: {
