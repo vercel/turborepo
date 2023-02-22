35b7236f3ee41b0ce69171b75baf906cba803320
Thomas Knickman <tom.knickman@vercel.com>
2023-02-22 16:22:44 -0500

    fix(examples): kitchen-sink (#3920
    Update lock and remix app for kitchen sink example

--- examples/kitchen-sink/apps/blog/remix.config.js
+++ examples/kitchen-sink/apps/blog/remix.config.js
@@ -2,15 +2,9 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "vercel",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: [".*"],
+  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
+  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  watchPaths: [require.resolve("ui")],
 };
