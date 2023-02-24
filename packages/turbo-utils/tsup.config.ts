84fd31fcf8f636a7a3de5ccd6141ba013016b1d5
tknickman <tom.knickman@vercel.com>
2023-02-24 15:42:43 -0500

    feat(turbo-utils): bundle to esm

--- packages/turbo-utils/tsup.config.ts
+++ packages/turbo-utils/tsup.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entry: ["src/index.ts"],
+  entry: ["src/**/*.ts"],
+  treeshake: true,
+  splitting: true,
+  format: ["esm", "cjs"],
   dts: true,
+  minify: true,
   clean: true,
   ...options,
 }));
