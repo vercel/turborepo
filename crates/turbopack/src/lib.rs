b3d9f6931e0c5db5e4b2770207e8dbcb9382ea79
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-27 20:01:45 +0900

    Fix

--- crates/turbopack/src/lib.rs
+++ crates/turbopack/src/lib.rs
@@ -128,7 +128,7 @@ async fn apply_module_type(
                 ) => {
                     let part = part.await?;
 
-                    return Ok(EcmascriptModulePartAssetVc::split(base, part));
+                    return Ok(EcmascriptModulePartAssetVc::from_splitted(base, part));
                 }
 
                 _ => {}
