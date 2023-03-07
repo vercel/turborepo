riseansmal/turbo

improve and enable issue path handling
attach some context information to high level operations

crates/next-core/src/next_config.rs
@@ -14,6 +14,7 @@ use turbopack_core::{
     asset::Asset,
     context::AssetContext,
     ident::AssetIdentVc,
+    issue::IssueVc,
     reference_type::{EntryReferenceSubType, ReferenceType},
     resolve::{
         find_context_file,
@@ -539,6 +540,25 @@ fn next_configs() -> StringsVc {
 
 #[turbo_tasks::function]
 pub async fn load_next_config(execution_context: ExecutionContextVc) -> Result<NextConfigVc> {
+    let ExecutionContext { project_root, .. } = *execution_context.await?;
+    let find_config_result = find_context_file(project_root, next_configs());
+    let config_file = match &*find_config_result.await? {
+        FindContextFileResult::Found(config_path, _) => Some(*config_path),
+        FindContextFileResult::NotFound(_) => None,
+    };
+    Ok(IssueVc::attach_context_or_description(
+        config_file,
+        "Loading Next.js config",
+        load_next_config_internal(execution_context, config_file),
+    )
+    .await?)
+}
+
+#[turbo_tasks::function]
+pub async fn load_next_config_internal(
+    execution_context: ExecutionContextVc,
+    config_file: Option<FileSystemPathVc>,
+) -> Result<NextConfigVc> {
     let ExecutionContext {
         project_root,
         intermediate_output_path,
@@ -552,11 +572,7 @@ pub async fn load_next_config(execution_context: ExecutionContextVc) -> Result<N
     import_map.insert_wildcard_alias("styled-jsx/", ImportMapping::External(None).into());
 
     let context = node_evaluate_asset_context(Some(import_map.cell()), None);
-    let find_config_result = find_context_file(project_root, next_configs());
-    let config_asset = match &*find_config_result.await? {
-        FindContextFileResult::Found(config_path, _) => Some(SourceAssetVc::new(*config_path)),
-        FindContextFileResult::NotFound(_) => None,
-    };
+    let config_asset = config_file.map(|config_path| SourceAssetVc::new(config_path));
 
     let runtime_entries = config_asset.map(|config_asset| {
         // TODO this is a hack to add the config to the bundling to make it watched

crates/turbopack-cli-utils/src/issue.rs
@@ -1,4 +1,5 @@
 use std::{
+    borrow::Cow,
     cmp::min,
     collections::{hash_map::Entry, HashMap, HashSet},
     fmt::Write as _,
@@ -12,16 +13,14 @@ use crossterm::style::{StyledContent, Stylize};
 use owo_colors::{OwoColorize as _, Style};
 use turbo_tasks::{
     primitives::BoolVc, RawVc, ReadRef, TransientInstance, TransientValue, TryJoinIterExt,
-    ValueToString,
 };
 use turbo_tasks_fs::{
-    attach::AttachedFileSystemVc,
     source_context::{get_source_context, SourceContextLine},
-    to_sys_path, FileLinesContent, FileSystemPathVc,
+    FileLinesContent,
 };
 use turbopack_core::issue::{
-    CapturedIssues, Issue, IssueProcessingPathItem, IssueReporter, IssueReporterVc, IssueSeverity,
-    OptionIssueProcessingPathItemsVc, PlainIssue, PlainIssueSource,
+    CapturedIssues, IssueReporter, IssueReporterVc, IssueSeverity, PlainIssue,
+    PlainIssueProcessingPathItem, PlainIssueProcessingPathItemReadRef, PlainIssueSource,
 };
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -173,32 +172,32 @@ fn format_source_content(source: &PlainIssueSource, formatted_issue: &mut String
     }
 }
 
-async fn format_optional_path(
-    path: &OptionIssueProcessingPathItemsVc,
+fn format_optional_path(
+    path: &Option<Vec<PlainIssueProcessingPathItemReadRef>>,
     formatted_issue: &mut String,
 ) -> Result<()> {
-    if let Some(path) = &*path.await? {
+    if let Some(path) = path {
         let mut last_context = None;
         for item in path.iter().rev() {
-            let IssueProcessingPathItem {
-                context,
-                description,
-            } = &*item.await?;
+            let PlainIssueProcessingPathItem {
+                ref context,
+                ref description,
+            } = **item;
             if let Some(context) = context {
-                let context = context.resolve().await?;
-                if last_context == Some(context) {
-                    writeln!(formatted_issue, " at {}", &*description.await?)?;
+                let option_context = Some(context.clone());
+                if last_context == option_context {
+                    writeln!(formatted_issue, " at {}", &*description)?;
                 } else {
                     writeln!(
                         formatted_issue,
                         " at {} ({})",
-                        context.to_string().await?.bright_blue(),
-                        &*description.await?
+                        context.to_string().bright_blue(),
+                        &*description
                     )?;
-                    last_context = Some(context);
+                    last_context = option_context;
                 }
             } else {
-                writeln!(formatted_issue, " at {}", &*description.await?)?;
+                writeln!(formatted_issue, " at {}", &*description)?;
                 last_context = None;
             }
         }
@@ -301,6 +300,7 @@ const ORDERED_GROUPS: &[IssueSeverity] = &[
 #[derive(Debug, Clone)]
 pub struct LogOptions {
     pub current_dir: PathBuf,
+    pub project_dir: PathBuf,
     pub show_all: bool,
     pub log_detail: bool,
     pub log_level: IssueSeverity,
@@ -438,6 +438,7 @@ impl IssueReporter for ConsoleUi {
         let issues = &*issues;
         let LogOptions {
             ref current_dir,
+            ref project_dir,
             show_all,
             log_detail,
             log_level,
@@ -448,18 +449,14 @@ impl IssueReporter for ConsoleUi {
         let issues = issues
             .iter_with_shortest_path()
             .map(|(issue, path)| async move {
-                // (issue.)
-                let plain_issue = issue.into_plain();
+                let plain_issue = issue.into_plain(path);
                 let id = plain_issue.internal_hash().await?;
-                Ok((plain_issue.await?, path, issue.context(), *id))
+                Ok((plain_issue.await?, *id))
             })
             .try_join()
             .await?;
 
-        let issue_ids = issues
-            .iter()
-            .map(|(_, _, _, id)| *id)
-            .collect::<HashSet<_>>();
+        let issue_ids = issues.iter().map(|(_, id)| *id).collect::<HashSet<_>>();
         let mut new_ids = self
             .seen
             .lock()
@@ -467,7 +464,7 @@ impl IssueReporter for ConsoleUi {
             .new_ids(source.into_value(), issue_ids);
 
         let mut has_fatal = false;
-        for (plain_issue, path, context, id) in issues {
+        for (plain_issue, id) in issues {
             if !new_ids.remove(&id) {
                 continue;
             }
@@ -477,9 +474,10 @@ impl IssueReporter for ConsoleUi {
                 has_fatal = true;
             }
 
-            let context_path = make_relative_to_cwd(context, current_dir).await?;
+            let context_path = make_relative_to_cwd(&plain_issue.context, project_dir, current_dir);
             let category = &plain_issue.category;
             let title = &plain_issue.title;
+            let processing_path = &*plain_issue.processing_path;
             let severity_map = grouped_issues
                 .entry(severity)
                 .or_insert_with(Default::default);
@@ -487,7 +485,7 @@ impl IssueReporter for ConsoleUi {
                 .entry(category.clone())
                 .or_insert_with(Default::default);
             let issues = category_map
-                .entry(context_path.clone())
+                .entry(context_path.to_string())
                 .or_insert_with(Default::default);
 
             let mut styled_issue = if let Some(source) = &plain_issue.source {
@@ -522,7 +520,7 @@ impl IssueReporter for ConsoleUi {
                 if !documentation_link.is_empty() {
                     writeln!(&mut styled_issue, "\ndocumentation: {documentation_link}")?;
                 }
-                format_optional_path(&path, &mut styled_issue).await?;
+                format_optional_path(processing_path, &mut styled_issue)?;
             }
             issues.push(styled_issue);
         }
@@ -616,21 +614,21 @@ impl IssueReporter for ConsoleUi {
     }
 }
 
-async fn make_relative_to_cwd(path: FileSystemPathVc, cwd: &PathBuf) -> Result<String> {
-    let path = if let Some(fs) = AttachedFileSystemVc::resolve_from(path.fs()).await? {
-        fs.get_inner_fs_path(path)
-    } else {
-        path
-    };
-    if let Some(sys_path) = to_sys_path(path).await? {
-        let relative = sys_path
+fn make_relative_to_cwd<'a>(path: &'a str, project_dir: &PathBuf, cwd: &PathBuf) -> Cow<'a, str> {
+    if let Some(path_in_project) = path.strip_prefix("[project]/") {
+        let abs_path = if std::path::MAIN_SEPARATOR != '/' {
+            project_dir.join(path_in_project.replace('/', std::path::MAIN_SEPARATOR_STR))
+        } else {
+            project_dir.join(path_in_project)
+        };
+        let relative = abs_path
             .strip_prefix(cwd)
-            .unwrap_or(&sys_path)
+            .unwrap_or(&abs_path)
             .to_string_lossy()
             .to_string();
-        Ok(relative)
+        relative.into()
     } else {
-        Ok(path.to_string().await?.clone_value())
+        path.into()
     }
 }
 

vercel/turborepo

crates/next-core/src/next_config.rs
@@ -15,6 +15,7 @@ use turbopack_core::{
     changed::any_content_changed,
     context::AssetContext,
     ident::AssetIdentVc,
+    issue::IssueVc,
     reference_type::{EntryReferenceSubType, ReferenceType},
     resolve::{
         find_context_file,
@@ -539,6 +540,25 @@ fn next_configs() -> StringsVc {
 
 #[turbo_tasks::function]
 pub async fn load_next_config(execution_context: ExecutionContextVc) -> Result<NextConfigVc> {
+    let ExecutionContext { project_path, .. } = *execution_context.await?;
+    let find_config_result = find_context_file(project_path, next_configs());
+    let config_file = match &*find_config_result.await? {
+        FindContextFileResult::Found(config_path, _) => Some(*config_path),
+        FindContextFileResult::NotFound(_) => None,
+    };
+    IssueVc::attach_context_or_description(
+        config_file,
+        "Loading Next.js config",
+        load_next_config_internal(execution_context, config_file),
+    )
+    .await
+}
+
+#[turbo_tasks::function]
+pub async fn load_next_config_internal(
+    execution_context: ExecutionContextVc,
+    config_file: Option<FileSystemPathVc>,
+) -> Result<NextConfigVc> {
     let ExecutionContext {
         project_path,
         intermediate_output_path,
@@ -552,11 +572,7 @@ pub async fn load_next_config(execution_context: ExecutionContextVc) -> Result<N
     import_map.insert_wildcard_alias("styled-jsx/", ImportMapping::External(None).into());
 
     let context = node_evaluate_asset_context(project_path, Some(import_map.cell()), None);
-    let find_config_result = find_context_file(project_path, next_configs());
-    let config_asset = match &*find_config_result.await? {
-        FindContextFileResult::Found(config_path, _) => Some(SourceAssetVc::new(*config_path)),
-        FindContextFileResult::NotFound(_) => None,
-    };
+    let config_asset = config_file.map(SourceAssetVc::new);
 
     let config_changed = config_asset.map_or_else(CompletionVc::immutable, |config_asset| {
         // This invalidates the execution when anything referenced by the config file

crates/turbopack-cli-utils/src/issue.rs
@@ -1,8 +1,9 @@
 use std::{
+    borrow::Cow,
     cmp::min,
     collections::{hash_map::Entry, HashMap, HashSet},
     fmt::Write as _,
-    path::PathBuf,
+    path::{Path, PathBuf},
     str::FromStr,
     sync::{Arc, Mutex},
 };
@@ -12,16 +13,14 @@ use crossterm::style::{StyledContent, Stylize};
 use owo_colors::{OwoColorize as _, Style};
 use turbo_tasks::{
     primitives::BoolVc, RawVc, ReadRef, TransientInstance, TransientValue, TryJoinIterExt,
-    ValueToString,
 };
 use turbo_tasks_fs::{
-    attach::AttachedFileSystemVc,
     source_context::{get_source_context, SourceContextLine},
-    to_sys_path, FileLinesContent, FileSystemPathVc,
+    FileLinesContent,
 };
 use turbopack_core::issue::{
-    CapturedIssues, Issue, IssueProcessingPathItem, IssueReporter, IssueReporterVc, IssueSeverity,
-    OptionIssueProcessingPathItemsVc, PlainIssue, PlainIssueSource,
+    CapturedIssues, IssueReporter, IssueReporterVc, IssueSeverity, PlainIssue,
+    PlainIssueProcessingPathItem, PlainIssueProcessingPathItemReadRef, PlainIssueSource,
 };
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -173,32 +172,32 @@ fn format_source_content(source: &PlainIssueSource, formatted_issue: &mut String
     }
 }
 
-async fn format_optional_path(
-    path: &OptionIssueProcessingPathItemsVc,
+fn format_optional_path(
+    path: &Option<Vec<PlainIssueProcessingPathItemReadRef>>,
     formatted_issue: &mut String,
 ) -> Result<()> {
-    if let Some(path) = &*path.await? {
+    if let Some(path) = path {
         let mut last_context = None;
         for item in path.iter().rev() {
-            let IssueProcessingPathItem {
-                context,
-                description,
-            } = &*item.await?;
+            let PlainIssueProcessingPathItem {
+                ref context,
+                ref description,
+            } = **item;
             if let Some(context) = context {
-                let context = context.resolve().await?;
-                if last_context == Some(context) {
-                    writeln!(formatted_issue, " at {}", &*description.await?)?;
+                let option_context = Some(context.clone());
+                if last_context == option_context {
+                    writeln!(formatted_issue, " at {}", description)?;
                 } else {
                     writeln!(
                         formatted_issue,
                         " at {} ({})",
-                        context.to_string().await?.bright_blue(),
-                        &*description.await?
+                        context.to_string().bright_blue(),
+                        description
                     )?;
-                    last_context = Some(context);
+                    last_context = option_context;
                 }
             } else {
-                writeln!(formatted_issue, " at {}", &*description.await?)?;
+                writeln!(formatted_issue, " at {}", description)?;
                 last_context = None;
             }
         }
@@ -301,6 +300,7 @@ const ORDERED_GROUPS: &[IssueSeverity] = &[
 #[derive(Debug, Clone)]
 pub struct LogOptions {
     pub current_dir: PathBuf,
+    pub project_dir: PathBuf,
     pub show_all: bool,
     pub log_detail: bool,
     pub log_level: IssueSeverity,
@@ -438,6 +438,7 @@ impl IssueReporter for ConsoleUi {
         let issues = &*issues;
         let LogOptions {
             ref current_dir,
+            ref project_dir,
             show_all,
             log_detail,
             log_level,
@@ -448,18 +449,14 @@ impl IssueReporter for ConsoleUi {
         let issues = issues
             .iter_with_shortest_path()
             .map(|(issue, path)| async move {
-                // (issue.)
-                let plain_issue = issue.into_plain();
+                let plain_issue = issue.into_plain(path);
                 let id = plain_issue.internal_hash().await?;
-                Ok((plain_issue.await?, path, issue.context(), *id))
+                Ok((plain_issue.await?, *id))
             })
             .try_join()
             .await?;
 
-        let issue_ids = issues
-            .iter()
-            .map(|(_, _, _, id)| *id)
-            .collect::<HashSet<_>>();
+        let issue_ids = issues.iter().map(|(_, id)| *id).collect::<HashSet<_>>();
         let mut new_ids = self
             .seen
             .lock()
@@ -467,7 +464,7 @@ impl IssueReporter for ConsoleUi {
             .new_ids(source.into_value(), issue_ids);
 
         let mut has_fatal = false;
-        for (plain_issue, path, context, id) in issues {
+        for (plain_issue, id) in issues {
             if !new_ids.remove(&id) {
                 continue;
             }
@@ -477,9 +474,10 @@ impl IssueReporter for ConsoleUi {
                 has_fatal = true;
             }
 
-            let context_path = make_relative_to_cwd(context, current_dir).await?;
+            let context_path = make_relative_to_cwd(&plain_issue.context, project_dir, current_dir);
             let category = &plain_issue.category;
             let title = &plain_issue.title;
+            let processing_path = &*plain_issue.processing_path;
             let severity_map = grouped_issues
                 .entry(severity)
                 .or_insert_with(Default::default);
@@ -487,7 +485,7 @@ impl IssueReporter for ConsoleUi {
                 .entry(category.clone())
                 .or_insert_with(Default::default);
             let issues = category_map
-                .entry(context_path.clone())
+                .entry(context_path.to_string())
                 .or_insert_with(Default::default);
 
             let mut styled_issue = if let Some(source) = &plain_issue.source {
@@ -522,7 +520,7 @@ impl IssueReporter for ConsoleUi {
                 if !documentation_link.is_empty() {
                     writeln!(&mut styled_issue, "\ndocumentation: {documentation_link}")?;
                 }
-                format_optional_path(&path, &mut styled_issue).await?;
+                format_optional_path(processing_path, &mut styled_issue)?;
             }
             issues.push(styled_issue);
         }
@@ -616,21 +614,21 @@ impl IssueReporter for ConsoleUi {
     }
 }
 
-async fn make_relative_to_cwd(path: FileSystemPathVc, cwd: &PathBuf) -> Result<String> {
-    let path = if let Some(fs) = AttachedFileSystemVc::resolve_from(path.fs()).await? {
-        fs.get_inner_fs_path(path)
-    } else {
-        path
-    };
-    if let Some(sys_path) = to_sys_path(path).await? {
-        let relative = sys_path
+fn make_relative_to_cwd<'a>(path: &'a str, project_dir: &Path, cwd: &Path) -> Cow<'a, str> {
+    if let Some(path_in_project) = path.strip_prefix("[project]/") {
+        let abs_path = if std::path::MAIN_SEPARATOR != '/' {
+            project_dir.join(path_in_project.replace('/', std::path::MAIN_SEPARATOR_STR))
+        } else {
+            project_dir.join(path_in_project)
+        };
+        let relative = abs_path
             .strip_prefix(cwd)
-            .unwrap_or(&sys_path)
+            .unwrap_or(&abs_path)
             .to_string_lossy()
             .to_string();
-        Ok(relative)
+        relative.into()
     } else {
-        Ok(path.to_string().await?.clone_value())
+        path.into()
     }
 }
 