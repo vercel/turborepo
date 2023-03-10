3c6feea609812ae58c21fcef0c5d4dd7707ccd44
Tobias Koppers <tobias.koppers@googlemail.com>
2023-03-10 14:15:45 +0100

    update snapshot

--- crates/turbopack-tests/tests/snapshot/basic/async_chunk/output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_85740a.js
+++ crates/turbopack-tests/tests/snapshot/basic/async_chunk/output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_85740a.js
@@ -1,10 +1,10 @@
-(self.TURBOPACK = self.TURBOPACK || []).push(["output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_95859b.js", {
+(self.TURBOPACK = self.TURBOPACK || []).push(["output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_85740a.js", {
 
 "[project]/crates/turbopack-tests/tests/snapshot/basic/async_chunk/input/import.js (ecmascript, manifest chunk)": (({ r: __turbopack_require__, x: __turbopack_external_require__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, k: __turbopack_register_chunk_list__, j: __turbopack_cjs__, p: process, g: global, __dirname }) => (() => {
 
 __turbopack_export_value__([
-  "output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_0d49f0.js",
-  "output/20803_foo_index_b32b98.js"
+  "output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_52aed8.js",
+  "output/20803_foo_index_f1c29b.js"
 ]);
 
 })()),
--- crates/turbopack-tests/tests/snapshot/imports/dynamic/output/crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs_d12c37._.js
+++ crates/turbopack-tests/tests/snapshot/imports/dynamic/output/crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs_d12c37._.js
@@ -1,4 +1,4 @@
-(self.TURBOPACK = self.TURBOPACK || []).push(["output/crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs_e25ebc._.js", {
+(self.TURBOPACK = self.TURBOPACK || []).push(["output/crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs_d12c37._.js", {
 
 "[project]/crates/turbopack-tests/tests/snapshot/imports/dynamic/input/vercel.mjs (ecmascript)": (({ r: __turbopack_require__, x: __turbopack_external_require__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, k: __turbopack_register_chunk_list__, j: __turbopack_cjs__, p: process, g: global, __dirname }) => (() => {
 
@@ -11,4 +11,4 @@ const __TURBOPACK__default__export__ = "turbopack";
 }]);
 
 
-//# sourceMappingURL=crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs_e25ebc._.js.map
+//# sourceMappingURL=crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs_d12c37._.js.map
