e9168ae81c2f8872b26cfc0f41b095c9bc3e9cf9
mehulkar <mehulkar@users.noreply.github.com>
2023-03-13 18:22:15 +0000

    Update docs (068f70975ec3e6355e2cf9ff2b74053645461d9f)

--- docs/docs.rs/implementors/clap/derive/trait.Parser.js
+++ docs/docs.rs/implementors/clap/derive/trait.Parser.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {
-"next_dev":[["impl Parser for <a class=\"struct\" href=\"next_dev/devserver_options/struct.DevServerOptions.html\" title=\"struct next_dev::devserver_options::DevServerOptions\">DevServerOptions</a>"]],
 "node_file_trace":[["impl Parser for <a class=\"enum\" href=\"node_file_trace/enum.Args.html\" title=\"enum node_file_trace::Args\">Args</a>"]],
 "swc_ast_explorer":[["impl Parser for <a class=\"struct\" href=\"swc_ast_explorer/struct.Args.html\" title=\"struct swc_ast_explorer::Args\">Args</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
