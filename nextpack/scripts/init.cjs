997483dd1b842fcf02a376b80b9bda5e68dfc3f9
Justin Ridgewell <justin@ridgewell.name>
2023-03-13 20:41:08 -0400

    Add "nextpack" integration setup
    Hopefully this'll ease our developing in Next.js

---
+++ nextpack/scripts/init.cjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const { existsSync, writeFileSync } = require("fs");
+
+function exec(command, opts) {
+  console.log(command);
+  execSync(command, {
+    stdio: "inherit",
+    ...opts,
+  });
+}
+
+if (!existsSync("./next.js")) {
+  exec("git clone 'https://github.com/vercel/next.js.git' --depth 100");
+}
+
+writeFileSync(
+  "./Cargo.toml",
+  `
+[workspace]
+resolver = "2"
+members = ["crates/*"]`
+);
+
+exec("cargo run --bin sync-workspace");
