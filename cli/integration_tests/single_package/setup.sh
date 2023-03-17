ac7747a98d0f83cb891cad708e4548888b55a154
Mehul Kar <mehul.kar@vercel.com>
2023-03-17 02:36:50 -0700

    rm echo

--- cli/integration_tests/single_package/setup.sh
+++ cli/integration_tests/single_package/setup.sh
@@ -5,9 +5,6 @@ TARGET_DIR=$1
 cp -a ${SCRIPT_DIR}/my-pkg/. ${TARGET_DIR}/
 ${SCRIPT_DIR}/../setup_git.sh ${TARGET_DIR}
 
-# If a package manager was passed, set it in package.json
-echo "$2"
-
 if [ "$2" != "" ]; then
   cat "$TARGET_DIR/package.json" | jq ".packageManager=\"$2\"" | sponge "$TARGET_DIR/package.json"
   git commit -am "Update package manager" --quiet
