08f3e43aa5d78b766c33a56f81c98de87cc78e71
Mehul Kar <mehul.kar@vercel.com>
2023-03-23 14:20:23 -0700

    Fixup

--- cli/integration_tests/single_package/setup.sh
+++ cli/integration_tests/single_package/setup.sh
@@ -6,8 +6,10 @@ cp -a ${SCRIPT_DIR}/my-pkg/. ${TARGET_DIR}/
 ${SCRIPT_DIR}/../setup_git.sh ${TARGET_DIR}
 
 if [ "$2" != "" ]; then
-  pm="$2"
-  jq --arg pm "$pm" '.packageManager = $pm' "$TARGET_DIR/package.json"
-  # cat "$TARGET_DIR/package.json" | jq ".packageManager=\"$2\"" | sponge "$TARGET_DIR/package.json"
+  # Use jq to write a new file with a .packageManager field set and then
+  # Overwrite original package.json. For some reason the command above won't send its output
+  # directly to the original file.
+  jq --arg pm "$2" '.packageManager = $pm' "$TARGET_DIR/package.json" > "$TARGET_DIR/package.json.new"
+  mv "$TARGET_DIR/package.json.new" "$TARGET_DIR/package.json"
   git commit -am "Update package manager" --quiet
 fi
