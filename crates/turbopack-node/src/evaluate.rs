827710c748a5671a8937e4cc81b90e5d33f1b50c
Tobias Koppers <tobias.koppers@googlemail.com>
2023-03-24 17:42:25 +0000

    allow to mark tasks as finished
    this allows to exclude them from strongly consistency
    
    mark evaluate stream as finished to allow to return early from strongly consitent reads

--- crates/turbopack-node/src/evaluate.rs
+++ crates/turbopack-node/src/evaluate.rs
@@ -12,6 +12,7 @@ use parking_lot::Mutex;
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
 use turbo_tasks::{
+    mark_finished,
     primitives::{JsonValueVc, StringVc},
     util::SharedError,
     CompletionVc, RawVc, TryJoinIterExt, Value, ValueToString,
@@ -311,6 +312,7 @@ async fn compute_evaluate_stream(
     args: Vec<JsonValueVc>,
     sender: JavaScriptStreamSenderVc,
 ) {
+    mark_finished();
     let Ok(sender) = sender.await else {
         // Impossible to handle the error in a good way.
         return;
