43adc703482c833cd51c27b41ff50d187837ec2a
Alexander Lyon <arlyon@me.com>
2023-03-31 16:49:11 +0200

    refactor globwatcher into functions

--- crates/turborepo-lib/src/globwatcher/mod.rs
+++ crates/turborepo-lib/src/globwatcher/mod.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::{HashMap, HashSet},
-    path::PathBuf,
+    path::{Path, PathBuf},
     sync::{Arc, Mutex},
 };
 
@@ -16,7 +16,7 @@ use notify::RecommendedWatcher;
 #[derive(Clone)]
 pub struct HashGlobWatcher<T: Watcher> {
     hash_globs: Arc<Mutex<HashMap<Arc<String>, Glob>>>,
-    glob_status: Arc<Mutex<HashMap<Arc<String>, HashSet<Arc<String>>>>>,
+    glob_statuses: Arc<Mutex<HashMap<Arc<String>, HashSet<Arc<String>>>>>,
     watcher: Arc<Mutex<Option<GlobWatcher<T>>>>,
     config: GlobSender,
 }
@@ -32,7 +32,7 @@ impl HashGlobWatcher<RecommendedWatcher> {
         let (watcher, config) = GlobWatcher::new(flush_folder)?;
         Ok(Self {
             hash_globs: Default::default(),
-            glob_status: Default::default(),
+            glob_statuses: Default::default(),
             watcher: Arc::new(Mutex::new(Some(watcher))),
             config,
         })
@@ -67,64 +67,26 @@ impl<T: Watcher> HashGlobWatcher<T> {
         while let Some(Ok(event)) = stream.next().await {
             trace!("event: {:?}", event);
 
-            let repo_relative_paths_iter = event
+            let repo_relative_paths = event
                 .paths
                 .iter()
                 .filter_map(|path| path.strip_prefix(&root_folder).ok());
 
-            let mut clear_glob_status = vec![];
-            let mut exclude_globs = vec![];
-
             // put these in a block so we can drop the locks before we await
-            {
-                let mut glob_status = self.glob_status.lock().expect("ok");
-                let mut hash_globs = self.hash_globs.lock().expect("ok");
+            let globs_to_exclude = {
+                let glob_statuses = self.glob_statuses.lock().expect("ok");
+                let hash_globs = self.hash_globs.lock().expect("ok");
 
-                for ((glob, hash_status), path) in glob_status
-                    .iter()
-                    .cartesian_product(repo_relative_paths_iter)
-                    .filter(|((glob, _), path)| {
-                        glob_match::glob_match(glob, path.to_str().unwrap())
-                    })
-                {
-                    for hash in hash_status.iter() {
-                        let globs = match hash_globs.get_mut(hash).filter(|globs| {
-                            globs
-                                .exclude
-                                .iter()
-                                .any(|f| glob_match::glob_match(f, path.to_str().unwrap()))
-                        }) {
-                            Some(globs) => globs,
-                            None => continue,
-                        };
+                // hash globs is unlocked after this
+                let (hash_globs_to_clear, globs_to_exclude) =
+                    populate_hash_globs(&glob_statuses, repo_relative_paths, hash_globs);
 
-                        // we can stop tracking that glob
-                        globs.include.remove(glob);
-                        if globs.include.is_empty() {
-                            hash_globs.remove(hash);
-                        }
+                clear_hash_globs(glob_statuses, hash_globs_to_clear);
 
-                        // store the hash and glob so we can remove it from the glob_status
-                        exclude_globs.push(glob.to_owned());
-                        clear_glob_status.push((hash.clone(), glob.clone()));
-                    }
-                }
+                globs_to_exclude
+            };
 
-                for (hash, glob) in clear_glob_status {
-                    let empty = if let Some(globs) = glob_status.get_mut(&hash) {
-                        globs.remove(&glob);
-                        globs.is_empty()
-                    } else {
-                        false
-                    };
-
-                    if empty {
-                        glob_status.remove(&hash);
-                    }
-                }
-            }
-
-            for glob in exclude_globs {
+            for glob in globs_to_exclude {
                 self.config.exclude(glob.to_string()).await.unwrap();
             }
         }
@@ -147,7 +109,7 @@ impl<T: Watcher> HashGlobWatcher<T> {
         }
 
         {
-            let mut glob_status = self.glob_status.lock().expect("no panic");
+            let mut glob_status = self.glob_statuses.lock().expect("no panic");
             glob_status
                 .entry(hash.clone())
                 .or_default()
@@ -176,6 +138,69 @@ impl<T: Watcher> HashGlobWatcher<T> {
                 candidates
             }
             None => candidates,
+        }
+    }
+}
+
+/// iterate each path-glob pair and stop tracking globs whose files have
+/// changed
+///
+/// return a list of hash-glob pairs to clear, and a list of globs to exclude
+fn populate_hash_globs<'a>(
+    glob_statuses: &std::sync::MutexGuard<HashMap<Arc<String>, HashSet<Arc<String>>>>,
+    repo_relative_paths: impl Iterator<Item = &'a Path> + Clone,
+    mut hash_globs: std::sync::MutexGuard<HashMap<Arc<String>, Glob>>,
+) -> (Vec<(Arc<String>, Arc<String>)>, Vec<Arc<String>>) {
+    let mut clear_glob_status = vec![];
+    let mut exclude_globs = vec![];
+
+    for ((glob, hash_status), path) in glob_statuses
+        .iter()
+        .cartesian_product(repo_relative_paths)
+        .filter(|((glob, _), path)| glob_match::glob_match(glob, path.to_str().unwrap()))
+    {
+        for hash in hash_status.iter() {
+            let globs = match hash_globs.get_mut(hash).filter(|globs| {
+                globs
+                    .exclude
+                    .iter()
+                    .any(|f| glob_match::glob_match(f, path.to_str().unwrap()))
+            }) {
+                Some(globs) => globs,
+                None => continue,
+            };
+
+            // we can stop tracking that glob
+            globs.include.remove(glob);
+            if globs.include.is_empty() {
+                hash_globs.remove(hash);
+            }
+
+            // store the hash and glob so we can remove it from the glob_status
+            exclude_globs.push(glob.to_owned());
+            clear_glob_status.push((hash.clone(), glob.clone()));
+        }
+    }
+
+    (clear_glob_status, exclude_globs)
+}
+
+/// given a list of hosh-glob pairs to stop tracking, remove them from the
+/// map and remove the entry if the set of globs for that hash is empty
+fn clear_hash_globs(
+    mut glob_status: std::sync::MutexGuard<HashMap<Arc<String>, HashSet<Arc<String>>>>,
+    hash_globs_to_clear: Vec<(Arc<String>, Arc<String>)>,
+) {
+    for (hash, glob) in hash_globs_to_clear {
+        let empty = if let Some(globs) = glob_status.get_mut(&hash) {
+            globs.remove(&glob);
+            globs.is_empty()
+        } else {
+            false
+        };
+
+        if empty {
+            glob_status.remove(&hash);
         }
     }
 }
