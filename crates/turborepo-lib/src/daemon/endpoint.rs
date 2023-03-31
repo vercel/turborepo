3d4815916421fe1db417f0229081422cf0949806
Alexander Lyon <arlyon@me.com>
2023-03-31 13:09:27 +0200

    implement AsyncRead and AsyncWrite for daemon connector on windows

--- crates/turborepo-lib/src/daemon/endpoint.rs
+++ crates/turborepo-lib/src/daemon/endpoint.rs
@@ -1,41 +1,15 @@
-use std::{
-    path::{Path, PathBuf},
-    sync::Arc,
-};
+use std::{pin::Pin, sync::Arc};
 
 use futures::Stream;
-use tokio::io::{AsyncRead, AsyncWrite};
+use log::debug;
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use tokio_util::compat::FuturesAsyncReadCompatExt;
 use tonic::transport::server::Connected;
-
-///
-pub async fn get_channel(
-    path: PathBuf,
-) -> Result<tonic::transport::Channel, tonic::transport::Error> {
-    let arc = Arc::new(path);
-    tonic::transport::Endpoint::try_from("http://[::]:50051")?
-        .connect_with_connector(tower::service_fn(move |_| {
-            // we clone the reference counter here and move it into the async closure
-            let arc = arc.clone();
-            #[cfg(unix)]
-            {
-                async move { tokio::net::UnixStream::connect::<&Path>(arc.as_path()).await }
-            }
-
-            #[cfg(windows)]
-            {
-                async move { uds_windows::UnixStream::connect(arc.as_path()) }
-            }
-        }))
-        .await
-}
 
 #[derive(thiserror::Error, Debug)]
 pub enum SocketOpenError {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
-    #[cfg(windows)]
-    #[error("windows socket error: {0}")]
-    UdsWindows(#[from] uds_windows::Error),
     #[error("pidlock error")]
     LockError(#[from] pidlock::PidlockError),
 }
@@ -56,6 +30,8 @@ pub async fn open_socket(
     let sock_path = path.join(turborepo_paths::ForwardRelativePath::new("turbod.sock").unwrap());
     let mut lock = pidlock::Pidlock::new(pid_path.to_path_buf());
 
+    debug!("opening socket at {} {}", pid_path, sock_path);
+
     // this will fail if the pid is already owned
     lock.acquire()?;
     std::fs::remove_file(&sock_path).ok();
@@ -72,52 +48,90 @@ pub async fn open_socket(
 
     #[cfg(windows)]
     {
-        let listener = uds_windows::UnixListener::bind(path);
-        futures::stream::unfold(listener, |listener| async move {
-            match listener.accept().await {
-                Ok((stream, _)) => Some((Ok(stream), listener)),
-                Err(err) => Some((Err(err), listener)),
-            }
-        })
+        let listener = Arc::new(uds_windows::UnixListener::bind(sock_path)?);
+        let stream = futures::stream::unfold(listener, |listener| async move {
+            let task_listener = listener.clone();
+            let task = tokio::task::spawn_blocking(move || task_listener.accept());
+
+            let result = task
+                .await
+                .expect("no panic")
+                .map(|(stream, _)| stream)
+                .and_then(async_io::Async::new)
+                .map(FuturesAsyncReadCompatExt::compat)
+                .map(UdsWindowsStream);
+
+            Some((result, listener))
+        });
+
+        Ok((lock, stream))
     }
 }
 
 /// An adaptor over uds_windows that implements AsyncRead and AsyncWrite.
+///
+/// It utilizes structural pinning to forward async read and write
+/// implementations onto the inner type.
 #[cfg(windows)]
-struct UdsWindowsStream(uds_windows::UnixStream);
+struct UdsWindowsStream<T>(T);
 
-#[cfg(windows)]
-impl AsyncRead for UdsWindowsStream {
+impl<T> UdsWindowsStream<T> {
+    /// Project the (pinned) uds windows stream to get the inner (pinned) type
+    ///
+    /// SAFETY
+    ///
+    /// structural pinning requires a few invariants to hold which can be seen
+    /// here https://doc.rust-lang.org/std/pin/#pinning-is-structural-for-field
+    ///
+    /// in short:
+    /// - we cannot implement Unpin for UdsWindowsStream
+    /// - we cannot use repr packed
+    /// - we cannot move in the drop impl (the default impl doesn't)
+    /// - we must uphold the rust 'drop guarantee'
+    /// - we cannot offer any api to move data out of the pinned value (such as
+    ///   Option::take)
+    fn project(self: Pin<&mut Self>) -> Pin<&mut T> {
+        unsafe { self.map_unchecked_mut(|s| &mut s.0) }
+    }
+}
+
+impl<T: AsyncRead> AsyncRead for UdsWindowsStream<T> {
     fn poll_read(
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
         buf: &mut ReadBuf<'_>,
-    ) -> std::task::Poll<std::io::Result<usize>> {
-        self.0.poll_read(cx, buf)
+    ) -> std::task::Poll<std::io::Result<()>> {
+        self.project().poll_read(cx, buf)
     }
 }
 
-#[cfg(windows)]
-impl AsyncWrite for UdsWindowsStream {
+impl<T: AsyncWrite> AsyncWrite for UdsWindowsStream<T> {
     fn poll_write(
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
         buf: &[u8],
-    ) -> std::task::Poll<std::io::Result<usize>> {
-        self.0.poll_write(cx, buf)
+    ) -> std::task::Poll<Result<usize, std::io::Error>> {
+        self.project().poll_write(cx, buf)
     }
 
     fn poll_flush(
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<std::io::Result<()>> {
-        self.0.poll_flush(cx)
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        self.project().poll_flush(cx)
     }
 
     fn poll_shutdown(
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<std::io::Result<()>> {
-        self.0.poll_shutdown(cx)
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        self.project().poll_shutdown(cx)
+    }
+}
+
+impl<T> Connected for UdsWindowsStream<T> {
+    type ConnectInfo = ();
+    fn connect_info(&self) -> Self::ConnectInfo {
+        ()
     }
 }
