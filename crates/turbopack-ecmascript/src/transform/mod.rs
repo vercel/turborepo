da1c20cad14afd2f2bad8790380aadefe268fde5
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-05 15:06:32 +0900

    hash

--- crates/turbopack-ecmascript/src/transform/mod.rs
+++ crates/turbopack-ecmascript/src/transform/mod.rs
@@ -1,7 +1,13 @@
 mod server_to_client_proxy;
 mod util;
 
-use std::{fmt::Debug, path::Path, sync::Arc};
+use std::{
+    collections::hash_map::DefaultHasher,
+    fmt::Debug,
+    hash::{Hash, Hasher},
+    path::Path,
+    sync::Arc,
+};
 
 use anyhow::Result;
 use swc_core::{
@@ -216,9 +222,15 @@ impl EcmascriptInputTransform {
                     ..Default::default()
                 };
                 let p = std::mem::replace(program, Program::Module(Module::dummy()));
+                let hash = {
+                    let mut hasher = DefaultHasher::new();
+                    p.hash(&mut hasher);
+                    hasher.finish()
+                };
                 *program = p.fold_with(&mut swc_emotion::emotion(
                     options,
                     Path::new(file_name_str),
+                    hash as u32,
                     source_map.clone(),
                     comments.clone(),
                 ))
