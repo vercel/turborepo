cb38502c248237f6391e58ce7ab50650e190afdb
henry-yu-lb <yuyuchia7423@gmail.com>
2023-04-07 06:02:46 +0800

    chore: update `with-tailwind` example (#4379)
    Co-authored-by: Mehul Kar <mehul.kar@vercel.com>
    Co-authored-by: Thomas Knickman <tom.knickman@vercel.com>
    Co-authored-by: Alexander Lyon <arlyon@me.com>

--- examples/with-tailwind/packages/ui/tailwind.config.js
+++ examples/with-tailwind/packages/ui/tailwind.config.js
@@ -3,5 +3,5 @@ const sharedConfig = require("tailwind-config/tailwind.config.js");
 module.exports = {
   // prefix ui lib classes to avoid conflicting with the app
   prefix: "ui-",
-  ...sharedConfig,
+  presets: [sharedConfig],
 };
