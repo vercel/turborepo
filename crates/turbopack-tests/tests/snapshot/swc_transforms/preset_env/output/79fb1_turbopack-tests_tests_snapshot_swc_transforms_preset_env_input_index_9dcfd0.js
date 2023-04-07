25b6d68f1ea00db9ba1e67c38f2a6f1cce6b53cb
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-07 10:37:54 +0900

    Update test refs

--- crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_9dcfd0.js
+++ crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_9dcfd0.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_e2f660.js",
+    "output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_9dcfd0.js",
     {},
     {"otherChunks":[{"path":"output/63a02_@swc_helpers_src__class_call_check.mjs_f682d0._.js","included":["[project]/crates/turbopack-tests/tests/node_modules/@swc/helpers/src/_class_call_check.mjs (ecmascript)"]},{"path":"output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_b53fce.js","included":["[project]/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/input/index.js (ecmascript)"]}],"runtimeModuleIds":["[project]/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/input/index.js (ecmascript)"]}
 ]);
