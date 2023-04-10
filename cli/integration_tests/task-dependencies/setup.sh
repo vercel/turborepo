bf795ca9b416c010569dfa81d621366ade48c035
Mehul Kar <mehul.kar@vercel.com>
2023-04-10 01:26:23 -0700

    Task dependencies fixtures

--- cli/integration_tests/task-dependencies/setup.sh
+++ cli/integration_tests/task-dependencies/setup.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 TARGET_DIR=$1
 MONOREPO_DIR=$2
-cp -a ${SCRIPT_DIR}/${MONOREPO_DIR}/. ${TARGET_DIR}/
+cp -a ${SCRIPT_DIR}/../_fixtures/task_dependencies/${MONOREPO_DIR}/. ${TARGET_DIR}/
 ${SCRIPT_DIR}/../setup_git.sh ${TARGET_DIR}
