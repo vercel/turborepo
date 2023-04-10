c458cc6bdf30d64a9a920a039bb0c799120a94a4
Mehul Kar <mehul.kar@vercel.com>
2023-04-10 01:21:48 -0700

    single package fixture

--- cli/integration_tests/single_package/setup.sh
+++ cli/integration_tests/single_package/setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 TARGET_DIR=$1
-cp -a ${SCRIPT_DIR}/my-pkg/. ${TARGET_DIR}/
+cp -a ${SCRIPT_DIR}/../_fixtures/single_package/. ${TARGET_DIR}/
 ${SCRIPT_DIR}/../setup_git.sh ${TARGET_DIR}
 
 if [ "$2" != "" ]; then
