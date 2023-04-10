cef7d6e698f53bed5c795ae0bff79d61bb21577c
Mehul Kar <mehul.kar@vercel.com>
2023-04-10 01:20:10 -0700

    monorepo with root dep fixture

--- cli/integration_tests/prune/setup.sh
+++ cli/integration_tests/prune/setup.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 TARGET_DIR=$1
-cp -a ${SCRIPT_DIR}/monorepo_with_root_dep/. ${TARGET_DIR}/
+cp -a ${SCRIPT_DIR}/../_fixtures/monorepo_with_root_dep/. ${TARGET_DIR}/
