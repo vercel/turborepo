7fe4b64e9211052c80b49618bc726357e8035604
alexkirsz <alexkirsz@users.noreply.github.com>
2023-04-11 15:32:48 +0000

    Update docs (8a6c4b81f4952627cfac60b1a77240c1ea02ec4b)

--- target/doc/implementors/turbopack_core/chunk/trait.ChunkableAssetReference.js
+++ target/doc/implementors/turbopack_core/chunk/trait.ChunkableAssetReference.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"turbopack_core":[],
-"turbopack_dev":[["impl ChunkableAssetReference for <a class=\"struct\" href=\"turbopack_dev/struct.ChunkListReferenceVc.html\" title=\"struct turbopack_dev::ChunkListReferenceVc\">ChunkListReferenceVc</a>"]]
+"turbopack_core":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
