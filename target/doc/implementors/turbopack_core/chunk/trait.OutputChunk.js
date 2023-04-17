e64ba214e3ea2e3fd29efc50f14499e111cb12f4
sokra <sokra@users.noreply.github.com>
2023-04-17 11:52:07 +0000

    Update docs (ab676c5388fc431dd3d7612aa00312bcf9ebe53c)

---
+++ target/doc/implementors/turbopack_core/chunk/trait.OutputChunk.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"turbopack_core":[],
+"turbopack_css":[["impl OutputChunk for <a class=\"struct\" href=\"turbopack_css/chunk/struct.CssChunkVc.html\" title=\"struct turbopack_css::chunk::CssChunkVc\">CssChunkVc</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
