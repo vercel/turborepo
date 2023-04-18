35e8584155c3b3fd6c39452dde088542369395d8
Chris Olszewski <chris.olszewski@vercel.com>
2023-04-18 08:41:10 -0700

    chore: delete unused npm lockfile impl in go (#4605)
    ### Description
    
    Now that we've been shipping Go code that calls Rust for a bit and the
    Rust lockfile implementation hasn't seen any major issues, I feel
    comfortable deleting the unused Go NPM lockfile code. If issues to crop
    up, this code is just a `git revert` away.
    
    ### Testing Instructions
    
    Rust unit tests and integration tests.

--- cli/internal/lockfile/lockfile.go
+++ cli/internal/lockfile/lockfile.go
@@ -61,6 +61,19 @@ func (p ByKey) Less(i, j int) bool {
 
 var _ (sort.Interface) = (*ByKey)(nil)
 
+// TransitiveClosure the set of all lockfile keys that pkg depends on
+func TransitiveClosure(
+	workspaceDir turbopath.AnchoredUnixPath,
+	unresolvedDeps map[string]string,
+	lockFile Lockfile,
+) (mapset.Set, error) {
+	if lf, ok := lockFile.(*NpmLockfile); ok {
+		// We special case as Rust implementations have their own dep crawl
+		return npmTransitiveDeps(lf, workspaceDir, unresolvedDeps)
+	}
+	return transitiveClosure(workspaceDir, unresolvedDeps, lockFile)
+}
+
 func transitiveClosure(
 	workspaceDir turbopath.AnchoredUnixPath,
 	unresolvedDeps map[string]string,
