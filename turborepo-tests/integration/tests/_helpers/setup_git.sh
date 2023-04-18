8e1fc0a43aa7734a6f5f5c6cf5e38e377ca8fe1b
Mehul Kar <mehul.kar@vercel.com>
2023-04-18 11:43:16 -0700

    fix more paths

--- turborepo-tests/integration/tests/_helpers/setup_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
-TURBOREPO_TESTS_DIR="$SCRIPT_DIR/../../.."
-
-REAL_SCRIPT="${TURBOREPO_TESTS_DIR}/helpers/setup_git.sh"
-. "${REAL_SCRIPT}"
--- turborepo-tests/integration/tests/inference/no_workspaces_setup.sh
+++ turborepo-tests/integration/tests/inference/no_workspaces_setup.sh
@@ -2,8 +2,10 @@
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 TARGET_DIR=$1
+TURBOREPO_TESTS_DIR="$SCRIPT_DIR/../../.."
+SETUP_GIT_SCRIPT="${TURBOREPO_TESTS_DIR}/helpers/setup_git.sh"
 
 cp -a ${SCRIPT_DIR}/../_fixtures/inference/no_workspaces/. ${TARGET_DIR}/
-${SCRIPT_DIR}/../_helpers/setup_git.sh ${TARGET_DIR}
-${SCRIPT_DIR}/../_helpers/setup_git.sh ${TARGET_DIR}/parent
-${SCRIPT_DIR}/../_helpers/setup_git.sh ${TARGET_DIR}/parent/child
+${SETUP_GIT_SCRIPT} ${TARGET_DIR}
+${SETUP_GIT_SCRIPT} ${TARGET_DIR}/parent
+${SETUP_GIT_SCRIPT} ${TARGET_DIR}/parent/child
