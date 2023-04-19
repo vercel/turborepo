bbb06c919a72d8ecbc8b25c2992bf005a3b84fb5
sokra <sokra@users.noreply.github.com>
2023-04-19 16:42:26 +0000

    Update docs (776e9d2e4c2525d4017007bbf18499d0f1457b6f)

--- docs/docs.rs/implementors/turbopack_core/chunk/chunking_context/trait.ChunkingContext.js
+++ docs/docs.rs/implementors/turbopack_core/chunk/chunking_context/trait.ChunkingContext.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "turbopack_core":[],
-"turbopack_dev":[["impl ChunkingContext for <a class=\"struct\" href=\"turbopack_dev/struct.DevChunkingContextVc.html\" title=\"struct turbopack_dev::DevChunkingContextVc\">DevChunkingContextVc</a>"]]
+"turbopack_dev":[["impl <a class=\"trait\" href=\"turbopack_core/chunk/chunking_context/trait.ChunkingContext.html\" title=\"trait turbopack_core::chunk::chunking_context::ChunkingContext\">ChunkingContext</a> for <a class=\"struct\" href=\"turbopack_dev/struct.DevChunkingContextVc.html\" title=\"struct turbopack_dev::DevChunkingContextVc\">DevChunkingContextVc</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/docs.rs/turbopack_core/plugin/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["CustomModuleTypeVc"],"trait":["CustomModuleType"],"type":["CustomModuleTypeTraitRef"]};
