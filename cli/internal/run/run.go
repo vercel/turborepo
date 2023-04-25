27b654b7c1e1462f4d5023e03f42bbcfafb5e758
Nicholas Yang <nicholas.yang@vercel.com>
2023-04-25 15:26:43 -0400

    port(turborepo): Config (#4520)
    ### Description
    
    Creates an `ExecutionState` struct that gets serialized to the Go code.
    The goal is to build up more and more of the info, such as config and
    graph state in this struct, so that we can do more work on the Rust
    side.
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->

--- cli/internal/run/run.go
+++ cli/internal/run/run.go
@@ -32,18 +32,18 @@ import (
 )
 
 // ExecuteRun executes the run command
-func ExecuteRun(ctx gocontext.Context, helper *cmdutil.Helper, signalWatcher *signals.Watcher, args *turbostate.ParsedArgsFromRust) error {
-	base, err := helper.GetCmdBase(args)
+func ExecuteRun(ctx gocontext.Context, helper *cmdutil.Helper, signalWatcher *signals.Watcher, executionState *turbostate.ExecutionState) error {
+	base, err := helper.GetCmdBase(executionState)
 	LogTag(base.Logger)
 	if err != nil {
 		return err
 	}
-	tasks := args.Command.Run.Tasks
-	passThroughArgs := args.Command.Run.PassThroughArgs
+	tasks := executionState.CLIArgs.Command.Run.Tasks
+	passThroughArgs := executionState.CLIArgs.Command.Run.PassThroughArgs
 	if len(tasks) == 0 {
 		return errors.New("at least one task must be specified")
 	}
-	opts, err := optsFromArgs(args)
+	opts, err := optsFromArgs(&executionState.CLIArgs)
 	if err != nil {
 		return err
 	}
@@ -66,8 +66,6 @@ func optsFromArgs(args *turbostate.ParsedArgsFromRust) (*Opts, error) {
 		return nil, err
 	}
 
-	// Cache flags
-	opts.clientOpts.Timeout = args.RemoteCacheTimeout
 	opts.cacheOpts.SkipFilesystem = runPayload.RemoteOnly
 	opts.cacheOpts.OverrideDir = runPayload.CacheDir
 	opts.cacheOpts.Workers = runPayload.CacheWorkers
