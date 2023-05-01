dd68a02dee749879825e3db06e1a2ef3190f9b1d
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-05-01 17:26:36 +0200

    Tracing layer

--- crates/turbo-binding/src/lib.rs
+++ crates/turbo-binding/src/lib.rs
@@ -100,8 +100,8 @@ pub mod features {
     pub use mdxjs;
     #[cfg(feature = "__feature_node_file_trace")]
     pub use node_file_trace;
-    #[cfg(feature = "__feature_signposter")]
-    pub use signposter;
     #[cfg(feature = "__feature_swc_ast_explorer")]
     pub use swc_ast_explorer;
+    #[cfg(feature = "__feature_tracing_signpost")]
+    pub use tracing_signpost;
 }
