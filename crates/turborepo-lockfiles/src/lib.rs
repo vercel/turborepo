87eeb704c6e528b3d62419f625eddfab5699bde7
Chris Olszewski <chris.olszewski@vercel.com>
2023-05-02 11:01:20 -0700

    add dep crawl benchmarks

--- crates/turborepo-lockfiles/src/lib.rs
+++ crates/turborepo-lockfiles/src/lib.rs
@@ -1,4 +1,6 @@
 #![feature(once_cell)]
+#![feature(test)]
+extern crate test;
 
 mod berry;
 mod error;
@@ -91,4 +93,155 @@ fn transitive_closure_helper<L: Lockfile>(
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+
+    use super::*;
+
+    fn make_workspaces(w: &[(&str, &[(&str, &str)])]) -> HashMap<String, HashMap<String, String>> {
+        w.iter()
+            .map(|(workspace, deps)| {
+                (
+                    workspace.to_string(),
+                    deps.iter()
+                        .map(|(k, v)| (k.to_string(), v.to_string()))
+                        .collect(),
+                )
+            })
+            .collect()
+    }
+
+    #[bench]
+    fn bench_npm_dependency_crawl(b: &mut Bencher) {
+        b.iter(|| {
+            let lockfile = NpmLockfile::load(include_bytes!("../fixtures/npm-lock.json")).unwrap();
+            all_transitive_closures(
+                &lockfile,
+                make_workspaces(&[
+                    ("", &[("prettier", "latest"), ("turbo", "latest")]),
+                    (
+                        "apps/docs",
+                        &[
+                            ("lodash", "^3.0.0"),
+                            ("next", "12.3.0"),
+                            ("react", "18.2.0"),
+                            ("react-dom", "18.2.0"),
+                            ("@babel/core", "^7.0.0"),
+                            ("@types/node", "^17.0.12"),
+                            ("@types/react", "18.0.17"),
+                            ("eslint", "7.32.0"),
+                            ("next-transpile-modules", "9.0.0"),
+                            ("typescript", "^4.5.3"),
+                        ],
+                    ),
+                    (
+                        "apps/web",
+                        &[
+                            ("lodash", "^4.17.21"),
+                            ("next", "12.3.0"),
+                            ("react", "18.2.0"),
+                            ("react-dom", "18.2.0"),
+                            ("@babel/core", "^7.0.0"),
+                            ("@types/node", "^17.0.12"),
+                            ("@types/react", "18.0.17"),
+                            ("eslint", "7.32.0"),
+                            ("next-transpile-modules", "9.0.0"),
+                            ("typescript", "^4.5.3"),
+                        ],
+                    ),
+                    (
+                        "packages/eslint-config-custom",
+                        &[
+                            ("eslint", "^7.23.0"),
+                            ("eslint-config-next", "^12.0.8"),
+                            ("eslint-config-prettier", "^8.3.0"),
+                            ("eslint-config-turbo", "latest"),
+                            ("eslint-plugin-react", "7.31.8"),
+                            ("typescript", "^4.7.4"),
+                        ],
+                    ),
+                    ("packages/tsconfig", &[]),
+                    (
+                        "packages/ui",
+                        &[
+                            ("@types/react", "^17.0.37"),
+                            ("@types/react-dom", "^17.0.11"),
+                            ("eslint", "^7.32.0"),
+                            ("react", "^18.2.0"),
+                            ("typescript", "^4.5.2"),
+                        ],
+                    ),
+                ]),
+            )
+        })
+    }
+
+    #[bench]
+    fn bench_berry_dependency_crawl(b: &mut Bencher) {
+        b.iter(|| {
+            let data = LockfileData::from_bytes(include_bytes!("../fixtures/berry.lock")).unwrap();
+            let lockfile = BerryLockfile::new(&data, None).unwrap();
+            all_transitive_closures(
+                &lockfile,
+                make_workspaces(&[
+                    ("", &[("prettier", "latest"), ("turbo", "latest")]),
+                    (
+                        "apps/docs",
+                        &[
+                            ("lodash", "^3.0.0"),
+                            ("next", "12.3.0"),
+                            ("react", "18.2.0"),
+                            ("react-dom", "18.2.0"),
+                            ("@babel/core", "^7.0.0"),
+                            ("@types/node", "^17.0.12"),
+                            ("@types/react", "18.0.17"),
+                            ("eslint", "7.32.0"),
+                            ("next-transpile-modules", "9.0.0"),
+                            ("typescript", "^4.5.3"),
+                        ],
+                    ),
+                    (
+                        "apps/web",
+                        &[
+                            ("lodash", "^4.17.21"),
+                            ("next", "12.3.0"),
+                            ("react", "18.2.0"),
+                            ("react-dom", "18.2.0"),
+                            ("@babel/core", "^7.0.0"),
+                            ("@types/node", "^17.0.12"),
+                            ("@types/react", "18.0.17"),
+                            ("eslint", "7.32.0"),
+                            ("next-transpile-modules", "9.0.0"),
+                            ("typescript", "^4.5.3"),
+                        ],
+                    ),
+                    (
+                        "packages/eslint-config-custom",
+                        &[
+                            ("eslint", "^7.23.0"),
+                            ("eslint-config-next", "^12.0.8"),
+                            ("eslint-config-prettier", "^8.3.0"),
+                            ("eslint-config-turbo", "latest"),
+                            ("eslint-plugin-react", "7.31.8"),
+                            ("typescript", "^4.7.4"),
+                        ],
+                    ),
+                    ("packages/tsconfig", &[]),
+                    (
+                        "packages/ui",
+                        &[
+                            ("@types/react", "^17.0.37"),
+                            ("@types/react-dom", "^17.0.11"),
+                            ("eslint", "^7.32.0"),
+                            ("react", "^18.2.0"),
+                            ("typescript", "^4.5.2"),
+                        ],
+                    ),
+                ]),
+            )
+        })
+    }
 }
