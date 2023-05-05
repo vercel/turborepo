5a22b609d470c69255828f9b7d040f132012e463
Anthony Shew <anthony.shew@vercel.com>
2023-05-05 13:43:29 -0700

    Fix some slop.

--- examples/basic/packages/ui/Button.tsx
+++ examples/basic/packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => console.log("boop")}>Boop</button>;
+  return <button onClick={() => alert("boop")}>Boop</button>;
 };
