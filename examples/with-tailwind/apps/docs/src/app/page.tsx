0ff46c90df823c88da42014dafea19f3852d185b
Anthony Shew <anthony.shew@vercel.com>
2023-05-09 06:36:43 -0700

    Update with-tailwind with App Router. (#4849)

--- examples/with-tailwind/apps/docs/src/app/page.tsx
+++ examples/with-tailwind/apps/docs/src/app/page.tsx
@@ -1,13 +1,14 @@
+import { Metadata } from "next";
 import Head from "next/head";
 import { Button } from "ui";
+
+export const metadata: Metadata = {
+  title: "Docs - Turborepo Example",
+};
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Docs - Turborepo Example</title>
-      </Head>
-
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           Docs
