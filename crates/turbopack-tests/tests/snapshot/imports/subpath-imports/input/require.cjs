138d03a2ee989e5b2862938651e041402fd9374c
Justin Ridgewell <justin@ridgewell.name>
2023-05-10 20:29:14 -0400

    Fix CJS require

--- crates/turbopack-tests/tests/snapshot/imports/subpath-imports/input/require.cjs
+++ crates/turbopack-tests/tests/snapshot/imports/subpath-imports/input/require.cjs
@@ -1,1 +1,1 @@
-export default "require";
+module.exports = "require";
--- crates/turbopack-tests/tests/snapshot/imports/subpath-imports/output/crates_turbopack-tests_tests_snapshot_imports_subpath-imports_input_index_b53fce.js
+++ crates/turbopack-tests/tests/snapshot/imports/subpath-imports/output/crates_turbopack-tests_tests_snapshot_imports_subpath-imports_input_index_b53fce.js
@@ -1,13 +1,10 @@
 (globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/crates_turbopack-tests_tests_snapshot_imports_subpath-imports_input_index_b53fce.js", {
 
-"[project]/crates/turbopack-tests/tests/snapshot/imports/subpath-imports/input/require.cjs (ecmascript)": (({ r: __turbopack_require__, x: __turbopack_external_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_cjs__, k: __turbopack_refresh__, g: global, __dirname }) => (() => {
+"[project]/crates/turbopack-tests/tests/snapshot/imports/subpath-imports/input/require.cjs (ecmascript)": (function({ r: __turbopack_require__, x: __turbopack_external_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_cjs__, k: __turbopack_refresh__, g: global, __dirname, m: module, e: exports }) { !function() {
 
-__turbopack_esm__({
-    "default": ()=>__TURBOPACK__default__export__
-});
-const __TURBOPACK__default__export__ = "require";
+module.exports = "require";
 
-})()),
+}.call(this) }),
 "[project]/crates/turbopack-tests/tests/snapshot/imports/subpath-imports/input/import.mjs (ecmascript)": (({ r: __turbopack_require__, x: __turbopack_external_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_cjs__, k: __turbopack_refresh__, g: global, __dirname }) => (() => {
 
 __turbopack_esm__({
