919ade6bd9bda74422d7ecdc271efff3f49a654b
Anthony Shew <anthony.shew@vercel.com>
2023-05-12 14:20:34 -0700

    Restoring old tsup config.

--- examples/kitchen-sink/packages/ui/tsup.config.ts
+++ examples/kitchen-sink/packages/ui/tsup.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entry: {
-    ".": "src/index.tsx",
-  },
   banner: {
     js: "'use client'",
   },
-  format: ["cjs", "esm"],
-  external: ["react"],
-  dts: true,
   ...options,
 }));
