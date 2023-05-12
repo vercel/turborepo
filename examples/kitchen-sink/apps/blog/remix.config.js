598fbdb44e0ac0006fbf5c2ae3afad6f1c94288b
Anthony Shew <anthony.shew@vercel.com>
2023-05-12 14:08:48 -0700

    Maybe it needs transpiled?

--- examples/kitchen-sink/apps/blog/remix.config.js
+++ examples/kitchen-sink/apps/blog/remix.config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  transpileModules: ["ui"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
