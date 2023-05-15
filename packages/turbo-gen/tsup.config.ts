00f75d6e2d0055b21f327c9221e82738029c8aee
tknickman <tom.knickman@vercel.com>
2023-05-15 13:35:02 -0400

    feat(turbo-gen): support TS config files

--- packages/turbo-gen/tsup.config.ts
+++ packages/turbo-gen/tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entry: ["src/cli.ts"],
+  entry: ["src/cli.ts", "src/types.ts"],
   format: ["cjs"],
+  dts: true,
   clean: true,
   minify: true,
   ...options,
