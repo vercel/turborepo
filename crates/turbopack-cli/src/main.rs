a642433e8705bdbd8ffe56f3982aaca8bd383451
Tobias Koppers <tobias.koppers@googlemail.com>
2023-05-15 15:47:13 +0000

    add default trace

--- crates/turbopack-cli/src/main.rs
+++ crates/turbopack-cli/src/main.rs
@@ -1,7 +1,10 @@
 #![feature(future_join)]
 #![feature(min_specialization)]
 
-use std::sync::{Arc, Mutex};
+use std::{
+    borrow::Cow,
+    sync::{Arc, Mutex},
+};
 
 use anyhow::{Context, Result};
 use clap::Parser;
@@ -39,7 +42,24 @@ fn main() {
 
     let subscriber = Registry::default();
 
-    let subscriber = subscriber.with(EnvFilter::from_default_env());
+    let subscriber = subscriber.with(
+        EnvFilter::builder()
+            .parse(std::env::var("TURBOPACK_TRACE").map_or_else(
+                |_| {
+                    Cow::Borrowed(
+                        "turbopack=info,turbopack_core=info,turbopack_ecmascript=info,\
+                         turbopack_css=info,turbopack_dev=info,turbopack_iamge=info,\
+                         turbopack_json=info,turbopack_mdx=info,turbopack_node=info,\
+                         turbopack_static=info,turbopack_dev_server=info,turbopack_cli_utils=info,\
+                         turbopack_cli=info,turbopack_ecmascript=info,turbo_tasks=info,\
+                         turbo_tasks_fs=info,turbo_tasks_bytes=info,turbo_tasks_env=info,\
+                         turbo_tasks_fetch=info,turbo_tasks_hash=info",
+                    )
+                },
+                |s| Cow::Owned(s),
+            ))
+            .unwrap(),
+    );
 
     std::fs::create_dir_all("./.turbopack")
         .context("Unable to create .turbopack directory")
