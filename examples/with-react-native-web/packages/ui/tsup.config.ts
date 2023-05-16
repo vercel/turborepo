293c0072a2692674730b484c783d61bc130815cd
Anthony Shew <anthony.shew@vercel.com>
2023-05-16 12:22:06 -0700

    Aha, bad entrypoint name.

--- examples/with-react-native-web/packages/ui/tsup.config.ts
+++ examples/with-react-native-web/packages/ui/tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
   entry: {
-    ".": "src/index.tsx",
+    index: "src/index.tsx",
   },
   banner: {
     js: "'use client'",
   },
+  clean: true,
   format: ["cjs", "esm"],
   external: ["react"],
   dts: true,
