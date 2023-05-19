0b37c8a27be16f206efca8a212c34d1feb455f06
tknickman <tom.knickman@vercel.com>
2023-05-19 10:55:27 -0400

    feat(turbo/gen): improved onboarding

--- packages/turbo-gen/src/generators/custom.ts
+++ packages/turbo-gen/src/generators/custom.ts
@@ -11,16 +11,18 @@ export async function generate({
   project,
   opts,
 }: CustomGeneratorArguments) {
+  let isOnboarding = false;
   let generators = getCustomGenerators({ project, configPath: opts.config });
   if (!generators.length) {
-    logger.error(`No custom generators found.`);
+    logger.error(`No generators found.`);
     console.log();
 
     const { answer } = await prompts.confirm({
-      message: `Would you like to add generators to ${project.name}?`,
+      message: `Would you like to add an example generator to ${project.name}?`,
     });
 
     if (answer) {
+      isOnboarding = true;
       const { answer: template } = await prompts.chooseGeneratorTemplate();
       try {
         await setupFromTemplate({ project, template });
@@ -32,9 +34,25 @@ export async function generate({
         throw err;
       }
 
+      // make it obvious that we're done creating a generator, and now we're running it
+      console.log();
+      logger.info(`Example generator config successfully created!`);
+      logger.info(`Loading generator config...`);
+      console.log();
+
       // fetch generators again, and continue to selection prompt
       generators = getCustomGenerators({ project, configPath: opts.config });
+
+      // something went wrong and we weren't able to find our new demo generator
+      if (!generators.length) {
+        logger.error(`Error loading generator.`);
+        return;
+      }
     } else {
+      console.log();
+      logger.dimmed(
+        "Learn more about generators: https://turbo.hotwire.dev/reference/generators"
+      );
       return;
     }
   }
@@ -57,7 +75,7 @@ export async function generate({
     }
 
     // capture any other errors and throw as GeneratorErrors
-    let message = "Failed to run custom generator";
+    let message = "Failed to run generator";
     if (err instanceof Error) {
       message = err.message;
     }
@@ -65,6 +83,14 @@ export async function generate({
     throw new GeneratorError(message, {
       type: "plop_error_running_generator",
     });
+  } finally {
+    if (isOnboarding) {
+      console.log();
+      logger.info(`Congrats! You just ran your first Turborepo generator`);
+      logger.dimmed(
+        "Learn more about Turborepo generators at https://turbo.hotwire.dev/reference/generators"
+      );
+    }
   }
 
   console.log();
