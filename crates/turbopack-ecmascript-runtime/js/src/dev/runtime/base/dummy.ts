bde928e63d012b923a61da13f8687672f092053d
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-05-25 17:49:13 +0200

    Add dummy commonJsRequireContext definition for the base runtime

--- crates/turbopack-ecmascript-runtime/js/src/dev/runtime/base/dummy.ts
+++ crates/turbopack-ecmascript-runtime/js/src/dev/runtime/base/dummy.ts
@@ -13,3 +13,4 @@ declare var _eval: (code: EcmascriptModuleEntry) => any;
 declare var augmentContext: (
   context: TurbopackDevBaseContext
 ) => TurbopackDevContext;
+declare var commonJsRequireContext: CommonJsRequireContext;
