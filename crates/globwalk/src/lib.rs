winning1120xx/turbo

add boilerplate for globwalk crate and ffi

crates/globwalk/src/lib.rs
@@ -1,0 +1,31 @@
+use std::fmt::Display;
+
+use turborepo_paths::AbsolutePathBuf;
+
+pub enum WalkType {
+    Files,
+    Folders,
+    All,
+}
+
+#[derive(Debug)]
+pub enum WalkError {}
+
+impl Display for WalkError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl std::error::Error for WalkError {}
+
+pub fn globwalk(
+    basePath: AbsolutePathBuf,
+    include: &[String],
+    exclude: &[String],
+    walk_type: WalkType,
+) -> Result<Vec<AbsolutePathBuf>, WalkError> {
+    let paths = Vec::new();
+
+    Ok(paths)
+}

vercel/turborepo

crates/globwalk/src/lib.rs
@@ -1,0 +1,31 @@
+use std::fmt::Display;
+
+use turbopath::AbsoluteSystemPathBuf;
+
+pub enum WalkType {
+    Files,
+    Folders,
+    All,
+}
+
+#[derive(Debug)]
+pub enum WalkError {}
+
+impl Display for WalkError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl std::error::Error for WalkError {}
+
+pub fn globwalk(
+    basePath: AbsoluteSystemPathBuf,
+    include: &[String],
+    exclude: &[String],
+    walk_type: WalkType,
+) -> Result<Vec<AbsoluteSystemPathBuf>, WalkError> {
+    let paths = Vec::new();
+
+    Ok(paths)
+}