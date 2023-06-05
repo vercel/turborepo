d89617850aedd082cbd0cf5fc3f0db18af5bdd61
hrmny <github.leah@hrmny.sh>
2023-06-05 23:31:38 +0200

    fix fmt

--- crates/turbopack-node/src/debug.rs
+++ crates/turbopack-node/src/debug.rs
@@ -4,8 +4,8 @@ const DEBUG_JS_VAR: &str = "TURBOPACK_DEBUG_JS";
 
 pub fn should_debug(operation: &str) -> bool {
     let Ok(val) = env::var(DEBUG_JS_VAR) else {
-		return false;
-	};
+        return false;
+    };
 
     val == "*" || val.split(",").any(|part| part == operation)
 }
