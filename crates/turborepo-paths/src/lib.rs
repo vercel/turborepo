6fc035a10ab8f1d189ce588c98f13cb2f413ef18
Greg Soltis <Greg Soltis>
2023-06-26 09:43:36 -0700

    Drop commented-out code

--- crates/turborepo-paths/src/lib.rs
+++ crates/turborepo-paths/src/lib.rs
@@ -78,30 +78,9 @@ impl PathError {
     }
 }
 
-// pub trait IntoSystem {
-//     fn into_system(self) -> Utf8PathBuf;
-// }
-
 pub trait IntoUnix {
     fn into_unix(self) -> Utf8PathBuf;
 }
-
-// // Checks if path contains a non system separator.
-// fn is_not_system(path: impl AsRef<str>) -> bool {
-//     let non_system_separator;
-
-//     #[cfg(windows)]
-//     {
-//         non_system_separator = '/';
-//     }
-
-//     #[cfg(not(windows))]
-//     {
-//         non_system_separator = '\\';
-//     }
-
-//     path.as_ref().contains(non_system_separator)
-// }
 
 #[cfg(windows)]
 fn convert_separator(
@@ -123,20 +102,6 @@ fn convert_separator(
             .collect::<String>(),
     )
 }
-
-// impl<T: AsRef<str>> IntoSystem for T {
-//     fn into_system(self) -> Utf8PathBuf {
-//         #[cfg(windows)]
-//         {
-//             convert_separator(self, '/', std::path::MAIN_SEPARATOR)
-//         }
-
-//         #[cfg(not(windows))]
-//         {
-//             Utf8PathBuf::from(self.as_ref())
-//         }
-//     }
-// }
 
 impl<T: AsRef<str>> IntoUnix for T {
     /// NOTE: `into_unix` *only* converts Windows paths to Unix paths *on* a
