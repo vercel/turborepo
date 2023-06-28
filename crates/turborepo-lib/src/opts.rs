774694c9d0967795ed8482e1189e5567b6dd747b
Greg Soltis <Greg Soltis>
2023-06-28 15:00:34 -0700

    Cleanup

--- crates/turborepo-lib/src/opts.rs
+++ crates/turborepo-lib/src/opts.rs
@@ -167,7 +167,7 @@ impl<'a> TryFrom<&'a RunArgs> for ScopeOpts {
         let pkg_inference_root = args
             .pkg_inference_root
             .as_ref()
-            .map(|root| AnchoredSystemPathBuf::from_raw::<&str>(root.as_ref()))
+            .map(|root| AnchoredSystemPathBuf::from_raw(root))
             .transpose()?;
         Ok(Self { pkg_inference_root })
     }
