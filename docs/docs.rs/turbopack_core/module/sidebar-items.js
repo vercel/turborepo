92708a90e25575c8348c84ca8434f0e9598c7021
sokra <sokra@users.noreply.github.com>
2023-07-10 11:16:42 +0000

    Update docs (b910aac1d669f356a96b963e32fbae9de777487e)

---
+++ docs/docs.rs/turbopack_core/module/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ModuleVc","OptionModule","OptionModuleVc"],"trait":["Module"],"type":["ModuleTraitRef","OptionModuleReadRef"]};
---
+++ target/doc/turbopack_core/raw_module/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["RawModule","RawModuleVc"],"type":["RawModuleReadRef"]};
