dd59487a6571736035aeb0500eb012a3e6163e65
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-07-11 21:10:14 +0200

    Add base path to dev html asset

--- crates/turbopack-cli/src/dev/web_entry_source.rs
+++ crates/turbopack-cli/src/dev/web_entry_source.rs
@@ -305,7 +305,8 @@ pub async fn create_web_entry_source(
         .try_join()
         .await?;
 
-    let entry_asset = DevHtmlAssetVc::new(server_root.join("index.html"), entries).into();
+    let entry_asset =
+        DevHtmlAssetVc::new(server_root.join("index.html"), Default::default(), entries).into();
 
     let graph = if eager_compile {
         AssetGraphContentSourceVc::new_eager(server_root, Default::default(), entry_asset)
