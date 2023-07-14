33549ce425af4a3fa1b8905b5786cc3b0ce473d2
Chris Olszewski <Chris Olszewski>
2023-07-14 12:49:26 -0700

    remove go prune impl

--- cli/internal/lockfile/lockfile.go
+++ cli/internal/lockfile/lockfile.go
@@ -3,7 +3,6 @@ package lockfile
 
 import (
 	"fmt"
-	"io"
 	"reflect"
 	"sort"
 
@@ -19,12 +18,6 @@ type Lockfile interface {
 	ResolvePackage(workspacePath turbopath.AnchoredUnixPath, name string, version string) (Package, error)
 	// AllDependencies Given a lockfile key return all (dev/optional/peer) dependencies of that package
 	AllDependencies(key string) (map[string]string, bool)
-	// Subgraph Given a list of lockfile keys returns a Lockfile based off the original one that only contains the packages given
-	Subgraph(workspacePackages []turbopath.AnchoredSystemPath, packages []string) (Lockfile, error)
-	// Encode encode the lockfile representation and write it to the given writer
-	Encode(w io.Writer) error
-	// Patches return a list of patches used in the lockfile
-	Patches() []turbopath.AnchoredUnixPath
 	// GlobalChange checks if there are any differences between lockfiles that would completely invalidate
 	// the cache.
 	GlobalChange(other Lockfile) bool
