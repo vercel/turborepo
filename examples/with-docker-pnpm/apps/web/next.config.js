0a91acdb95ae31702fe10dab8b21a81459a47c23
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-07-15 12:23:35 +0300

    simplify some configs

--- examples/with-docker-pnpm/apps/web/next.config.js
+++ examples/with-docker-pnpm/apps/web/next.config.js
@@ -1,11 +1,6 @@
-const path = require("path");
-
 module.exports = {
   reactStrictMode: true,
-  // transpilePackages: ["ui"],
-  output: "standalone",
   experimental: {
     appDir: true,
-    outputFileTracingRoot: path.join(__dirname, "../../"),
   },
 };
