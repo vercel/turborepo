735145000ff58b265629cfce6d6e0e2d9b576811
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-07-15 12:39:03 +0300

    Fix jest tests

--- examples/with-docker-pnpm/apps/api/src/__tests__/server.test.js
+++ examples/with-docker-pnpm/apps/api/src/__tests__/server.test.js
@@ -2,21 +2,25 @@ import supertest from "supertest";
 import { createServer } from "../server.js";
 
 describe("server", () => {
-  it("health check returns 200", async () => {
+  it("health check returns 200", async (done) => {
     await supertest(createServer())
       .get("/healthz")
       .expect(200)
       .then((res) => {
         expect(res.body.ok).toBe(true);
       });
+
+    done();
   });
 
-  it("message endpoint says hello", async () => {
+  it("message endpoint says hello", async (done) => {
     await supertest(createServer())
       .get("/message/jared")
       .expect(200)
       .then((res) => {
         expect(res.body).toEqual({ message: "hello jared" });
       });
+
+    done();
   });
 });
