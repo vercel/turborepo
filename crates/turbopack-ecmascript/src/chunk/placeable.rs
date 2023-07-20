sammyfilly/turbo

top-level-await support

crates/turbopack-ecmascript/src/chunk/placeable.rs
@@ -6,12 +6,15 @@ use turbopack_core::{
 };
 
 use super::{item::EcmascriptChunkItemVc, EcmascriptChunkingContextVc};
-use crate::references::esm::EsmExportsVc;
+use crate::references::{async_module::OptionAsyncModuleOptionsVc, esm::EsmExportsVc};
 
 #[turbo_tasks::value_trait]
 pub trait EcmascriptChunkPlaceable: ChunkableModule + Module + Asset {
     fn as_chunk_item(&self, context: EcmascriptChunkingContextVc) -> EcmascriptChunkItemVc;
     fn get_exports(&self) -> EcmascriptExportsVc;
+    fn get_async_module_options(&self) -> OptionAsyncModuleOptionsVc {
+        OptionAsyncModuleOptionsVc::cell(None)
+    }
 }
 
 #[turbo_tasks::value(transparent)]

crates/turbopack-ecmascript/src/lib.rs
@@ -88,7 +88,10 @@ use self::{
 use crate::{
     chunk::{EcmascriptChunkPlaceable, EcmascriptChunkPlaceableVc},
     code_gen::CodeGenerateable,
-    references::analyze_ecmascript_module,
+    references::{
+        analyze_ecmascript_module,
+        async_module::{OptionAsyncModuleOptionsReadRef, OptionAsyncModuleOptionsVc},
+    },
     transform::remove_shebang,
 };
 
@@ -136,7 +139,7 @@ struct MemoizedSuccessfulAnalysis {
     operation: RawVc,
     references: AssetReferencesReadRef,
     exports: EcmascriptExportsReadRef,
-    has_top_level_await: bool,
+    async_module_options: OptionAsyncModuleOptionsReadRef,
 }
 
 pub struct EcmascriptModuleAssetBuilder {
@@ -314,13 +317,13 @@ impl EcmascriptModuleAssetVc {
                     // We need to store the ReadRefs since we want to keep a snapshot.
                     references: result_value.references.await?,
                     exports: result_value.exports.await?,
-                    has_top_level_await: result_value.has_top_level_await,
+                    async_module_options: result_value.async_module_options.await?,
                 }));
         } else if let Some(MemoizedSuccessfulAnalysis {
             operation,
             references,
             exports,
-            has_top_level_await,
+            async_module_options,
         }) = &*this.last_successful_analysis.get()
         {
             // It's important to connect to the last operation here to keep it active, so
@@ -330,7 +333,7 @@ impl EcmascriptModuleAssetVc {
                 references: ReadRef::cell(references.clone()),
                 exports: ReadRef::cell(exports.clone()),
                 code_generation: result_value.code_generation,
-                has_top_level_await: *has_top_level_await,
+                async_module_options: ReadRef::cell(async_module_options.clone()),
                 successful: false,
             }
             .cell());
@@ -449,6 +452,13 @@ impl EcmascriptChunkPlaceable for EcmascriptModuleAsset {
     async fn get_exports(self_vc: EcmascriptModuleAssetVc) -> Result<EcmascriptExportsVc> {
         Ok(self_vc.failsafe_analyze().await?.exports)
     }
+
+    #[turbo_tasks::function]
+    async fn get_async_module_options(
+        self_vc: EcmascriptModuleAssetVc,
+    ) -> Result<OptionAsyncModuleOptionsVc> {
+        Ok(self_vc.failsafe_analyze().await?.async_module_options)
+    }
 }
 
 #[turbo_tasks::value_impl]
@@ -520,7 +530,13 @@ impl EcmascriptChunkItem for ModuleChunkItem {
     ) -> Result<EcmascriptChunkItemContentVc> {
         let this = self_vc.await?;
         let content = this.module.module_content(this.context, availability_info);
-        Ok(EcmascriptChunkItemContentVc::new(content, this.context))
+        let async_module_options = this.module.get_async_module_options();
+
+        Ok(EcmascriptChunkItemContentVc::new(
+            content,
+            this.context,
+            async_module_options,
+        ))
     }
 }
 

crates/turbopack-ecmascript/src/references/mod.rs
@@ -1,4 +1,5 @@
 pub mod amd;
+pub mod async_module;
 pub mod cjs;
 pub mod constant_condition;
 pub mod constant_value;
@@ -45,7 +46,7 @@ use swc_core::{
     },
 };
 use turbo_tasks::{
-    primitives::{BoolVc, RegexVc},
+    primitives::{BoolVc, RegexVc, StringVc},
     TryJoinIterExt, Value,
 };
 use turbo_tasks_fs::{FileJsonContent, FileSystemPathVc};
@@ -53,7 +54,7 @@ use turbopack_core::{
     asset::{Asset, AssetVc},
     compile_time_info::{CompileTimeInfoVc, FreeVarReference},
     error::PrettyPrintError,
-    issue::{IssueSourceVc, OptionIssueSourceVc},
+    issue::{analyze::AnalyzeIssue, IssueSeverity, IssueSourceVc, OptionIssueSourceVc},
     reference::{AssetReferenceVc, AssetReferencesVc, SourceMapReferenceVc},
     reference_type::{CommonJsReferenceSubType, ReferenceType},
     resolve::{
@@ -118,6 +119,7 @@ use crate::{
     },
     magic_identifier,
     references::{
+        async_module::{AsyncModule, OptionAsyncModuleOptionsVc},
         cjs::{
             CjsRequireAssetReferenceVc, CjsRequireCacheAccess, CjsRequireResolveAssetReferenceVc,
         },
@@ -136,7 +138,7 @@ pub struct AnalyzeEcmascriptModuleResult {
     pub references: AssetReferencesVc,
     pub code_generation: CodeGenerateablesVc,
     pub exports: EcmascriptExportsVc,
-    pub has_top_level_await: bool,
+    pub async_module_options: OptionAsyncModuleOptionsVc,
     /// `true` when the analysis was successful.
     pub successful: bool,
 }
@@ -173,7 +175,7 @@ pub(crate) struct AnalyzeEcmascriptModuleResultBuilder {
     references: IndexSet<AssetReferenceVc>,
     code_gens: Vec<CodeGen>,
     exports: EcmascriptExports,
-    has_top_level_await: bool,
+    async_module_options: OptionAsyncModuleOptionsVc,
     successful: bool,
 }
 
@@ -183,7 +185,7 @@ impl AnalyzeEcmascriptModuleResultBuilder {
             references: IndexSet::new(),
             code_gens: Vec::new(),
             exports: EcmascriptExports::None,
-            has_top_level_await: false,
+            async_module_options: OptionAsyncModuleOptionsVc::cell(None),
             successful: false,
         }
     }
@@ -222,9 +224,9 @@ impl AnalyzeEcmascriptModuleResultBuilder {
         self.exports = exports;
     }
 
-    /// Sets whether the analysed module has a top level await.
-    pub fn set_top_level_await(&mut self, top_level_await: bool) {
-        self.has_top_level_await = top_level_await;
+    /// Sets the analysis result ES export.
+    pub fn set_async_module_options(&mut self, options: OptionAsyncModuleOptionsVc) {
+        self.async_module_options = options;
     }
 
     /// Sets whether the analysis was successful.
@@ -254,7 +256,7 @@ impl AnalyzeEcmascriptModuleResultBuilder {
                 references: AssetReferencesVc::cell(references),
                 code_generation: CodeGenerateablesVc::cell(self.code_gens),
                 exports: self.exports.into(),
-                has_top_level_await: self.has_top_level_await,
+                async_module_options: self.async_module_options,
                 successful: self.successful,
             },
         ))
@@ -451,11 +453,6 @@ pub(crate) async fn analyze_ecmascript_module(
         }),
     );
 
-    let has_top_level_await =
-        set_handler_and_globals(&handler, globals, || has_top_level_await(program));
-
-    analysis.set_top_level_await(has_top_level_await);
-
     let mut var_graph =
         set_handler_and_globals(&handler, globals, || create_graph(program, eval_context));
 
@@ -569,6 +566,10 @@ pub(crate) async fn analyze_ecmascript_module(
         }
     }
 
+    let top_level_await_span =
+        set_handler_and_globals(&handler, globals, || has_top_level_await(program));
+    let has_top_level_await = top_level_await_span.is_some();
+
     let exports = if !esm_exports.is_empty() || !esm_star_exports.is_empty() {
         if matches!(specified_type, SpecifiedModuleType::CommonJs) {
             SpecifiedModuleTypeIssue {
@@ -580,15 +581,31 @@ pub(crate) async fn analyze_ecmascript_module(
             .emit();
         }
 
+        let async_module = AsyncModule {
+            references: import_references.iter().copied().collect(),
+            has_top_level_await,
+        }
+        .cell();
+        analysis.set_async_module_options(async_module.module_options());
+
         let esm_exports: EsmExportsVc = EsmExports {
             exports: esm_exports,
             star_exports: esm_star_exports,
         }
         .cell();
+
         analysis.add_code_gen(esm_exports);
+        analysis.add_code_gen(async_module);
 
         EcmascriptExports::EsmExports(esm_exports)
     } else if matches!(specified_type, SpecifiedModuleType::EcmaScript) {
+        let async_module = AsyncModule {
+            references: import_references.iter().copied().collect(),
+            has_top_level_await,
+        }
+        .cell();
+        analysis.set_async_module_options(async_module.module_options());
+
         match detect_dynamic_export(program) {
             DetectedDynamicExportType::CommonJs => {
                 SpecifiedModuleTypeIssue {
@@ -598,6 +615,9 @@ pub(crate) async fn analyze_ecmascript_module(
                 .cell()
                 .as_issue()
                 .emit();
+
+                analysis.add_code_gen(async_module);
+
                 EcmascriptExports::EsmExports(
                     EsmExports {
                         exports: Default::default(),
@@ -609,29 +629,62 @@ pub(crate) async fn analyze_ecmascript_module(
             DetectedDynamicExportType::Namespace => EcmascriptExports::DynamicNamespace,
             DetectedDynamicExportType::Value => EcmascriptExports::Value,
             DetectedDynamicExportType::UsingModuleDeclarations
-            | DetectedDynamicExportType::None => EcmascriptExports::EsmExports(
-                EsmExports {
-                    exports: Default::default(),
-                    star_exports: Default::default(),
-                }
-                .cell(),
-            ),
+            | DetectedDynamicExportType::None => {
+                analysis.add_code_gen(async_module);
+
+                EcmascriptExports::EsmExports(
+                    EsmExports {
+                        exports: Default::default(),
+                        star_exports: Default::default(),
+                    }
+                    .cell(),
+                )
+            }
         }
     } else {
         match detect_dynamic_export(program) {
             DetectedDynamicExportType::CommonJs => EcmascriptExports::CommonJs,
             DetectedDynamicExportType::Namespace => EcmascriptExports::DynamicNamespace,
             DetectedDynamicExportType::Value => EcmascriptExports::Value,
-            DetectedDynamicExportType::UsingModuleDeclarations => EcmascriptExports::EsmExports(
-                EsmExports {
-                    exports: Default::default(),
-                    star_exports: Default::default(),
+            DetectedDynamicExportType::UsingModuleDeclarations => {
+                let async_module = AsyncModule {
+                    references: import_references.iter().copied().collect(),
+                    has_top_level_await,
                 }
-                .cell(),
-            ),
+                .cell();
+                analysis.add_code_gen(async_module);
+                analysis.set_async_module_options(async_module.module_options());
+
+                EcmascriptExports::EsmExports(
+                    EsmExports {
+                        exports: Default::default(),
+                        star_exports: Default::default(),
+                    }
+                    .cell(),
+                )
+            }
             DetectedDynamicExportType::None => EcmascriptExports::None,
         }
     };
+
+    if let Some(span) = top_level_await_span {
+        if !matches!(exports, EcmascriptExports::EsmExports(_)) {
+            AnalyzeIssue {
+                code: None,
+                category: StringVc::cell("analyze".to_string()),
+                message: StringVc::cell(
+                    "top level await is only supported in ESM modules.".to_string(),
+                ),
+                source_ident: source.ident(),
+                severity: IssueSeverity::Error.into(),
+                source: Some(issue_source(source, span)),
+                title: StringVc::cell("unexpected top level await".to_string()),
+            }
+            .cell()
+            .as_issue()
+            .emit();
+        }
+    }
 
     analysis.set_exports(exports);
 

crates/turbopack-ecmascript/src/tree_shake/chunk_item.rs
@@ -11,7 +11,7 @@ use super::{asset::EcmascriptModulePartAssetVc, part_of_module, split_module};
 use crate::{
     chunk::{
         EcmascriptChunkItem, EcmascriptChunkItemContentVc, EcmascriptChunkItemVc,
-        EcmascriptChunkingContextVc,
+        EcmascriptChunkPlaceable, EcmascriptChunkingContextVc,
     },
     EcmascriptModuleContentVc,
 };
@@ -56,7 +56,13 @@ impl EcmascriptChunkItem for EcmascriptModulePartChunkItem {
             availability_info,
         );
 
-        Ok(EcmascriptChunkItemContentVc::new(content, this.context))
+        let async_module_options = module.full_module.get_async_module_options();
+
+        Ok(EcmascriptChunkItemContentVc::new(
+            content,
+            this.context,
+            async_module_options,
+        ))
     }
 
     #[turbo_tasks::function]

vercel/turborepo

crates/turbopack-ecmascript/src/chunk/placeable.rs
@@ -2,7 +2,7 @@ use turbo_tasks::Vc;
 use turbopack_core::{asset::Asset, chunk::ChunkableModule, module::Module};
 
 use super::{item::EcmascriptChunkItem, EcmascriptChunkingContext};
-use crate::references::esm::EsmExports;
+use crate::references::{async_module::OptionAsyncModuleOptions, esm::EsmExports};
 
 #[turbo_tasks::value_trait]
 pub trait EcmascriptChunkPlaceable: ChunkableModule + Module + Asset {
@@ -11,6 +11,9 @@ pub trait EcmascriptChunkPlaceable: ChunkableModule + Module + Asset {
         context: Vc<Box<dyn EcmascriptChunkingContext>>,
     ) -> Vc<Box<dyn EcmascriptChunkItem>>;
     fn get_exports(self: Vc<Self>) -> Vc<EcmascriptExports>;
+    fn get_async_module_options(&self) -> Vc<OptionAsyncModuleOptions> {
+        Vc::cell(None)
+    }
 }
 
 #[turbo_tasks::value(transparent)]

crates/turbopack-ecmascript/src/lib.rs
@@ -73,7 +73,10 @@ use self::{
     tree_shake::asset::EcmascriptModulePartAsset,
 };
 use crate::{
-    chunk::EcmascriptChunkPlaceable, references::analyze_ecmascript_module,
+    chunk::EcmascriptChunkPlaceable, references::{
+        analyze_ecmascript_module,
+        async_module::{OptionAsyncModuleOptions},
+    },
     transform::remove_shebang,
 };
 
@@ -121,7 +124,7 @@ struct MemoizedSuccessfulAnalysis {
     operation: RawVc,
     references: ReadRef<AssetReferences>,
     exports: ReadRef<EcmascriptExports>,
-    has_top_level_await: bool,
+    async_module_options: ReadRef<OptionAsyncModuleOptions>,
 }
 
 pub struct EcmascriptModuleAssetBuilder {
@@ -307,13 +310,13 @@ impl EcmascriptModuleAsset {
                     // We need to store the ReadRefs since we want to keep a snapshot.
                     references: result_value.references.await?,
                     exports: result_value.exports.await?,
-                    has_top_level_await: result_value.has_top_level_await,
+                    async_module_options: result_value.async_module_options.await?,
                 }));
         } else if let Some(MemoizedSuccessfulAnalysis {
             operation,
             references,
             exports,
-            has_top_level_await,
+            async_module_options,
         }) = &*this.last_successful_analysis.get()
         {
             // It's important to connect to the last operation here to keep it active, so
@@ -323,7 +326,7 @@ impl EcmascriptModuleAsset {
                 references: ReadRef::cell(references.clone()),
                 exports: ReadRef::cell(exports.clone()),
                 code_generation: result_value.code_generation,
-                has_top_level_await: *has_top_level_await,
+                async_module_options: ReadRef::cell(async_module_options.clone()),
                 successful: false,
             }
             .cell());
@@ -442,6 +445,13 @@ impl EcmascriptChunkPlaceable for EcmascriptModuleAsset {
     async fn get_exports(self: Vc<Self>) -> Result<Vc<EcmascriptExports>> {
         Ok(self.failsafe_analyze().await?.exports)
     }
+
+    #[turbo_tasks::function]
+    async fn get_async_module_options(
+        self_vc: EcmascriptModuleAssetVc,
+    ) -> Result<OptionAsyncModuleOptionsVc> {
+        Ok(self_vc.failsafe_analyze().await?.async_module_options)
+    }
 }
 
 #[turbo_tasks::value_impl]
@@ -511,7 +521,13 @@ impl EcmascriptChunkItem for ModuleChunkItem {
     ) -> Result<Vc<EcmascriptChunkItemContent>> {
         let this = self.await?;
         let content = this.module.module_content(this.context, availability_info);
-        Ok(EcmascriptChunkItemContent::new(content, this.context))
+        let async_module_options = this.module.get_async_module_options();
+
+        Ok(EcmascriptChunkItemContent::new(
+            content,
+            this.context,
+            async_module_options,
+        ))
     }
 }
 

crates/turbopack-ecmascript/src/references/mod.rs
@@ -1,4 +1,5 @@
 pub mod amd;
+pub mod async_module;
 pub mod cjs;
 pub mod constant_condition;
 pub mod constant_value;
@@ -49,7 +50,7 @@ use turbo_tasks_fs::{FileJsonContent, FileSystemPath};
 use turbopack_core::{
     compile_time_info::{CompileTimeInfo, FreeVarReference},
     error::PrettyPrintError,
-    issue::{IssueExt, IssueSource, OptionIssueSource},
+    issue::{analyze::AnalyzeIssue, IssueExt, IssueSource, OptionIssueSource},
     module::Module,
     reference::{AssetReference, AssetReferences, SourceMapReference},
     reference_type::{CommonJsReferenceSubType, ReferenceType},
@@ -114,6 +115,7 @@ use crate::{
     },
     magic_identifier,
     references::{
+        async_module::{AsyncModule, OptionAsyncModuleOptionsVc},
         cjs::{CjsRequireAssetReference, CjsRequireCacheAccess, CjsRequireResolveAssetReference},
         esm::{module_id::EsmModuleIdAssetReference, EsmBinding},
         require_context::{RequireContextAssetReference, RequireContextMap},
@@ -130,7 +132,7 @@ pub struct AnalyzeEcmascriptModuleResult {
     pub references: Vc<AssetReferences>,
     pub code_generation: Vc<CodeGenerateables>,
     pub exports: Vc<EcmascriptExports>,
-    pub has_top_level_await: bool,
+    pub async_module_options: Vc<OptionAsyncModuleOptions>,
     /// `true` when the analysis was successful.
     pub successful: bool,
 }
@@ -167,7 +169,7 @@ pub(crate) struct AnalyzeEcmascriptModuleResultBuilder {
     references: IndexSet<Vc<Box<dyn AssetReference>>>,
     code_gens: Vec<CodeGen>,
     exports: EcmascriptExports,
-    has_top_level_await: bool,
+    async_module_options: Vc<OptionAsyncModuleOptions>,
     successful: bool,
 }
 
@@ -177,7 +179,7 @@ impl AnalyzeEcmascriptModuleResultBuilder {
             references: IndexSet::new(),
             code_gens: Vec::new(),
             exports: EcmascriptExports::None,
-            has_top_level_await: false,
+            async_module_options: Vc::cell(None),
             successful: false,
         }
     }
@@ -216,9 +218,9 @@ impl AnalyzeEcmascriptModuleResultBuilder {
         self.exports = exports;
     }
 
-    /// Sets whether the analysed module has a top level await.
-    pub fn set_top_level_await(&mut self, top_level_await: bool) {
-        self.has_top_level_await = top_level_await;
+    /// Sets the analysis result ES export.
+    pub fn set_async_module_options(&mut self, options: OptionAsyncModuleOptionsVc) {
+        self.async_module_options = options;
     }
 
     /// Sets whether the analysis was successful.
@@ -248,7 +250,7 @@ impl AnalyzeEcmascriptModuleResultBuilder {
                 references: Vc::cell(references),
                 code_generation: Vc::cell(self.code_gens),
                 exports: self.exports.into(),
-                has_top_level_await: self.has_top_level_await,
+                async_module_options: self.async_module_options,
                 successful: self.successful,
             },
         ))
@@ -447,11 +449,6 @@ pub(crate) async fn analyze_ecmascript_module(
         }),
     );
 
-    let has_top_level_await =
-        set_handler_and_globals(&handler, globals, || has_top_level_await(program));
-
-    analysis.set_top_level_await(has_top_level_await);
-
     let mut var_graph =
         set_handler_and_globals(&handler, globals, || create_graph(program, eval_context));
 
@@ -565,6 +562,10 @@ pub(crate) async fn analyze_ecmascript_module(
         }
     }
 
+    let top_level_await_span =
+        set_handler_and_globals(&handler, globals, || has_top_level_await(program));
+    let has_top_level_await = top_level_await_span.is_some();
+
     let exports = if !esm_exports.is_empty() || !esm_star_exports.is_empty() {
         if matches!(specified_type, SpecifiedModuleType::CommonJs) {
             SpecifiedModuleTypeIssue {
@@ -575,15 +576,31 @@ pub(crate) async fn analyze_ecmascript_module(
             .emit();
         }
 
+        let async_module = AsyncModule {
+            references: import_references.iter().copied().collect(),
+            has_top_level_await,
+        }
+        .cell();
+        analysis.set_async_module_options(async_module.module_options());
+
         let esm_exports: Vc<EsmExports> = EsmExports {
             exports: esm_exports,
             star_exports: esm_star_exports,
         }
         .cell();
+
         analysis.add_code_gen(esm_exports);
+        analysis.add_code_gen(async_module);
 
         EcmascriptExports::EsmExports(esm_exports)
     } else if matches!(specified_type, SpecifiedModuleType::EcmaScript) {
+        let async_module = AsyncModule {
+            references: import_references.iter().copied().collect(),
+            has_top_level_await,
+        }
+        .cell();
+        analysis.set_async_module_options(async_module.module_options());
+
         match detect_dynamic_export(program) {
             DetectedDynamicExportType::CommonJs => {
                 SpecifiedModuleTypeIssue {
@@ -592,6 +609,9 @@ pub(crate) async fn analyze_ecmascript_module(
                 }
                 .cell()
                 .emit();
+
+                analysis.add_code_gen(async_module);
+
                 EcmascriptExports::EsmExports(
                     EsmExports {
                         exports: Default::default(),
@@ -603,29 +623,62 @@ pub(crate) async fn analyze_ecmascript_module(
             DetectedDynamicExportType::Namespace => EcmascriptExports::DynamicNamespace,
             DetectedDynamicExportType::Value => EcmascriptExports::Value,
             DetectedDynamicExportType::UsingModuleDeclarations
-            | DetectedDynamicExportType::None => EcmascriptExports::EsmExports(
-                EsmExports {
-                    exports: Default::default(),
-                    star_exports: Default::default(),
-                }
-                .cell(),
-            ),
+            | DetectedDynamicExportType::None => {
+                analysis.add_code_gen(async_module);
+
+                EcmascriptExports::EsmExports(
+                    EsmExports {
+                        exports: Default::default(),
+                        star_exports: Default::default(),
+                    }
+                    .cell(),
+                )
+            }
         }
     } else {
         match detect_dynamic_export(program) {
             DetectedDynamicExportType::CommonJs => EcmascriptExports::CommonJs,
             DetectedDynamicExportType::Namespace => EcmascriptExports::DynamicNamespace,
             DetectedDynamicExportType::Value => EcmascriptExports::Value,
-            DetectedDynamicExportType::UsingModuleDeclarations => EcmascriptExports::EsmExports(
-                EsmExports {
-                    exports: Default::default(),
-                    star_exports: Default::default(),
+            DetectedDynamicExportType::UsingModuleDeclarations => {
+                let async_module = AsyncModule {
+                    references: import_references.iter().copied().collect(),
+                    has_top_level_await,
                 }
-                .cell(),
-            ),
+                .cell();
+                analysis.add_code_gen(async_module);
+                analysis.set_async_module_options(async_module.module_options());
+
+                EcmascriptExports::EsmExports(
+                    EsmExports {
+                        exports: Default::default(),
+                        star_exports: Default::default(),
+                    }
+                    .cell(),
+                )
+            }
             DetectedDynamicExportType::None => EcmascriptExports::None,
         }
     };
+
+    if let Some(span) = top_level_await_span {
+        if !matches!(exports, EcmascriptExports::EsmExports(_)) {
+            AnalyzeIssue {
+                code: None,
+                category: StringVc::cell("analyze".to_string()),
+                message: StringVc::cell(
+                    "top level await is only supported in ESM modules.".to_string(),
+                ),
+                source_ident: source.ident(),
+                severity: IssueSeverity::Error.into(),
+                source: Some(issue_source(source, span)),
+                title: StringVc::cell("unexpected top level await".to_string()),
+            }
+            .cell()
+            .as_issue()
+            .emit();
+        }
+    }
 
     analysis.set_exports(exports);
 

crates/turbopack-ecmascript/src/tree_shake/chunk_item.rs
@@ -54,7 +54,13 @@ impl EcmascriptChunkItem for EcmascriptModulePartChunkItem {
             availability_info,
         );
 
-        Ok(EcmascriptChunkItemContent::new(content, this.context))
+        let async_module_options = module.full_module.get_async_module_options();
+
+        Ok(EcmascriptChunkItemContent::new(
+            content,
+            this.context,
+            async_module_options,
+        ))
     }
 
     #[turbo_tasks::function]