sammyfilly/turbo

shitty fix for import cycles

crates/turbopack-ecmascript/src/chunk/placeable.rs
@@ -6,14 +6,14 @@ use turbopack_core::{
 };
 
 use super::{item::EcmascriptChunkItemVc, EcmascriptChunkingContextVc};
-use crate::references::{async_module::OptionAsyncModuleOptionsVc, esm::EsmExportsVc};
+use crate::references::{async_module::OptionAsyncModuleVc, esm::EsmExportsVc};
 
 #[turbo_tasks::value_trait]
 pub trait EcmascriptChunkPlaceable: ChunkableModule + Module + Asset {
     fn as_chunk_item(&self, context: EcmascriptChunkingContextVc) -> EcmascriptChunkItemVc;
     fn get_exports(&self) -> EcmascriptExportsVc;
-    fn get_async_module_options(&self) -> OptionAsyncModuleOptionsVc {
-        OptionAsyncModuleOptionsVc::cell(None)
+    fn get_async_module(&self) -> OptionAsyncModuleVc {
+        OptionAsyncModuleVc::none()
     }
 }
 

vercel/turborepo

crates/turbopack-ecmascript/src/chunk/placeable.rs
@@ -2,7 +2,7 @@ use turbo_tasks::Vc;
 use turbopack_core::{asset::Asset, chunk::ChunkableModule, module::Module};
 
 use super::{item::EcmascriptChunkItem, EcmascriptChunkingContext};
-use crate::references::{async_module::OptionAsyncModuleOptions, esm::EsmExports};
+use crate::references::{async_module::OptionAsyncModule, esm::EsmExports};
 
 #[turbo_tasks::value_trait]
 pub trait EcmascriptChunkPlaceable: ChunkableModule + Module + Asset {
@@ -11,7 +11,7 @@ pub trait EcmascriptChunkPlaceable: ChunkableModule + Module + Asset {
         context: Vc<Box<dyn EcmascriptChunkingContext>>,
     ) -> Vc<Box<dyn EcmascriptChunkItem>>;
     fn get_exports(self: Vc<Self>) -> Vc<EcmascriptExports>;
-    fn get_async_module_options(&self) -> Vc<OptionAsyncModuleOptions> {
+    fn get_async_module_options(&self) -> Vc<OptionAsyncModule> {
         Vc::cell(None)
     }
 }