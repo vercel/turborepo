80ef14eaf8fded3cc84cbbacfc438c37c4682903
Will Binns-Smith <wbinnssmith@gmail.com>
2023-07-24 14:04:37 -0700

    fixup! Turbopack build: Implement minification with swc minify

--- crates/turbopack-ecmascript/src/parse.rs
+++ crates/turbopack-ecmascript/src/parse.rs
@@ -117,7 +117,7 @@ impl GenerateSourceMap for ParseResultSourceMap {
 /// A config to generate a source map which includes the source content of every
 /// source file. SWC doesn't inline sources content by default when generating a
 /// sourcemap, so we need to provide a custom config to do it.
-pub struct InlineSourcesContentConfig {}
+struct InlineSourcesContentConfig {}
 
 impl SourceMapGenConfig for InlineSourcesContentConfig {
     fn file_name_to_source(&self, f: &FileName) -> String {
