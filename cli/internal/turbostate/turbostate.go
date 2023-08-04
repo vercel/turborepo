46b62519b98ea41555633762efa7ce2b9d7222c5
Nathan Hammond <Nathan Hammond>
2023-08-04 23:44:41 +0800

    Spaces gets its own API client.

--- cli/internal/turbostate/turbostate.go
+++ cli/internal/turbostate/turbostate.go
@@ -92,9 +92,10 @@ type ParsedArgsFromRust struct {
 
 // ExecutionState is the entire state of a turbo execution that is passed from the Rust shim.
 type ExecutionState struct {
-	APIClientConfig APIClientConfig    `json:"api_client_config"`
-	PackageManager  string             `json:"package_manager"`
-	CLIArgs         ParsedArgsFromRust `json:"cli_args"`
+	APIClientConfig       APIClientConfig    `json:"api_client_config"`
+	SpacesAPIClientConfig APIClientConfig    `json:"spaces_api_client_config"`
+	PackageManager        string             `json:"package_manager"`
+	CLIArgs               ParsedArgsFromRust `json:"cli_args"`
 }
 
 // APIClientConfig holds the authentication and endpoint details for the API client
