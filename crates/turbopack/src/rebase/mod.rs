c79d43fa383ac8f326b06fad9656cded3233883a
Justin Ridgewell <justin@ridgewell.name>
2023-08-15 14:14:25 -0400

    Reuse iter in RebasedAsset

--- crates/turbopack/src/rebase/mod.rs
+++ crates/turbopack/src/rebase/mod.rs
@@ -8,7 +8,7 @@ use turbopack_core::{
     ident::AssetIdent,
     module::Module,
     output::{OutputAsset, OutputAssets},
-    reference::all_referenced_modules,
+    reference::all_modules_iter,
 };
 
 /// Converts a [Module] graph into an [OutputAsset] graph by placing it into a
@@ -50,14 +50,10 @@ impl OutputAsset for RebasedAsset {
 
     #[turbo_tasks::function]
     async fn references(&self) -> Result<Vc<OutputAssets>> {
-        let mut references = Vec::new();
-        for &module in all_referenced_modules(self.source).await?.iter() {
-            references.push(Vc::upcast(RebasedAsset::new(
-                module,
-                self.input_dir,
-                self.output_dir,
-            )));
-        }
+        let references = all_modules_iter([self.source].into_iter())
+            .await?
+            .map(|module| Vc::upcast(RebasedAsset::new(module, self.input_dir, self.output_dir)))
+            .collect();
         Ok(Vc::cell(references))
     }
 }
