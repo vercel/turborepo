3e7fe45d5190d9923e8ec0116a9b34529d316624
tknickman <tom.knickman@vercel.com>
2023-08-18 11:35:29 -0400

    remove redundant rules

--- packages/turbo-gen/.eslintrc.js
+++ packages/turbo-gen/.eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
   extends: ["@turbo/eslint-config/library"],
-  rules: {
-    "unicorn/filename-case": ["off"],
-    "@typescript-eslint/explicit-function-return-type": ["off"],
-  },
 };
