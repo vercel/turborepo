130e7bbf6b83f6afb1af01bcf2fec0cf49f046d2
Thomas Knickman <tom.knickman@vercel.com>
2023-08-21 11:24:38 -0400

    chore(types): consolidate packagejson type (#5754)

--- packages/turbo-gen/src/generators/empty.ts
+++ packages/turbo-gen/src/generators/empty.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import fs from "fs-extra";
 import chalk from "chalk";
-import { logger } from "@turbo/utils";
+import { logger, type PackageJson, type DependencyGroups } from "@turbo/utils";
 import { gatherAddRequirements } from "../utils/gatherAddRequirements";
 import type { TurboGeneratorArguments } from "./types";
-import type { PackageJson, DependencyGroups } from "../types";
 
 export async function generate({ project, opts }: TurboGeneratorArguments) {
   const { name, location, dependencies } = await gatherAddRequirements({
