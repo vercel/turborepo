9d685ad5a992bdaf6fe5ad4b991ad836c0f9bb8b
tknickman <tom.knickman@vercel.com>
2023-08-22 15:40:35 -0400

    feat(examples): update tailwind lint

--- examples/with-tailwind/apps/docs/src/app/page.tsx
+++ examples/with-tailwind/apps/docs/src/app/page.tsx
@@ -1,12 +1,11 @@
-import { Metadata } from "next";
-import Head from "next/head";
+import type { Metadata } from "next";
 import { Button } from "ui";
 
 export const metadata: Metadata = {
   title: "Docs - Turborepo Example",
 };
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
