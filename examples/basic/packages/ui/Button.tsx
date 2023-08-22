cb6105a8666dfe706f14f60a627117ea72839b4b
Thomas Knickman <tom.knickman@vercel.com>
2023-08-22 12:39:44 -0400

    feat(examples): fix basic lint (#5775)

--- examples/basic/packages/ui/Button.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import * as React from "react";
-
-export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
-};
--- examples/basic/packages/ui/index.tsx
+++ examples/basic/packages/ui/index.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-
 // component exports
-export * from "./Button";
-export * from "./Header";
+export * from "./button";
+export * from "./header";
