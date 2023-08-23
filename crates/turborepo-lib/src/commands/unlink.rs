23d7b322480b75acd10150bab4ece42f180e0251
Nathan Hammond <nathan.hammond@vercel.com>
2023-08-23 13:40:25 +0800

    Reduce logging ceremony.

--- crates/turborepo-lib/src/commands/unlink.rs
+++ crates/turborepo-lib/src/commands/unlink.rs
@@ -14,20 +14,14 @@ fn unlink_remote_caching(base: &mut CommandBase) -> Result<()> {
     let repo_config: &mut crate::config::RepoConfig = base.repo_config_mut()?;
     let needs_disabling = repo_config.team_id().is_some() || repo_config.team_slug().is_some();
 
-    if needs_disabling {
-        base.repo_config_mut()?.set_team_id(None)?;
-
-        println!(
-            "{}",
-            base.ui.apply(GREY.apply_to("> Disabled Remote Caching"))
-        );
+    let output = if needs_disabling {
+        repo_config.set_team_id(None)?;
+        "> Disabled Remote Caching"
     } else {
-        println!(
-            "{}",
-            base.ui
-                .apply(GREY.apply_to("> No Remote Caching config found"))
-        );
-    }
+        "> No Remote Caching config found"
+    };
+
+    println!("{}", base.ui.apply(GREY.apply_to(output)));
 
     Ok(())
 }
@@ -38,27 +32,20 @@ fn unlink_spaces(base: &mut CommandBase) -> Result<()> {
         repo_config.space_team_id().is_some() || repo_config.space_team_slug().is_some();
 
     if needs_disabling {
-        base.repo_config_mut()?.set_space_team_id(None)?;
+        repo_config.set_space_team_id(None)?;
     }
 
     // Space config is _also_ in turbo.json.
     let result =
         remove_spaces_from_turbo_json(base).context("could not unlink. Something went wrong")?;
 
-    match (needs_disabling, result) {
-        (_, UnlinkSpacesResult::Unlinked) => {
-            println!("{}", base.ui.apply(GREY.apply_to("> Unlinked Spaces")));
-        }
-        (true, _) => {
-            println!("{}", base.ui.apply(GREY.apply_to("> Unlinked Spaces")));
-        }
-        (false, UnlinkSpacesResult::NoSpacesFound) => {
-            println!(
-                "{}",
-                base.ui.apply(GREY.apply_to("> No Spaces config found"))
-            );
-        }
-    }
+    let output = match (needs_disabling, result) {
+        (_, UnlinkSpacesResult::Unlinked) => "> Unlinked Spaces",
+        (true, _) => "> Unlinked Spaces",
+        (false, UnlinkSpacesResult::NoSpacesFound) => "> No Spaces config found",
+    };
+
+    println!("{}", base.ui.apply(GREY.apply_to(output)));
 
     Ok(())
 }
