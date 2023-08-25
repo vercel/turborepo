0458fa091a4434004375a20f984caa54816e53fd
tknickman <tom.knickman@vercel.com>
2023-08-25 09:53:38 -0400

    fix(lint): add node env

--- examples/basic/packages/eslint-config-custom/next.js
+++ examples/basic/packages/eslint-config-custom/next.js
@@ -13,6 +13,7 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 module.exports = {
   extends: [
+    "@vercel/style-guide/eslint/node",
     "@vercel/style-guide/eslint/typescript",
     "@vercel/style-guide/eslint/browser",
     "@vercel/style-guide/eslint/react",
