04c4712f61e6243a866b16cd2c2f225a7fd17e9e
tknickman <tom.knickman@vercel.com>
2023-09-01 10:30:11 -0400

    feat(example): update tailwind starter template

---
+++ examples/with-tailwind/packages/tailwind-config/tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "glow-conic":
+          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
