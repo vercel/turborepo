0634f032f7db8f48e8d7d509c4bee8e7faab32ce
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-05 18:24:30 +0900

    fix

--- crates/turbopack-css/src/asset.rs
+++ crates/turbopack-css/src/asset.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use turbo_tasks::{TryJoinIterExt, Value, ValueToString, Vc};
+use turbo_tasks::{task::IntoTaskFn, TaskInput, TryJoinIterExt, Value, ValueToString, Vc};
 use turbo_tasks_fs::FileSystemPath;
 use turbopack_core::{
     asset::{Asset, AssetContent},
