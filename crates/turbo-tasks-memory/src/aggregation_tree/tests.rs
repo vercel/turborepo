765267f527c1cfbaa0512e88fba21b9e285ffc7a
Tobias Koppers <tobias.koppers@googlemail.com>
2023-09-07 17:00:12 +0200

    increase blue likelyhood

--- crates/turbo-tasks-memory/src/aggregation_tree/tests.rs
+++ crates/turbo-tasks-memory/src/aggregation_tree/tests.rs
@@ -306,12 +306,12 @@ fn chain() {
         assert_eq!(aggregated.value, 25151);
     }
     // This should be way less the 100 to prove that we are reusing trees
-    assert_eq!(context.additions.load(Ordering::SeqCst), 10);
+    assert_eq!(context.additions.load(Ordering::SeqCst), 12);
     context.additions.store(0, Ordering::SeqCst);
 
     leaf.incr(&context);
     // This should be less the 20 to prove that we are reusing trees
-    assert_eq!(context.additions.load(Ordering::SeqCst), 9);
+    assert_eq!(context.additions.load(Ordering::SeqCst), 10);
     context.additions.store(0, Ordering::SeqCst);
 
     {
@@ -364,8 +364,8 @@ fn chain_double_connected() {
 
     {
         let aggregated = aggregation_info(&context, &current);
-        assert_eq!(aggregated.lock().value, 106545);
+        assert_eq!(aggregated.lock().value, 39868);
     }
-    assert_eq!(context.additions.load(Ordering::SeqCst), 493);
+    assert_eq!(context.additions.load(Ordering::SeqCst), 521);
     context.additions.store(0, Ordering::SeqCst);
 }
