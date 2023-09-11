963845945f38f24acfbfd5239f807b36846d38e9
Thomas Knickman <tom.knickman@vercel.com>
2023-09-11 13:25:20 -0400

    feat(examples): update gatsby (#5925)

--- examples/with-gatsby/apps/web/.eslintrc.js
+++ examples/with-gatsby/apps/web/.eslintrc.js
@@ -1,4 +1,3 @@
 module.exports = {
-  root: true,
-  extends: ["custom/react"],
+  extends: ["custom/gatsby"],
 };
--- examples/with-gatsby/packages/ui/.eslintrc.js
+++ examples/with-gatsby/packages/ui/.eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  root: true,
-  extends: ["custom/react"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-  },
+  extends: ["custom/react-internal"],
 };
