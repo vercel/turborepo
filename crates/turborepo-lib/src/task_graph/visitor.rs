b3fe2deb9684a4df0f8e71a30266fbac381940f3
Chris Olszewski <chris.olszewski@vercel.com>
2023-09-12 12:16:46 -0700

    feat: use prefixed ui in task graph visitor (#5930)
    ### Description
    
    Various plumbing changes that allow us to construct a prefixed UI
    instance for each task. This instance is necessary for interaction with
    the task cache.
    
    Reviewer Notes:
    Each commit is a fairly simple addition/change to the visitor and can be
    reviewed on its own.
    
    ### Testing Instructions
    
    Observe that colors are displayed when running tasks with the run stub
    still:
    <img width="592" alt="Screenshot 2023-09-11 at 2 25 08 PM"
    src="https://github.com/vercel/turbo/assets/4131117/558f6114-435a-49dd-b8ac-3885ee281d43">
    
    
    
    Closes TURBO-1322
    
    ---------
    
    Co-authored-by: Chris Olszewski <Chris Olszewski>

--- crates/turborepo-lib/src/task_graph/visitor.rs
+++ crates/turborepo-lib/src/task_graph/visitor.rs
@@ -1,10 +1,16 @@
-use std::sync::{Arc, OnceLock};
+use std::{
+    borrow::Cow,
+    io::Write,
+    sync::{Arc, OnceLock},
+};
 
+use console::{Style, StyledObject};
 use futures::{stream::FuturesUnordered, StreamExt};
 use regex::Regex;
 use tokio::sync::mpsc;
-use tracing::debug;
+use tracing::{debug, error};
 use turborepo_env::{EnvironmentVariableMap, ResolvedEnvMode};
+use turborepo_ui::{ColorSelector, OutputClient, OutputSink, OutputWriter, PrefixedUI, UI};
 
 use crate::{
     cli::EnvMode,
@@ -26,6 +32,9 @@ pub struct Visitor<'a> {
     opts: &'a Opts<'a>,
     task_hasher: TaskHasher<'a>,
     global_env_mode: EnvMode,
+    sink: OutputSink<StdWriter>,
+    color_cache: ColorSelector,
+    ui: UI,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -48,6 +57,10 @@ pub enum Error {
 }
 
 impl<'a> Visitor<'a> {
+    // Disabling this lint until we stop adding state to the visitor.
+    // Once we have the full picture we will go about grouping these pieces of data
+    // together
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         package_graph: Arc<PackageGraph>,
         run_cache: Arc<RunCache>,
@@ -56,6 +69,7 @@ impl<'a> Visitor<'a> {
         env_at_execution_start: &'a EnvironmentVariableMap,
         global_hash: &'a str,
         global_env_mode: EnvMode,
+        ui: UI,
     ) -> Self {
         let task_hasher = TaskHasher::new(
             package_inputs_hashes,
@@ -63,6 +77,8 @@ impl<'a> Visitor<'a> {
             env_at_execution_start,
             global_hash,
         );
+        let sink = Self::sink(opts);
+        let color_cache = ColorSelector::default();
 
         Self {
             run_cache,
@@ -70,6 +86,9 @@ impl<'a> Visitor<'a> {
             opts,
             task_hasher,
             global_env_mode,
+            sink,
+            color_cache,
+            ui,
         }
     }
 
@@ -86,6 +105,7 @@ impl<'a> Visitor<'a> {
 
         while let Some(message) = node_stream.recv().await {
             let crate::engine::Message { info, callback } = message;
+            let is_github_actions = self.opts.run_opts.is_github_actions;
             let package_name = WorkspaceName::from(info.package());
             let workspace_dir =
                 self.package_graph
@@ -153,12 +173,23 @@ impl<'a> Visitor<'a> {
                 self.run_cache
                     .task_cache(task_definition, workspace_dir, info.clone(), &task_hash);
 
+            // TODO(gsoltis): if/when we fix https://github.com/vercel/turbo/issues/937
+            // the following block should never get hit. In the meantime, keep it after
+            // hashing so that downstream tasks can count on the hash existing
+            //
+            // bail if the script doesn't exist
+            let Some(command) = command else { continue };
+
+            let output_client = self.output_client();
+            let prefix = self.prefix(&info);
+            let pretty_prefix = self.color_cache.prefix_with_color(&task_hash, &prefix);
+            let ui = self.ui;
+
             tasks.push(tokio::spawn(async move {
-                println!(
-                    "Executing {info}: {}",
-                    command.as_deref().unwrap_or("no script def")
-                );
                 let _task_cache = task_cache;
+                let mut prefixed_ui =
+                    Self::prefixed_ui(ui, is_github_actions, &output_client, pretty_prefix);
+                prefixed_ui.output(command);
                 callback.send(Ok(())).unwrap();
             }));
         }
@@ -171,6 +202,103 @@ impl<'a> Visitor<'a> {
         }
 
         Ok(())
+    }
+
+    fn sink(opts: &Opts) -> OutputSink<StdWriter> {
+        let err_writer = match matches!(
+            opts.run_opts.log_order,
+            crate::opts::ResolvedLogOrder::Grouped
+        ) && opts.run_opts.is_github_actions
+        {
+            // If we're running on Github Actions, force everything to stdout
+            // so as not to have out-of-order log lines
+            true => std::io::stdout().into(),
+            false => std::io::stderr().into(),
+        };
+        OutputSink::new(std::io::stdout().into(), err_writer)
+    }
+
+    fn output_client(&self) -> OutputClient<impl std::io::Write> {
+        let behavior = match self.opts.run_opts.log_order {
+            // TODO once run summary is implemented, we can skip keeping an in memory buffer if it
+            // is disabled
+            crate::opts::ResolvedLogOrder::Stream => {
+                turborepo_ui::OutputClientBehavior::InMemoryBuffer
+            }
+            crate::opts::ResolvedLogOrder::Grouped => turborepo_ui::OutputClientBehavior::Grouped,
+        };
+        self.sink.logger(behavior)
+    }
+
+    fn prefix<'b>(&self, task_id: &'b TaskId) -> Cow<'b, str> {
+        match self.opts.run_opts.log_prefix {
+            crate::opts::ResolvedLogPrefix::Task if self.opts.run_opts.single_package => {
+                task_id.task().into()
+            }
+            crate::opts::ResolvedLogPrefix::Task => {
+                format!("{}:{}", task_id.package(), task_id.task()).into()
+            }
+            crate::opts::ResolvedLogPrefix::None => "".into(),
+        }
+    }
+
+    fn prefixed_ui<W: Write>(
+        ui: UI,
+        is_github_actions: bool,
+        client: &OutputClient<W>,
+        prefix: StyledObject<String>,
+    ) -> PrefixedUI<OutputWriter<'_, W>> {
+        let mut prefixed_ui = PrefixedUI::new(ui, client.stdout(), client.stderr())
+            .with_output_prefix(prefix.clone())
+            // TODO: we can probably come up with a more ergonomic way to achieve this
+            .with_error_prefix(
+                Style::new().apply_to(format!("{}ERROR: ", ui.apply(prefix.clone()))),
+            )
+            .with_warn_prefix(prefix);
+        if is_github_actions {
+            prefixed_ui = prefixed_ui
+                .with_error_prefix(Style::new().apply_to("[ERROR]".to_string()))
+                .with_warn_prefix(Style::new().apply_to("[WARN]".to_string()));
+        }
+        prefixed_ui
+    }
+}
+
+// A tiny enum that allows us to use the same type for stdout and stderr without
+// the use of Box<dyn Write>
+enum StdWriter {
+    Out(std::io::Stdout),
+    Err(std::io::Stderr),
+}
+
+impl StdWriter {
+    fn writer(&mut self) -> &mut dyn std::io::Write {
+        match self {
+            StdWriter::Out(out) => out,
+            StdWriter::Err(err) => err,
+        }
+    }
+}
+
+impl From<std::io::Stdout> for StdWriter {
+    fn from(value: std::io::Stdout) -> Self {
+        Self::Out(value)
+    }
+}
+
+impl From<std::io::Stderr> for StdWriter {
+    fn from(value: std::io::Stderr) -> Self {
+        Self::Err(value)
+    }
+}
+
+impl std::io::Write for StdWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.writer().write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.writer().flush()
     }
 }
 
