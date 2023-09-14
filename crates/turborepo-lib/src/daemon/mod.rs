eeb0b56248d6199b025aad05330f3a46d9c4c81a
Greg Soltis <greg.soltis@vercel.com>
2023-09-14 11:02:46 -0700

    Wire filewatching to daemon server (#5916)

--- crates/turborepo-lib/src/daemon/mod.rs
+++ crates/turborepo-lib/src/daemon/mod.rs
@@ -7,7 +7,7 @@ mod server;
 
 pub use client::{DaemonClient, DaemonError};
 pub use connector::DaemonConnector;
-pub use server::{CloseReason, DaemonServer};
+pub use server::{serve, CloseReason};
 
 pub(crate) mod proto {
     tonic::include_proto!("turbodprotocol");
