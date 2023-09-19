e50fa2df7a1c65369f3781a508137b857dc15d7f
Thomas Knickman <tom.knickman@vercel.com>
2023-09-19 00:19:07 -0400

    feat(create-turbo): support package manager opt (#5979)

--- packages/create-turbo/src/commands/create/types.ts
+++ packages/create-turbo/src/commands/create/types.ts
@@ -1,6 +1,9 @@
+import type { PackageManager } from "@turbo/utils";
+
 export type CreateCommandArgument = string | undefined;
 
 export interface CreateCommandOptions {
+  manager?: PackageManager;
   skipInstall?: boolean;
   skipTransforms?: boolean;
   turboVersion?: string;
