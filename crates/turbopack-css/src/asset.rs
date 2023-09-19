4650f09d2447511a11f4b92bd5592694cc5d16bc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-19 12:10:42 +0900

    fixup

--- crates/turbopack-css/src/asset.rs
+++ crates/turbopack-css/src/asset.rs
@@ -15,7 +15,10 @@ use turbopack_core::{
 use crate::{
     chunk::{CssChunkItem, CssChunkItemContent, CssChunkPlaceable, CssChunkType, CssImport},
     code_gen::CodeGenerateable,
-    process::{finalize_css, parse_css, FinalCssResult, ParseCssResult, ProcessCss},
+    process::{
+        finalize_css, parse_css, process_css_with_placeholder, CssWithPlaceholderResult,
+        FinalCssResult, ParseCssResult, ProcessCss,
+    },
     references::{compose::CssModuleComposeReference, import::ImportAssetReference},
     CssModuleAssetType,
 };
@@ -76,9 +79,9 @@ impl ProcessCss for CssModuleAsset {
         self: Vc<Self>,
         chunking_context: Vc<Box<dyn ChunkingContext>>,
     ) -> Result<Vc<FinalCssResult>> {
-        let parse_result = self.parse_css();
+        let process_result = self.get_css_with_placeholder();
 
-        Ok(finalize_css(parse_result, chunking_context))
+        Ok(finalize_css(process_result, chunking_context))
     }
 }
 
