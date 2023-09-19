9aedbf90d5c5c0a64ce6873f2843aa572eba0456
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-19 12:20:43 +0900

    Fix compilation

--- crates/turbopack-css/src/lifetime_util.rs
+++ crates/turbopack-css/src/lifetime_util.rs
@@ -1,5 +1,9 @@
-use lightningcss::stylesheet::StyleSheet;
+use lightningcss::{rules::import::ImportRule, stylesheet::StyleSheet};
 
 pub fn stylesheet_into_static<'i, 'o>(ss: &StyleSheet) -> StyleSheet<'i, 'o> {
+    todo!()
+}
+
+pub fn import_rule_to_static<'i>(i: &ImportRule) -> ImportRule<'i> {
     todo!()
 }
