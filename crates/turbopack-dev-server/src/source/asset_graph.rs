f3ac96899cc9d48bcae1e99ff4fc0036672b453e
Tobias Koppers <tobias.koppers@googlemail.com>
2023-09-26 03:45:06 +0000

    improve consistency of asset graph print full route tree

--- crates/turbopack-dev-server/src/source/asset_graph.rs
+++ crates/turbopack-dev-server/src/source/asset_graph.rs
@@ -1,11 +1,11 @@
 use std::{
-    collections::{HashMap, HashSet, VecDeque},
+    collections::{HashSet, VecDeque},
     iter::once,
 };
 
 use anyhow::Result;
-use indexmap::{indexset, IndexSet};
-use turbo_tasks::{Completion, State, Value, ValueToString, Vc};
+use indexmap::{indexset, IndexMap, IndexSet};
+use turbo_tasks::{Completion, State, TryJoinIterExt, Value, ValueToString, Vc};
 use turbo_tasks_fs::FileSystemPath;
 use turbopack_core::{
     asset::Asset,
@@ -20,9 +20,9 @@ use super::{
 };
 
 #[turbo_tasks::value(transparent)]
-struct OutputAssetsMap(HashMap<String, Vc<Box<dyn OutputAsset>>>);
+struct OutputAssetsMap(IndexMap<String, Vc<Box<dyn OutputAsset>>>);
 
-type ExpandedState = State<HashSet<Vc<Box<dyn OutputAsset>>>>;
+type ExpandedState = State<HashSet<String>>;
 
 #[turbo_tasks::value(serialization = "none", eq = "manual", cell = "new")]
 pub struct AssetGraphContentSource {
@@ -105,94 +105,96 @@ async fn expand(
     root_assets: &IndexSet<Vc<Box<dyn OutputAsset>>>,
     root_path: &FileSystemPath,
     expanded: Option<&ExpandedState>,
-) -> Result<HashMap<String, Vc<Box<dyn OutputAsset>>>> {
-    let mut map = HashMap::new();
+) -> Result<IndexMap<String, Vc<Box<dyn OutputAsset>>>> {
+    let mut map = IndexMap::new();
     let mut assets = Vec::new();
     let mut queue = VecDeque::with_capacity(32);
     let mut assets_set = HashSet::new();
+    let root_assets_with_path = root_assets
+        .iter()
+        .map(|&asset| async move {
+            let path = asset.ident().path().await?;
+            Ok((path, asset))
+        })
+        .try_join()
+        .await?;
     if let Some(expanded) = &expanded {
         let expanded = expanded.get();
-        for root_asset in root_assets.iter() {
-            let expanded = expanded.contains(root_asset);
-            assets.push((root_asset.ident().path(), *root_asset));
-            assets_set.insert(*root_asset);
-            if expanded {
-                queue.push_back(root_asset.references());
+        for (path, root_asset) in root_assets_with_path.into_iter() {
+            if let Some(sub_path) = root_path.get_path_to(&path) {
+                assets.push((sub_path.to_string(), root_asset));
+                let expanded = expanded.contains(sub_path);
+                assets_set.insert(root_asset);
+                if expanded {
+                    queue.push_back(root_asset.references());
+                }
             }
         }
     } else {
-        for root_asset in root_assets.iter() {
-            assets.push((root_asset.ident().path(), *root_asset));
-            assets_set.insert(*root_asset);
-            queue.push_back(root_asset.references());
+        for (path, root_asset) in root_assets_with_path.into_iter() {
+            if let Some(sub_path) = root_path.get_path_to(&path) {
+                assets.push((sub_path.to_string(), root_asset));
+                assets_set.insert(root_asset);
+                queue.push_back(root_asset.references());
+            }
         }
     }
 
     while let Some(references) = queue.pop_front() {
         for asset in references.await?.iter() {
             if assets_set.insert(*asset) {
-                let expanded = if let Some(expanded) = &expanded {
-                    // We lookup the unresolved asset in the expanded set.
-                    // We could resolve the asset here, but that would require waiting on the
-                    // computation here and it doesn't seem to be neccessary in this case. We just
-                    // have to be sure that we consistently use the unresolved asset.
-                    expanded.get().contains(asset)
-                } else {
-                    true
-                };
-                if expanded {
-                    queue.push_back(asset.references());
+                let path = asset.ident().path().await?;
+                if let Some(sub_path) = root_path.get_path_to(&path) {
+                    let expanded = if let Some(expanded) = &expanded {
+                        // We lookup the unresolved asset in the expanded set.
+                        // We could resolve the asset here, but that would require waiting on the
+                        // computation here and it doesn't seem to be neccessary in this case. We
+                        // just have to be sure that we consistently use the
+                        // unresolved asset.
+                        expanded.get().contains(sub_path)
+                    } else {
+                        true
+                    };
+                    if expanded {
+                        queue.push_back(asset.references());
+                    }
+                    assets.push((sub_path.to_string(), *asset));
                 }
-                assets.push((asset.ident().path(), *asset));
             }
         }
     }
-    for (p_vc, asset) in assets {
-        // For clippy -- This explicit deref is necessary
-        let p = &*p_vc.await?;
+    for (sub_path, asset) in assets {
         let asset = asset.resolve().await?;
-        if let Some(sub_path) = root_path.get_path_to(p) {
-            map.insert(sub_path.to_string(), asset);
-            if sub_path == "index.html" {
-                map.insert("".to_string(), asset);
-            } else if let Some(p) = sub_path.strip_suffix("/index.html") {
-                map.insert(p.to_string(), asset);
-                map.insert(format!("{p}/"), asset);
-            } else if let Some(p) = sub_path.strip_suffix(".html") {
-                map.insert(p.to_string(), asset);
-            }
+        if sub_path == "index.html" {
+            map.insert("".to_string(), asset);
+        } else if let Some(p) = sub_path.strip_suffix("/index.html") {
+            map.insert(p.to_string(), asset);
+            map.insert(format!("{p}/"), asset);
+        } else if let Some(p) = sub_path.strip_suffix(".html") {
+            map.insert(p.to_string(), asset);
         }
+        map.insert(sub_path, asset);
     }
     Ok(map)
 }
-
-/// A unresolve asset. We need to have a unresolve Asset here as we need to
-/// lookup the Vc identity in the expanded set.
-///
-/// This must not be a TaskInput since this would resolve the embedded asset.
-#[turbo_tasks::value(serialization = "auto_for_input")]
-#[derive(Hash, PartialOrd, Ord, Debug, Clone)]
-struct UnresolvedAsset(Vc<Box<dyn OutputAsset>>);
 
 #[turbo_tasks::value_impl]
 impl ContentSource for AssetGraphContentSource {
     #[turbo_tasks::function]
     async fn get_routes(self: Vc<Self>) -> Result<Vc<RouteTree>> {
         let assets = self.all_assets_map().strongly_consistent().await?;
+        let mut paths = Vec::new();
         let routes = assets
             .iter()
             .map(|(path, asset)| {
+                paths.push(path.as_str());
                 RouteTree::new_route(
                     BaseSegment::from_static_pathname(path).collect(),
                     RouteType::Exact,
                     Vc::upcast(AssetGraphGetContentSourceContent::new(
-                        self, /* Passing the asset to a function would normally resolve that
-                               * asset. But */
-                        // in this special case we want to avoid that and just pass the unresolved
-                        // asset. So to enforce that we need to wrap it in this special value.
-                        // Technically it would be preferable to have some kind of `#[unresolved]`
-                        // attribute on function arguments, but we don't have that yet.
-                        Value::new(UnresolvedAsset(*asset)),
+                        self,
+                        path.to_string(),
+                        *asset,
                     )),
                 )
             })
@@ -204,17 +206,22 @@ impl ContentSource for AssetGraphContentSource {
 #[turbo_tasks::value]
 struct AssetGraphGetContentSourceContent {
     source: Vc<AssetGraphContentSource>,
-    /// The unresolved asset.
+    path: String,
     asset: Vc<Box<dyn OutputAsset>>,
 }
 
 #[turbo_tasks::value_impl]
 impl AssetGraphGetContentSourceContent {
     #[turbo_tasks::function]
-    pub fn new(source: Vc<AssetGraphContentSource>, asset: Value<UnresolvedAsset>) -> Vc<Self> {
+    pub fn new(
+        source: Vc<AssetGraphContentSource>,
+        path: String,
+        asset: Vc<Box<dyn OutputAsset>>,
+    ) -> Vc<Self> {
         Self::cell(AssetGraphGetContentSourceContent {
             source,
-            asset: asset.into_value().0,
+            path,
+            asset,
         })
     }
 }
@@ -242,11 +249,7 @@ impl ContentSourceSideEffect for AssetGraphGetContentSourceContent {
         let source = self.source.await?;
 
         if let Some(expanded) = &source.expanded {
-            let asset = self.asset;
-            expanded.update_conditionally(|expanded| {
-                // Insert the unresolved asset into the set
-                expanded.insert(asset)
-            });
+            expanded.update_conditionally(|expanded| expanded.insert(self.path.to_string()));
         }
         Ok(Completion::new())
     }
