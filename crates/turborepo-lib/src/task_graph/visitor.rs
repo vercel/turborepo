6f2631c00c787ac444f815ff9b5eb0600665b2e4
Chris Olszewski <Chris Olszewski>
2023-09-27 11:35:52 -0700

    make task error clonable

--- crates/turborepo-lib/src/task_graph/visitor.rs
+++ crates/turborepo-lib/src/task_graph/visitor.rs
@@ -322,17 +322,18 @@ fn turbo_regex() -> &'static Regex {
 }
 
 // Error that comes from the execution of the task
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error, Clone)]
 #[error("{task_id}: {cause}")]
 pub struct TaskError {
     task_id: String,
     cause: TaskErrorCause,
 }
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error, Clone)]
 enum TaskErrorCause {
-    #[error("unable to spawn child process: {err}")]
-    Spawn { err: std::io::Error },
+    #[error("unable to spawn child process: {msg}")]
+    // We eagerly serialize this in order to allow us to implement clone
+    Spawn { msg: String },
     #[error("command {command} exited ({exit_code})")]
     Exit { command: String, exit_code: i32 },
 }
@@ -348,7 +349,9 @@ impl TaskError {
     fn from_spawn(task_id: String, err: std::io::Error) -> Self {
         Self {
             task_id,
-            cause: TaskErrorCause::Spawn { err },
+            cause: TaskErrorCause::Spawn {
+                msg: err.to_string(),
+            },
         }
     }
 
