11db178818910ca3745eec66db6c65cad7496da5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-05 14:57:32 +0900

    `FileType`

--- crates/turbopack-ecmascript/src/tree_shake/barrel/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/barrel/mod.rs
@@ -1,1 +1,11 @@
+pub struct BarrelOptimizer {}
 
+/// The type of an ECMAScript file
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum FileType {
+    Normal,
+    Barrel,
+    Full,
+}
+
+impl FileType {}
