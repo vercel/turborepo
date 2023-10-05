1157aa549b532bd3698c5b9fd13b01be75cf3818
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-05 15:28:10 +0900

    `is_side_effect_free`

--- crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
@@ -1,7 +1,13 @@
+use anyhow::Result;
 use serde::Deserialize;
+use turbo_tasks::Vc;
+use turbo_tasks_fs::FileSystemPath;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct PackageJson {
     side_effects: bool,
 }
+
+#[turbo_tasks::function]
+pub async fn is_side_effect_free(filename: Vc<FileSystemPath>) -> Result<Vc<bool>> {}
