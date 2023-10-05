2f025e0a3d07afcf2096d1d5503ec8b6aeca07d8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-05 16:06:05 +0900

    `read_package_json_for`

--- crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
@@ -1,7 +1,7 @@
 use anyhow::{bail, Result};
 use serde::Deserialize;
 use turbo_tasks::Vc;
-use turbo_tasks_fs::FileSystemPath;
+use turbo_tasks_fs::{File, FileContent, FileSystemPath};
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -16,25 +16,31 @@ fn true_by_default() -> bool {
 
 #[turbo_tasks::function]
 pub async fn is_side_effect_free(filename: Vc<FileSystemPath>) -> Result<Vc<bool>> {
-    let package_json = find_package_json_for(filename.clone());
+    let package_json = read_package_json_for(filename.clone()).await?;
 
-    let content = package_json.read().await?;
+    match &*package_json {
+        FileContent::Content(file) => {
+            let json = serde_json::from_slice::<PackageJson>(&**file)?;
 
-    let json = serde_json::from_slice::<PackageJson>(&content)?;
-
-    Ok(!json.side_effects.into())
+            Ok(!json.side_effects.into())
+        }
+        FileContent::NotFound => Ok(false),
+    }
 }
 
 #[turbo_tasks::function]
-async fn find_package_json_for(filename: Vc<FileSystemPath>) -> Result<Vc<FileSystemPath>> {
+async fn read_package_json_for(filename: Vc<FileSystemPath>) -> Result<Vc<FileContent>> {
     let mut f = filename;
 
     while !f.await?.is_root() {
         let cur = f.parent();
         let p = cur.join("package.json".to_string());
 
-        if p.exists() {
-            return Ok(p.into());
+        let content_vc = p.read();
+        let content = content_vc.await?;
+
+        if let FileContent::Content(f) = &*content {
+            return Ok(content_vc);
         }
 
         f = cur;
