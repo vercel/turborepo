36abf60b4b105a6a9a428b32375b352e68772dd5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-05 15:08:01 +0900

    `detect`

--- crates/turbopack-ecmascript/src/tree_shake/barrel/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/barrel/mod.rs
@@ -1,11 +1,32 @@
+use swc_core::ecma::ast::{Module, NamedExport};
+
 pub struct BarrelOptimizer {}
 
 /// The type of an ECMAScript file
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum FileType {
     Normal,
+    /// File with only re-exports.
     Barrel,
     Full,
 }
 
-impl FileType {}
+impl FileType {
+    pub fn detect(module: &Module) -> Self {
+        if module.body.iter().all(|i| match i {
+            swc_core::ecma::ast::ModuleItem::ModuleDecl(s) => match s {
+                swc_core::ecma::ast::ModuleDecl::Import(_)
+                | swc_core::ecma::ast::ModuleDecl::ExportNamed(NamedExport {
+                    src: Some(..), ..
+                })
+                | swc_core::ecma::ast::ModuleDecl::ExportAll(_) => true,
+                _ => false,
+            },
+            swc_core::ecma::ast::ModuleItem::Stmt(_) => false,
+        }) {
+            return Self::Barrel;
+        }
+
+        Self::Full
+    }
+}
