148b913fe2712cc25cecd9082385411ad4d35ac5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-05 15:59:33 +0900

    more

--- crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
@@ -14,17 +14,17 @@ pub async fn is_side_effect_free(filename: Vc<FileSystemPath>) -> Result<Vc<bool
 
 #[turbo_tasks::function]
 async fn find_package_json_for(filename: Vc<FileSystemPath>) -> Result<Vc<FileSystemPath>> {
-    let mut f = Some(filename);
+    let mut f = filename;
 
-    while let Some(cur) = f {
-        let cur = cur.parent();
+    while !f.await?.is_root() {
+        let cur = f.parent();
         let p = cur.join("package.json".to_string());
 
         if p.exists() {
             return Ok(p.into());
         }
 
-        f = cur.parent().map(|p| p.into());
+        f = cur;
     }
 
     bail!("Could not find package.json for {}", filename.display())
