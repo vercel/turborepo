69d3d59cca4eeab4917e149111e68a66249321e6
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-05 15:47:50 +0900

    `find_package_json_for`

--- crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/pkg_json/mod.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use serde::Deserialize;
 use turbo_tasks::Vc;
 use turbo_tasks_fs::FileSystemPath;
@@ -13,4 +13,19 @@ struct PackageJson {
 pub async fn is_side_effect_free(filename: Vc<FileSystemPath>) -> Result<Vc<bool>> {}
 
 #[turbo_tasks::function]
-async fn find_package_json_for(filename: Vc<FileSystemPath>) -> Result<Vc<FileSystemPath>> {}
+async fn find_package_json_for(filename: Vc<FileSystemPath>) -> Result<Vc<FileSystemPath>> {
+    let mut f = Some(filename);
+
+    while let Some(cur) = f {
+        let cur = cur.parent();
+        let p = cur.join("package.json".to_string());
+
+        if p.exists() {
+            return Ok(p.into());
+        }
+
+        f = cur.parent().map(|p| p.into());
+    }
+
+    bail!("Could not find package.json for {}", filename.display())
+}
