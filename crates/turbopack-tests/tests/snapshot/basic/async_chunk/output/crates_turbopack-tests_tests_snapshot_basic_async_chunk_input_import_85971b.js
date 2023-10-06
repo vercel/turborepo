e3ea47f99cb97c7f1599a271488c406b790a66c7
Justin Ridgewell <justin@ridgewell.name>
2023-10-06 01:03:50 -0400

    Update snapshots

--- crates/turbopack-tests/tests/snapshot/basic/async_chunk/output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_85971b.js
+++ crates/turbopack-tests/tests/snapshot/basic/async_chunk/output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_85971b.js
@@ -1,9 +1,9 @@
 (globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_35b556.js",
+    "output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_85971b.js",
     {},
 ]);
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-  "path": "output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_35b556.js",
+  "path": "output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_85971b.js",
   "chunks": [
     "output/crates_turbopack-tests_tests_snapshot_basic_async_chunk_input_import_571bb9.js"
   ],
--- crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_675597._.js
+++ crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_675597._.js
@@ -1,9 +1,9 @@
 (globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_35b556._.js",
+    "output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_675597._.js",
     {},
 ]);
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-  "path": "output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_35b556._.js",
+  "path": "output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_675597._.js",
   "chunks": [
     "output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_10fe7a._.js"
   ],
