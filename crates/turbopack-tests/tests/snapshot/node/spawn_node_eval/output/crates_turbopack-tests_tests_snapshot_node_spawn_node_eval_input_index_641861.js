7e286c00b95153cc93a6329458bf4549020fbdee
Justin Ridgewell <justin@ridgewell.name>
2023-10-11 00:15:27 -0400

    Update snapshots

---
+++ crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_641861.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_641861.js",
+    {},
+    {"otherChunks":["output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_d7882b.js"],"runtimeModuleIds":["[project]/crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/input/index.js (ecmascript)"]}
+]);
+// Dummy runtime
--- crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_a238f1.js
+++ crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_a238f1.js
@@ -5,8 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_a238f1.js",
   "chunks": [
-    "output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_b53fce.js",
-    "output/134fc_child_process_index_64e768.js"
+    "output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_d7882b.js"
   ],
   "source": "entry"
 });
