152df72065806a9ff53164cbce7e40b11456086e
Mehul Kar <mehul.kar@vercel.com>
2023-10-12 15:48:58 -0500

    Remove unnecessary if condition since it is always truthy

--- turborepo-tests/e2e/index.ts
+++ turborepo-tests/e2e/index.ts
@@ -554,151 +554,135 @@ function runSmokeTests<T>(
     }
   });
 
-  if (["npm", "yarn", "pnpm6", "pnpm", "berry"].includes(npmClient)) {
-    // Test `turbo prune a`
-    // @todo refactor with other package managers
-    const [installCmd, ...installArgs] =
-      getImmutableInstallForPackageManager(npmClient);
+  // Test `turbo prune a`
+  // @todo refactor with other package managers
+  const [installCmd, ...installArgs] =
+    getImmutableInstallForPackageManager(npmClient);
 
-    suite(`${npmClient} + turbo prune${suffix}`, async () => {
-      const scope = "a";
-      const pruneCommandOutput = getCommandOutputAsArray(
-        repo.turbo("prune", [scope], options)
-      );
-      assert.fixture(pruneCommandOutput[1], " - Added a");
-      assert.fixture(pruneCommandOutput[2], " - Added b");
+  suite(`${npmClient} + turbo prune${suffix}`, async () => {
+    const scope = "a";
+    const pruneCommandOutput = getCommandOutputAsArray(
+      repo.turbo("prune", [scope], options)
+    );
+    assert.fixture(pruneCommandOutput[1], " - Added a");
+    assert.fixture(pruneCommandOutput[2], " - Added b");
 
-      let files = [];
-      assert.not.throws(() => {
-        files = repo.globbySync("out/**/*", {
-          cwd: options.cwd ?? repo.root,
-        });
-      }, `Could not read generated \`out\` directory after \`turbo prune\``);
-      const expected = [
-        "out/package.json",
-        "out/turbo.json",
-        `out/${getLockfileForPackageManager(npmClient)}`,
-        "out/packages/a/build.js",
-        "out/packages/a/lint.js",
-        "out/packages/a/package.json",
-        "out/packages/a/test.js",
-        "out/packages/b/build.js",
-        "out/packages/b/lint.js",
-        "out/packages/b/package.json",
-        "out/packages/b/test.js",
-      ];
-      for (const file of expected) {
-        assert.ok(
-          files.includes(file),
-          `Expected file ${file} to be generated`
-        );
-      }
-
-      // grab the first turbo.json in an out folder
-      let turbos = repo
-        .globbySync("**/out/turbo.json")
-        .map((t: string) => JSON.parse(repo.readFileSync(t)));
-      for (const turbo of turbos) {
-        const pipelines = Object.keys(turbo.pipeline);
-        const missingInclude = includePrune.filter(
-          (i) => !pipelines.includes(i)
-        );
-        const presentExclude = excludePrune.filter((i) =>
-          pipelines.includes(i)
-        );
-
-        if (missingInclude.length || presentExclude.length) {
-          assert.unreachable(
-            "failed to validate prune in pipeline" +
-              (missingInclude.length ? `, expecting ${missingInclude}` : "") +
-              (presentExclude.length ? `, not expecting ${presentExclude}` : "")
-          );
-        }
-      }
-
-      const install = repo.run(installCmd, installArgs, {
-        cwd: options.cwd
-          ? path.join(options.cwd, "out")
-          : path.join(repo.root, "out"),
+    let files = [];
+    assert.not.throws(() => {
+      files = repo.globbySync("out/**/*", {
+        cwd: options.cwd ?? repo.root,
       });
-      assert.is(
-        install.exitCode,
-        0,
-        `Expected ${npmClient} install --frozen-lockfile to succeed`
-      );
+    }, `Could not read generated \`out\` directory after \`turbo prune\``);
+    const expected = [
+      "out/package.json",
+      "out/turbo.json",
+      `out/${getLockfileForPackageManager(npmClient)}`,
+      "out/packages/a/build.js",
+      "out/packages/a/lint.js",
+      "out/packages/a/package.json",
+      "out/packages/a/test.js",
+      "out/packages/b/build.js",
+      "out/packages/b/lint.js",
+      "out/packages/b/package.json",
+      "out/packages/b/test.js",
+    ];
+    for (const file of expected) {
+      assert.ok(files.includes(file), `Expected file ${file} to be generated`);
+    }
+
+    // grab the first turbo.json in an out folder
+    let turbos = repo
+      .globbySync("**/out/turbo.json")
+      .map((t: string) => JSON.parse(repo.readFileSync(t)));
+    for (const turbo of turbos) {
+      const pipelines = Object.keys(turbo.pipeline);
+      const missingInclude = includePrune.filter((i) => !pipelines.includes(i));
+      const presentExclude = excludePrune.filter((i) => pipelines.includes(i));
+
+      if (missingInclude.length || presentExclude.length) {
+        assert.unreachable(
+          "failed to validate prune in pipeline" +
+            (missingInclude.length ? `, expecting ${missingInclude}` : "") +
+            (presentExclude.length ? `, not expecting ${presentExclude}` : "")
+        );
+      }
+    }
+
+    const install = repo.run(installCmd, installArgs, {
+      cwd: options.cwd
+        ? path.join(options.cwd, "out")
+        : path.join(repo.root, "out"),
     });
+    assert.is(
+      install.exitCode,
+      0,
+      `Expected ${npmClient} install --frozen-lockfile to succeed`
+    );
+  });
 
-    suite(`${npmClient} + turbo prune --docker${suffix}`, async () => {
-      const scope = "a";
-      const pruneCommandOutput = getCommandOutputAsArray(
-        repo.turbo("prune", [scope, "--docker"], options)
-      );
-      assert.fixture(pruneCommandOutput[1], " - Added a");
-      assert.fixture(pruneCommandOutput[2], " - Added b");
+  suite(`${npmClient} + turbo prune --docker${suffix}`, async () => {
+    const scope = "a";
+    const pruneCommandOutput = getCommandOutputAsArray(
+      repo.turbo("prune", [scope, "--docker"], options)
+    );
+    assert.fixture(pruneCommandOutput[1], " - Added a");
+    assert.fixture(pruneCommandOutput[2], " - Added b");
 
-      let files: string[] = [];
-      assert.not.throws(() => {
-        files = repo.globbySync("out/**/*", {
-          cwd: options.cwd ?? repo.root,
-        });
-      }, `Could not read generated \`out\` directory after \`turbo prune\``);
-      const expected = [
-        "out/full/package.json",
-        "out/json/package.json",
-        "out/full/turbo.json",
-        `out/${getLockfileForPackageManager(npmClient)}`,
-        "out/full/packages/a/build.js",
-        "out/full/packages/a/lint.js",
-        "out/full/packages/a/package.json",
-        "out/json/packages/a/package.json",
-        "out/full/packages/a/test.js",
-        "out/full/packages/b/build.js",
-        "out/full/packages/b/lint.js",
-        "out/full/packages/b/package.json",
-        "out/json/packages/b/package.json",
-        "out/full/packages/b/test.js",
-      ];
-      for (const file of expected) {
-        assert.ok(
-          files.includes(file),
-          `Expected file ${file} to be generated`
-        );
-      }
-
-      // grab the first turbo.json in an out folder
-      let turbos = repo
-        .globbySync("**/out/turbo.json")
-        .map((t: string) => JSON.parse(repo.readFileSync(t)));
-      for (const turbo of turbos) {
-        const pipelines = Object.keys(turbo.pipeline);
-        const missingInclude = includePrune.filter(
-          (i) => !pipelines.includes(i)
-        );
-        const presentExclude = excludePrune.filter((i) =>
-          pipelines.includes(i)
-        );
-
-        if (missingInclude.length || presentExclude.length) {
-          assert.unreachable(
-            "failed to validate prune in pipeline" +
-              (missingInclude.length ? `, expecting ${missingInclude}` : "") +
-              (presentExclude.length ? `, not expecting ${presentExclude}` : "")
-          );
-        }
-      }
-
-      const install = repo.run(installCmd, installArgs, {
-        cwd: options.cwd
-          ? path.join(options.cwd, "out")
-          : path.join(repo.root, "out"),
+    let files: string[] = [];
+    assert.not.throws(() => {
+      files = repo.globbySync("out/**/*", {
+        cwd: options.cwd ?? repo.root,
       });
-      assert.is(
-        install.exitCode,
-        0,
-        `Expected ${npmClient} install --frozen-lockfile to succeed`
-      );
+    }, `Could not read generated \`out\` directory after \`turbo prune\``);
+    const expected = [
+      "out/full/package.json",
+      "out/json/package.json",
+      "out/full/turbo.json",
+      `out/${getLockfileForPackageManager(npmClient)}`,
+      "out/full/packages/a/build.js",
+      "out/full/packages/a/lint.js",
+      "out/full/packages/a/package.json",
+      "out/json/packages/a/package.json",
+      "out/full/packages/a/test.js",
+      "out/full/packages/b/build.js",
+      "out/full/packages/b/lint.js",
+      "out/full/packages/b/package.json",
+      "out/json/packages/b/package.json",
+      "out/full/packages/b/test.js",
+    ];
+    for (const file of expected) {
+      assert.ok(files.includes(file), `Expected file ${file} to be generated`);
+    }
+
+    // grab the first turbo.json in an out folder
+    let turbos = repo
+      .globbySync("**/out/turbo.json")
+      .map((t: string) => JSON.parse(repo.readFileSync(t)));
+    for (const turbo of turbos) {
+      const pipelines = Object.keys(turbo.pipeline);
+      const missingInclude = includePrune.filter((i) => !pipelines.includes(i));
+      const presentExclude = excludePrune.filter((i) => pipelines.includes(i));
+
+      if (missingInclude.length || presentExclude.length) {
+        assert.unreachable(
+          "failed to validate prune in pipeline" +
+            (missingInclude.length ? `, expecting ${missingInclude}` : "") +
+            (presentExclude.length ? `, not expecting ${presentExclude}` : "")
+        );
+      }
+    }
+
+    const install = repo.run(installCmd, installArgs, {
+      cwd: options.cwd
+        ? path.join(options.cwd, "out")
+        : path.join(repo.root, "out"),
     });
-  }
+    assert.is(
+      install.exitCode,
+      0,
+      `Expected ${npmClient} install --frozen-lockfile to succeed`
+    );
+  });
 }
 
 type PackageManager = "yarn" | "pnpm6" | "pnpm" | "npm" | "berry";
