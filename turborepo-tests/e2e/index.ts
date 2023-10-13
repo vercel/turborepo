6e2d4bf940097972d9b595d2b6f3b3460ff69249
Mehul Kar <mehul.kar@vercel.com>
2023-10-12 23:48:51 -0500

    Move tests back into main index.ts

--- turborepo-tests/e2e/index.ts
+++ turborepo-tests/e2e/index.ts
@@ -2,9 +2,12 @@ import execa from "execa";
 import * as uvu from "uvu";
 import { Monorepo, createMonorepo } from "./monorepo";
 import path from "path";
-import { basicPipeline } from "./fixtures";
+import {
+  basicPipeline,
+  prunePipeline,
+  explicitPrunePipeline,
+} from "./fixtures";
 import type { PackageManager } from "./types";
-import pruneTests from "./prune-test";
 
 import testBuild from "./tests/builds";
 import testBuild from "./tests/builds";
@@ -67,4 +70,71 @@ for (const mgr of packageManagers) {
   BasicFromSubDir.run();
 }
 
-pruneTests();
+////// More explicit prune tests ///////////////////
+
+// prune and prune --docker with a
+// expect c#build to be removed, since there is no dep between a -> c
+const Prune = uvu.suite("basicPrune");
+const repo = createMonorepo("yarn", prunePipeline);
+prune(Prune, repo, "yarn", {}, ["a#build"], ["c#build"]);
+pruneDocker(Prune, repo, "yarn", {}, ["a#build"], ["c#build"]);
+
+// prune and prune --docker from subdir
+// expect c#build to be removed, since there is no dep between a -> c
+const PruneFromSubDir = uvu.suite("basicPrune from subdirectory");
+const repo1 = createMonorepo("yarn", prunePipeline, "js");
+const cwd = path.join(repo1.root, repo1.subdir);
+prune(PruneFromSubDir, repo, "yarn", { cwd }, ["a#build"], ["c#build"]);
+pruneDocker(PruneFromSubDir, repo1, "yarn", { cwd }, ["a#build"], ["c#build"]);
+
+////////////////// Explicit Deps Prune //////////////////
+
+// prune and prune --docker
+// expect c#build to be included, since a depends on c
+const ExplicitDepPrune = uvu.suite("explicitDepPrune");
+const repo2 = createMonorepo("yarn", explicitPrunePipeline);
+prune(ExplicitDepPrune, repo2, "yarn", {}, ["a#build", "b#build"], ["c#build"]);
+pruneDocker(
+  ExplicitDepPrune,
+  repo2,
+  "yarn",
+  {},
+  ["a#build", "b#build"],
+  ["c#build"]
+);
+
+// prune and prune --docker from subdir
+// expect b#build to be included, since a depends on b
+const ExplicitDepPruneFromSubDir = uvu.suite(
+  "explicitDepPrune from subdirectory"
+);
+const repo3 = createMonorepo("yarn", explicitPrunePipeline, "js");
+const repo3cwd = path.join(repo3.root, repo3.subdir);
+prune(
+  ExplicitDepPruneFromSubDir,
+  repo,
+  "yarn",
+  { cwd: repo3cwd },
+  ["a#build", "b#build"],
+  ["c#build"]
+);
+pruneDocker(
+  ExplicitDepPruneFromSubDir,
+  repo3,
+  "yarn",
+  { cwd: repo3cwd },
+  ["a#build", "b#build"],
+  ["c#build"]
+);
+
+// Setup the cleanup in after hooks for each suite
+Prune.after(() => repo.cleanup());
+PruneFromSubDir.after(() => repo1.cleanup());
+ExplicitDepPrune.after(() => repo2.cleanup());
+ExplicitDepPruneFromSubDir.after(() => repo3.cleanup());
+
+// Actually execute all the tests
+Prune.run();
+PruneFromSubDir.run();
+ExplicitDepPrune.run();
+ExplicitDepPruneFromSubDir.run();
