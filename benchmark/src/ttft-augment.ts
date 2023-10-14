e33bb7d1473bd6372a2c6be9e04dbbec9072b17b
Mehul Kar <mehul.kar@vercel.com>
2023-10-13 21:16:49 -0500

    yes

--- benchmark/src/ttft-augment.ts
+++ benchmark/src/ttft-augment.ts
@@ -5,10 +5,16 @@ process.argv.forEach((val, index) => {
   console.log({ index, val });
 });
 
-const contents = fs.readFileSync("../ttft.json");
+const filePath = process.argv[2];
+const runID = process.argv[3];
+
+const contents = fs.readFileSync(filePath);
 const data = JSON.parse(contents.toString());
+
 const commitDetails = getCommitDetails();
+
 data.commitSha = commitDetails.commitSha;
 data.commitTimestamp = commitDetails.commitTimestamp;
+data.runID = runID;
 
-fs.writeFileSync("../ttft.json", JSON.stringify(data, null, 2));
+fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
