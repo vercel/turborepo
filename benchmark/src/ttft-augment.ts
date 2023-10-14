f7266f84487ac83c377656e6123ff755dbad6dc1
Mehul Kar <mehul.kar@vercel.com>
2023-10-13 21:43:04 -0500

    Send data with node also for cross compat

--- benchmark/src/ttft-augment.ts
+++ benchmark/src/ttft-augment.ts
@@ -1,16 +1,31 @@
 import fs from "fs";
+import fetch from "node-fetch";
 import { getCommitDetails } from "./helpers";
 
 const filePath = process.argv[2];
-const runID = process.argv[3];
+const url = process.argv[3];
 
-const contents = fs.readFileSync(filePath);
-const data = JSON.parse(contents.toString());
+const DATA_SOURCE_URL =
+  "https://api.us-east.tinybird.co/v0/events?name=turborepo_perf_ttft";
 
-const commitDetails = getCommitDetails();
+(async function () {
+  const contents = fs.readFileSync(filePath);
+  const data = JSON.parse(contents.toString());
 
-data.commitSha = commitDetails.commitSha;
-data.commitTimestamp = commitDetails.commitTimestamp;
-data.url = `https://github.com/vercel/turbo/actions/runs/${runID}`;
+  const commitDetails = getCommitDetails();
 
-fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  data.commitSha = commitDetails.commitSha;
+  data.commitTimestamp = commitDetails.commitTimestamp;
+  data.url = url;
+
+  console.log("Sending data to Tinybird: ", data);
+
+  await fetch(DATA_SOURCE_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.TINYBIRD_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+})();
