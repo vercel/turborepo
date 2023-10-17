55b1b24dedd0803ead57c703ec44f2be8cce86d6
Mehul Kar <mehul.kar@vercel.com>
2023-10-17 09:23:36 -0500

    Bring back the try/catch

--- benchmark/src/ttft.ts
+++ benchmark/src/ttft.ts
@@ -23,10 +23,17 @@ console.log(
 
 // Path to profile.json is ../profile.json because we are in
 // benchmark/large-monorepo (i.e. REPO_PATH) when this runs
-cp.execSync(
-  `${TURBO_BIN} run build -vvv --experimental-rust-codepath --dry --skip-infer --profile=../profile.json`,
-  DEFAULT_EXEC_OPTS
-);
+try {
+  cp.execSync(
+    `${TURBO_BIN} run build -vvv --experimental-rust-codepath --dry --skip-infer --profile=../profile.json`,
+    DEFAULT_EXEC_OPTS
+  );
+} catch (e) {
+  // catch errors and exit. the build command seems to be erroring out due to very large output?
+  // need to chase it down, but the benchmark seems to still be working, and when the same turbo run build
+  // is executed _without_ a child process, it works and has a 0 exit code.
+  console.error("Error running turbo build", e);
+}
 
 // TODO: just do this in JS and send to TB here instead of child process?
 cp.execSync("jq -f src/fold.jq < profile.json > ttft.json", {
