3934c37dc1fced148b2db86dfd0b67ee7fb69a82
Mehul Kar <mehul.kar@vercel.com>
2023-10-19 15:35:55 -0500

    Make the linter work

--- examples/with-svelte/apps/docs/.eslintrc.cjs
+++ examples/with-svelte/apps/docs/.eslintrc.cjs
@@ -1,14 +1,3 @@
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+	extends: ['custom']
 };
