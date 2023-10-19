6e3f307ee1ae75c00961b13b396962fd13ddc678
Mehul Kar <mehul.kar@vercel.com>
2023-10-19 15:26:40 -0500

    Add ui package to each app

--- examples/with-svelte/apps/docs/src/routes/+page.svelte
+++ examples/with-svelte/apps/docs/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import { MyCounterButton } from 'ui';
+</script>
+
+<h1>Docs</h1>
+<MyCounterButton />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- examples/with-svelte/apps/web/src/routes/+page.svelte
+++ examples/with-svelte/apps/web/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import { MyCounterButton } from 'ui';
+</script>
+
+<h1>Web</h1>
+<MyCounterButton />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
