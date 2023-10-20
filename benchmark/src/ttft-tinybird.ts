1aa610a7d8cfeda5b8fe2a193960a7ab2615cab6
Mehul Kar <mehul.kar@vercel.com>
2023-10-20 16:44:05 -0500

    feat(benchmark): post TTFT to slack (#6220)

--- benchmark/src/ttft-tinybird.ts
+++ benchmark/src/ttft-tinybird.ts
@@ -1,6 +1,5 @@
-import fs from "node:fs";
 import fetch from "node-fetch";
-import { getCommitDetails } from "./helpers";
+import { getTTFTData } from "./helpers";
 
 const filePath = process.argv[2];
 const runID = process.argv[3];
@@ -10,19 +9,15 @@ if (!token) {
   throw new Error("Missing TINYBIRD_TOKEN env variable");
 }
 
-const DATA_SOURCE_URL =
-  "https://api.us-east.tinybird.co/v0/events?name=turborepo_perf_ttft";
+if (!runID) {
+  throw new Error("Missing runID");
+}
+
+const DATA_SOURCE_NAME = "turborepo_perf_ttft";
+const DATA_SOURCE_URL = `https://api.us-east.tinybird.co/v0/events?name=${DATA_SOURCE_NAME}`;
 
 async function main() {
-  const contents = fs.readFileSync(filePath);
-  const data = JSON.parse(contents.toString());
-
-  const commitDetails = getCommitDetails();
-
-  data.commitSha = commitDetails.commitSha;
-  data.commitTimestamp = commitDetails.commitTimestamp;
-  data.url = `https://github.com/vercel/turbo/actions/runs/${runID}`;
-
+  const data = getTTFTData(filePath, runID);
   console.log("Sending data to Tinybird: ", data);
 
   const res = await fetch(DATA_SOURCE_URL, {
