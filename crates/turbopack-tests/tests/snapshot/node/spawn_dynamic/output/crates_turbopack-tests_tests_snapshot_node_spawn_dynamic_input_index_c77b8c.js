25176dab0e9f5ac07f9ad2d08668dc57f6a058bf
Will Binns-Smith <wbinnssmith@gmail.com>
2023-10-23 11:44:01 -0700

    fixup! Merge remote-tracking branch 'origin/main' into wbinnssmith/node-match-spawn

--- crates/turbopack-tests/tests/snapshot/node/spawn_dynamic/output/crates_turbopack-tests_tests_snapshot_node_spawn_dynamic_input_index_c77b8c.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/crates_turbopack-tests_tests_snapshot_node_spawn_dynamic_input_index_c77b8c.js",
-    {},
-    {"otherChunks":["output/crates_turbopack-tests_tests_snapshot_node_spawn_dynamic_input_7d9470._.js"],"runtimeModuleIds":["[project]/crates/turbopack-tests/tests/snapshot/node/spawn_dynamic/input/index.js (ecmascript)"]}
-]);
-// Dummy runtime
