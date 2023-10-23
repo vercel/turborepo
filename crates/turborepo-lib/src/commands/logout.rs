4bcb71ab14b583bb77a956bae76812e3b29ae984
nicholaslyang <Nicholas Yang>
2023-10-23 13:25:21 -0400

    Refactored logout to no longer use closure

--- crates/turborepo-lib/src/commands/logout.rs
+++ crates/turborepo-lib/src/commands/logout.rs
@@ -1,33 +1,37 @@
 use anyhow::{anyhow, Error, Result};
+use tracing::error;
 use turborepo_auth::logout as auth_logout;
 
 use crate::{commands::CommandBase, rewrite_json::unset_path};
 
 pub fn logout(base: &mut CommandBase) -> Result<()> {
-    let ui = base.ui;
+    if let Err(err) = remove_token(base) {
+        error!("could not logout. Something went wrong: {}", err);
+        return Err(err);
+    }
 
-    // Passing a closure here while we figure out how to make turborepo-auth
-    // crate manage its own configuration for the path to the token.
-    let set_token = || -> Result<(), Error> {
-        let global_config_path = base.global_config_path()?;
-        let before = global_config_path
-            .read_existing_to_string_or(Ok("{}"))
-            .map_err(|e| {
-                anyhow!(
-                    "Encountered an IO error while attempting to read {}: {}",
-                    global_config_path,
-                    e
-                )
-            })?;
+    auth_logout(&base.ui);
 
-        if let Some(after) = unset_path(&before, &["token"], true)? {
-            global_config_path
-                .create_with_contents(after)
-                .map_err(Error::from)
-        } else {
-            Ok(())
-        }
-    };
+    Ok(())
+}
 
-    auth_logout(&ui, set_token)
+fn remove_token(base: &mut CommandBase) -> Result<()> {
+    let global_config_path = base.global_config_path()?;
+    let before = global_config_path
+        .read_existing_to_string_or(Ok("{}"))
+        .map_err(|e| {
+            anyhow!(
+                "Encountered an IO error while attempting to read {}: {}",
+                global_config_path,
+                e
+            )
+        })?;
+
+    if let Some(after) = unset_path(&before, &["token"], true)? {
+        global_config_path
+            .create_with_contents(after)
+            .map_err(Error::from)
+    } else {
+        Ok(())
+    }
 }
