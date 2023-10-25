8bd530c37154ecc347384b3ff33a48415fa04f66
Mehul Kar <mehul.kar@vercel.com>
2023-10-25 14:59:43 -0500

    more xcompat

--- turborepo-tests/integration/setup.mjs
+++ turborepo-tests/integration/setup.mjs
@@ -1,5 +1,11 @@
 import { execSync } from "child_process";
 
+const venvName = ".cram_env";
+const venvPath = path.join(__dirname, venvName);
+const venvBin = path.join(venvPath, "bin");
+const venvPython = path.join(venvBin, "python3");
+const venvPip = path.join(venvBin, "pip");
+
 execSync("python3 -m venv .cram_env");
-execSync(".cram_env/bin/python3 -m pip install --quiet --upgrade pip");
-execSync('.cram_env/bin/pip install "prysk==0.15.0"');
+execSync(`${venvPython} -m pip install --quiet --upgrade pip`);
+execSync(`${venvPip} install "prysk==0.15.0"`);
