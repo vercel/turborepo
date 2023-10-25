4667da34e7ada6da8049eef9565257c821e6c5ce
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-24 21:33:52 -0700

    Remove leak

--- crates/turbopack-css/src/lifetime_util.rs
+++ crates/turbopack-css/src/lifetime_util.rs
@@ -8,9 +8,9 @@ pub fn stylesheet_into_static<'i, 'o>(
     options: ParserOptions<'o, 'i>,
 ) -> StyleSheet<'i, 'o> {
     let sources = ss.sources.clone();
-    dbg!("stylesheet_into_static::after sources.clone", &ss.sources);
     let rules = ss.rules.clone().into_owned();
-    dbg!("stylesheet_into_static::after rules.clone", &ss.sources);
+
+    //
 
     StyleSheet::new(sources, rules, options)
 }
