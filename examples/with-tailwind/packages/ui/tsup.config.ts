7822dfd447083c7815b516b3900861cef8511d73
Willi#m ⬣ <email@williamli.dev>
2023-10-28 10:22:55 +1000

    Update tsup.config.ts to support "use client" of Next.js
    Right now the example works because nothing inside `ui` requires client-side rendering.
    
    `tsup` removes "use client" banners from files and breaks Client Components.
    
    Problem and fixes found at https://stackoverflow.com/questions/76525345/client-components-in-turborepo-cause-error

--- examples/with-tailwind/packages/ui/tsup.config.ts
+++ examples/with-tailwind/packages/ui/tsup.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
+  banner: {
+    js: `"use client"`,
+  },
   treeshake: true,
   splitting: true,
   entry: ["src/**/*.tsx"],
