1b1ec04e3e4921e22ec4d3a4e8a98d042bca6805
Mehul Kar <mehul.kar@vercel.com>
2023-10-30 16:41:14 -0500

    some debug for npm install

--- turborepo-tests/helpers/setup_git.sh
+++ turborepo-tests/helpers/setup_git.sh
@@ -4,6 +4,8 @@ TARGET_DIR=$1
 # If a second parameter isn't passed, default to true
 SHOULD_INSTALL=${2:-true}
 
+echo "setup_git.sh: TARGET_DIR: ${TARGET_DIR}"
+
 # Set the autocrlf option to true to avoid issues with line endings on Windows
 if [[ "$OSTYPE" == "msys" ]]; then
   git config --global core.autocrlf true
@@ -13,10 +15,13 @@ git init ${TARGET_DIR} --quiet --initial-branch=main
 GIT_ARGS="--git-dir=${TARGET_DIR}/.git --work-tree=${TARGET_DIR}"
 git ${GIT_ARGS} config user.email "turbo-test@example.com"
 git ${GIT_ARGS} config user.name "Turbo Test"
+
 echo "script-shell=$(which bash)" > ${TARGET_DIR}/.npmrc
 
 if [ $SHOULD_INSTALL == "true" ]; then
+  echo "setup_git.sh: npm install"
   npm --prefix=${TARGET_DIR} install --silent
 fi
+
 git ${GIT_ARGS} add .
 git ${GIT_ARGS} commit -m "Initial" --quiet
