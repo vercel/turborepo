707b818a21a78c4c1bba512557781455c9015524
William Li <email@williamli.dev>
2023-10-30 17:21:01 +1100

    put server content inside dist/server

--- examples/with-tailwind/packages/ui/tsup.config.ts
+++ examples/with-tailwind/packages/ui/tsup.config.ts
@@ -1,13 +1,38 @@
 import { defineConfig, Options } from "tsup";
+import path from "path";
+import fs from "fs-extra";
 
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
   entry: ["src/index.tsx", "src/client/index.tsx"],
-  format: ["esm"],
+  outDir: 'dist/server',
+  format: ['esm', 'cjs'],
   dts: true,
   minify: true,
   clean: true,
   external: ["react"],
   ...options,
+  async onSuccess() {
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // add "use client" banner to /dist/client entry point
+    await fs.writeFile(
+      path.join(__dirname, "dist", "server", "client", "index.js"),
+      '"use client";\n' + await fs.readFile(path.join(__dirname, "dist", "server", "client", "index.js"))
+    );
+    await fs.writeFile(
+      path.join(__dirname, "dist", "server", "client", "index.mjs"),
+      '"use client";\n' + await fs.readFile(path.join(__dirname, "dist", "server", "client", "index.mjs"))
+    );
+      
+    // move /dist/server/client to /dist/client
+    try {
+      fs.moveSync(path.join(__dirname, "dist", "server", "client"), path.join(__dirname, "dist", "client"), { overwrite: true })
+      
+    } catch (err) {
+      console.error(err)
+    }
+  },
 }));
