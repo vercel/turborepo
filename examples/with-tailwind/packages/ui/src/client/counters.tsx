f6ce1636925cb0822d9f21c2ec0f7aba4a2441b4
William Li <email@williamli.dev>
2023-10-31 13:07:39 +1100

    clean up

--- examples/with-tailwind/packages/ui/src/client/counters.tsx
+++ examples/with-tailwind/packages/ui/src/client/counters.tsx
@@ -9,12 +9,14 @@ export function Counters() {
 
   return (
     <div>
-      <p>You clicked A {countA} times.</p>
-      <p>You clicked B {countB} times.</p>
+      <p>You clicked A {countA} times,</p>
+      <p>You clicked B {countB} times,</p>
       <p>You clicked C {countC} times.</p>
-      <button className="ui-bg-neutral-500 ui-p-4 ui-rounded-lg ui-pointer-cursor ui-border-2 border-solid ui-border-neutral-300 hover:ui-bg-white hover:ui-text-neutral-500 ui-m-4 ui-mx-auto" onClick={() => setCountA(countA + 1)}>A</button>
-      <button className="ui-bg-neutral-500 ui-p-4 ui-rounded-lg ui-pointer-cursor ui-border-2 border-solid ui-border-neutral-300 hover:ui-bg-white hover:ui-text-neutral-500 ui-m-4 ui-mx-auto" onClick={() => setCountB(countB + 1)}>B</button>
-      <button className="ui-bg-neutral-500 ui-p-4 ui-rounded-lg ui-pointer-cursor ui-border-2 border-solid ui-border-neutral-300 hover:ui-bg-white hover:ui-text-neutral-500 ui-m-4 ui-mx-auto" onClick={() => setCountC(countC + 1)}>C</button>
+      <div className="ui-flex space-x-2">
+        <button className="ui-bg-neutral-600 ui-p-4 ui-rounded-lg ui-pointer-cursor ui-border-2 border-solid ui-border-neutral-300 hover:ui-bg-white hover:ui-text-neutral-600 ui-m-4 ui-mx-auto" onClick={() => setCountA(countA + 1)}>A</button>
+        <button className="ui-bg-purple-900 ui-p-4 ui-rounded-lg ui-pointer-cursor ui-border-2 border-solid ui-border-neutral-300 hover:ui-bg-white hover:ui-text-red-900 ui-m-4 ui-mx-auto" onClick={() => setCountB(countB + 1)}>B</button>
+        <button className="ui-bg-red-900 ui-p-4 ui-rounded-lg ui-pointer-cursor ui-border-2 border-solid ui-border-neutral-300 hover:ui-bg-white hover:ui-text-red-900 ui-m-4 ui-mx-auto" onClick={() => setCountC(countC + 1)}>C</button>
+      </div>
     </div>
   )
 }
--- examples/with-tailwind/packages/ui/tsup.config.ts
+++ examples/with-tailwind/packages/ui/tsup.config.ts
@@ -31,13 +31,8 @@ export default defineConfig((options: Options) => ({
   external: ["react"],
   ...options,
   async onSuccess() {
-    console.log("tsup onSuccess...")
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    console.log("tsup onSuccess after 1000ms...")
 
     const paths = jsFileScan(path.join(__dirname, "dist", "client"))
-
-    // console.log({paths})
 
     await Promise.all(paths.map(async (file) => {
       await fs.writeFile(
@@ -45,21 +40,6 @@ export default defineConfig((options: Options) => ({
         '"use client";\n' + await fs.readFile(file)
       );
     }));
-
-    
-    
-    // fs.writeFileSync(
-    //   path.join(__dirname, "dist", "client", "index.mjs"),
-    //   '"use client";\n' + await fs.readFile(path.join(__dirname, "dist", "client", "index.mjs"))
-    // );
-
-    // move /dist/server/client to /dist/client
-    // try {
-    //   fs.moveSync(path.join(__dirname, "dist", "server", "client"), path.join(__dirname, "dist", "client"), { overwrite: true })
-      
-    // } catch (err) {
-    //   console.error(err)
-    // }
 
     
   },
