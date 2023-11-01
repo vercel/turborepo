f9db4e4b57ff46520e9821121adf4758f78456c3
William Li <email@williamli.dev>
2023-11-01 12:49:08 +1100

    update docs workspace to use ui/card for <Card/>

--- examples/with-tailwind/apps/docs/src/app/page.tsx
+++ examples/with-tailwind/apps/docs/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Card } from "ui";
+import { Card } from "ui/card";
 
 function Gradient({
   conic,
@@ -12,9 +12,8 @@ function Gradient({
 }): JSX.Element {
   return (
     <span
-      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
-        small ? "blur-[32px]" : "blur-[75px]"
-      } ${conic ? "bg-glow-conic" : ""} ${className}`}
+      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${small ? "blur-[32px]" : "blur-[75px]"
+        } ${conic ? "bg-glow-conic" : ""} ${className}`}
     />
   );
 }
