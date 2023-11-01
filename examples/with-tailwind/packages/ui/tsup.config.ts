13ab401a5d07ac41981b5f1a3aec307d6caf19a4
William Li <email@williamli.dev>
2023-11-01 13:55:00 +1100

    set clean to false

--- examples/with-tailwind/packages/ui/tsup.config.ts
+++ examples/with-tailwind/packages/ui/tsup.config.ts
@@ -2,45 +2,43 @@ import { defineConfig, Options } from "tsup";
 import path from "path";
 import fs from "fs-extra";
 
-const jsFileScan = (dir:string):string[] => {
-  
-  const paths = []
+const jsFileScan = (dir: string): string[] => {
+  const paths = [];
   const nodes = fs.readdirSync(dir);
   for (const node of nodes) {
     const nodePath = path.join(dir, node);
     const stat = fs.statSync(nodePath);
     if (stat.isDirectory()) {
       paths.push(...jsFileScan(nodePath));
-    } else if (stat.isFile() && (node.endsWith(".js") || node.endsWith(".mjs"))) {
+    } else if (
+      stat.isFile() &&
+      (node.endsWith(".js") || node.endsWith(".mjs"))
+    ) {
       paths.push(nodePath);
     }
   }
 
   return paths;
-}
+};
 
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
   entry: ["src/**/*.tsx"],
-  outDir: 'dist',
-  format: ['esm'],
+  outDir: "dist",
+  format: ["esm"],
   dts: true,
   minify: true,
-  clean: true,
+  clean: false,
   external: ["react"],
   ...options,
   async onSuccess() {
+    const paths = jsFileScan(path.join(__dirname, "dist", "client"));
 
-    const paths = jsFileScan(path.join(__dirname, "dist", "client"))
-
-    await Promise.all(paths.map(async (file) => {
-      await fs.writeFile(
-        file,
-        '"use client";\n' + await fs.readFile(file)
-      );
-    }));
-
-    
+    await Promise.all(
+      paths.map(async (file) => {
+        await fs.writeFile(file, '"use client";\n' + (await fs.readFile(file)));
+      }),
+    );
   },
 }));
