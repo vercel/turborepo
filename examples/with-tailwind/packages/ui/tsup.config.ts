59721f13090c47639d65d7470cb550abfc11c4cd
William Li <email@williamli.dev>
2023-11-01 13:53:12 +1100

    Revert "implemented option 2"
    This reverts commit 31ebfff6a83e44a9de0647bf9dd840db73c40aea.

--- examples/with-tailwind/packages/ui/tsup.config.ts
+++ examples/with-tailwind/packages/ui/tsup.config.ts
@@ -2,87 +2,45 @@ import { defineConfig, Options } from "tsup";
 import path from "path";
 import fs from "fs-extra";
 
-const cfg = {
-  treeshake: true,
-  splitting: true,
-  entry: ["src/**/*.tsx"],
-  outDir: "dist",
-  format: ["esm"],
-  dts: true,
-  minify: true,
-  clean: true,
-  external: ["react"],
-};
-
-const jsFileScan = (dir: string): string[] => {
-  const paths = [];
+const jsFileScan = (dir:string):string[] => {
+  
+  const paths = []
   const nodes = fs.readdirSync(dir);
   for (const node of nodes) {
     const nodePath = path.join(dir, node);
     const stat = fs.statSync(nodePath);
     if (stat.isDirectory()) {
       paths.push(...jsFileScan(nodePath));
-    } else if (
-      stat.isFile() &&
-      (node.endsWith(".js") || node.endsWith(".mjs"))
-    ) {
+    } else if (stat.isFile() && (node.endsWith(".js") || node.endsWith(".mjs"))) {
       paths.push(nodePath);
     }
   }
 
   return paths;
-};
+}
 
-// export default defineConfig((options: Options) => ({
-//   treeshake: true,
-//   splitting: true,
-//   entry: ["src/**/*.tsx"],
-//   outDir: "dist",
-//   format: ["esm"],
-//   dts: true,
-//   minify: true,
-//   clean: true,
-//   external: ["react"],
-//   ...options,
-//   async onSuccess() {
-//     const paths = jsFileScan(path.join(__dirname, "dist", "client"));
+export default defineConfig((options: Options) => ({
+  treeshake: true,
+  splitting: true,
+  entry: ["src/**/*.tsx"],
+  outDir: 'dist',
+  format: ['esm'],
+  dts: true,
+  minify: true,
+  clean: true,
+  external: ["react"],
+  ...options,
+  async onSuccess() {
 
-//     await Promise.all(
-//       paths.map(async (file) => {
-//         await fs.writeFile(file, '"use client";\n' + (await fs.readFile(file)));
-//       }),
-//     );
-//   },
-// }));
+    const paths = jsFileScan(path.join(__dirname, "dist", "client"))
 
-export default defineConfig([
-  {
-    ...cfg,
-    entry: ["src/client/**/*.tsx"],
-    outDir: "dist/client",
-    // esbuildOptions: (options) => {
-    // // this will not work when treeshake is enabled
-    //   // Append "use client" to the top of the react entry point
-    //   options.banner = {
-    //     js: '"use client";',
-    //   };
-    // },
-    async onSuccess() {
-      const paths = jsFileScan(path.join(__dirname, "dist", "client"));
-
-      await Promise.all(
-        paths.map(async (file) => {
-          await fs.writeFile(
-            file,
-            '"use client";\n' + (await fs.readFile(file)),
-          );
-        }),
+    await Promise.all(paths.map(async (file) => {
+      await fs.writeFile(
+        file,
+        '"use client";\n' + await fs.readFile(file)
       );
-    },
+    }));
+
+    
   },
-  {
-    ...cfg,
-    entry: ["src/**/*.tsx"],
-    outDir: "dist/server",
-  },
-]);
+}));
