31b56cc6b0d4bd581d770ab917dd5f1cd3f3327e
Mehul Kar <mehul.kar@vercel.com>
2023-11-02 12:36:42 -0500

    Revert "debug"
    This reverts commit e56d8f2bf794918e47134528bc0b5b7ce28184c1.

--- turborepo-tests/integration/tests/_helpers/setup_monorepo.sh
+++ turborepo-tests/integration/tests/_helpers/setup_monorepo.sh
@@ -7,13 +7,7 @@ TURBOREPO_TESTS_DIR="$SCRIPT_DIR/../../.."
 TARGET_DIR=$1
 cp -a ${SCRIPT_DIR}/../$FIXTURE/. ${TARGET_DIR}/
 
-echo "before git setup"
-cat -vet "${TARGET_DIR}/foo.txt"
-
 ${TURBOREPO_TESTS_DIR}/helpers/setup_git.sh ${TARGET_DIR}
-
-echo "after git setup"
-cat -vet "${TARGET_DIR}/foo.txt"
 
 # Update package manager
 if [ "$3" != "" ]; then
