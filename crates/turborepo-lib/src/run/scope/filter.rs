6507eda0a03870b32426fdf7eb4101e97ae0afdb
Greg Soltis <greg.soltis@vercel.com>
2023-11-02 10:11:05 -0700

    Update crates/turborepo-lib/src/run/scope/filter.rs
    Co-authored-by: Chris Olszewski <chris.olszewski@vercel.com>

--- crates/turborepo-lib/src/run/scope/filter.rs
+++ crates/turborepo-lib/src/run/scope/filter.rs
@@ -148,7 +148,7 @@ impl<'a, T: PackageChangeDetector> FilterResolver<'a, T> {
             // return all packages in the workspace
             self.pkg_graph
                 .workspaces()
-                .filter(|(name, _)| **name != WorkspaceName::Root)
+                .filter(|(name, _)| matches!(name, WorkspaceName::Other(_)))
                 .map(|(name, _)| name.to_owned())
                 .collect()
         } else {
