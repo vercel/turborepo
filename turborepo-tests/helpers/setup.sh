2c9bc65c8b5133cd98e2f99161a52f7ace3fd55f
Mehul Kar <mehul.kar@vercel.com>
2023-11-03 18:14:25 -0500

    Re-anebla local turbo build

--- turborepo-tests/helpers/setup.sh
+++ turborepo-tests/helpers/setup.sh
@@ -3,7 +3,6 @@
 THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 ROOT_DIR="${THIS_DIR}/../.."
 
-#TURBO=${ROOT_DIR}/target/debug/turbo
-TURBO=$(which turbo)
+TURBO=${ROOT_DIR}/target/debug/turbo
 VERSION=${ROOT_DIR}/version.txt
 TMPDIR=$(mktemp -d)
