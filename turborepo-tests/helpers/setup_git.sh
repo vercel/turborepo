9b9a12addf9948074ef97c99f26d6633c606ca1b
Mehul Kar <mehul.kar@vercel.com>
2023-11-03 13:16:32 -0500

    Update crlf issues

--- turborepo-tests/helpers/setup_git.sh
+++ turborepo-tests/helpers/setup_git.sh
@@ -6,19 +6,6 @@ SHOULD_INSTALL=${2:-true}
 
 git init ${TARGET_DIR} --quiet --initial-branch=main
 
-# Set the autocrlf option to "true", that when we init this new git repo
-# any new files added to the index will convert CRLF to LF. The files in the fixture
-# at the start should all be LF already because they were created with LF line endings
-# and in CI, on Windows, we set autocrlf to `input` to tell git to retain those line endings
-# when the monorepo is cloned (see .github/workflows/test.yml). We do this after `git init`
-# and set the config locally so it doesn't interfere with the global config set in test.yml.
-# https://www.git-scm.com/book/en/v2/Customizing-Git-Git-Configuration#_core_autocrlf
-# if [[ "$OSTYPE" == "msys" ]]; then
-#   git config --local core.autocrlf input
-# fi
-
-git config --list
-
 GIT_ARGS="--git-dir=${TARGET_DIR}/.git --work-tree=${TARGET_DIR}"
 git ${GIT_ARGS} config user.email "turbo-test@example.com"
 git ${GIT_ARGS} config user.name "Turbo Test"
