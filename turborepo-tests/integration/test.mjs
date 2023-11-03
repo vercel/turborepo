98d71b2030aa508126bb50fd121e659f68bf249f
Mehul Kar <mehul.kar@vercel.com>
2023-11-03 11:10:02 -0500

    instal fork of prysk to see if it fixes windows cleanup issue

--- turborepo-tests/integration/test.mjs
+++ turborepo-tests/integration/test.mjs
@@ -25,8 +25,8 @@ execSync(`${python3} -m pip install --quiet --upgrade pip`, {
   stdio: "inherit",
 });
 
-console.log("install prysk@15");
-execSync(`${pip} install "prysk"`, { stdio: "inherit" });
+console.log("install prysk");
+execSync(`${pip} install "prysk-windows"`, { stdio: "inherit" });
 
 // disable package manager update notifiers
 process.env.NO_UPDATE_NOTIFIER = 1;
