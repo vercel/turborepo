62c5df081d85a78c2db09eb826c9ec4eeffc476a
Mehul Kar <mehul.kar@vercel.com>
2023-11-03 02:33:48 -0500

    rm dupe

--- turborepo-tests/integration/tests/_helpers/setup.sh
+++ turborepo-tests/integration/tests/_helpers/setup.sh
@@ -8,5 +8,4 @@ FIXTURE_NAME="${1-basic_monorepo}"
 
 # Run global setup script. Using source means that it executes the script in the current
 # shell instead of a subshell, so env vars are preserved.
-source "${TURBOREPO_TESTS_DIR}/helpers/setup.sh"
 source "${TURBOREPO_TESTS_DIR}/helpers/setup_repo.sh"
