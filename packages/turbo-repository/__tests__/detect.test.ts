0177606c171d5bd7b6b3d17b53dfe6faa9cd08ef
Greg Soltis <Greg Soltis>
2023-11-06 16:05:26 -0800

    Update for proper api

--- packages/turbo-repository/__tests__/detect.test.ts
+++ packages/turbo-repository/__tests__/detect.test.ts
@@ -1,16 +1,16 @@
 import * as path from "node:path";
-import { detectJsRepository } from "../js/dist/index.js";
+import { Repository } from "../js/dist/index.js";
 
 describe("detect", () => {
   it("detects a repo", async () => {
-    const repo = await detectJsRepository();
+    const repo = await Repository.detectJS();
     console.log(repo);
     const expectedRoot = path.resolve(__dirname, "../../..");
     expect(repo.root).toBe(expectedRoot);
   });
 
   it("enumerates workspaces", async () => {
-    const repo = await detectJsRepository();
+    const repo = await Repository.detectJS();
     const workspaces = await repo.workspaces();
     expect(workspaces.length).not.toBe(0);
   });
