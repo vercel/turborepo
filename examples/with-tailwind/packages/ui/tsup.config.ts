88f94144fe846c474170acb85b63b877212d2e55
Nikita <nikita2008101@gmail.com>
2023-11-08 13:04:39 +0100

    docs(examples): add types d.ts gen instead of .d.mts

--- examples/with-tailwind/packages/ui/tsup.config.ts
+++ examples/with-tailwind/packages/ui/tsup.config.ts
@@ -4,6 +4,10 @@ export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
   entry: ["src/**/*.tsx"],
+  // package.json["types"] should match the generated file ext.
+  // example: esm -> index.d.mts and cjs -> index.d.ts.
+  // problem: eslint `import/recommended` can't resolve package.json["types"] with `.d.mts` (esm only).
+  // ref: https://github.com/vercel/turbo/pull/6390
   format: ["esm", "cjs"],
   dts: true,
   minify: true,
