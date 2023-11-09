c50e95816998b1824e5658a451741f08ec8c3e91
Mehul Kar <mehul.kar@vercel.com>
2023-11-09 01:12:43 -0600

    Remove debug

--- turborepo-tests/helpers/setup.sh
+++ turborepo-tests/helpers/setup.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 
 THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
-
-# if [[ "$OSTYPE" == "msys" ]]; then
-#     ROOT_DIR="${THIS_DIR}\\..\\.."
-#     TURBO=${ROOT_DIR}\\target\\debug\\turbo.exe
-# else
-#     ROOT_DIR="${THIS_DIR}/../.."
-#     TURBO=${ROOT_DIR}/target/debug/turbo
-# fi
-
 ROOT_DIR="${THIS_DIR}/../.."
 TURBO=${ROOT_DIR}/target/debug/turbo
-
-echo "------------------------"
-echo "BASHSOURCE: ${BASH_SOURCE[0]}"
-echo "THIS_DIR: ${THIS_DIR}"
-echo "ROOT_DIR: ${ROOT_DIR}"
-echo "TURBO: ${TURBO}"
-echo "readlink TURBO: $(readlink -f ${TURBO})"
-echo "------------------------"
-
 VERSION=${ROOT_DIR}/version.txt
 TMPDIR=$(mktemp -d)
--- turborepo-tests/integration/tests/command-bin.t
+++ turborepo-tests/integration/tests/command-bin.t
@@ -2,10 +2,6 @@ Setup
   $ . ${TESTDIR}/../../helpers/setup.sh
   $ . ${TESTDIR}/_helpers/setup_monorepo.sh $(pwd)
 
-Run info
-  $ ls -al $PWD/node_modules/turbo*
-
-  $ ${TURBO} bin -vvv
   $ ${TURBO} bin -vvv > out.log
   $ grep --quiet -E "Global turbo version: .*" out.log
   $ grep --quiet -E "No local turbo binary found at" out.log
