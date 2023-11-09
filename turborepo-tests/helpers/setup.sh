a7fd1c6dda26985af99aa0e5b9a466f9b6037fd9
Mehul Kar <mehul.kar@vercel.com>
2023-11-08 22:33:36 -0600

    try assigning TURBO with backslashes

--- turborepo-tests/helpers/setup.sh
+++ turborepo-tests/helpers/setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
-ROOT_DIR="${THIS_DIR}/../.."
 
 if [[ "$OSTYPE" == "msys" ]]; then
-    TURBO=${ROOT_DIR}/target/debug/turbo.exe
+    ROOT_DIR="${THIS_DIR}\\..\\.."
+    TURBO=${ROOT_DIR}\\target\\debug\\turbo.exe
 else
+    ROOT_DIR="${THIS_DIR}/../.."
     TURBO=${ROOT_DIR}/target/debug/turbo
 fi
 
