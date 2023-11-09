c834e34fa9c696a692c4c9b1811df72aac958969
Mehul Kar <mehul.kar@vercel.com>
2023-11-09 14:59:42 -0600

    add exe extension for turbo that is being tested on windows

--- turborepo-tests/helpers/setup.sh
+++ turborepo-tests/helpers/setup.sh
@@ -2,6 +2,14 @@
 
 THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 ROOT_DIR="${THIS_DIR}/../.."
-TURBO=${ROOT_DIR}/target/debug/turbo
+
+if [[ "${OSTYPE}" == "msys" ]]; then
+  EXT=".exe"
+else
+  EXT=""
+fi
+
+TURBO=${ROOT_DIR}/target/debug/turbo${EXT}
+
 VERSION=${ROOT_DIR}/version.txt
 TMPDIR=$(mktemp -d)
