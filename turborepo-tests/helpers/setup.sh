07c2ddcb23873b5a032430b09d46510045f41112
Mehul Kar <mehul.kar@vercel.com>
2023-11-08 23:27:44 -0600

    more debug, go back to real paths, turbo run is working just fine

--- turborepo-tests/helpers/setup.sh
+++ turborepo-tests/helpers/setup.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-THIS_DIR=$(dirname "$(realpath "$0")")
+REALPATH="$(realpath "$0")"
+THIS_DIR=$(dirname "${REALPATH}")
 
-if [[ "$OSTYPE" == "msys" ]]; then
-    ROOT_DIR="${THIS_DIR}\\..\\.."
-    TURBO=${ROOT_DIR}\\target\\debug\\turbo.exe
-else
-    ROOT_DIR="${THIS_DIR}/../.."
-    TURBO=${ROOT_DIR}/target/debug/turbo
-fi
+# if [[ "$OSTYPE" == "msys" ]]; then
+#     ROOT_DIR="${THIS_DIR}\\..\\.."
+#     TURBO=${ROOT_DIR}\\target\\debug\\turbo.exe
+# else
+#     ROOT_DIR="${THIS_DIR}/../.."
+#     TURBO=${ROOT_DIR}/target/debug/turbo
+# fi
+
+ROOT_DIR="${THIS_DIR}/../.."
+TURBO=${ROOT_DIR}/target/debug/turbo
 
 echo "------------------------"
 echo "BASHSOURCE: ${BASH_SOURCE[0]}"
+echo "REALPATH: ${REALPATH}"
 echo "THIS_DIR: ${THIS_DIR}"
 echo "ROOT_DIR: ${ROOT_DIR}"
 echo "TURBO: ${TURBO}"
