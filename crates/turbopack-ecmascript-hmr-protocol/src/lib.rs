6df3c66638afa63af0167152ca0b2250f31e4f42
Tobias Koppers <tobias.koppers@googlemail.com>
2023-11-09 08:36:40 +0000

    update protocol

--- crates/turbopack-ecmascript-hmr-protocol/src/lib.rs
+++ crates/turbopack-ecmascript-hmr-protocol/src/lib.rs
@@ -124,7 +124,13 @@ pub struct Asset<'a> {
 #[derive(Serialize)]
 pub struct IssueSource<'a> {
     pub asset: Asset<'a>,
-    pub range: Option<(SourcePos, SourcePos)>,
+    pub range: Option<IssueSourceRange>,
+}
+
+#[derive(Serialize)]
+pub struct IssueSourceRange {
+    pub start: SourcePos,
+    pub end: SourcePos,
 }
 
 #[derive(Serialize)]
@@ -150,7 +156,9 @@ impl<'a> From<&'a PlainIssue> for Issue<'a> {
             asset: Asset {
                 path: &source.asset.ident,
             },
-            range: source.range,
+            range: source
+                .range
+                .map(|(start, end)| IssueSourceRange { start, end }),
         });
 
         Issue {
