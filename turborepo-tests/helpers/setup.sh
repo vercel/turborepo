8c4299074551863b8c30779fe6bc2cafc8a3c600
Mehul Kar <mehul.kar@vercel.com>
2023-11-08 23:45:46 -0600

    go back to bash source and readlink it

--- turborepo-tests/helpers/setup.sh
+++ turborepo-tests/helpers/setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-REALPATH="$(realpath "$0")"
-THIS_DIR=$(dirname "${REALPATH}")
+THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 # if [[ "$OSTYPE" == "msys" ]]; then
 #     ROOT_DIR="${THIS_DIR}\\..\\.."
@@ -16,10 +15,10 @@ TURBO=${ROOT_DIR}/target/debug/turbo
 
 echo "------------------------"
 echo "BASHSOURCE: ${BASH_SOURCE[0]}"
-echo "REALPATH: ${REALPATH}"
 echo "THIS_DIR: ${THIS_DIR}"
 echo "ROOT_DIR: ${ROOT_DIR}"
 echo "TURBO: ${TURBO}"
+echo "readlink TURBO: $(readlink -f ${TURBO})"
 echo "------------------------"
 
 VERSION=${ROOT_DIR}/version.txt
