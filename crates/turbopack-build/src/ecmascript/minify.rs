3287a6b5108d9cd5f9cd9b3aa4393e6938af9074
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-10 15:13:27 +0900

    fi

--- crates/turbopack-build/src/ecmascript/minify.rs
+++ crates/turbopack-build/src/ecmascript/minify.rs
@@ -6,7 +6,6 @@ use swc_common::{
     comments::{Comments, SingleThreadedComments},
     BytePos, FileName, FilePathMapping, LineCol, Mark, SourceMap as SwcSourceMap, GLOBALS,
 };
-use swc_core::ecma::{self};
 use swc_ecma_ast::{EsVersion, Program};
 use swc_ecma_codegen::{
     text_writer::{
@@ -97,7 +96,7 @@ async fn perform_minify(path: Vc<FileSystemPath>, code_vc: Vc<Code>) -> Result<V
                     },
                 );
 
-                program.fold_with(&mut ecma::transforms::base::fixer::fixer(Some(
+                program.fold_with(&mut swc_ecma_transforms_base::fixer::fixer(Some(
                     &comments as &dyn Comments,
                 )))
             }))
