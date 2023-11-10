cad7bfb9afe85235c246b7bc53f445d1339aa3d1
Chris Olszewski <Chris Olszewski>
2023-11-09 16:20:01 -0800

    feat: make rust codepath opt-out

--- crates/turborepo-lib/src/cli/mod.rs
+++ crates/turborepo-lib/src/cli/mod.rs
@@ -821,12 +821,12 @@ pub async fn run(
             }
             let base = CommandBase::new(cli_args.clone(), repo_root, version, ui);
 
-            if env::var("EXPERIMENTAL_RUST_CODEPATH").as_deref() == Ok("true") {
+            if env::var("EXPERIMENTAL_RUST_CODEPATH").as_deref() == Ok("false") {
+                Ok(Payload::Go(Box::new(base)))
+            } else {
                 use crate::commands::run;
                 let exit_code = run::run(base).await?;
                 Ok(Payload::Rust(Ok(exit_code)))
-            } else {
-                Ok(Payload::Go(Box::new(base)))
             }
         }
         Command::Prune {
