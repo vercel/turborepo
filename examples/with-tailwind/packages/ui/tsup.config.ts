a2da1d9d34b32a4bb46e2b408d302eaf726bf0c3
Sébastien Beaujard <sebastien.beaujard@gmail.com>
2023-11-14 16:42:21 +0100

    Update tsup.config.ts related to problems with typescript
    in package.json of ui it use "types": "./dist/index.d.ts",
    But with the current configuration of tsup, i won't generate the index.d.ts file but a index.d.mts file.

--- examples/with-tailwind/packages/ui/tsup.config.ts
+++ examples/with-tailwind/packages/ui/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
   entry: ["src/**/*.tsx"],
-  format: ["esm"],
+  format: ["cjs"],
   dts: true,
   minify: true,
   clean: true,
