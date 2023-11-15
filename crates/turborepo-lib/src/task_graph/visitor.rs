39b0fd46b853b8d576ae3e39f42ed0f20296e089
Alexander Lyon <arlyon@me.com>
2023-11-15 13:02:27 +0100

    adjust tracing for run finalization (#6410)
    ### Description
    
    The final steps of our runs are missing some detail so this PR adds
    them.
    
    
    Closes TURBO-1623

--- crates/turborepo-lib/src/task_graph/visitor.rs
+++ crates/turborepo-lib/src/task_graph/visitor.rs
@@ -270,6 +270,13 @@ impl<'a> Visitor<'a> {
 
     /// Finishes visiting the tasks, creates the run summary, and either
     /// prints, saves, or sends it to spaces.
+    #[tracing::instrument(skip(
+        self,
+        packages,
+        global_hash_inputs,
+        engine,
+        env_at_execution_start
+    ))]
     pub(crate) async fn finish(
         self,
         exit_code: i32,
