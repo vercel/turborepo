1548f8b018ab29e79830059eb5b3941f01a09fa0
Anthony Shew <anthonyshew@gmail.com>
2023-11-16 15:08:54 -0700

    Lint generators.

--- examples/basic/packages/ui/.eslintrc.js
+++ examples/basic/packages/ui/.eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   extends: ["@repo/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: "./tsconfig.lint.json",
   },
-  ignorePatterns: ["turbo/**"],
 };
--- examples/basic/packages/ui/turbo/generators/config.ts
+++ examples/basic/packages/ui/turbo/generators/config.ts
@@ -2,7 +2,6 @@ import type { PlopTypes } from "@turbo/gen";
 
 // Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
 
-// eslint-disable-next-line import/no-default-export -- Turbo generators require default export
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   // A simple generator to add a new React component to the internal UI library
   plop.setGenerator("react-component", {
