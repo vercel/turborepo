77cbfc071cd8eadf6e571114420193f1612daa51
Anthony Shew <anthonyshew@gmail.com>
2023-11-16 15:04:02 -0700

    Use Workspace Configurations.

--- examples/basic/apps/web/.eslintrc.js
+++ examples/basic/apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: [require.resolve("@repo/eslint-config/next.js")],
+  extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
