793ab4aab9757319e3559e053e7100f0029e2aeb
Mehul Kar <mehul.kar@vercel.com>
2023-11-19 11:25:55 -0600

    set file permissions with icacls

--- turborepo-tests/integration/tests/find-turbo/setup.sh
+++ turborepo-tests/integration/tests/find-turbo/setup.sh
@@ -33,7 +33,10 @@ if [[ "$OSTYPE" == "msys" && $FIXTURE_NAME == "linked" ]]; then
   rm -rf node_modules/turbo
   pushd node_modules > /dev/null || exit 1
   cmd //c mklink turbo .pnpm\\turbo@1.0.0\\node_modules\\turbo
+  echo "running chmod on new symlink turbo"
   chmod +rwx turbo
+  echo "running icacls on new symlink turbo"
+  cmd //c icacls turbo /grant Everyone:\(F\)
   ls -al
   popd > /dev/null || exit 1
   echo "PWD: $PWD"
