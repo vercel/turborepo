bcfabc64650e95e9d5a374e81a20b821f4819339
Mehul Kar <mehul.kar@vercel.com>
2023-11-19 20:50:20 -0600

    Copy turbo.exe to fixtures directory instead of keeping it in fixture

--- packages/turbo-exe-stub/build.sh
+++ packages/turbo-exe-stub/build.sh
@@ -1,9 +1,39 @@
 #!/usr/bin/env bash
 
-if [[ "$OSTYPE" == "msys" ]]; then
-  echo "Building stub turbo.exe for windows platform"
-  g++ turbo.cpp -o turbo.exe
-else
+if [[ "$OSTYPE" != "msys" ]]; then
   echo "Skipping build for non-windows platform"
+  exit
 fi
 
+
+echo "Building stub turbo.exe for windows platform"
+g++ turbo.cpp -o turbo.exe
+
+echo "PWD: $PWD"
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+ROOT_DIR="$SCRIPT_DIR/../.."
+
+FIND_TURBO_FIXTURES_DIR="${ROOT_DIR}/turborepo-tests/integration/tests/_fixtures/find_turbo"
+
+
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/hoisted/node_modules/turbo-windows-64/bin/"
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/hoisted/node_modules/turbo-windows-arm64/bin/"
+
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/linked/node_modules/.pnpm/turbo-windows-64@1.0.0/node_modules/turbo-windows-64/bin/"
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/linked/node_modules/.pnpm/turbo-windows-arm64@1.0.0/node_modules/turbo-windows-arm64/bin/"
+
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/nested/node_modules/turbo/node_modules/turbo-windows-64/bin/"
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/nested/node_modules/turbo/node_modules/turbo-windows-arm64/bin/"
+
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/self/node_modules/turbo-windows-64/bin/"
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/self/node_modules/turbo-windows-arm64/bin/"
+
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/unplugged/.yarn/unplugged/turbo-windows-64-npm-1.0.0-520925a700/node_modules/turbo-windows-64/bin/"
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/unplugged/.yarn/unplugged/turbo-windows-arm64-npm-1.0.0-520925a700/node_modules/turbo-windows-arm64/bin/"
+
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/unplugged_env_moved/.moved/unplugged/turbo-windows-64-npm-1.0.0-520925a700/node_modules/turbo-windows-64/bin/"
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/unplugged_env_moved/.moved/unplugged/turbo-windows-arm64-npm-1.0.0-520925a700/node_modules/turbo-windows-arm64/bin/"
+
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/unplugged_moved/.moved/unplugged/turbo-windows-64-npm-1.0.0-520925a700/node_modules/turbo-windows-64/bin/"
+cp turbo.exe "${FIND_TURBO_FIXTURES_DIR}/unplugged_moved/.moved/unplugged/turbo-windows-arm64-npm-1.0.0-520925a700/node_modules/turbo-windows-arm64/bin/"
