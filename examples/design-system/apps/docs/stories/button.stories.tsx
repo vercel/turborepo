f3cf513d6db8e5fec3e578962030bcf4fbcb198a
Anthony Shew <anthonyshew@gmail.com>
2023-11-22 16:54:22 -0700

    Rename packages.

--- examples/design-system/apps/docs/stories/button.stories.tsx
+++ examples/design-system/apps/docs/stories/button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "ui";
+import { Button } from "@repo/ui";
 
 const meta: Meta<typeof Button> = {
   component: Button,
