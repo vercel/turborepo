6646133bac12db02ff78967e85691e244ebf06c8
Anthony Shew <anthony.shew@vercel.com>
2023-11-24 13:34:28 -0700

    Update tailwind.config.ts
    Missed a slash.

--- examples/with-tailwind/packages/ui/tailwind.config.ts
+++ examples/with-tailwind/packages/ui/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src**/*.tsx"],
+  content: ["./src/**/*.tsx"],
   prefix: "ui-",
   presets: [sharedConfig],
 };
