29c59a2b7fb86a2135bc9fc06c6a1d13d88238fc
Tobias Koppers <tobias.koppers@googlemail.com>
2023-11-27 15:12:25 +0000

    documentation

--- crates/turbopack-core/src/context.rs
+++ crates/turbopack-core/src/context.rs
@@ -14,13 +14,20 @@ use crate::{
 /// type (e. g. from FileSource to ModuleAsset).
 #[turbo_tasks::value_trait]
 pub trait AssetContext {
+    /// Gets the compile time info of the asset context.
     fn compile_time_info(self: Vc<Self>) -> Vc<CompileTimeInfo>;
+
+    /// Gets the layer of the asset context.
     fn layer(self: Vc<Self>) -> Vc<String>;
+
+    /// Gets the resolve options for a given path.
     fn resolve_options(
         self: Vc<Self>,
         origin_path: Vc<FileSystemPath>,
         reference_type: Value<ReferenceType>,
     ) -> Vc<ResolveOptions>;
+
+    /// Resolves an request to an [ModuleResolveResult].
     fn resolve_asset(
         self: Vc<Self>,
         origin_path: Vc<FileSystemPath>,
@@ -28,15 +35,23 @@ pub trait AssetContext {
         resolve_options: Vc<ResolveOptions>,
         reference_type: Value<ReferenceType>,
     ) -> Vc<ModuleResolveResult>;
+
+    /// Process a source into a module. This might return None, if this should
+    /// lead to no module at all, e. g. in cases where side effect free module
+    /// is imported for its side effects.
     fn process(
         self: Vc<Self>,
         asset: Vc<Box<dyn Source>>,
         reference_type: Value<ReferenceType>,
     ) -> Vc<OptionModule>;
+
+    /// Process an [ResolveResult] into an [ModuleResolveResult].
     fn process_resolve_result(
         self: Vc<Self>,
         result: Vc<ResolveResult>,
         reference_type: Value<ReferenceType>,
     ) -> Vc<ModuleResolveResult>;
+
+    /// Gets a new AssetContext with the transition applied.
     fn with_transition(self: Vc<Self>, transition: String) -> Vc<Box<dyn AssetContext>>;
 }
