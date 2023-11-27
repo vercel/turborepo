31a0be5ed8a0ff8ec14b1a936de256143feff3ea
Chris Olszewski <Chris Olszewski>
2023-11-27 15:09:55 -0800

    fix: print partial seconds

--- crates/turborepo-lib/src/run/summary/duration.rs
+++ crates/turborepo-lib/src/run/summary/duration.rs
@@ -41,10 +41,27 @@ impl fmt::Display for TurboDuration {
         }
 
         if duration.num_seconds() > 0 {
-            let seconds = duration.num_seconds() - duration.num_minutes() * 60;
+            let seconds_in_ms = duration.num_milliseconds() - duration.num_minutes() * 60 * 1000;
+            let seconds = (seconds_in_ms as f64) / 1000.0;
             write!(f, "{}s", seconds)?;
         }
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use pretty_assertions::assert_eq;
+    use test_case::test_case;
+
+    use super::*;
+
+    #[test_case(TurboDuration::from(Duration::milliseconds(120)), "120ms")]
+    #[test_case(TurboDuration::from(Duration::milliseconds(1500)), "1.5s")]
+    #[test_case(TurboDuration::from(Duration::milliseconds(1234)), "1.234s")]
+    #[test_case(TurboDuration::from(Duration::seconds(90)), "1m30s")]
+    fn duration_formatting(duration: TurboDuration, expected: &str) {
+        assert_eq!(duration.to_string(), expected);
     }
 }
