db1ecce4bd36c34e0e767196e3b348c2b5efc61e
Anthony Shew <anthonyshew@gmail.com>
2023-11-27 15:53:14 -0700

    Remove ESLint warning.

--- examples/with-prisma/packages/database/src/client.ts
+++ examples/with-prisma/packages/database/src/client.ts
@@ -1,10 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // eslint-disable-next-line no-unused-vars
-  var prisma: PrismaClient | undefined;
-}
-
 export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
