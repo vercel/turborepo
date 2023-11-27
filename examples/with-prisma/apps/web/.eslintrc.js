6299edc40b6fef7762299eabcd542dfca8e730f7
Anthony Shew <anthony.shew@vercel.com>
2023-11-27 16:32:52 -0700

    Update with-prisma example. (#6571)
    Updating the with-prisma example!
    
    Closes TURBO-1708
    
    Things to pay attention to:
    
    - [x] Is tasking correct? Are all tasks successful out-of-the-box? Do I
    hit cache when I'm expecting?
    - [x] If I add an unused variable in a workspace, do I see a warning in
    both my editor and when I run turbo lint?
    - [ ] Does auto-importing work across packages? _This is suspiciously
    not working and I can't get it to...Is `yarn` doing something weird?_
    - [x] Anything else wonky in your editor?
    - [x] Do I get proper hot-reloading in turbo dev?
    - [x] Anything else that may come up!

--- examples/with-prisma/apps/web/.eslintrc.js
+++ examples/with-prisma/apps/web/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/library.js"],
+  extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
