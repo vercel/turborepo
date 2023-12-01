a8c3fe5773995cc01988256d31e4e67c708aa4ab
Anthony Shew <anthony.shew@vercel.com>
2023-12-01 08:37:42 -0700

    Update with-yarn example. (#6557)
    This example was freshly bootstrapped with our new `create-turbo`.
    
    ---------
    
    Co-authored-by: Turbobot <turbobot@vercel.com>

--- examples/with-yarn/packages/ui/.eslintrc.js
+++ examples/with-yarn/packages/ui/.eslintrc.js
@@ -1,9 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@repo/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: "./tsconfig.lint.json",
   },
 };
