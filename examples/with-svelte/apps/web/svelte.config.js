e0e1685233a03cb82ae360cde639a50deff1996f
Anthony Shew <anthonyshew@gmail.com>
2023-12-01 08:56:40 -0700

    Nevermind.

--- examples/with-svelte/apps/web/svelte.config.js
+++ examples/with-svelte/apps/web/svelte.config.js
@@ -12,7 +12,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-  runtime: "nodejs20.x"
 };
 
 export default config;
