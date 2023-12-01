d3e81d9797aa667e3e95dd46cdfd5f88d25abe84
Mehul Kar <mehul.kar@vercel.com>
2023-12-01 07:01:32 -0600

    Use correct script

--- turborepo-tests/helpers/setup_integration_test.sh
+++ turborepo-tests/helpers/setup_integration_test.sh
@@ -26,7 +26,7 @@ TURBOREPO_INTEGRATION_TESTS_DIR="${TURBOREPO_TESTS_DIR}/integration/tests"
 
 cp -a "${TURBOREPO_INTEGRATION_TESTS_DIR}/$FIXTURE/." "${TARGET_DIR}/"
 
-"${TURBOREPO_TESTS_DIR}/helpers/setup_git_old.sh" ${TARGET_DIR}
+"${TURBOREPO_TESTS_DIR}/helpers/setup_git.sh" ${TARGET_DIR}
 
 # default package manager is npm
 PACKAGE_MANAGER_NAME="npm"
