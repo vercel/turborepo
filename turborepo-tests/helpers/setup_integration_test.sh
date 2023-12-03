79bdf77446c6f5c3c67d653bd90ae68de138f9b2
Mehul Kar <mehul.kar@vercel.com>
2023-12-03 11:46:13 -0600

    chore(tests): remove duplicate setup_git script (#6680)
    We can setup our fixture directory the same way for examples and
    integration tests.
     
    This incrementally moves our setup to be able to run some or
    all of our tests against a particular fixture/example/or OSS repo, a
    particular package manager, on a particular platform.

--- turborepo-tests/helpers/setup_integration_test.sh
+++ turborepo-tests/helpers/setup_integration_test.sh
@@ -26,16 +26,10 @@ TURBOREPO_INTEGRATION_TESTS_DIR="${TURBOREPO_TESTS_DIR}/integration/tests"
 
 cp -a "${TURBOREPO_INTEGRATION_TESTS_DIR}/$FIXTURE/." "${TARGET_DIR}/"
 
-"${TURBOREPO_TESTS_DIR}/helpers/setup_git_old.sh" ${TARGET_DIR}
-
-# default package manager is npm
-PACKAGE_MANAGER_NAME="npm"
+"${TURBOREPO_TESTS_DIR}/helpers/setup_git.sh" ${TARGET_DIR}
 
 # Update package manager if one was provided
 if [ "$PACKAGE_MANAGER" != "" ]; then
-  # If a package manager was provided, set the PACKAGE_MANAGER_NAME by removing the
-  # specific version from the argument.
-  PACKAGE_MANAGER_NAME=$(echo "$PACKAGE_MANAGER" | sed 's/@.*//')
   # Use jq to write a new file with a .packageManager field set and then
   # Overwrite original package.json. For some reason the command above won't send its output
   # directly to the original file.
@@ -48,5 +42,13 @@ if [ "$PACKAGE_MANAGER" != "" ]; then
     dos2unix --quiet "$TARGET_DIR/package.json"
   fi
 
-  git commit -am "Updated package manager to $PACKAGE_MANAGER_NAME" --quiet
+  git commit -am "Updated package manager to $PACKAGE_MANAGER" --quiet
 fi
+
+# Install dependencies with the given package manager
+PACKAGE_MANAGER_NAME="npm"
+if [ "$PACKAGE_MANAGER" != "" ]; then
+  PACKAGE_MANAGER_NAME=$(echo "$PACKAGE_MANAGER" | sed 's/@.*//')
+fi
+
+"${TURBOREPO_TESTS_DIR}/helpers/install_deps.sh" "$PACKAGE_MANAGER_NAME"
--- turborepo-tests/integration/tests/inference/no_workspaces_setup.sh
+++ turborepo-tests/integration/tests/inference/no_workspaces_setup.sh
@@ -3,9 +3,23 @@
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 TARGET_DIR=$1
 TURBOREPO_TESTS_DIR="$SCRIPT_DIR/../../.."
-SETUP_GIT_SCRIPT="${TURBOREPO_TESTS_DIR}/helpers/setup_git_old.sh"
+SETUP_GIT_SCRIPT="${TURBOREPO_TESTS_DIR}/helpers/setup_git.sh"
+
+function install_deps() {
+  local dir=$1
+  pushd $dir > /dev/null || exit 1
+  npm install --silent
+  popd > /dev/null || exit 1
+  git --git-dir="${dir}/.git" --work-tree="${dir}" add .
+  git --git-dir="${dir}/.git" --work-tree="${dir}" commit -m "Install dependencies" --quiet
+}
 
 cp -a ${SCRIPT_DIR}/../_fixtures/inference/no_workspaces/. ${TARGET_DIR}/
 ${SETUP_GIT_SCRIPT} ${TARGET_DIR}
+install_deps "${TARGET_DIR}"
+
 ${SETUP_GIT_SCRIPT} ${TARGET_DIR}/parent
+install_deps "${TARGET_DIR}/parent"
+
 ${SETUP_GIT_SCRIPT} ${TARGET_DIR}/parent/child
+install_deps "${TARGET_DIR}/parent/child"
