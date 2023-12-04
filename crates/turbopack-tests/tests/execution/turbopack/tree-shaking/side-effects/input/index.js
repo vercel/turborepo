3a2cb34d1ed24f32f26bba12fcae5211cffafe8e
Tobias Koppers <tobias.koppers@googlemail.com>
2023-12-04 14:58:50 +0000

    add test case

--- crates/turbopack-tests/tests/execution/turbopack/tree-shaking/side-effects/input/index.js
+++ crates/turbopack-tests/tests/execution/turbopack/tree-shaking/side-effects/input/index.js
@@ -49,12 +49,19 @@ import { a as a6 } from "package-reexport-side-effect";
 import { effects as effects6 } from "package-reexport-side-effect/check-side-effect";
 it("should run side effects of a reexporting module with side effects", () => {
   expect(a6).toBe("a");
-  expect(effects6).toEqual(["side-effect.js", "index.js"]);
+  expect(effects6).toEqual(["side-effect.js", "side-effect2.js", "index.js"]);
 });
 
 import { a as a7 } from "package-reexport-tla-side-effect";
 import { effects as effects7 } from "package-reexport-tla-side-effect/check-side-effect";
 it("should run side effects of a reexporting module with side effects (async modules)", () => {
   expect(a7).toBe("a");
-  expect(effects7).toEqual(["side-effect.js", "index.js"]);
+  expect(effects7).toEqual(["side-effect.js", "side-effect2.js", "index.js"]);
+});
+
+import { effects as effects8 } from "package-require-side-effect/check-side-effect";
+it("should run side effects of a reexporting module with side effects (async modules)", () => {
+  expect(effects8).toEqual([]);
+  require("package-require-side-effect");
+  expect(effects8).toEqual(["side-effect.js", "side-effect2.js", "index.js"]);
 });
