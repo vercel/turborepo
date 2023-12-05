b2fa86b8824088d022ffdec33b3a5e7e23a367b1
Mehul Kar <Mehul Kar>
2023-12-05 00:16:22 -0800

    Consistent env var for root dir

--- turborepo-tests/helpers/setup.sh
+++ turborepo-tests/helpers/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
-ROOT_DIR="${THIS_DIR}/../.."
+MONOREPO_ROOT_DIR="${THIS_DIR}/../.."
 
 if [[ "${OSTYPE}" == "msys" ]]; then
   EXT=".exe"
@@ -9,4 +9,4 @@ else
   EXT=""
 fi
 
-TURBO=${ROOT_DIR}/target/debug/turbo${EXT}
+TURBO=${MONOREPO_ROOT_DIR}/target/debug/turbo${EXT}
