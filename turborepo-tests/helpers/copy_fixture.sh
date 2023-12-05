9a9463e345502761e2671deb4d3fd64c42a08074
Mehul Kar <Mehul Kar>
2023-12-04 21:45:08 -0800

    fix merge conflict

--- turborepo-tests/helpers/copy_fixture.sh
+++ turborepo-tests/helpers/copy_fixture.sh
@@ -3,13 +3,6 @@ set -eo pipefail
 
 TARGET_DIR=$1
 FIXTURE="${2-basic_monorepo}"
-<<<<<<< HEAD
-
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
-MONOREPO_ROOT_DIR="${SCRIPT_DIR}/../.."
-TURBOREPO_TESTS_DIR="${MONOREPO_ROOT_DIR}/turborepo-tests"
-FIXTURES_DIR="${TURBOREPO_TESTS_DIR}/integration/fixtures"
-=======
 FIXTURES_DIR="$3"
 
 # If a fixtures directory isn't provided, we use the default one we use for integration tests
@@ -17,6 +10,5 @@ if [ "$FIXTURES_DIR" == "" ]; then
   echo "Pass a fixtures directory"
   exit 1
 fi
->>>>>>> main
 
 cp -a "${FIXTURES_DIR}/$FIXTURE/." "${TARGET_DIR}/"
