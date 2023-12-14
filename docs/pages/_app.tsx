d17f97a52ca2ff900516a9ef6aeb9bf3cbd763f7
Anthony Shew <anthonyshew@gmail.com>
2023-12-14 16:52:21 -0700

    Satisfy TypeScript.

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -14,7 +14,9 @@ type NextraAppProps = AppProps & {
 
 // Shim requestIdleCallback in Safari
 if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
+  // @ts-expect-error -- window isn't typed
   window.requestIdleCallback = (fn) => setTimeout(fn, 1);
+  // @ts-expect-error -- window isn't typed
   window.cancelIdleCallback = (e) => {
     clearTimeout(e);
   };
