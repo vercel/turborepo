f33d6fc88ec587b8d4bdb2202d4adcdc6fe0f01d
Anthony Shew <anthonyshew@gmail.com>
2023-12-21 13:32:27 -0700

    Ignoring and downranking.

--- docs/components/Main.tsx
+++ docs/components/Main.tsx
@@ -1,3 +1,42 @@
+import { useRouter } from "next/router";
+
+const ignoredRoutes = ["/blog", "/terms", "/privacy", "/confirm"];
+
+const downrankedRoutes = [
+  "/repo/docs/acknowledgements",
+  // Deprecations
+  "/repo/docs/core-concepts/scopes",
+];
+
 export function Main(props) {
-  return <main data-pagefind-body>{props.children}</main>;
+  const router = useRouter();
+
+  if (ignoredRoutes.some((route) => route === router.asPath)) {
+    return <main {...props} />;
+  }
+
+  if (
+    router.asPath.startsWith("/blog") ||
+    downrankedRoutes.some((route) => route === router.asPath)
+  ) {
+    return (
+      <main data-pagefind-body data-pagefind-weight=".2">
+        {props.children}
+      </main>
+    );
+  }
+
+  // Hiding the footer from search.
+  return (
+    <main data-pagefind-body {...props}>
+      {props.children.props.children.slice(0, -1)}
+      <div data-pagefind-ignore="all">
+        {
+          props.children.props.children[
+            props.children.props.children.length - 1
+          ]
+        }
+      </div>
+    </main>
+  );
 }
