cbc03d0a8125c8cbdc2df2c87c53446c583b023c
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2024-01-02 16:19:07 -0600

    more comment addressing

--- crates/turborepo-lib/src/commands/mod.rs
+++ crates/turborepo-lib/src/commands/mod.rs
@@ -4,7 +4,7 @@ use sha2::{Digest, Sha256};
 use turbopath::{AbsoluteSystemPath, AbsoluteSystemPathBuf};
 use turborepo_api_client::{APIAuth, APIClient, Client};
 use turborepo_auth::{
-    AuthFile, TURBOREPO_AUTH_FILE_NAME, TURBOREPO_CONFIG_DIR, TURBOREPO_LEGACY_AUTH_FILE_NAME,
+    TURBOREPO_AUTH_FILE_NAME, TURBOREPO_CONFIG_DIR, TURBOREPO_LEGACY_AUTH_FILE_NAME,
 };
 use turborepo_dirs::config_dir;
 use turborepo_ui::UI;
