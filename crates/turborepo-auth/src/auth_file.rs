e2e6f261a597322fd1688ec1cf0118b086f3631a
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2024-01-03 16:04:42 -0600

    Revert "add getters to auth token"
    This reverts commit b23caaef5be23c1487b5dd458e5fcb4f4c4711f7.

--- crates/turborepo-auth/src/auth_file.rs
+++ crates/turborepo-auth/src/auth_file.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, ops::Deref};
+use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 use turbopath::{AbsoluteSystemPath, AbsoluteSystemPathBuf};
@@ -92,12 +92,6 @@ impl AuthToken {
         } else {
             &self.api
         }
-    }
-    pub fn api(&self) -> &str {
-        &self.api
-    }
-    pub fn token(&self) -> &str {
-        &self.token
     }
 }
 
