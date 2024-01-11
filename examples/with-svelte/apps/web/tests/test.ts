d58ac6c0a00df4ee53f6723d5df606d0b635c6cd
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-01-11 16:19:20 +0100

    Fix with-svelte-example (#6986)
    ### Description
    
    - vite-plugin-svelte is now a peer dependency and therefore needs to
    appear in the user's `package.json`
    - fixed preprocessor import in `svelte.config.js`
    - removed `optimizeDeps.include` which makes Vite treat the package as
    an external dependency that it should bundle once, resulting in changes
    not getting picked up (this was introduced in #6576 but I'm not sure
    why)
    - fixed e2e tests
    
    ### Testing Instructions
    
    check manually
    
    Co-authored-by: Anthony Shew <anthony.shew@vercel.com>

--- examples/with-svelte/apps/web/tests/test.ts
+++ examples/with-svelte/apps/web/tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Web' })).toBeVisible();
 });
