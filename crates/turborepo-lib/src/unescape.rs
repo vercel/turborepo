4927d6ce4617e65b119f3bc9af89bf1fb43a4b65
nicholaslyang <nicholas.yang@vercel.com>
2024-01-19 13:02:17 -0600

    Switched to serde_json instead of hand rolled escape logic

--- crates/turborepo-lib/src/unescape.rs
+++ crates/turborepo-lib/src/unescape.rs
@@ -1,7 +1,6 @@
 use std::{fmt, fmt::Display, ops::Deref};
 
 use biome_deserialize::{Deserializable, DeserializableValue, DeserializationDiagnostic};
-use thiserror::Error;
 
 // We're using a newtype here because biome currently doesn't
 // handle escapes and we can't override the String deserializer
@@ -23,96 +22,10 @@ impl Deref for UnescapedString {
     }
 }
 
-#[derive(Copy, Clone)]
-enum EscapeState {
-    Normal,
-    // We just saw a `\`
-    Escape,
-    // We just saw a `\u` and now expect 4 hex digits
-    Unicode,
-}
+fn unescape_str(s: String) -> Result<String, serde_json::Error> {
+    let wrapped_s = format!("\"{}\"", s);
 
-#[derive(Debug, Error)]
-enum Error {
-    #[error("Invalid escape sequence: `{seq}`")]
-    InvalidEscapeSequence { seq: String },
-    #[error("Invalid unicode escape sequence ({reason}): `{seq}`")]
-    InvalidUnicodeEscapeSequence { seq: String, reason: &'static str },
-    #[error("Unexpected end of input, expected {expected}")]
-    UnexpectedEndOfInput { expected: &'static str },
-}
-
-fn unescape_str(s: String) -> Result<String, Error> {
-    let mut state = EscapeState::Normal;
-    // Invariant: unicode_digits.len() < 4 at the end of the loop
-    let mut unicode_digits = String::new();
-    let mut out = String::new();
-    for c in s.chars() {
-        match (c, state) {
-            ('\\' | '"' | '/', EscapeState::Escape) => {
-                out.push(c);
-                state = EscapeState::Normal;
-            }
-            ('b', EscapeState::Escape) => out.push('\x08'),
-            ('f', EscapeState::Escape) => out.push('\x0c'),
-            ('n', EscapeState::Escape) => out.push('\n'),
-            ('r', EscapeState::Escape) => out.push('\r'),
-            ('t', EscapeState::Escape) => out.push('\t'),
-            ('u', EscapeState::Escape) => {
-                unicode_digits = String::new();
-                state = EscapeState::Unicode;
-            }
-            (c, EscapeState::Escape) => {
-                return Err(Error::InvalidEscapeSequence {
-                    seq: format!("\\{}", c),
-                })
-            }
-            (c, EscapeState::Unicode) if c.is_ascii_hexdigit() => {
-                unicode_digits.push(c);
-            }
-            (c, EscapeState::Unicode) => {
-                return Err(Error::InvalidUnicodeEscapeSequence {
-                    seq: format!("\\u{}{}", unicode_digits, c),
-                    reason: "not a hex digit",
-                })
-            }
-            ('\\', EscapeState::Normal) => state = EscapeState::Escape,
-            (c, EscapeState::Normal) => out.push(c),
-        }
-
-        if unicode_digits.len() == 4 {
-            let codepoint = u32::from_str_radix(&unicode_digits, 16).map_err(|_| {
-                Error::InvalidUnicodeEscapeSequence {
-                    seq: format!("\\u{}", unicode_digits),
-                    reason: "not a valid hex number",
-                }
-            })?;
-            out.push(char::from_u32(codepoint).ok_or_else(|| {
-                Error::InvalidUnicodeEscapeSequence {
-                    seq: format!("\\u{}", unicode_digits),
-                    reason: "not a valid unicode codepoint",
-                }
-            })?);
-            unicode_digits = String::new();
-            state = EscapeState::Normal;
-        }
-    }
-
-    match state {
-        EscapeState::Normal => {}
-        EscapeState::Escape => {
-            return Err(Error::UnexpectedEndOfInput {
-                expected: "escape sequence",
-            })
-        }
-        EscapeState::Unicode => {
-            return Err(Error::UnexpectedEndOfInput {
-                expected: "unicode escape sequence",
-            })
-        }
-    }
-
-    Ok(out)
+    serde_json::from_str(&wrapped_s)
 }
 
 impl Deserializable for UnescapedString {
@@ -121,7 +34,7 @@ impl Deserializable for UnescapedString {
         name: &str,
         diagnostics: &mut Vec<DeserializationDiagnostic>,
     ) -> Option<Self> {
-        let Some(str) = String::deserialize(value, &name, diagnostics) else {
+        let Some(str) = String::deserialize(value, name, diagnostics) else {
             return None;
         };
 
