182015c99e948ff8dae6fb12d30fc091c5f9b901
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-22 20:05:37 +0900

    minify

--- crates/turbopack-ecmascript-plugins/src/transform/styled_jsx.rs
+++ crates/turbopack-ecmascript-plugins/src/transform/styled_jsx.rs
@@ -61,7 +61,7 @@ impl CustomTransformer for StyledJsxTransformer {
                         };
 
                         let output = ss.to_css(PrinterOptions {
-                            minify: false,
+                            minify: true,
                             source_map: None,
                             project_root: None,
                             targets,
