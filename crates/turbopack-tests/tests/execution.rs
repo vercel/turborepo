bc6ac9d4acc08a5d83b6ad1d91b852fd6b1e704b
Tobias Koppers <tobias.koppers@googlemail.com>
2024-01-23 10:25:50 +0000

    update usage of DevChunkingContext::builder

--- crates/turbopack-tests/tests/execution.rs
+++ crates/turbopack-tests/tests/execution.rs
@@ -292,7 +292,8 @@ async fn run_test(prepared_test: Vc<PreparedTest>) -> Result<Vc<RunTestResult>> 
 
     let chunking_context = DevChunkingContext::builder(
         project_root,
-        chunk_root_path,
+        path,
+        path,
         chunk_root_path,
         static_root_path,
         env,
