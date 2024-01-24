89b4083489565f045ea5101c57b635ecce3575b5
nicholaslyang <nicholas.yang@vercel.com>
2024-01-24 15:58:29 -0500

    Add tests for errors

--- turborepo-tests/helpers/replace_turbo_config.sh
+++ turborepo-tests/helpers/replace_turbo_config.sh
@@ -11,8 +11,7 @@ TURBO_CONFIGS_DIR="${MONOREPO_ROOT_DIR}/turborepo-tests/integration/fixtures/tur
 cp "${TURBO_CONFIGS_DIR}/$CONFIG_NAME" "$PROJECT_DIR/turbo.json"
 
 # Check if there are changes before trying to run git commit
-# Since we're replacing an existing turbo.json, git commit -a should work and we
-# don't need to git add anything.
 if [[ $(git status --porcelain) ]]; then
-  git commit --quiet -am "Use $CONFIG_NAME as turbo.json"
+  git add .
+  git commit --quiet -m "Use $CONFIG_NAME as turbo.json"
 fi
