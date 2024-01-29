4abfb910a43527178845a09880f6988918b2c9fd
Anthony Shew <anthonyshew@gmail.com>
2024-01-29 13:36:30 -0700

    Cleanup.

--- turborepo-tests/helpers/setup_example_test.sh
+++ turborepo-tests/helpers/setup_example_test.sh
@@ -19,18 +19,15 @@ fi
 # Use the right command for each package manager
 if [ "$package_manager" == "npm" ]; then
   package_manager_command="npm install"
-  update_turbo_version="npm install turbo@latest -w"
 elif [ "$package_manager" == "pnpm" ]; then
   package_manager_command="pnpm install"
-  update_turbo_version="pnpm install turbo@latest -w"
 elif [ "$package_manager" == "yarn" ]; then
   package_manager_command="yarn"
-  update_turbo_version="yarn upgrade turbo@latest -W"
 fi
 
 # All examples implement these two tasks
 # and it's reasonable to assume that they will continue to do so
-turbo_command="turbo build lint --output-logs=errors-only"
+turbo_command="turbo build lint"
 
 # Head into a temporary directory
 mkdir -p ../../examples-tests-tmp
@@ -52,10 +49,10 @@ mkdir -p ./tmp
 echo -e "$(cat .gitignore)" "\n/tmp/" >.gitignore
 
 # Simulating the user's first run and dumping logs to a file
-$package_manager_command >./tmp/first-install.txt 2>&1
+$package_manager_command >./tmp/install.txt 2>&1
 $turbo_command >./tmp/grep-me-for-miss.txt
 
-# We do not want to hit cache on first run because we're acting like a user.
+# We don't want to hit cache on first run because we're acting like a user.
 # A user would never hit cache on first run. Why should we?
 if grep -q ">>> FULL TURBO" ./tmp/grep-me-for-miss.txt; then
   echo "A FULL TURBO was found. This test is misconfigured (since it can hit a cache)."
@@ -65,10 +62,9 @@ if grep -q ">>> FULL TURBO" ./tmp/grep-me-for-miss.txt; then
 fi
 
 # Simulating the user's second run
-$package_manager_command >./tmp/second-install.txt 2>&1
 $turbo_command >./tmp/grep-me-for-hit.txt
 
-# Make sure the runs hit FULL TURBO on the second go
+# Make sure the user hits FULL TURBO on the second go
 if ! grep -q ">>> FULL TURBO" ./tmp/grep-me-for-hit.txt; then
   echo "No FULL TURBO was found. Dumping logs:"
   cat ./tmp/grep-me-for-hit.txt >&2
