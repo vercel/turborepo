6a9e7d66da599afa4f2d1a402984851d828b54d7
Mehul Kar <mehul.kar@vercel.com>
2024-01-31 23:05:51 -0600

    chore(turbo): delete Go related release code and config

--- cli/build_turbo.bat
+++
@@ -1,6 +0,0 @@
-cd ..\crates\turborepo
-cargo build --target x86_64-pc-windows-gnu
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-copy ..\..\target\x86_64-pc-windows-gnu\debug\turbo.exe ..\..\target\debug\turbo.exe
-cd ..\..\cli
