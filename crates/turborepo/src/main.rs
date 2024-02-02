2e99da269ff298c20a79cd5bfcb803ebf718c9c7
Nicholas Yang <nicholas.yang@vercel.com>
2024-02-01 20:24:21 -0500

    Update crates/turborepo/src/main.rs
    Co-authored-by: Chris Olszewski <chris.olszewski@vercel.com>

--- crates/turborepo/src/main.rs
+++ crates/turborepo/src/main.rs
@@ -15,13 +15,10 @@ use crate::panic_handler::panic_handler;
 fn main() -> Result<()> {
     std::panic::set_hook(Box::new(panic_handler));
 
-    let exit_code = match turborepo_lib::main() {
-        Ok(code) => code,
-        Err(err) => {
-            println!("{:?}", Report::new(err));
-            1
-        }
-    };
+    let exit_code = match turborepo_lib::main().unwrap_or_else(|err| {
+        println!("{:?}", Report::new(err));
+        1
+    });
 
     process::exit(exit_code)
 }
