a06d3210e3acace3f14db75968e91b761045dbb8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-05 18:57:41 +0900

    Patch one

--- crates/turbopack-ecmascript/src/references/mod.rs
+++ crates/turbopack-ecmascript/src/references/mod.rs
@@ -42,7 +42,7 @@ use swc_core::{
     ecma::{
         ast::*,
         visit::{
-            fields::{AssignExprField, ExprField, PatField, PatOrExprField},
+            fields::{AssignExprField, AssignTargetField, SimpleAssignTargetField},
             AstParentKind, AstParentNodeRef, VisitAstPath, VisitWithPath,
         },
     },
@@ -1839,9 +1839,8 @@ async fn handle_free_var_reference(
         [
             ..,
             AstParentKind::AssignExpr(AssignExprField::Left),
-            AstParentKind::PatOrExpr(PatOrExprField::Pat),
-            AstParentKind::Pat(PatField::Expr),
-            AstParentKind::Expr(ExprField::Member),
+            AstParentKind::AssignTarget(AssignTargetField::Simple),
+            AstParentKind::SimpleAssignTarget(SimpleAssignTargetField::Member),
         ]
     ) {
         return Ok(false);
