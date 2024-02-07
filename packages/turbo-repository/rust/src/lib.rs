b3404b7bf7f34efa3640a1bd69d5a0e1ca78c4c5
Mehul Kar <mehul.kar@vercel.com>
2024-02-07 16:29:59 -0600

    feat(@turbo/repository): add method to return package dependents (#7242)
    Add `.findPackagesAndDependents()` method that returns a map where keys
    are relative package paths and values are a list of package paths that depend
    on that package. All paths are anchored at the root of the monorepo.
    
    This method can be able to answer the question: Given that Package X
    changed, what other packages should also be considered changed?
    
    Co-authored-by: Chris Olszewski <chris.olszewski@vercel.com>
    Co-authored-by: Thomas Knickman <tom.knickman@vercel.com>

--- packages/turbo-repository/rust/src/lib.rs
+++ packages/turbo-repository/rust/src/lib.rs
@@ -1,11 +1,18 @@
+use std::{collections::HashMap, hash::Hash};
+
+use napi::Error;
 use napi_derive::napi;
 use turbopath::AbsoluteSystemPath;
-use turborepo_repository::inference::RepoState as WorkspaceState;
-
+use turborepo_repository::{
+    inference::RepoState as WorkspaceState,
+    package_graph::{PackageGraph, WorkspaceName, WorkspaceNode},
+};
 mod internal;
 
 #[napi]
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub struct Package {
+    pub name: String,
     /// The absolute path to the package root.
     #[napi(readonly)]
     pub absolute_path: String,
@@ -35,17 +42,49 @@ pub struct Workspace {
     /// The package manager used by the workspace.
     #[napi(readonly)]
     pub package_manager: PackageManager,
+    /// The package graph for the workspace.
+    graph: PackageGraph,
 }
 
+#[napi]
 impl Package {
-    fn new(workspace_path: &AbsoluteSystemPath, package_path: &AbsoluteSystemPath) -> Self {
+    fn new(
+        name: String,
+        workspace_path: &AbsoluteSystemPath,
+        package_path: &AbsoluteSystemPath,
+    ) -> Self {
         let relative_path = workspace_path
             .anchor(package_path)
             .expect("Package path is within the workspace");
         Self {
+            name,
             absolute_path: package_path.to_string(),
             relative_path: relative_path.to_string(),
         }
+    }
+
+    fn dependents(
+        &self,
+        graph: &PackageGraph,
+        workspace_path: &AbsoluteSystemPath,
+    ) -> Vec<Package> {
+        let node = WorkspaceNode::Workspace(WorkspaceName::Other(self.name.clone()));
+        let ancestors = match graph.immediate_ancestors(&node) {
+            Some(ancestors) => ancestors,
+            None => return vec![],
+        };
+
+        ancestors
+            .iter()
+            .filter_map(|node| {
+                let info = graph.workspace_info(node.as_workspace())?;
+                // If we don't get a package name back, we'll just skip it.
+                let name = info.package_name()?;
+                let anchored_package_path = info.package_path();
+                let package_path = workspace_path.resolve(anchored_package_path);
+                Some(Package::new(name, workspace_path, &package_path))
+            })
+            .collect()
     }
 }
 
@@ -62,5 +101,32 @@ impl Workspace {
     #[napi]
     pub async fn find_packages(&self) -> std::result::Result<Vec<Package>, napi::Error> {
         self.packages_internal().await.map_err(|e| e.into())
+    }
+
+    #[napi]
+    pub async fn find_packages_and_dependents(
+        &self,
+    ) -> Result<HashMap<String, Vec<String>>, Error> {
+        let packages = self.find_packages().await?;
+
+        let workspace_path = match AbsoluteSystemPath::new(self.absolute_path.as_str()) {
+            Ok(path) => path,
+            Err(e) => return Err(Error::from_reason(e.to_string())),
+        };
+
+        let map: HashMap<String, Vec<String>> = packages
+            .into_iter()
+            .map(|package| {
+                let deps = package.dependents(&self.graph, workspace_path);
+                let dep_names = deps
+                    .into_iter()
+                    .map(|p| p.relative_path)
+                    .collect::<Vec<String>>();
+
+                (package.relative_path, dep_names)
+            })
+            .collect();
+
+        Ok(map)
     }
 }
