c5a52b719caea9a3702c0f35a1b49bea1ca8c459
Chris Olszewski <chris.olszewski@vercel.com>
2024-02-08 09:40:46 -0800

    chore: add shutdown method for async cache (#7309)
    ### Description
    
    As per discussion in #7293 we should probably have a shutdown method.
    This PR adds a method that will prevent any future writes from being
    queued.
    
    ### Testing Instructions
    
    Added some shutdown tests to make sure that trying to use the cache
    post-shutdown will error. These probably should belong in their own
    testing methods, but considering the amount of work required to
    construct an AsyncCache I think this is acceptable.
    
    
    Closes TURBO-2282

--- crates/turborepo-lib/src/run/cache.rs
+++ crates/turborepo-lib/src/run/cache.rs
@@ -116,8 +116,9 @@ impl RunCache {
         }
     }
 
-    pub async fn wait_for_cache(&self) {
-        self.cache.wait().await
+    pub async fn shutdown_cache(&self) {
+        // Ignore errors coming from cache already shutting down
+        self.cache.shutdown().await.ok();
     }
 }
 
