9802395207aaa6bc36000d363ab5e99f3c66fb99
Mehul Kar <mehul.kar@vercel.com>
2024-02-13 14:55:00 -0600

    Revert "chore: update @turbo/repository callers of PackageGraph methods"
    This reverts commit b793673e8eb9d72a8f8557ce5f5aa3d57c676d69.

--- packages/turbo-repository/rust/src/lib.rs
+++ packages/turbo-repository/rust/src/lib.rs
@@ -9,7 +9,7 @@ use turbopath::{AbsoluteSystemPath, AnchoredSystemPathBuf};
 use turborepo_repository::{
     change_mapper::{ChangeMapper, PackageChanges},
     inference::RepoState as WorkspaceState,
-    package_graph::{PackageGraph, PackageName, PackageNode, WorkspacePackage, ROOT_PKG_NAME},
+    package_graph::{PackageGraph, PackageNode, WorkspaceName, WorkspacePackage, ROOT_PKG_NAME},
 };
 mod internal;
 
@@ -72,7 +72,7 @@ impl Package {
         graph: &PackageGraph,
         workspace_path: &AbsoluteSystemPath,
     ) -> Vec<Package> {
-        let node = PackageNode::Workspace(PackageName::Other(self.name.clone()));
+        let node = PackageNode::Workspace(WorkspaceName::Other(self.name.clone()));
         let ancestors = match graph.immediate_ancestors(&node) {
             Some(ancestors) => ancestors,
             None => return vec![],
@@ -81,7 +81,7 @@ impl Package {
         ancestors
             .iter()
             .filter_map(|node| {
-                let info = graph.package_info(node.as_package_name())?;
+                let info = graph.workspace_info(node.as_workspace())?;
                 // If we don't get a package name back, we'll just skip it.
                 let name = info.package_name()?;
                 let anchored_package_path = info.package_path();
@@ -166,7 +166,7 @@ impl Workspace {
         let packages = match package_changes {
             PackageChanges::All => self
                 .graph
-                .packages()
+                .workspaces()
                 .map(|(name, info)| WorkspacePackage {
                     name: name.to_owned(),
                     path: info.package_path().to_owned(),
@@ -178,8 +178,8 @@ impl Workspace {
         let mut serializable_packages: Vec<Package> = packages
             .into_iter()
             .filter(|p| match &p.name {
-                PackageName::Root => false,
-                PackageName::Other(name) => name != ROOT_PKG_NAME,
+                WorkspaceName::Root => false,
+                WorkspaceName::Other(name) => name != ROOT_PKG_NAME,
             })
             .map(|p| {
                 let package_path = workspace_root.resolve(&p.path);
