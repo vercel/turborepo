bc537773b702336622b0776c8e24e105f845d157
Tobias Koppers <tobias.koppers@googlemail.com>
2024-02-14 14:54:35 +0000

    clippy

--- crates/turbopack-core/src/resolve/mod.rs
+++ crates/turbopack-core/src/resolve/mod.rs
@@ -1636,7 +1636,7 @@ async fn resolve_into_folder(
                 if let Some(package_json) = &*read_package_json(package_json_path).await? {
                     if let Some(field_value) = package_json[name].as_str() {
                         let normalized_request = normalize_request(field_value);
-                        if normalized_request == ""
+                        if normalized_request.is_empty()
                             || normalized_request == "."
                             || normalized_request == "./"
                         {
