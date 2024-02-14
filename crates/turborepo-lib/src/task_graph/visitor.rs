d4ccb0b6bc6ff8e5efba7c776a58cbf19c8be381
Chris Olszewski <chris.olszewski@vercel.com>
2024-02-13 21:57:15 -0800

    fix: avoid grouping logs from tasks which ended in error (#7335)
    ### Description
    
    Fixes https://github.com/vercel/turbo/issues/6709
    
    Github Action doesn't provide a mechanism for grouping logs together and
    keeping them expanded which is helpful for highlighting errors.
    
    This PR changes the behavior so we avoid emitting the grouping
    header/footer if task execution was a failure.
    
    ### Testing Instructions
    
    Updated GH Action log grouping integration test
    
    
    Closes TURBO-2320

--- crates/turborepo-lib/src/task_graph/visitor.rs
+++ crates/turborepo-lib/src/task_graph/visitor.rs
@@ -392,6 +392,12 @@ impl<'a> Visitor<'a> {
                 (vendor_behavior.group_suffix)(&group_name),
             );
             logger.with_header_footer(Some(header), Some(footer));
+
+            let (error_header, error_footer) = (
+                vendor_behavior.error_group_prefix.map(|f| f(&group_name)),
+                vendor_behavior.error_group_suffix.map(|f| f(&group_name)),
+            );
+            logger.with_error_header_footer(error_header, error_footer);
         }
         logger
     }
@@ -690,7 +696,10 @@ impl ExecContext {
             .instrument(span)
             .await;
 
-        let logs = match output_client.finish() {
+        // If the task resulted in an error, do not group in order to better highlight
+        // the error.
+        let is_error = matches!(result, ExecOutcome::Task { .. });
+        let logs = match output_client.finish(is_error) {
             Ok(logs) => logs,
             Err(e) => {
                 telemetry.track_error(TrackedErrors::DaemonFailedToMarkOutputsAsCached);
