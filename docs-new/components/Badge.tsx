7be0fa96fe9a747aeb25c76858a6d772ea5feb5e
Anthony Shew <anthonyshew@gmail.com>
2024-02-20 00:14:57 -0700

    Blog looks done for now.

--- docs-new/components/Badge.tsx
+++ docs-new/components/Badge.tsx
@@ -12,7 +12,7 @@ export function Badge(props: BadgeProps) {
   return (
     <span
       className={cn(
-        "dark:text-black text-white inline-flex items-center justify-center shrink-0 box-border rounded-lg capitalize whitespace-nowrap font-bold tabular-nums h-5 px-2 text-xs bg-gradient-to-r from-[#d74a41] to-[#407aeb] align-middle",
+        "text-background inline-flex items-center justify-center shrink-0 box-border rounded-lg capitalize whitespace-nowrap font-bold tabular-nums h-5 px-2 text-xs bg-gradient-to-r from-[#d74a41] to-[#407aeb] align-middle",
         className
       )}
       {...rest}
