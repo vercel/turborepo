101f4d3cbe16c25777bda174bce4fae05f00c620
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-23 17:21:49 +0900

    Typo

--- crates/turbopack-tests/tests/snapshot/cssmodules/coposes/output/crates_turbopack-tests_tests_snapshot_cssmodules_coposes_input_index_1fe0c2.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/crates_turbopack-tests_tests_snapshot_cssmodules_coposes_input_index_1fe0c2.js",
-    {},
-    {"otherChunks":[{"path":"output/79fb1_turbopack-tests_tests_snapshot_cssmodules_coposes_input_index_module_cfac9c.css","included":["[project]/crates/turbopack-tests/tests/snapshot/cssmodules/coposes/input/index.module.css [test] (css)"],"moduleChunks":["output/79fb1_turbopack-tests_tests_snapshot_cssmodules_coposes_input_index_module_7d7e1c.css"]},"output/crates_turbopack-tests_tests_snapshot_cssmodules_coposes_input_9f4d82._.js"],"runtimeModuleIds":["[project]/crates/turbopack-tests/tests/snapshot/cssmodules/coposes/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
