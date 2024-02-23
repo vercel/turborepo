2ac2b9692fea017c18fce724a13f31e1b16eea94
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-23 15:56:25 +0900

    Fix?

--- crates/turbopack-css/src/process.rs
+++ crates/turbopack-css/src/process.rs
@@ -165,26 +165,39 @@ impl<'i, 'o> StyleSheetLike<'i, 'o> {
 
                     let mut map = CssModuleExports::default();
 
+                    dbg!(&output.renamed);
+
                     for (class_name, export_class_names) in output.renamed {
                         for export_class_name in export_class_names {
+                            // If the class name is already in the map, the first
+                            // one is the reference to itself and the current one is
+                            // the reference to the other class.
                             match export_class_name {
                                 CssClassName::Local { name } => {
-                                    map.entry(class_name.to_string()).or_insert_with(|| {
-                                        CssModuleExport {
+                                    map.entry(class_name.to_string())
+                                        .and_modify(|e| {
+                                            e.composes.push(CssModuleReference::Local {
+                                                name: name.value.to_string(),
+                                            })
+                                        })
+                                        .or_insert_with(|| CssModuleExport {
                                             name: name.value.to_string(),
                                             composes: Vec::new(),
                                             is_referenced: true,
-                                        }
-                                    });
+                                        });
                                 }
                                 CssClassName::Global { name } => {
-                                    map.entry(class_name.to_string()).or_insert_with(|| {
-                                        CssModuleExport {
+                                    map.entry(class_name.to_string())
+                                        .and_modify(|e| {
+                                            e.composes.push(CssModuleReference::Global {
+                                                name: name.value.to_string(),
+                                            })
+                                        })
+                                        .or_insert_with(|| CssModuleExport {
                                             name: name.value.to_string(),
                                             composes: Vec::new(),
                                             is_referenced: true,
-                                        }
-                                    });
+                                        });
                                 }
                                 CssClassName::Import { name, from } => {
                                     let e =
@@ -204,6 +217,8 @@ impl<'i, 'o> StyleSheetLike<'i, 'o> {
                             }
                         }
                     }
+
+                    dbg!(&map);
 
                     css_module_exports = Some(map);
                 }
@@ -492,6 +507,8 @@ async fn process_content(
             flags: config.flags,
         }
     }
+
+    dbg!(&*code);
 
     let config = ParserOptions {
         css_modules: match ty {
