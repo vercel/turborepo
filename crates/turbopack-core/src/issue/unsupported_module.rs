96ea64add0f2f950038ff75aba7d47029e340cf0
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-02-27 16:23:31 +0900

    Update crates/turbopack-core/src/issue/unsupported_module.rs
    Co-authored-by: Tobias Koppers <tobias.koppers@googlemail.com>

--- crates/turbopack-core/src/issue/unsupported_module.rs
+++ crates/turbopack-core/src/issue/unsupported_module.rs
@@ -25,7 +25,7 @@ impl Issue for UnsupportedModuleIssue {
 
     #[turbo_tasks::function]
     fn stage(&self) -> Vc<IssueStage> {
-        IssueStage::Load.into()
+        IssueStage::ProcessModule.into()
     }
 
     #[turbo_tasks::function]
