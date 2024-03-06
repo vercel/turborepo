zsh77/turbo

feat: add turbocharge subcommand

crates/turborepo-lib/src/commands/mod.rs
@@ -20,6 +20,7 @@ pub(crate) mod login;
 pub(crate) mod logout;
 pub(crate) mod prune;
 pub(crate) mod run;
+pub(crate) mod turbocharge;
 pub(crate) mod unlink;
 
 #[derive(Debug)]

vercel/turborepo

crates/turborepo-lib/src/commands/mod.rs
@@ -19,6 +19,7 @@ pub(crate) mod login;
 pub(crate) mod logout;
 pub(crate) mod prune;
 pub(crate) mod run;
+pub(crate) mod scan;
 pub(crate) mod telemetry;
 pub(crate) mod unlink;
 