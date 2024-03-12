e412451b799d278e194213d98af12606eea436be
Thomas Knickman <tom.knickman@vercel.com>
2024-03-12 11:43:29 -0400

    chore(docs): move client token to env var (#7707)
    ### Description
    
    Move the client token to env var so we can more easily rotate it

--- docs/lib/useTurborepoMinutesSaved.ts
+++ docs/lib/useTurborepoMinutesSaved.ts
@@ -4,8 +4,7 @@ import axios from "axios";
 const fetcher = (url: string) =>
   axios.get(url).then((res) => res.data as QueryResponse);
 
-const path =
-  "https://api.us-east.tinybird.co/v0/pipes/turborepo_time_saved_ticker.json?token=p.eyJ1IjogIjAzYzA0Y2MyLTM1YTAtNDhhNC05ZTZjLThhMWE0NGNhNjhkZiIsICJpZCI6ICJmOWIzMTU5Yi0wOTVjLTQyM2UtOWIwNS04ZDZlNzIyNjEwNzIifQ.A3TOPdm3Lhmn-1x5m6jNvulCQbbgUeQfAIO3IaaAt5k";
+const path = `https://api.us-east.tinybird.co/v0/pipes/turborepo_time_saved_ticker.json?token=${process.env.NEXT_PUBLIC_TINYBIRD_TIME_SAVED_PUBLIC_TOKEN}`;
 
 const REFRESH_INTERVAL_IN_MS = 3500;
 
