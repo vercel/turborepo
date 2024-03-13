2ee67631b9471d77c29b44f1d96ffbd22348622c
Tobias Koppers <tobias.koppers@googlemail.com>
2024-03-13 08:28:37 +0000

    extra test case

--- crates/turbopack-tests/tests/execution/turbopack/tree-shaking/side-effects/input/index.js
+++ crates/turbopack-tests/tests/execution/turbopack/tree-shaking/side-effects/input/index.js
@@ -93,18 +93,21 @@ it("should generate a correct facade from async modules", async () => {
     expect.objectContaining({
       tla: "tla",
       reexported: "reexported",
+      reexported2: "reexported",
     })
   );
   expect(await import("tla/reexport")).toEqual(
     expect.objectContaining({
       local: "local",
       tlaReexported: "tla-reexported",
+      tlaReexported2: "tla-reexported",
     })
   );
   expect(await import("tla/both")).toEqual(
     expect.objectContaining({
       tla: "tla",
       tlaReexported: "tla-reexported",
+      tlaReexported2: "tla-reexported",
     })
   );
 });
@@ -117,18 +120,28 @@ it("should generate a correct namespace object from async modules", async () => 
     expect.objectContaining({
       tla: "tla",
       reexported: "reexported",
+      reexported2: "reexported",
     })
   );
   expect(tlaReexport).toEqual(
     expect.objectContaining({
       local: "local",
       tlaReexported: "tla-reexported",
+      tlaReexported2: "tla-reexported",
     })
   );
   expect(tlaBoth).toEqual(
     expect.objectContaining({
       tla: "tla",
       tlaReexported: "tla-reexported",
+      tlaReexported2: "tla-reexported",
     })
   );
+});
+
+import { tlaReexported2 as tlaReexported } from "tla/reexport";
+import { tlaReexported2 as tlaReexportedBoth } from "tla/both";
+it("should generate correct renaming facades from async modules", async () => {
+  expect(tlaReexported).toBe("tla-reexported");
+  expect(tlaReexportedBoth).toBe("tla-reexported");
 });
