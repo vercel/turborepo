2362582fc8803723e613cdfe1aa15e0fbb3a4347
zahra shahrouzi <44067788+zsh77@users.noreply.github.com>
2024-03-15 02:54:27 +0330

    Bug fix on with rollup (#7651)
    ### Description
    
    - fix `Parsing error: The keyword 'const' is reserved` in eslint-config
    package by excluding `apps/**` and `packages/**` from the root
    .eslintrc.js
    - include next.config.js in tsconfig.json to fix the error `ESLint was
    configured to run on next.config.js .However, that TSConfig does not
    include this file.`
    - set eslint to ignore .eslintrc.js in packages/ui (it was missed to
    ignore because it ends in .cjs not .js )
    - add external packages in rollup to fix:
    ```
    (!) Unresolved dependencies
     https://rollupjs.org/troubleshooting/#warning-treating-module-as-external-dependency
     react/jsx-runtime (imported by "Header.tsx")
    ```
    - update README
    
    ### Testing Instructions
    
    Running `npx create-turbo@latest -e with-rollup` and then `pnpm dev`
    should work fine.
    
    ---------
    
    Co-authored-by: Anthony Shew <anthony.shew@vercel.com>

--- examples/with-rollup/.eslintrc.js
+++ examples/with-rollup/.eslintrc.js
@@ -1,4 +1,7 @@
+// This configuration only applies to the package manager root.
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
   extends: ["@repo/eslint-config/library.js"],
+  ignorePattern: ["apps/**", "packages/**"],
 };
