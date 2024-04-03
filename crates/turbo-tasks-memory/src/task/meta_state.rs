8d92d9b74ebb14920bf541364dbf6b8650ae4ce4
Will Binns-Smith <wbinnssmith@gmail.com>
2024-04-03 14:18:11 -0700

    fixup! Fix clippy

--- crates/turbo-tasks-memory/src/task/meta_state.rs
+++ crates/turbo-tasks-memory/src/task/meta_state.rs
@@ -77,16 +77,16 @@ impl TaskMetaState {
     }
 }
 
-// These need to be impl types since there is no way to reference the zero-sized
-// function item type
-pub(super) type TaskMetaStateAsFull = impl Fn(&TaskMetaState) -> Option<&TaskState>;
-pub(super) type TaskMetaStateAsPartial = impl Fn(&TaskMetaState) -> Option<&PartialTaskState>;
-pub(super) type TaskMetaStateAsUnloaded = impl Fn(&TaskMetaState) -> Option<&UnloadedTaskState>;
-pub(super) type TaskMetaStateAsFullMut = impl Fn(&mut TaskMetaState) -> Option<&mut TaskState>;
+pub(super) type TaskMetaStateAsFull = for<'a> fn(&'a TaskMetaState) -> Option<&'a TaskState>;
+pub(super) type TaskMetaStateAsPartial = for<'a> fn(&'a TaskMetaState) -> Option<&PartialTaskState>;
+pub(super) type TaskMetaStateAsUnloaded =
+    for<'a> fn(&'a TaskMetaState) -> Option<&'a UnloadedTaskState>;
+pub(super) type TaskMetaStateAsFullMut =
+    for<'a> fn(&'a mut TaskMetaState) -> Option<&'a mut TaskState>;
 pub(super) type TaskMetaStateAsPartialMut =
-    impl Fn(&mut TaskMetaState) -> Option<&mut PartialTaskState>;
+    for<'a> fn(&'a mut TaskMetaState) -> Option<&'a mut PartialTaskState>;
 pub(super) type TaskMetaStateAsUnloadedMut =
-    impl Fn(&mut TaskMetaState) -> Option<&mut UnloadedTaskState>;
+    for<'a> fn(&'a mut TaskMetaState) -> Option<&'a mut UnloadedTaskState>;
 
 pub(super) enum TaskMetaStateReadGuard<'a> {
     Full(ReadGuard<'a, TaskMetaState, TaskState, TaskMetaStateAsFull>),
