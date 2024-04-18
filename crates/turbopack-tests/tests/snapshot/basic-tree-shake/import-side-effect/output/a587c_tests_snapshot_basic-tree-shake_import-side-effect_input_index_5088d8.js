e1b55401b3f4a38d2da0efbffb3a81cb71a58346
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-18 10:09:47 +0900

    Update test refs

---
+++ crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-side-effect/output/a587c_tests_snapshot_basic-tree-shake_import-side-effect_input_index_5088d8.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/a587c_tests_snapshot_basic-tree-shake_import-side-effect_input_index_5088d8.js", {
+
+"[project]/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-side-effect/input/index.js [test] (ecmascript) <exports>": (function({ r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname, k: __turbopack_refresh__, m: module, e: exports, t: require }) { !function() {
+
+
+}.call(this) }),
+}]);
+
+//# sourceMappingURL=a587c_tests_snapshot_basic-tree-shake_import-side-effect_input_index_5088d8.js.map
