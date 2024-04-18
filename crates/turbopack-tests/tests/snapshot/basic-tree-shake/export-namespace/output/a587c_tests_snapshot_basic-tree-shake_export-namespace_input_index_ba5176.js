29a58f8b2a38cfd0ccf7b4b7753819a218605920
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-18 14:26:08 +0900

    Remove output

--- crates/turbopack-tests/tests/snapshot/basic-tree-shake/export-namespace/output/a587c_tests_snapshot_basic-tree-shake_export-namespace_input_index_ba5176.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/a587c_tests_snapshot_basic-tree-shake_export-namespace_input_index_ba5176.js", {
-
-"[project]/crates/turbopack-tests/tests/snapshot/basic-tree-shake/export-namespace/input/index.js [test] (ecmascript) <exports>": (function({ r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname, k: __turbopack_refresh__, m: module, e: exports, t: require }) { !function() {
-
-
-}.call(this) }),
-}]);
-
-//# sourceMappingURL=a587c_tests_snapshot_basic-tree-shake_export-namespace_input_index_ba5176.js.map
