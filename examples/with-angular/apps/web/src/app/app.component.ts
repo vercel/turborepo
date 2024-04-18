2df67c6b11452322fe5089aa13d1c580c51b8f79
Mohamed Aly <alydemah@gmail.com>
2024-04-18 21:25:13 +0200

    feat(example): add angular getting started example

---
+++ examples/with-angular/apps/web/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'turbo-angular';
+}
