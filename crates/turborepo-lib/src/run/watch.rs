da53c14fc29b2d8824ca8753bb7e72be26816de2
Nicholas Yang <nicholas.yang@vercel.com>
2024-04-23 17:24:04 -0400

    feat(turborepo): Persistent Tasks in Watch Mode (#7922)
    ### Description
    
    Implements handling of persistent tasks. Basically, we strip out any
    persistent tasks from the task graph when we do either an execution for
    package rediscovery or package changes. Instead, we run the persistent
    tasks in a separate thread, so they don't block anything.
    
    ### Testing Instructions
    
    Added tests for both pruning methods.
    
    Closes TURBO-2775

--- crates/turborepo-lib/src/run/watch.rs
+++ crates/turborepo-lib/src/run/watch.rs
@@ -76,9 +76,6 @@ impl WatchClient {
             execution_args: execution_args.clone(),
         });
 
-        // We currently don't actually need the whole Run struct, just the filtered
-        // packages. But in the future we'll likely need it to more efficiently
-        // spawn tasks.
         let mut run = RunBuilder::new(new_base)?
             .build(&handler, telemetry.clone())
             .await?;
@@ -96,6 +93,8 @@ impl WatchClient {
         let mut events = client.package_changes().await?;
         let mut current_runs: HashMap<PackageName, JoinHandle<Result<i32, run::Error>>> =
             HashMap::new();
+        let mut persistent_tasks_handle = None;
+
         let event_fut = async {
             while let Some(event) = events.next().await {
                 let event = event?;
@@ -106,6 +105,7 @@ impl WatchClient {
                     &base,
                     &telemetry,
                     &handler,
+                    &mut persistent_tasks_handle,
                 )
                 .await?;
             }
@@ -133,6 +133,7 @@ impl WatchClient {
         base: &CommandBase,
         telemetry: &CommandEventBuilder,
         handler: &SignalHandler,
+        persistent_tasks_handle: &mut Option<JoinHandle<Result<i32, run::Error>>>,
     ) -> Result<(), Error> {
         // Should we recover here?
         match event {
@@ -215,8 +216,24 @@ impl WatchClient {
                     .build(handler, telemetry.clone())
                     .await?;
 
-                // Execute run
-                run.run().await?;
+                if run.has_persistent_tasks() {
+                    // Abort old run
+                    if let Some(run) = persistent_tasks_handle.take() {
+                        run.abort();
+                    }
+
+                    let mut persistent_run = run.create_run_for_persistent_tasks();
+                    // If we have persistent tasks, we run them on a separate thread
+                    // since persistent tasks don't finish
+                    *persistent_tasks_handle =
+                        Some(tokio::spawn(async move { persistent_run.run().await }));
+
+                    // But we still run the regular tasks blocking
+                    let mut non_persistent_run = run.create_run_without_persistent_tasks();
+                    non_persistent_run.run().await?;
+                } else {
+                    run.run().await?;
+                }
             }
             proto::package_change_event::Event::Error(proto::PackageChangeError { message }) => {
                 return Err(DaemonError::Unavailable(message).into());
