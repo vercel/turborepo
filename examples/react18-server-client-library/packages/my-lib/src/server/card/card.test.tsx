9220dfd5a9832f989adf2839b902d296384de10f
Mayank <mayank.srmu@gmail.com>
2024-04-23 05:55:44 +0000

    Add unit tests for Card component

--- examples/react18-server-client-library/packages/my-lib/src/server/card/card.test.tsx
+++ examples/react18-server-client-library/packages/my-lib/src/server/card/card.test.tsx
@@ -1,12 +1,38 @@
 import { cleanup, render, screen } from "@testing-library/react";
-import { afterEach, describe, test } from "vitest";
+import { afterEach, beforeEach, describe, test } from "vitest";
 import { Card } from "./card";
 
-describe.concurrent("card", () => {
-	afterEach(cleanup);
+describe("card", () => {
+  const className = "test";
+  const href = "test-href";
+  const title = "test-title";
+  const someDesc = "this is a test card";
+  beforeEach(() => {
+    render(
+      <Card className={className} href={href} title={title}>
+        {someDesc}
+      </Card>,
+    );
+  });
+  afterEach(cleanup);
 
-	test("check if h1 heading exists", ({ expect }) => {
-		render(<Card />);
-		expect(screen.getByTestId("card-h1").textContent).toBe("card");
-	});
+  test("test className", ({ expect }) => {
+    expect(screen.getByTestId("card").className).toBe(className);
+  });
+
+  test("test href", ({ expect }) => {
+    expect(screen.getByTestId("card").getAttribute("href")).toBe(href);
+  });
+
+  test("test title", ({ expect }) => {
+    expect(
+      screen.getByTestId("card").getElementsByTagName("h2")[0]?.textContent,
+    ).toBe(title + " ->");
+  });
+
+  test("test description", ({ expect }) => {
+    expect(
+      screen.getByTestId("card").getElementsByTagName("p")[0]?.textContent,
+    ).toBe(someDesc);
+  });
 });
