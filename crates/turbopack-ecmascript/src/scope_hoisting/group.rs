c8117f190fc9ecfba3e89ccd3cc088ba92dbe021
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-25 14:36:23 +0900

    dep

--- crates/turbopack-ecmascript/src/scope_hoisting/group.rs
+++ crates/turbopack-ecmascript/src/scope_hoisting/group.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
+use rustc_hash::FxHashMap;
 use turbo_tasks::Vc;
+use turbopack_core::chunk::{ModuleId, ModuleIds};
 
 /// Counterpart of `Chunk` in webpack scope hoisting
 #[turbo_tasks::value]
 pub struct ModuleGroup {}
 
 #[turbo_tasks::function]
-pub async fn group_modules() -> Result<Vc<ModuleGroup>> {}
+pub async fn group_modules(deps: &FxHashMap<ModuleId, ModuleIds>) -> Result<Vc<ModuleGroup>> {}
