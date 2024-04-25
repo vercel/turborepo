310771cd2cebcdbcab1c0610582f9121e34b2bff
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-25 13:50:03 +0900

    chore

--- crates/turbopack-ecmascript/src/scope_hoisting/module_merger.rs
+++ crates/turbopack-ecmascript/src/scope_hoisting/module_merger.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use turbo_tasks::Vc;
 
 use crate::parse::ParseResult;
 
@@ -11,6 +12,6 @@ pub struct ModuleMerger {}
 
 #[turbo_tasks::value_impl]
 impl ModuleMerger {
-    #[turbo_tasks::value]
-    pub async fn merge_modules(&mut self, modules: Vec<Vc<ParseResult>>) -> Result<Vc> {}
+    #[turbo_tasks::function]
+    pub async fn merge_modules(&self, modules: Vec<Vc<ParseResult>>) -> Result<Vc> {}
 }
