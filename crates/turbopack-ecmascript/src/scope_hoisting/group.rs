9548736266fd05af4a4bef4007c74b7b65b87b22
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-25 15:39:29 +0900

    ModuleScope

--- crates/turbopack-ecmascript/src/scope_hoisting/group.rs
+++ crates/turbopack-ecmascript/src/scope_hoisting/group.rs
@@ -3,9 +3,13 @@ use rustc_hash::FxHashMap;
 use turbo_tasks::Vc;
 use turbopack_core::chunk::{ModuleId, ModuleIds};
 
-/// Counterpart of `Chunk` in webpack scope hoisting
+/// Counterpart of `Scope` in webpack scope hoisting
 #[turbo_tasks::value]
-pub struct ModuleGroup {}
+pub struct ModuleScope {}
 
 #[turbo_tasks::function]
-pub async fn group_modules(deps: Vc<FxHashMap<ModuleId, ModuleIds>>) -> Result<Vc<ModuleGroup>> {}
+pub async fn split_scopes(
+    entry: Vc<ModuleId>,
+    deps: Vc<FxHashMap<ModuleId, ModuleIds>>,
+) -> Result<Vc<ModuleScope>> {
+}
