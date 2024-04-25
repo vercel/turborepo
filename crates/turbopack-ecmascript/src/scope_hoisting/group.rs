bfd8dd83ae2c5ecbdcec39e3106a956910ad419e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-25 14:34:31 +0900

    declare

--- crates/turbopack-ecmascript/src/scope_hoisting/group.rs
+++ crates/turbopack-ecmascript/src/scope_hoisting/group.rs
@@ -1,1 +1,9 @@
+use anyhow::Result;
+use turbo_tasks::Vc;
 
+/// Counterpart of `Chunk` in webpack scope hoisting
+#[turbo_tasks::value]
+pub struct ModuleGroup {}
+
+#[turbo_tasks::function]
+pub async fn group_modules() -> Result<Vc<ModuleGroup>> {}
