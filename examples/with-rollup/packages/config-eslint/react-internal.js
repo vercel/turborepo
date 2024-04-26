52422eba232e48fc122a4df53238681162daf2e5
tknickman <tom.knickman@vercel.com>
2024-04-26 14:17:11 -0400

    fix(examples): lint docs

--- examples/with-rollup/packages/config-eslint/react-internal.js
+++ examples/with-rollup/packages/config-eslint/react-internal.js
@@ -6,10 +6,6 @@ const project = resolve(process.cwd(), "tsconfig.json");
  * This is a custom ESLint configuration for use with
  * internal (bundled by their consumer) libraries
  * that utilize React.
- *
- * This config extends the Vercel Engineering Style Guide.
- * For more information, see https://github.com/vercel/style-guide
- *
  */
 
 /** @type {import("eslint").Linter.Config} */
