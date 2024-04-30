6cf4bea40594cde8c07b74a2d1cb257c3cfdeefd
nicholaslyang <nicholas.yang@vercel.com>
2024-04-30 16:32:29 -0400

    Split package changes watcher into an event processing loop and a change mapper loop

--- crates/turborepo-lib/src/package_changes_watcher.rs
+++ crates/turborepo-lib/src/package_changes_watcher.rs
@@ -1,8 +1,9 @@
-use std::collections::HashSet;
+use std::{cell::RefCell, collections::HashSet, ops::DerefMut, path::PathBuf};
 
 use ignore::gitignore::Gitignore;
 use notify::Event;
-use tokio::sync::{broadcast, oneshot};
+use radix_trie::{Trie, TrieCommon};
+use tokio::sync::{broadcast, oneshot, Mutex};
 use turbopath::{AbsoluteSystemPathBuf, AnchoredSystemPath, AnchoredSystemPathBuf};
 use turborepo_filewatch::{NotifyError, OptionalWatch};
 use turborepo_repository::{
@@ -53,8 +54,29 @@ impl PackageChangesWatcher {
     }
 }
 
+enum ChangedFiles {
+    All,
+    Some(Trie<PathBuf, ()>),
+}
+
+impl ChangedFiles {
+    fn is_empty(&self) -> bool {
+        match self {
+            ChangedFiles::All => false,
+            ChangedFiles::Some(trie) => trie.is_empty(),
+        }
+    }
+}
+
+impl Default for ChangedFiles {
+    fn default() -> Self {
+        ChangedFiles::Some(Trie::new())
+    }
+}
+
 struct Subscriber {
     file_events_lazy: OptionalWatch<broadcast::Receiver<Result<Event, NotifyError>>>,
+    changed_files: Mutex<RefCell<ChangedFiles>>,
     repo_root: AbsoluteSystemPathBuf,
     package_change_events_tx: broadcast::Sender<PackageChangeEvent>,
 }
@@ -108,11 +130,12 @@ impl Subscriber {
         Subscriber {
             repo_root,
             file_events_lazy,
+            changed_files: Default::default(),
             package_change_events_tx,
         }
     }
 
-    async fn initialize_repo_state(&mut self) -> Option<RepoState> {
+    async fn initialize_repo_state(&self) -> Option<(RepoState, Gitignore)> {
         let Ok(root_package_json) =
             PackageJson::load(&self.repo_root.join_component("package.json"))
         else {
@@ -128,6 +151,9 @@ impl Subscriber {
         )
         .ok();
 
+        let gitignore_path = self.repo_root.join_component(".gitignore");
+        let (root_gitignore, _) = Gitignore::new(&gitignore_path);
+
         let Ok(pkg_dep_graph) = PackageGraphBuilder::new(&self.repo_root, root_package_json)
             .build()
             .await
@@ -136,24 +162,59 @@ impl Subscriber {
             return None;
         };
 
-        Some(RepoState {
-            root_turbo_json,
-            pkg_dep_graph,
-        })
+        Some((
+            RepoState {
+                root_turbo_json,
+                pkg_dep_graph,
+            },
+            root_gitignore,
+        ))
     }
 
     async fn watch(mut self, exit_rx: oneshot::Receiver<()>) {
-        let process = async {
-            let root_pkg = WorkspacePackage::root();
-            let Ok(mut file_events) = self.file_events_lazy.get().await.map(|r| r.resubscribe())
-            else {
-                // if we get here, it means that file watching has not started, so we should
-                // just report that the package watcher is not available
-                tracing::debug!("file watching shut down, package watcher not available");
-                return;
-            };
+        let Ok(mut file_events) = self.file_events_lazy.get().await.map(|r| r.resubscribe()) else {
+            // if we get here, it means that file watching has not started, so we should
+            // just report that the package watcher is not available
+            tracing::debug!("file watching shut down, package watcher not available");
+            return;
+        };
 
-            let Some(mut repo_state) = self.initialize_repo_state().await else {
+        let event_fut = async {
+            loop {
+                match file_events.recv().await {
+                    Ok(Ok(Event { paths, .. })) => {
+                        if let ChangedFiles::Some(trie) =
+                            self.changed_files.lock().await.borrow_mut().deref_mut()
+                        {
+                            for path in paths {
+                                trie.insert(path, ());
+                            }
+                        }
+                    }
+                    Ok(Err(err)) => {
+                        tracing::error!("file event error: {:?}", err);
+                        break;
+                    }
+                    Err(broadcast::error::RecvError::Lagged(_)) => {
+                        tracing::warn!("file event lagged");
+                        // Lagged essentially means we're not keeping up with
+                        // the file events, so
+                        // we can catch up by declaring all files changed
+                        *self.changed_files.lock().await.borrow_mut() = ChangedFiles::All;
+                    }
+                    Err(broadcast::error::RecvError::Closed) => {
+                        tracing::debug!("file event channel closed");
+                        break;
+                    }
+                }
+            }
+        };
+
+        let changes_fut = async {
+            let root_pkg = WorkspacePackage::root();
+
+            let Some((mut repo_state, mut root_gitignore)) = self.initialize_repo_state().await
+            else {
                 return;
             };
 
@@ -167,121 +228,133 @@ impl Subscriber {
             self.package_change_events_tx
                 .send(PackageChangeEvent::Rediscover)
                 .ok();
+            let mut interval = tokio::time::interval(std::time::Duration::from_millis(100));
 
             loop {
-                match file_events.recv().await {
-                    Ok(Ok(Event { paths, .. })) => {
-                        // No point in raising an error for an invalid .gitignore
-                        // This is slightly incorrect because we should also search for the
-                        // .gitignore files in the workspaces.
-                        let (root_gitignore, _) =
-                            Gitignore::new(&self.repo_root.join_component(".gitignore"));
+                interval.tick().await;
 
-                        let changed_files: HashSet<_> = paths
-                            .into_iter()
-                            .filter_map(|p| {
-                                let p = AbsoluteSystemPathBuf::try_from(p).ok()?;
-                                self.repo_root.anchor(p).ok()
-                            })
-                            .filter(|p| {
-                                // If in .gitignore or in .git, filter out
-                                !(ancestors_is_ignored(&root_gitignore, p) || is_in_git_folder(p))
-                            })
-                            .collect();
+                let changed_files = self.changed_files.lock().await;
+                if changed_files.borrow().is_empty() {
+                    continue;
+                }
 
-                        let changes = change_mapper.changed_packages(changed_files.clone(), None);
+                let ChangedFiles::Some(trie) = changed_files.take() else {
+                    let _ = self
+                        .package_change_events_tx
+                        .send(PackageChangeEvent::Rediscover);
 
-                        match changes {
-                            Ok(PackageChanges::All) => {
-                                // We tell the client that we need to rediscover the packages, i.e.
-                                // all bets are off, just re-run everything
-                                let _ = self
-                                    .package_change_events_tx
-                                    .send(PackageChangeEvent::Rediscover);
-                                match self.initialize_repo_state().await {
-                                    Some(new_repo_state) => {
-                                        repo_state = new_repo_state;
-                                        change_mapper = match repo_state.get_change_mapper() {
-                                            Some(change_mapper) => change_mapper,
-                                            None => {
-                                                break;
-                                            }
-                                        };
-                                    }
-                                    None => {
-                                        break;
-                                    }
+                    match self.initialize_repo_state().await {
+                        Some((new_repo_state, new_gitignore)) => {
+                            repo_state = new_repo_state;
+                            root_gitignore = new_gitignore;
+                            change_mapper = match repo_state.get_change_mapper() {
+                                Some(change_mapper) => change_mapper,
+                                None => {
+                                    break;
                                 }
-                            }
-                            Ok(PackageChanges::Some(mut changed_pkgs)) => {
-                                if !changed_pkgs.is_empty() {
-                                    tracing::debug!(
-                                        "changed files: {:?} changed packages: {:?}",
-                                        changed_files,
-                                        changed_pkgs
-                                    );
-                                }
-
-                                // If the root package has changed, we only send it if we have root
-                                // tasks. Otherwise it's not worth sending as it will only
-                                // pollute up the output logs
-                                if changed_pkgs.contains(&root_pkg) {
-                                    let has_root_tasks = repo_state
-                                        .root_turbo_json
-                                        .as_ref()
-                                        .map_or(false, |turbo| turbo.has_root_tasks());
-                                    if !has_root_tasks {
-                                        changed_pkgs.remove(&root_pkg);
-                                    }
-                                }
-
-                                for pkg in changed_pkgs {
-                                    let _ = self.package_change_events_tx.send(
-                                        PackageChangeEvent::Package {
-                                            name: pkg.name.clone(),
-                                        },
-                                    );
-                                }
-                            }
-                            Err(err) => {
-                                // Log the error, rediscover the packages and try again
-                                tracing::error!("error: {:?}", err);
-
-                                let _ = self
-                                    .package_change_events_tx
-                                    .send(PackageChangeEvent::Rediscover);
-                                match self.initialize_repo_state().await {
-                                    Some(new_repo_state) => {
-                                        repo_state = new_repo_state;
-                                        change_mapper = match repo_state.get_change_mapper() {
-                                            Some(change_mapper) => change_mapper,
-                                            None => {
-                                                break;
-                                            }
-                                        }
-                                    }
-                                    None => {
-                                        break;
-                                    }
-                                }
-                            }
+                            };
+                        }
+                        None => {
+                            break;
                         }
                     }
-                    Ok(Err(err)) => {
-                        tracing::error!("file event error: {:?}", err);
-                        break;
-                    }
-                    Err(broadcast::error::RecvError::Lagged(_)) => {
-                        tracing::warn!("file event lagged");
-                        // Lagged essentially means we're not keeping up with the file events, so
-                        // we can catch up by sending a rediscover event
+                    continue;
+                };
+
+                let gitignore_path = self.repo_root.join_component(".gitignore");
+                if trie.get(gitignore_path.as_std_path()).is_some() {
+                    let (new_root_gitignore, _) = Gitignore::new(&gitignore_path);
+                    root_gitignore = new_root_gitignore;
+                }
+
+                let changed_files: HashSet<_> = trie
+                    .keys()
+                    .filter_map(|p| {
+                        let p = AbsoluteSystemPathBuf::try_from(p.as_path()).ok()?;
+                        self.repo_root.anchor(p).ok()
+                    })
+                    .filter(|p| {
+                        // If in .gitignore or in .git, filter out
+                        !(ancestors_is_ignored(&root_gitignore, p) || is_in_git_folder(p))
+                    })
+                    .collect();
+
+                if changed_files.is_empty() {
+                    continue;
+                }
+
+                let changed_packages = change_mapper.changed_packages(changed_files.clone(), None);
+
+                tracing::warn!("changed_files: {:?}", changed_files);
+                tracing::warn!("changed_packages: {:?}", changed_packages);
+
+                match changed_packages {
+                    Ok(PackageChanges::All) => {
+                        // We tell the client that we need to rediscover the packages, i.e.
+                        // all bets are off, just re-run everything
                         let _ = self
                             .package_change_events_tx
                             .send(PackageChangeEvent::Rediscover);
+                        match self.initialize_repo_state().await {
+                            Some((new_repo_state, new_gitignore)) => {
+                                repo_state = new_repo_state;
+                                root_gitignore = new_gitignore;
+                                change_mapper = match repo_state.get_change_mapper() {
+                                    Some(change_mapper) => change_mapper,
+                                    None => {
+                                        break;
+                                    }
+                                };
+                            }
+                            None => {
+                                break;
+                            }
+                        }
                     }
-                    Err(broadcast::error::RecvError::Closed) => {
-                        tracing::debug!("file event channel closed");
-                        break;
+                    Ok(PackageChanges::Some(mut filtered_pkgs)) => {
+                        // If the root package has changed, we only send it if we have root
+                        // tasks. Otherwise it's not worth sending as it will only
+                        // pollute up the output logs
+                        if filtered_pkgs.contains(&root_pkg) {
+                            let has_root_tasks = repo_state
+                                .root_turbo_json
+                                .as_ref()
+                                .map_or(false, |turbo| turbo.has_root_tasks());
+                            if !has_root_tasks {
+                                filtered_pkgs.remove(&root_pkg);
+                            }
+                        }
+
+                        for pkg in filtered_pkgs {
+                            let _ =
+                                self.package_change_events_tx
+                                    .send(PackageChangeEvent::Package {
+                                        name: pkg.name.clone(),
+                                    });
+                        }
+                    }
+                    Err(err) => {
+                        // Log the error, rediscover the packages and try again
+                        tracing::error!("error: {:?}", err);
+
+                        let _ = self
+                            .package_change_events_tx
+                            .send(PackageChangeEvent::Rediscover);
+                        match self.initialize_repo_state().await {
+                            Some((new_repo_state, new_gitignore)) => {
+                                repo_state = new_repo_state;
+                                root_gitignore = new_gitignore;
+                                change_mapper = match repo_state.get_change_mapper() {
+                                    Some(change_mapper) => change_mapper,
+                                    None => {
+                                        break;
+                                    }
+                                }
+                            }
+                            None => {
+                                break;
+                            }
+                        }
                     }
                 }
             }
@@ -292,7 +365,10 @@ impl Subscriber {
             _ = exit_rx => {
                 tracing::debug!("exiting package changes watcher due to signal");
             },
-            _ = process => {
+            _ = event_fut => {
+                tracing::debug!("exiting package changes watcher due to file event end");
+            },
+            _ = changes_fut => {
                 tracing::debug!("exiting package changes watcher due to process end");
             }
         }
