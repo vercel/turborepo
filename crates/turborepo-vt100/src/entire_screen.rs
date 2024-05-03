abdcc948e858350060260f89a8ed2ec2d11c20f1
Chris Olszewski <chris.olszewski@vercel.com>
2024-05-03 09:56:40 -0700

    chore(ui): remove unused hack

--- crates/turborepo-vt100/src/entire_screen.rs
+++ crates/turborepo-vt100/src/entire_screen.rs
@@ -3,8 +3,6 @@ use std::io::Write;
 
 pub struct EntireScreen<'a> {
     screen: &'a crate::Screen,
-    // If present, screen will be truncated to only display lines that fit in those cells
-    max_lines: Option<usize>,
     size: (usize, u16),
 }
 
@@ -14,30 +12,12 @@ impl<'a> EntireScreen<'a> {
         Self {
             size: screen.grid().size_with_contents(),
             screen,
-            max_lines: None,
         }
-    }
-
-    pub fn with_max_lines(&mut self, max_lines: Option<usize>) {
-        self.max_lines = max_lines;
     }
 
     #[must_use]
     pub fn cell(&self, row: u16, col: u16) -> Option<&crate::Cell> {
-        match self.max_lines {
-            // We need to do some trimming
-            Some(max_lines) if self.size().0 > max_lines => {
-                // in this case we fuck ourselves :) HARD
-                let (height, _) = self.size();
-                // Skip over these
-                let lines_to_cut = (height - max_lines) as u16;
-                self.screen
-                    .grid()
-                    .all_row(lines_to_cut + row)
-                    .and_then(|r| r.get(col))
-            }
-            _ => self.screen.grid().all_row(row).and_then(|r| r.get(col)),
-        }
+        self.screen.grid().all_row(row).and_then(|r| r.get(col))
     }
 
     #[must_use]
