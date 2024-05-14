2f4a65da143c9cbeec19a56d39b7bd007fc82a7f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-14 17:38:15 +0900

    ChunkItem::is_self_async

--- crates/turbopack-ecmascript/src/tree_shake/asset.rs
+++ crates/turbopack-ecmascript/src/tree_shake/asset.rs
@@ -46,6 +46,13 @@ impl EcmascriptModulePartAsset {
         }
         .cell()
     }
+
+    #[turbo_tasks::function]
+    pub async fn is_async_module(&self) -> Result<Vc<bool>> {
+        let result = self.full_module.failsafe_analyze();
+
+        Ok(Vc::cell(result.await?.async_module.await?.is_some()))
+    }
 }
 
 #[turbo_tasks::value_impl]
