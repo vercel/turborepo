cbfe1228b60237df845a1dd74f0d0680e529f8c8
Tobias Koppers <tobias.koppers@googlemail.com>
2024-05-17 18:15:10 +0200

    reorganize test cases

---
+++ crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/namespace-object-identity/input/esm.js
@@ -0,0 +1,5 @@
+import * as self from "./esm";
+
+export const getSelf = function getSelf() {
+  return self;
+};
---
+++ crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/namespace-object-identity/input/index.js
@@ -0,0 +1,10 @@
+import { getSelf } from "./esm";
+import * as esm from "./esm";
+const requiredEsm = require("./esm");
+
+it("should have the same identity on all namespace objects", async () => {
+  expect(getSelf()).toBe(esm);
+  expect(requiredEsm).toBe(esm);
+  const importedEsm = await import("./esm");
+  expect(importedEsm).toBe(esm);
+});
