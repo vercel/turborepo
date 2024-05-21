5633c8a653e8eb967cbfe106b45b970ae8f152bc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-21 15:38:46 +0900

    ImportedSymbol::Exports

--- crates/turbopack-ecmascript/src/analyzer/imports.rs
+++ crates/turbopack-ecmascript/src/analyzer/imports.rs
@@ -443,6 +443,6 @@ fn get_import_symbol_from_export(
             ImportedSymbol::Symbol(orig_name(orig))
         }
         ExportSpecifier::Default(..) => ImportedSymbol::Symbol(js_word!("default")),
-        ExportSpecifier::Namespace(..) => ImportedSymbol::Namespace,
+        ExportSpecifier::Namespace(..) => ImportedSymbol::Exports,
     }
 }
