akshatarora0013/turbo

add support for upload speed / remaining in the cache upload step

crates/turborepo-cache/src/async_cache.rs
@@ -1,13 +1,15 @@
-use std::sync::{atomic::AtomicU8, Arc};
+use std::sync::{atomic::AtomicU8, Arc, Mutex};
 
 use futures::{stream::FuturesUnordered, StreamExt};
-use tokio::sync::{mpsc, Semaphore};
+use tokio::sync::{mpsc, oneshot, Semaphore};
 use tracing::{warn, Instrument, Level};
 use turbopath::{AbsoluteSystemPath, AbsoluteSystemPathBuf, AnchoredSystemPathBuf};
 use turborepo_analytics::AnalyticsSender;
 use turborepo_api_client::{APIAuth, APIClient};
 
-use crate::{multiplexer::CacheMultiplexer, CacheError, CacheHitMetadata, CacheOpts};
+use crate::{
+    http::UploadMap, multiplexer::CacheMultiplexer, CacheError, CacheHitMetadata, CacheOpts,
+};
 
 const WARNING_CUTOFF: u8 = 4;
 
@@ -24,8 +26,11 @@ enum WorkerRequest {
         duration: u64,
         files: Vec<AnchoredSystemPathBuf>,
     },
-    Flush(tokio::sync::oneshot::Sender<()>),
-    Shutdown(tokio::sync::oneshot::Sender<()>),
+    Flush(oneshot::Sender<()>),
+    /// Shutdown the cache. The first oneshot notifies when shutdown starts and
+    /// allows the user to inspect the status of the uploads. The second
+    /// oneshot notifies when the shutdown is complete.
+    Shutdown(oneshot::Sender<Arc<Mutex<UploadMap>>>, oneshot::Sender<()>),
 }
 
 impl AsyncCache {
@@ -95,8 +100,8 @@ impl AsyncCache {
                         }
                         drop(callback);
                     }
-                    WorkerRequest::Shutdown(callback) => {
-                        shutdown_callback = Some(callback);
+                    WorkerRequest::Shutdown(callback, callback2) => {
+                        shutdown_callback = Some((callback, callback2));
                         break;
                     }
                 };
@@ -104,12 +109,20 @@ impl AsyncCache {
             // Drop write consumer to immediately notify callers that cache is shutting down
             drop(write_consumer);
 
+            let done = if let Some((closing, done)) = shutdown_callback {
+                closing.send(real_cache.requests().unwrap_or_default()).ok();
+                Some(done)
+            } else {
+                None
+            };
+
             // wait for all writers to finish
             while let Some(worker) = workers.next().await {
                 let _ = worker;
             }
-            if let Some(callback) = shutdown_callback {
-                callback.send(()).ok();
+
+            if let Some(done) = done {
+                done.send(()).ok();
             }
         });
 
@@ -162,7 +175,7 @@ impl AsyncCache {
     // before checking the cache.
     #[tracing::instrument(skip_all)]
     pub async fn wait(&self) -> Result<(), CacheError> {
-        let (tx, rx) = tokio::sync::oneshot::channel();
+        let (tx, rx) = oneshot::channel();
         self.writer_sender
             .send(WorkerRequest::Flush(tx))
             .await
@@ -172,14 +185,31 @@ impl AsyncCache {
         Ok(())
     }
 
+    /// Shut down the cache, waiting for all workers to finish writing.
+    /// This function returns as soon as the shut down has started,
+    /// returning a channel through which workers can report on their
+    /// progress.
     #[tracing::instrument(skip_all)]
-    pub async fn shutdown(&self) -> Result<(), CacheError> {
-        let (tx, rx) = tokio::sync::oneshot::channel();
+    pub async fn start_shutdown(
+        &self,
+    ) -> Result<(Arc<Mutex<UploadMap>>, oneshot::Receiver<()>), CacheError> {
+        let (closing_tx, closing_rx) = oneshot::channel::<Arc<Mutex<UploadMap>>>();
+        let (closed_tx, closed_rx) = oneshot::channel::<()>();
         self.writer_sender
-            .send(WorkerRequest::Shutdown(tx))
+            .send(WorkerRequest::Shutdown(closing_tx, closed_tx))
             .await
             .map_err(|_| CacheError::CacheShuttingDown)?;
-        rx.await.ok();
+        Ok((closing_rx.await.unwrap(), closed_rx)) // todo
+    }
+
+    /// Shut down the cache, waiting for all workers to finish writing.
+    /// This function returns as soon as the shut down has started,
+    /// returning a channel through which workers can report on their
+    /// progress.
+    #[tracing::instrument(skip_all)]
+    pub async fn shutdown(&self) -> Result<(), CacheError> {
+        let (_, closed_rx) = self.start_shutdown().await?;
+        closed_rx.await.ok();
         Ok(())
     }
 }

vercel/turborepo

crates/turborepo-cache/src/async_cache.rs
@@ -1,13 +1,15 @@
-use std::sync::{atomic::AtomicU8, Arc};
+use std::sync::{atomic::AtomicU8, Arc, Mutex};
 
 use futures::{stream::FuturesUnordered, StreamExt};
-use tokio::sync::{mpsc, Semaphore};
+use tokio::sync::{mpsc, oneshot, Semaphore};
 use tracing::{warn, Instrument, Level};
 use turbopath::{AbsoluteSystemPath, AbsoluteSystemPathBuf, AnchoredSystemPathBuf};
 use turborepo_analytics::AnalyticsSender;
 use turborepo_api_client::{APIAuth, APIClient};
 
-use crate::{multiplexer::CacheMultiplexer, CacheError, CacheHitMetadata, CacheOpts};
+use crate::{
+    http::UploadMap, multiplexer::CacheMultiplexer, CacheError, CacheHitMetadata, CacheOpts,
+};
 
 const WARNING_CUTOFF: u8 = 4;
 
@@ -24,8 +26,11 @@ enum WorkerRequest {
         duration: u64,
         files: Vec<AnchoredSystemPathBuf>,
     },
-    Flush(tokio::sync::oneshot::Sender<()>),
-    Shutdown(tokio::sync::oneshot::Sender<()>),
+    Flush(oneshot::Sender<()>),
+    /// Shutdown the cache. The first oneshot notifies when shutdown starts and
+    /// allows the user to inspect the status of the uploads. The second
+    /// oneshot notifies when the shutdown is complete.
+    Shutdown(oneshot::Sender<Arc<Mutex<UploadMap>>>, oneshot::Sender<()>),
 }
 
 impl AsyncCache {
@@ -95,8 +100,8 @@ impl AsyncCache {
                         }
                         drop(callback);
                     }
-                    WorkerRequest::Shutdown(callback) => {
-                        shutdown_callback = Some(callback);
+                    WorkerRequest::Shutdown(closing, done) => {
+                        shutdown_callback = Some((closing, done));
                         break;
                     }
                 };
@@ -104,10 +109,18 @@ impl AsyncCache {
             // Drop write consumer to immediately notify callers that cache is shutting down
             drop(write_consumer);
 
+            let shutdown_callback = if let Some((closing, done)) = shutdown_callback {
+                closing.send(real_cache.requests().unwrap_or_default()).ok();
+                Some(done)
+            } else {
+                None
+            };
+
             // wait for all writers to finish
             while let Some(worker) = workers.next().await {
                 let _ = worker;
             }
+
             if let Some(callback) = shutdown_callback {
                 callback.send(()).ok();
             }
@@ -162,7 +175,7 @@ impl AsyncCache {
     // before checking the cache.
     #[tracing::instrument(skip_all)]
     pub async fn wait(&self) -> Result<(), CacheError> {
-        let (tx, rx) = tokio::sync::oneshot::channel();
+        let (tx, rx) = oneshot::channel();
         self.writer_sender
             .send(WorkerRequest::Flush(tx))
             .await
@@ -172,14 +185,30 @@ impl AsyncCache {
         Ok(())
     }
 
+    /// Shut down the cache, waiting for all workers to finish writing.
+    /// This function returns as soon as the shut down has started,
+    /// returning a channel through which workers can report on their
+    /// progress.
     #[tracing::instrument(skip_all)]
-    pub async fn shutdown(&self) -> Result<(), CacheError> {
-        let (tx, rx) = tokio::sync::oneshot::channel();
+    pub async fn start_shutdown(
+        &self,
+    ) -> Result<(Arc<Mutex<UploadMap>>, oneshot::Receiver<()>), CacheError> {
+        let (closing_tx, closing_rx) = oneshot::channel::<Arc<Mutex<UploadMap>>>();
+        let (closed_tx, closed_rx) = oneshot::channel::<()>();
         self.writer_sender
-            .send(WorkerRequest::Shutdown(tx))
+            .send(WorkerRequest::Shutdown(closing_tx, closed_tx))
             .await
             .map_err(|_| CacheError::CacheShuttingDown)?;
-        rx.await.ok();
+        Ok((closing_rx.await.unwrap(), closed_rx)) // todo
+    }
+
+    /// Shut down the cache, waiting for all workers to finish writing.
+    /// This function returns only when the last worker is complete.
+    /// It is a convenience wrapper around `start_shutdown`.
+    #[tracing::instrument(skip_all)]
+    pub async fn shutdown(&self) -> Result<(), CacheError> {
+        let (_, closed_rx) = self.start_shutdown().await?;
+        closed_rx.await.ok();
         Ok(())
     }
 }