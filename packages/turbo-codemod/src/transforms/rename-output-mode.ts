36da86a6ba17c1fa5c709491001ddeb80034ccfc
Thomas Knickman <tom.knickman@vercel.com>
2024-05-23 11:01:25 -0400

    feat(codemod): add missing package names (#8186)
    ### Description
    
    Codemod to add missing name (or fix duplicates)

--- packages/turbo-codemod/src/transforms/rename-output-mode.ts
+++ packages/turbo-codemod/src/transforms/rename-output-mode.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import { readJsonSync, existsSync } from "fs-extra";
 import { type PackageJson, getTurboConfigs } from "@turbo/utils";
 import type { Schema as TurboJsonSchema, OutputMode } from "@turbo/types";
-import type { TransformerArgs } from "../types";
+import type { Transformer, TransformerArgs } from "../types";
 import { getTransformerHelpers } from "../utils/getTransformerHelpers";
 import type { TransformerResults } from "../runner";
 
@@ -80,7 +80,7 @@ export function transformer({
   return runner.finish();
 }
 
-const transformerMeta = {
+const transformerMeta: Transformer = {
   name: TRANSFORMER,
   description: DESCRIPTION,
   introducedIn: INTRODUCED_IN,
