dfd815ed83a8c5997b49eafaade83fd2eabc7dcd
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2024-05-23 12:45:36 -0700

    chore: Remove unused base16 dependency
    Noticed this dependency was unused.
    
    The `encode_base16` function was a bit useless too, as you can do this
    with `format!()`, not requiring a third party dependency. Parsing base16
    could make sense for this as a dependency, but we're not doing that.

--- crates/turbo-tasks-hash/src/lib.rs
+++ crates/turbo-tasks-hash/src/lib.rs
@@ -4,14 +4,12 @@
 //! invalidation, and encoding the hash to an hexadecimal string for use in a
 //! file name.
 
-mod base16;
 mod deterministic_hash;
 mod hex;
 mod md4;
 mod xxh3_hash64;
 
 pub use crate::{
-    base16::encode_base16,
     deterministic_hash::{DeterministicHash, DeterministicHasher},
     hex::{encode_hex, encode_hex_string},
     md4::hash_md4,
