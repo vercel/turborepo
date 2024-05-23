017a9b7ea2f114cdae85616aa7335940eb2d5fab
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2024-05-23 13:30:27 -0700

    Also remove dead `encode_hex_string` function and remove entries from Cargo.toml

--- crates/turbo-tasks-hash/src/lib.rs
+++ crates/turbo-tasks-hash/src/lib.rs
@@ -11,7 +11,7 @@ mod xxh3_hash64;
 
 pub use crate::{
     deterministic_hash::{DeterministicHash, DeterministicHasher},
-    hex::{encode_hex, encode_hex_string},
+    hex::encode_hex,
     md4::hash_md4,
     xxh3_hash64::{hash_xxh3_hash64, Xxh3Hash64Hasher},
 };
