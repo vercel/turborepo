71d8eee83d150867b2cf2222ec2c450ff6c8b379
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-29 09:16:33 +0900

    fix more

--- crates/turbopack-css/src/references/import.rs
+++ crates/turbopack-css/src/references/import.rs
@@ -150,15 +150,17 @@ impl ImportAttributes {
             } => turbopack_core::reference_type::ImportAttributes {
                 layer: layer_name
                     .as_ref()
-                    .map(|l| l.to_css_string(Default::default()).unwrap()),
+                    .map(|l| l.to_css_string(Default::default()).unwrap())
+                    .map(From::from),
                 supports: supports
                     .as_ref()
-                    .map(|s| s.to_css_string(Default::default()).unwrap()),
+                    .map(|s| s.to_css_string(Default::default()).unwrap())
+                    .map(From::from),
                 media: {
                     if media.always_matches() {
                         None
                     } else {
-                        Some(media.to_css_string(Default::default()).unwrap())
+                        Some(media.to_css_string(Default::default()).unwrap().into())
                     }
                 },
             },
@@ -167,11 +169,11 @@ impl ImportAttributes {
                 supports,
                 media,
             } => turbopack_core::reference_type::ImportAttributes {
-                layer: layer_name.as_ref().map(gen_swc_node),
-                supports: supports.as_ref().map(gen_swc_node),
+                layer: layer_name.as_ref().map(gen_swc_node).map(From::from),
+                supports: supports.as_ref().map(gen_swc_node).map(From::from),
                 media: media
                     .as_ref()
-                    .map(|queries| queries.iter().map(gen_swc_node).collect()),
+                    .map(|queries| queries.iter().map(gen_swc_node).collect::<String>().into()),
             },
         }
     }
