8f163830b5808c6cba8cacbeb84ba883fd14a6eb
Chris Olszewski <chris.olszewski@vercel.com>
2024-05-30 13:49:23 -0700

    feat(codemod): change migrate behavior for majors

--- packages/turbo-codemod/__tests__/migrate.test.ts
+++ packages/turbo-codemod/__tests__/migrate.test.ts
@@ -924,4 +924,91 @@ describe("migrate", () => {
     // restore mocks
     mockedCheckGitStatus.mockRestore();
   });
+
+  it.only("migrates across majors with all required codemods", async () => {
+    const { root, readJson } = useFixture({
+      fixture: "old-turbo",
+    });
+
+    const packageManager = "pnpm";
+    const packageManagerVersion = "1.2.3";
+
+    // setup mocks
+    const mockedCheckGitStatus = jest
+      .spyOn(checkGitStatus, "checkGitStatus")
+      .mockReturnValue(undefined);
+    const mockedGetCurrentVersion = jest
+      .spyOn(getCurrentVersion, "getCurrentVersion")
+      .mockReturnValue("1.99.99");
+    const mockedGetLatestVersion = jest
+      .spyOn(getLatestVersion, "getLatestVersion")
+      .mockResolvedValue("2.0.0");
+    const mockedGetTurboUpgradeCommand = jest
+      .spyOn(getTurboUpgradeCommand, "getTurboUpgradeCommand")
+      .mockResolvedValue("pnpm install -g turbo@latest");
+    const mockedGetAvailablePackageManagers = jest
+      .spyOn(turboUtils, "getAvailablePackageManagers")
+      .mockResolvedValue({
+        pnpm: packageManagerVersion,
+        npm: undefined,
+        yarn: undefined,
+        bun: undefined,
+      });
+    const mockedGetWorkspaceDetails = jest
+      .spyOn(turboWorkspaces, "getWorkspaceDetails")
+      .mockResolvedValue(
+        getWorkspaceDetailsMockReturnValue({
+          root,
+          packageManager,
+        })
+      );
+
+    await migrate(root, {
+      force: false,
+      dry: false,
+      print: false,
+      install: false,
+    });
+
+    expect(readJson("package.json")).toStrictEqual({
+      dependencies: {},
+      devDependencies: {
+        turbo: "1.0.0",
+      },
+      name: "no-turbo-json",
+      packageManager: "pnpm@1.2.3",
+      version: "1.0.0",
+    });
+    expect(readJson("turbo.json")).toStrictEqual({
+      $schema: "https://turbo.build/schema.json",
+      pipeline: {
+        build: {
+          outputs: [".next/**", "!.next/cache/**"],
+        },
+        dev: {
+          cache: false,
+        },
+        lint: {},
+        test: {
+          outputs: ["dist/**", "build/**"],
+        },
+      },
+    });
+
+    // verify mocks were called
+    expect(mockedCheckGitStatus).toHaveBeenCalled();
+    expect(mockedGetCurrentVersion).toHaveBeenCalled();
+    expect(mockedGetLatestVersion).toHaveBeenCalled();
+    expect(mockedGetTurboUpgradeCommand).toHaveBeenCalled();
+    expect(mockedGetAvailablePackageManagers).toHaveBeenCalled();
+    expect(mockedGetWorkspaceDetails).toHaveBeenCalled();
+
+    // restore mocks
+    mockedCheckGitStatus.mockRestore();
+    mockedGetCurrentVersion.mockRestore();
+    mockedGetLatestVersion.mockRestore();
+    mockedGetTurboUpgradeCommand.mockRestore();
+    mockedGetAvailablePackageManagers.mockRestore();
+    mockedGetWorkspaceDetails.mockRestore();
+  });
 });
