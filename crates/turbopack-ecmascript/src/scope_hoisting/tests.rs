efc98eabdd09b6b98d31f51f7d4828ad67bd199e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-03 13:56:26 +0900

    testx

--- crates/turbopack-ecmascript/src/scope_hoisting/tests.rs
+++ crates/turbopack-ecmascript/src/scope_hoisting/tests.rs
@@ -1,3 +1,11 @@
+use turbo_tasks::Vc;
+use turbo_tasks_fs::{FileContent, FileSystemPath};
+use turbopack_core::{
+    asset::{Asset, AssetContent},
+    ident::AssetIdent,
+    module::Module,
+};
+
 use super::group::DepGraph;
 
 #[turbo_tasks::value]
@@ -12,5 +20,26 @@ impl DepGraph for TestDepGraph {
     fn depandants(&self, id: Vc<Box<dyn Module>>) -> Vc<Vec<Vc<Box<dyn Module>>>> {}
 
     fn get_edge(&self, from: Vc<Box<dyn Module>>, to: Vc<Box<dyn Module>>) -> Vc<Option<EdgeData>> {
+    }
+}
+
+#[turbo_tasks::value]
+struct TestModule {
+    path: Vc<FileSystemPath>,
+}
+
+#[turbo_tasks::value_impl]
+impl Asset for TestModule {
+    #[turbo_tasks::function]
+    fn content(self: Vc<Self>) -> Vc<AssetContent> {
+        AssetContent::File(FileContent::NotFound.cell()).cell()
+    }
+}
+
+#[turbo_tasks::value_impl]
+impl Module for TestModule {
+    #[turbo_tasks::function]
+    fn ident(&self) -> Vc<AssetIdent> {
+        AssetIdent::from_path(self.path)
     }
 }
