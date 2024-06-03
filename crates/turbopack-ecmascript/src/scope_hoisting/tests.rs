cd7219a30c89ad9ad5649362230750e9e6b78bea
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-03 13:17:08 +0900

    How to test

--- crates/turbopack-ecmascript/src/scope_hoisting/tests.rs
+++ crates/turbopack-ecmascript/src/scope_hoisting/tests.rs
@@ -1,7 +1,16 @@
 use super::group::DepGraph;
 
 #[turbo_tasks::value]
-pub struct TestDepGraph {}
+pub struct TestDepGraph {
+    deps: Vec<()>,
+}
 
 #[turbo_tasks::value_impl]
-impl DepGraph for TestDepGraph {}
+impl DepGraph for TestDepGraph {
+    fn deps(&self, id: Vc<Box<dyn Module>>) -> Vc<Vec<Vc<Box<dyn Module>>>> {}
+
+    fn depandants(&self, id: Vc<Box<dyn Module>>) -> Vc<Vec<Vc<Box<dyn Module>>>> {}
+
+    fn get_edge(&self, from: Vc<Box<dyn Module>>, to: Vc<Box<dyn Module>>) -> Vc<Option<EdgeData>> {
+    }
+}
