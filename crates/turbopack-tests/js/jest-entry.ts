e87a43402cb1becb9c57cd141d455413b523c7aa
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-06-04 17:50:02 +0200

    work around tree shaking issue

--- crates/turbopack-tests/js/jest-entry.ts
+++ crates/turbopack-tests/js/jest-entry.ts
@@ -3,18 +3,20 @@
 const jest = __turbopack_external_require__("jest-circus");
 const expectMod = __turbopack_external_require__("expect");
 
-globalThis.describe = jest.describe;
-globalThis.it = jest.it;
-globalThis.test = jest.test;
-globalThis.expect = expectMod.expect;
+function setupGlobals() {
+  globalThis.describe = jest.describe;
+  globalThis.it = jest.it;
+  globalThis.test = jest.test;
+  globalThis.expect = expectMod.expect;
 
-// From https://github.com/webpack/webpack/blob/9fcaa243573005d6fdece9a3f8d89a0e8b399613/test/TestCases.template.js#L422
-globalThis.nsObj = function nsObj(obj) {
-  Object.defineProperty(obj, Symbol.toStringTag, {
-    value: "Module",
-  });
-  return obj;
-};
+  // From https://github.com/webpack/webpack/blob/9fcaa243573005d6fdece9a3f8d89a0e8b399613/test/TestCases.template.js#L422
+  globalThis.nsObj = function nsObj(obj) {
+    Object.defineProperty(obj, Symbol.toStringTag, {
+      value: "Module",
+    });
+    return obj;
+  };
+}
 
 const uncaughtExceptions: string[] = [];
 const unhandledRejections: string[] = [];
@@ -28,6 +30,8 @@ process.on("unhandledRejection", (e) => {
 });
 
 export default async function run() {
+  setupGlobals();
+
   await import("TESTS");
 
   const jestResult = await jest.run();
