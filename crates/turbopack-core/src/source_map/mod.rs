5c25eee1189ba0c9fbb9f7adc197b69700e58dd0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-05 13:33:48 +0900

    fix tests

--- crates/turbopack-core/src/source_map/mod.rs
+++ crates/turbopack-core/src/source_map/mod.rs
@@ -270,9 +270,9 @@ impl SourceMap {
                 // in Node.js that requires sectioned source maps to have a `sources` array.
                 let mut rope = RopeBuilder::from(
                     r#"{
-      "version": 3,
-      "sources": [],
-      "sections": ["#,
+  "version": 3,
+  "sources": [],
+  "sections": ["#,
                 );
 
                 let sections = s
@@ -302,7 +302,7 @@ impl SourceMap {
                 }
 
                 rope += "]
-    }";
+}";
 
                 rope.build()
             }
