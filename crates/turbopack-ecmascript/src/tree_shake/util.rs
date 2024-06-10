0bc9de0993f42be4ab6e3fe848927af6841f7aff
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-10 21:33:03 +0900

    Merge two visitors

--- crates/turbopack-ecmascript/src/tree_shake/util.rs
+++ crates/turbopack-ecmascript/src/tree_shake/util.rs
@@ -20,18 +20,36 @@ enum Mode {
     Write,
 }
 
+#[derive(Default)]
+struct Target {
+    direct: bool,
+    eventual: bool,
+}
+
 /// A visitor which collects variables which are read or written.
 #[derive(Default)]
 pub(crate) struct IdentUsageCollector {
     unresolved: SyntaxContext,
     top_level: SyntaxContext,
     vars: Vars,
-    ignore_nested: bool,
+
+    is_nested: bool,
+    target: Target,
     /// None means both read and write
     mode: Option<Mode>,
 }
 
 impl IdentUsageCollector {
+    fn with_nested(&mut self, f: impl FnOnce(&mut Self)) {
+        if !self.target.eventual {
+            return;
+        }
+
+        let old = self.is_nested;
+        self.is_nested = true;
+        f(self);
+        self.is_nested = old;
+    }
     fn with_mode(&mut self, mode: Option<Mode>, f: impl FnOnce(&mut Self)) {
         let old = self.mode;
         self.mode = mode;
@@ -48,19 +66,15 @@ impl Visit for IdentUsageCollector {
     }
 
     fn visit_block_stmt_or_expr(&mut self, n: &BlockStmtOrExpr) {
-        if self.ignore_nested {
-            return;
-        }
-
-        n.visit_children_with(self);
+        self.with_nested(|this| {
+            n.visit_children_with(this);
+        })
     }
 
     fn visit_constructor(&mut self, n: &Constructor) {
-        if self.ignore_nested {
-            return;
-        }
-
-        n.visit_children_with(self);
+        self.with_nested(|this| {
+            n.visit_children_with(this);
+        })
     }
 
     fn visit_export_named_specifier(&mut self, n: &ExportNamedSpecifier) {
@@ -80,14 +94,16 @@ impl Visit for IdentUsageCollector {
     }
 
     fn visit_function(&mut self, n: &Function) {
-        if self.ignore_nested {
-            return;
-        }
-
-        n.visit_children_with(self);
+        self.with_nested(|this| {
+            n.visit_children_with(this);
+        })
     }
 
     fn visit_ident(&mut self, n: &Ident) {
+        if !self.target.direct && !self.is_nested {
+            return;
+        }
+
         if n.span.ctxt == self.unresolved {
             self.vars.found_unresolved = true;
             return;
@@ -107,10 +123,6 @@ impl Visit for IdentUsageCollector {
                 self.vars.write.insert(n.to_id());
             }
             None => {
-                // Ignore calls like console.log()
-                if n.span.ctxt == self.unresolved {
-                    return;
-                }
                 self.vars.read.insert(n.to_id());
                 self.vars.write.insert(n.to_id());
             }
@@ -131,139 +143,12 @@ impl Visit for IdentUsageCollector {
             n.visit_children_with(self);
         }
     }
-
     fn visit_named_export(&mut self, n: &NamedExport) {
         if n.src.is_some() {
             return;
         }
 
         n.visit_children_with(self);
-    }
-
-    fn visit_pat(&mut self, p: &Pat) {
-        self.with_mode(Some(Mode::Write), |this| {
-            p.visit_children_with(this);
-        })
-    }
-
-    fn visit_prop_name(&mut self, n: &PropName) {
-        if let PropName::Computed(..) = n {
-            n.visit_children_with(self);
-        }
-    }
-
-    noop_visit_type!();
-}
-
-/// A visitor which collects variables which are read or written, but not at the
-/// evaluation time.
-#[derive(Default)]
-pub(crate) struct CapturedIdCollector {
-    unresolved: SyntaxContext,
-    top_level: SyntaxContext,
-
-    vars: Vars,
-    is_nested: bool,
-    /// None means both read and write
-    mode: Option<Mode>,
-}
-
-impl CapturedIdCollector {
-    fn with_nested(&mut self, f: impl FnOnce(&mut Self)) {
-        let old = self.is_nested;
-        self.is_nested = true;
-        f(self);
-        self.is_nested = old;
-    }
-
-    fn with_mode(&mut self, mode: Option<Mode>, f: impl FnOnce(&mut Self)) {
-        let old = self.mode;
-        self.mode = mode;
-        f(self);
-        self.mode = old;
-    }
-}
-
-impl Visit for CapturedIdCollector {
-    fn visit_assign_target(&mut self, n: &AssignTarget) {
-        self.with_mode(Some(Mode::Write), |this| {
-            n.visit_children_with(this);
-        })
-    }
-
-    fn visit_block_stmt_or_expr(&mut self, n: &BlockStmtOrExpr) {
-        self.with_nested(|this| {
-            n.visit_children_with(this);
-        })
-    }
-
-    fn visit_constructor(&mut self, n: &Constructor) {
-        self.with_nested(|this| {
-            n.visit_children_with(this);
-        })
-    }
-
-    fn visit_export_specifier(&mut self, n: &ExportSpecifier) {
-        self.with_mode(Some(Mode::Read), |this| {
-            n.visit_children_with(this);
-        })
-    }
-
-    fn visit_expr(&mut self, e: &Expr) {
-        self.with_mode(Some(Mode::Read), |this| {
-            e.visit_children_with(this);
-        })
-    }
-
-    fn visit_function(&mut self, n: &Function) {
-        self.with_nested(|this| {
-            n.visit_children_with(this);
-        })
-    }
-
-    fn visit_ident(&mut self, n: &Ident) {
-        if !self.is_nested {
-            return;
-        }
-
-        if n.span.ctxt == self.unresolved {
-            self.vars.found_unresolved = true;
-            return;
-        }
-
-        // We allow SyntaxContext::empty() because Some built-in files do not go into
-        // resolver()
-        if n.span.ctxt != self.top_level && n.span.ctxt != SyntaxContext::empty() {
-            return;
-        }
-
-        match self.mode {
-            Some(Mode::Read) => {
-                self.vars.read.insert(n.to_id());
-            }
-            Some(Mode::Write) => {
-                self.vars.write.insert(n.to_id());
-            }
-            None => {
-                self.vars.read.insert(n.to_id());
-                self.vars.write.insert(n.to_id());
-            }
-        }
-    }
-
-    fn visit_member_expr(&mut self, e: &MemberExpr) {
-        self.with_mode(None, |this| {
-            // Skip visit_expr
-            e.obj.visit_children_with(this);
-        });
-
-        e.prop.visit_with(self);
-    }
-
-    fn visit_member_prop(&mut self, n: &MemberProp) {
-        if let MemberProp::Computed(..) = n {
-            n.visit_children_with(self);
-        }
     }
 
     fn visit_pat(&mut self, p: &Pat) {
@@ -298,12 +183,15 @@ pub(crate) struct Vars {
 /// are not interesting. We only need to analyze top-level variables.
 pub(crate) fn ids_captured_by<N>(n: &N, unresolved: SyntaxContext, top_level: SyntaxContext) -> Vars
 where
-    N: VisitWith<CapturedIdCollector>,
+    N: VisitWith<IdentUsageCollector>,
 {
-    let mut v = CapturedIdCollector {
+    let mut v = IdentUsageCollector {
         unresolved,
         top_level,
-        is_nested: false,
+        target: Target {
+            direct: false,
+            eventual: true,
+        },
         ..Default::default()
     };
     n.visit_with(&mut v);
@@ -321,7 +209,10 @@ where
     let mut v = IdentUsageCollector {
         unresolved,
         top_level,
-        ignore_nested: false,
+        target: Target {
+            direct: true,
+            eventual: true,
+        },
         ..Default::default()
     };
     n.visit_with(&mut v);
@@ -343,7 +234,10 @@ where
     let mut v = IdentUsageCollector {
         unresolved,
         top_level,
-        ignore_nested: true,
+        target: Target {
+            direct: true,
+            eventual: false,
+        },
         ..Default::default()
     };
     n.visit_with(&mut v);
