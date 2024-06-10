ae4ab4fc1519f7d9608b9c67af1172f6f2faf4fd
Anthony Shew <anthonyshew@gmail.com>
2024-06-10 09:44:45 -0600

    reconfigure

--- examples/kitchen-sink/packages/config-eslint/next.js
+++ examples/kitchen-sink/packages/config-eslint/next.js
@@ -13,13 +13,15 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 module.exports = {
   extends: [
-    "@vercel/style-guide/eslint/node",
-    "@vercel/style-guide/eslint/typescript",
-    "@vercel/style-guide/eslint/browser",
-    "@vercel/style-guide/eslint/react",
-    "@vercel/style-guide/eslint/next",
+    ...[
+      "@vercel/style-guide/eslint/node",
+      "@vercel/style-guide/eslint/typescript",
+      "@vercel/style-guide/eslint/browser",
+      "@vercel/style-guide/eslint/react",
+      "@vercel/style-guide/eslint/next",
+    ].map(require.resolve),
     "turbo",
-  ].map(require.resolve),
+  ],
   parserOptions: {
     project,
   },
