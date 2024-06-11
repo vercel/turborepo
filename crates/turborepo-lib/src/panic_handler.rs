9dc93c3c0062b5b93db326a49497c15abce7c604
Chris Olszewski <chris.olszewski@vercel.com>
2024-06-11 10:42:34 -0700

    fix: no longer write backtrace to tmp file in CI

--- crates/turborepo-lib/src/panic_handler.rs
+++ crates/turborepo-lib/src/panic_handler.rs
@@ -2,6 +2,9 @@ use human_panic::report::{Method, Report};
 
 use crate::get_version;
 
+const OPEN_ISSUE_MESSAGE: &str =
+    "Please open an issue at https://github.com/vercel/turbo/issues/new/choose";
+
 pub fn panic_handler(panic_info: &std::panic::PanicInfo) {
     let cause = panic_info
         .message()
@@ -14,25 +17,37 @@ pub fn panic_handler(panic_info: &std::panic::PanicInfo) {
     };
 
     let report = Report::new("turbo", get_version(), Method::Panic, explanation, cause);
+    // If we're in CI we don't persist the backtrace to a temp file as this is hard
+    // to retrieve.
+    let should_persist = !turborepo_ci::is_ci() && turborepo_ci::Vendor::infer().is_none();
 
-    let report_message = match report.persist() {
-        Ok(f) => {
-            format!(
-                "A report has been written to {}\n
-Please open an issue at https://github.com/vercel/turbo/issues/new/choose \
-                 and include this file",
-                f.display()
-            )
+    let report_message = if should_persist {
+        match report.persist() {
+            Ok(f) => {
+                format!(
+                    "A report has been written to {}\n\n{OPEN_ISSUE_MESSAGE} and include this file",
+                    f.display()
+                )
+            }
+            Err(e) => {
+                format!(
+                    "An error has occurred while attempting to write a \
+                     report.\n\n{OPEN_ISSUE_MESSAGE} and include the following error in your \
+                     issue: {}",
+                    e
+                )
+            }
         }
-        Err(e) => {
-            format!(
-                "An error has occurred while attempting to write a report.\n
-Please open an issue at \
-                 https://github.com/vercel/turbo/issues/new/choose and include the following \
-                 error in your issue: {}",
-                e
-            )
-        }
+    } else if let Some(backtrace) = report.serialize() {
+        format!(
+            "Caused by \n{backtrace}\n\n{OPEN_ISSUE_MESSAGE} and include this message in your \
+             issue"
+        )
+    } else {
+        format!(
+            "Unable to serialize backtrace.\n\n{OPEN_ISSUE_MESSAGE} and include this message in \
+             your issue"
+        )
     };
 
     eprintln!(
