170bc484867ecc9fd61debabcf653364f0c7f343
Alexander Lyon <arlyon@me.com>
2024-06-17 13:03:42 +0100

    release LSP 2.0

--- crates/turborepo-lib/src/lib.rs
+++ crates/turborepo-lib/src/lib.rs
@@ -42,7 +42,9 @@ mod unescape;
 pub use crate::{
     child::spawn_child,
     cli::Args,
-    daemon::{DaemonClient, DaemonConnector, Paths as DaemonPaths},
+    daemon::{
+        DaemonClient, DaemonConnector, DaemonConnectorError, DaemonError, Paths as DaemonPaths,
+    },
     panic_handler::panic_handler,
     run::package_discovery::DaemonPackageDiscovery,
 };
