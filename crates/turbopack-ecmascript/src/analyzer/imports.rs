c7f2499d41fb5a9b9ce42045235145f453ab6d2a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-17 12:53:53 +0900

    Do not drop internal_symbol

--- crates/turbopack-ecmascript/src/analyzer/imports.rs
+++ crates/turbopack-ecmascript/src/analyzer/imports.rs
@@ -310,6 +310,17 @@ impl Visit for Analyzer<'_> {
 
             self.data.imports.insert(local, (i, orig_sym));
         }
+
+        if import.specifiers.is_empty() {
+            if let Some(internal_symbol) = internal_symbol {
+                self.ensure_reference(
+                    import.span,
+                    import.src.value.clone(),
+                    internal_symbol,
+                    annotations,
+                );
+            }
+        }
     }
 
     fn visit_export_all(&mut self, export: &ExportAll) {
