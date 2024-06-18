330d378c1c34c0d60b05c65172e78608934aa56c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-18 09:55:02 +0900

    Remove useless unit test

--- crates/turbopack-ecmascript/tests/tree-shaker/analyzer/jsx-dev-1/input.js
+++
@@ -1,8 +0,0 @@
-import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
-export default ()=>_jsxDEV("div", {
-        children: "Hello World"
-    }, void 0, false, {
-        fileName: "[project]/test/integration/dist-dir/pages/index.js",
-        lineNumber: 1,
-        columnNumber: 22
-    }, this);
