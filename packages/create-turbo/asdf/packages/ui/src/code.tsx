13b3dc0222198883850ad02ddadeb6e779087254
Anthony Shew <anthonyshew@gmail.com>
2024-06-19 10:50:29 -0600

    Oops.

--- packages/create-turbo/asdf/packages/ui/src/code.tsx
+++
@@ -1,9 +0,0 @@
-export function Code({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}): JSX.Element {
-  return <code className={className}>{children}</code>;
-}
