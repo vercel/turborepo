8ca5842f05b570c646bb7102afb28f221989b2bc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-20 08:57:01 +0900

    fixup

--- crates/turbopack-ecmascript/tests/tree-shaker/analyzer/amphtml-document/input.js
+++ crates/turbopack-ecmascript/tests/tree-shaker/analyzer/amphtml-document/input.js
@@ -1,35 +1,34 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `html { background: hotpink; }`,
-            }}
-          />
-        </>
-      ),
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+            styles: _jsxs(_Fragment, {
+                children: [
+                    initialProps.styles,
+                    _jsx("style", {
+                        dangerouslySetInnerHTML: {
+                            __html: `html { background: hotpink; }`
+                        }
+                    })
+                ]
+            })
+        };
     }
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render() {
+        return _jsxs(Html, {
+            children: [
+                _jsx(Head, {}),
+                _jsxs("body", {
+                    children: [
+                        _jsx(Main, {}),
+                        _jsx(NextScript, {})
+                    ]
+                })
+            ]
+        });
+    }
 }
-
-export default MyDocument
+export default MyDocument;
