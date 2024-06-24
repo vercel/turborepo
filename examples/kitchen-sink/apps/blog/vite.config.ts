9a30ec93f636b176483aeaa769cfdc2300ea070e
Anthony Shew <anthonyshew@gmail.com>
2024-06-24 17:57:46 -0600

    More.

--- examples/kitchen-sink/apps/blog/vite.config.ts
+++ examples/kitchen-sink/apps/blog/vite.config.ts
@@ -1,10 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite";
 
 export default defineConfig({
   plugins: [
     remix({
+      presets: [vercelPreset()],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
