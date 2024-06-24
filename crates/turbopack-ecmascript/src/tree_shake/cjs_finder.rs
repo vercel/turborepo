a16cbe10103835c6054e537b8678044f50adafbe
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-24 11:06:20 +0900

    should_skip_tree_shaking

--- crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
+++ crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
@@ -1,8 +1,26 @@
 use swc_core::ecma::ast::*;
 
+use crate::TURBOPACK_HELPER;
+
 pub fn should_skip_tree_shaking(m: &Program) -> bool {
     if let Program::Module(m) = m {
-        if m.body.iter().any(|s| s.is_module_decl()) {
+        if m.body.iter().any(|item| {
+            if let ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {
+                with: Some(with), ..
+            })) = item
+            {
+                let with = with.as_import_with();
+                if let Some(with) = with {
+                    for item in with.values.iter() {
+                        if item.key.sym == *TURBOPACK_HELPER {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }) {
             return false;
         }
     }
