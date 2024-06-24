94d2fafc4934bbdd362d757bea094b006022dbc7
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-24 12:31:59 +0900

    disable some

--- crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
+++ crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
@@ -4,7 +4,7 @@ use crate::TURBOPACK_HELPER;
 
 pub fn should_skip_tree_shaking(m: &Program) -> bool {
     if let Program::Module(m) = m {
-        if m.body.iter().any(|item| {
+        match m.body.iter().any(|item| {
             if let ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {
                 with: Some(with), ..
             })) = item
@@ -20,14 +20,45 @@ pub fn should_skip_tree_shaking(m: &Program) -> bool {
                 }
             }
 
-            // We don't have logic to tree shake export * from
-            if let ModuleItem::ModuleDecl(ModuleDecl::ExportAll(..)) = item {
-                return false;
+            match item {
+                // We don't have logic to tree shake export * from
+                ModuleItem::ModuleDecl(ModuleDecl::ExportAll(..)) => return false,
+
+                // Tree shaking has a bug related to ModuleExportName::Str
+                ModuleItem::ModuleDecl(ModuleDecl::Import(import)) => {
+                    for s in import.specifiers.iter() {
+                        if let ImportSpecifier::Named(is) = s {
+                            if matches!(is.imported, Some(ModuleExportName::Str(..))) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                // Tree shaking has a bug related to ModuleExportName::Str
+                ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(NamedExport {
+                    src: Some(..),
+                    specifiers,
+                    ..
+                })) => {
+                    for s in specifiers {
+                        if let ExportSpecifier::Named(es) = s {
+                            if matches!(es.orig, ModuleExportName::Str(..))
+                                || matches!(es.exported, Some(ModuleExportName::Str(..)))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                _ => (),
             }
 
             item.is_module_decl()
         }) {
-            return false;
+            true => return false,
+            false => (),
         }
     }
 
