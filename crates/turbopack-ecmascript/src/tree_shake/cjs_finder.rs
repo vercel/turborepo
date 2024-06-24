d849c857b5513283fe340f70302479913ece13fb
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-24 15:00:54 +0900

    `should_skip_tree_shaking`

--- crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
+++ crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
@@ -4,32 +4,32 @@ use crate::TURBOPACK_HELPER;
 
 pub fn should_skip_tree_shaking(m: &Program) -> bool {
     if let Program::Module(m) = m {
-        if m.body.iter().any(|item| {
-            if let ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {
-                with: Some(with), ..
-            })) = item
-            {
-                let with = with.as_import_with();
-                if let Some(with) = with {
-                    for item in with.values.iter() {
-                        if item.key.sym == *TURBOPACK_HELPER {
-                            // Skip tree shaking if the import is from turbopack-helper
-                            return false;
+        for item in m.body.iter() {
+            match item {
+                // Skip turbopack helpers.
+                ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {
+                    with: Some(with), ..
+                })) => {
+                    let with = with.as_import_with();
+                    if let Some(with) = with {
+                        for item in with.values.iter() {
+                            if item.key.sym == *TURBOPACK_HELPER {
+                                // Skip tree shaking if the import is from turbopack-helper
+                                return true;
+                            }
                         }
                     }
                 }
-            }
 
-            match item {
                 // We don't have logic to tree shake export * from
-                ModuleItem::ModuleDecl(ModuleDecl::ExportAll(..)) => return false,
+                ModuleItem::ModuleDecl(ModuleDecl::ExportAll(..)) => return true,
 
                 // Tree shaking has a bug related to ModuleExportName::Str
                 ModuleItem::ModuleDecl(ModuleDecl::Import(import)) => {
                     for s in import.specifiers.iter() {
                         if let ImportSpecifier::Named(is) = s {
                             if matches!(is.imported, Some(ModuleExportName::Str(..))) {
-                                return false;
+                                return true;
                             }
                         }
                     }
@@ -46,7 +46,7 @@ pub fn should_skip_tree_shaking(m: &Program) -> bool {
                             if matches!(es.orig, ModuleExportName::Str(..))
                                 || matches!(es.exported, Some(ModuleExportName::Str(..)))
                             {
-                                return false;
+                                return true;
                             }
                         }
                     }
@@ -58,14 +58,16 @@ pub fn should_skip_tree_shaking(m: &Program) -> bool {
                 ModuleItem::Stmt(Stmt::Decl(Decl::Var(box VarDecl {
                     kind: VarDeclKind::Let,
                     ..
-                }))) => return false,
+                }))) => return true,
 
-                _ => (),
+                _ => {}
             }
+        }
 
-            item.is_module_decl()
-        }) {
-            return false;
+        for item in m.body.iter() {
+            if item.is_module_decl() {
+                return false;
+            }
         }
     }
 
