51507c9394a0f3de5bd349f4d5ae75308a90fb53
Anthony Shew <anthony.shew@vercel.com>
2024-06-25 08:56:18 -0600

    Fixing kitchen-sink example. (#8600)
    ### Description
    
    <!--
      ✍️ Write a short summary of your work.
      If necessary, include relevant screenshots.
    -->
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->

--- examples/kitchen-sink/apps/blog/vite.config.ts
+++ examples/kitchen-sink/apps/blog/vite.config.ts
@@ -1,18 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+installGlobals();
 
 export default defineConfig({
-  plugins: [
-    remix({
-      presets: [vercelPreset()],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
 });
