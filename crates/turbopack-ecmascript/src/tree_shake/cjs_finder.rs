8f52523b7224d28aaf40cb13886daad48c18c185
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-01 14:32:04 +0900

    Cleanup cjs

--- crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
+++ crates/turbopack-ecmascript/src/tree_shake/cjs_finder.rs
@@ -11,13 +11,24 @@ pub fn should_skip_tree_shaking(m: &Program) -> bool {
             match item {
                 // Skip turbopack helpers.
                 ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {
-                    with: Some(with), ..
+                    with,
+                    src,
+                    specifiers,
+                    ..
                 })) => {
-                    let with = with.as_import_with();
-                    if let Some(with) = with {
+                    if let Some(with) = with.as_deref().and_then(|v| v.as_import_with()) {
                         for item in with.values.iter() {
                             if item.key.sym == *TURBOPACK_HELPER {
                                 // Skip tree shaking if the import is from turbopack-helper
+                                return true;
+                            }
+                        }
+                    }
+
+                    // Tree shaking has a bug related to ModuleExportName::Str
+                    for s in specifiers.iter() {
+                        if let ImportSpecifier::Named(is) = s {
+                            if matches!(is.imported, Some(ModuleExportName::Str(..))) {
                                 return true;
                             }
                         }
@@ -26,17 +37,6 @@ pub fn should_skip_tree_shaking(m: &Program) -> bool {
 
                 // We don't have logic to tree shake export * from
                 ModuleItem::ModuleDecl(ModuleDecl::ExportAll(..)) => return true,
-
-                // Tree shaking has a bug related to ModuleExportName::Str
-                ModuleItem::ModuleDecl(ModuleDecl::Import(import)) => {
-                    for s in import.specifiers.iter() {
-                        if let ImportSpecifier::Named(is) = s {
-                            if matches!(is.imported, Some(ModuleExportName::Str(..))) {
-                                return true;
-                            }
-                        }
-                    }
-                }
 
                 // Tree shaking has a bug related to ModuleExportName::Str
                 ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(NamedExport {
