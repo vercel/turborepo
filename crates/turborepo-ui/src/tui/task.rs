43473b4ae1a43d196c1995cd69d030bb1aa00e7e
Anthony Shew <anthony.shew@vercel.com>
2024-07-03 16:08:37 -0600

    (refactor)ui: Refactoring/simplifying TUI state (#8650)
    ### Description
    
    Previously, we held the state for the TUI in the sub-components of
    `app.rs`. In this PR, we're hoisting those state variables up so we can
    have better access to that shared state and create more robust UI.
    
    ### Testing Instructions
    
    There two behavioral changes in this PR: 
    
    - Reordering of the task list so that running tasks are first in the
    list, then planned tasks, then done tasks.
    - We are removing the spinner in favor of the "two arrow" indicator.
    Users mentioned that the visual noise was more annoying than helpful, so
    let's quiet it down.
    
    Beyond that, you should see no behavioral changes.
    
    ---------
    
    Co-authored-by: Chris Olszewski <chris.olszewski@vercel.com>

--- crates/turborepo-ui/src/tui/task.rs
+++ crates/turborepo-ui/src/tui/task.rs
@@ -24,6 +24,12 @@ pub struct Task<S> {
     state: S,
 }
 
+pub enum TaskType {
+    Planned,
+    Running,
+    Finished,
+}
+
 impl<S> Task<S> {
     pub fn name(&self) -> &str {
         &self.name
@@ -80,5 +86,56 @@ impl Task<Finished> {
 
     pub fn result(&self) -> TaskResult {
         self.state.result
+    }
+}
+
+pub struct TaskNamesByStatus {
+    pub running: Vec<String>,
+    pub planned: Vec<String>,
+    pub finished: Vec<String>,
+}
+
+#[derive(Clone)]
+pub struct TasksByStatus {
+    pub running: Vec<Task<Running>>,
+    pub planned: Vec<Task<Planned>>,
+    pub finished: Vec<Task<Finished>>,
+}
+
+impl TasksByStatus {
+    pub fn all_empty(&self) -> bool {
+        self.planned.is_empty() && self.finished.is_empty() && self.running.is_empty()
+    }
+
+    pub fn count_all(&self) -> usize {
+        self.task_names_in_displayed_order().count()
+    }
+
+    pub fn task_names_in_displayed_order(&self) -> impl Iterator<Item = &str> + '_ {
+        let running_names = self.running.iter().map(|task| task.name());
+        let planned_names = self.planned.iter().map(|task| task.name());
+        let finished_names = self.finished.iter().map(|task| task.name());
+
+        running_names.chain(planned_names).chain(finished_names)
+    }
+
+    pub fn task_name(&self, index: usize) -> &str {
+        self.task_names_in_displayed_order().nth(index).unwrap()
+    }
+
+    pub fn tasks_started(&self) -> Vec<String> {
+        let (errors, success): (Vec<_>, Vec<_>) = self
+            .finished
+            .iter()
+            .partition(|task| matches!(task.result(), TaskResult::Failure));
+
+        // We return errors last as they most likely have information users want to see
+        success
+            .into_iter()
+            .map(|task| task.name())
+            .chain(self.running.iter().map(|task| task.name()))
+            .chain(errors.into_iter().map(|task| task.name()))
+            .map(|task| task.to_string())
+            .collect()
     }
 }
---
+++ crates/turborepo-ui/src/tui/term_output.rs
@@ -0,0 +1,68 @@
+use std::io::Write;
+
+use turborepo_vt100 as vt100;
+
+use super::{app::Direction, Error};
+
+pub struct TerminalOutput<W> {
+    rows: u16,
+    cols: u16,
+    pub parser: vt100::Parser,
+    pub stdin: Option<W>,
+    pub status: Option<String>,
+}
+
+impl<W> TerminalOutput<W> {
+    pub fn new(rows: u16, cols: u16, stdin: Option<W>) -> Self {
+        Self {
+            parser: vt100::Parser::new(rows, cols, 1024),
+            stdin,
+            rows,
+            cols,
+            status: None,
+        }
+    }
+
+    pub fn title(&self, task_name: &str) -> String {
+        match self.status.as_deref() {
+            Some(status) => format!(" {task_name} > {status} "),
+            None => format!(" {task_name} > "),
+        }
+    }
+
+    pub fn resize(&mut self, rows: u16, cols: u16) {
+        if self.rows != rows || self.cols != cols {
+            self.parser.screen_mut().set_size(rows, cols);
+        }
+        self.rows = rows;
+        self.cols = cols;
+    }
+
+    pub fn scroll(&mut self, direction: Direction) -> Result<(), Error> {
+        let scrollback = self.parser.screen().scrollback();
+        let new_scrollback = match direction {
+            Direction::Up => scrollback + 1,
+            Direction::Down => scrollback.saturating_sub(1),
+        };
+        self.parser.screen_mut().set_scrollback(new_scrollback);
+        Ok(())
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub fn persist_screen(&self, task_name: &str) -> std::io::Result<()> {
+        let screen = self.parser.entire_screen();
+        let title = self.title(task_name);
+        let mut stdout = std::io::stdout().lock();
+        stdout.write_all("┌".as_bytes())?;
+        stdout.write_all(title.as_bytes())?;
+        stdout.write_all(b"\r\n")?;
+        for row in screen.rows_formatted(0, self.cols) {
+            stdout.write_all("│ ".as_bytes())?;
+            stdout.write_all(&row)?;
+            stdout.write_all(b"\r\n")?;
+        }
+        stdout.write_all("└────>\r\n".as_bytes())?;
+
+        Ok(())
+    }
+}
