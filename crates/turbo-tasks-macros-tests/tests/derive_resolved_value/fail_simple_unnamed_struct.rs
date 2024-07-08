3af0f86ed2dec2adb829748f43f6e92599626c62
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2024-07-08 16:55:17 -0700

    Split failing tests
    Error ordering was non-deterministic. One error per file should mitigate
    this.

--- crates/turbo-tasks-macros-tests/tests/derive_resolved_value/fail_simple_unnamed_struct.rs
+++ crates/turbo-tasks-macros-tests/tests/derive_resolved_value/fail_simple_unnamed_struct.rs
@@ -5,11 +5,6 @@ use turbo_tasks::ResolvedValue;
 struct UnresolvedValue;
 
 #[derive(ResolvedValue)]
-struct ContainsUnresolvedValueNamed {
-    a: UnresolvedValue,
-}
-
-#[derive(ResolvedValue)]
 struct ContainsUnresolvedValueUnnamed(UnresolvedValue);
 
 fn main() {}
