f13f59f040135dd18b8152cbe17dec006eea8016
Anthony Shew <anthony.shew@vercel.com>
2024-07-08 16:41:21 -0600

    Add VSCode's debugger variables to default passthroughs. (#8689)
    ### Description
    
    From
    https://github.com/microsoft/vscode-js-debug/blob/5b0f41dbe845d693a541c1fae30cec04c878216f/src/targets/node/nodeLauncherBase.ts#L320

--- crates/turborepo-lib/src/task_hash.rs
+++ crates/turborepo-lib/src/task_hash.rs
@@ -477,6 +477,9 @@ impl<'a> TaskHasher<'a> {
                         "COLORTERM",
                         "TERM",
                         "TERM_PROGRAM",
+                        // VSCode IDE - https://github.com/microsoft/vscode-js-debug/blob/5b0f41dbe845d693a541c1fae30cec04c878216f/src/targets/node/nodeLauncherBase.ts#L320
+                        "VSCODE_*",
+                        "ELECTRON_RUN_AS_NODE",
                         // Docker - https://docs.docker.com/engine/reference/commandline/cli/#environment-variables
                         "DOCKER_*",
                         "BUILDKIT_*",
