3d7a277bf5dd299f1ef361a498016d695d889dd9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-11 17:06:22 +0900

    Eanble optimization again

--- crates/turbopack-ecmascript/src/tree_shake/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/mod.rs
@@ -213,14 +213,9 @@ impl Analyzer<'_> {
                     // Optimization: Remove each module item to which we
                     // just created a strong dependency from LAST_WRITES
 
-                    // TODO(kdy1): Re-enable this optimization
-                    // I disabled this optimization because it causes some issues while enabling
-                    // tree shaking in next.js
-                    //
-                    // state
-                    //     .last_writes
-                    //     .retain(|last_write| !self.g.has_dep(item_id,
-                    // last_write, true));
+                    state
+                        .last_writes
+                        .retain(|last_write| !self.g.has_dep(item_id, last_write, true));
 
                     // Drop all writes which are not reachable from this item.
                     //
