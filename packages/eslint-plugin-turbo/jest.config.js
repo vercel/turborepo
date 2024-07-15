26065a541f22576ceb4e3cf5c69773e21e3b96dd
torresgol10.it <torresgol10.it@gmail.com>
2024-07-15 20:19:35 +0000

    Update in eslint-plugin-turbo

--- packages/eslint-plugin-turbo/jest.config.js
+++ packages/eslint-plugin-turbo/jest.config.js
@@ -1,4 +1,4 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
   roots: ["<rootDir>"],
   transform: {
