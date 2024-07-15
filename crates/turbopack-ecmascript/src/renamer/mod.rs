b0b89e5d472b3818d0c4174bd17f5ae6d9561aa5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-15 16:02:09 +0900

    buf

--- crates/turbopack-ecmascript/src/renamer/mod.rs
+++ crates/turbopack-ecmascript/src/renamer/mod.rs
@@ -7,12 +7,23 @@ use crate::{analyzer::graph::EvalContext, parse::ParseResult};
 /// Rename `modules` so those modules has no conflict regardless of the way we
 /// use for merging.
 ///
+/// The returned modules are guaranteed to have same order as input.
+///
 /// Designed after the renamer of esbuild.
 ///
 /// This renamer renames non-top-level identifiers in parallel, and top-level
 /// identifiers in series.
 #[turbo_tasks::function]
-pub async fn rename_modules(modules: Vc<Vec<Vc<ParseResult>>>) -> Result<Vec<Vc<ParseResult>>> {}
+pub async fn rename_modules(modules: Vc<Vec<Vc<ParseResult>>>) -> Result<Vc<Vec<Vc<ParseResult>>>> {
+    let modules = modules.await?;
+    let mut buf = Vec::with_capacity(modules.len());
+
+    for &module in modules.iter() {
+        buf.push(rename_module(module).resolve().await?);
+    }
+
+    Ok(Vc::cell(buf))
+}
 
 #[turbo_tasks::function]
 async fn rename_module(module: Vc<ParseResult>) -> Result<Vc<ParseResult>> {
