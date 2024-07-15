d40e7f298b41ef3b1c9c7007162663dd294d5f25
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-15 15:40:19 +0900

    `rename_module`

--- crates/turbopack-ecmascript/src/renamer/mod.rs
+++ crates/turbopack-ecmascript/src/renamer/mod.rs
@@ -1,1 +1,24 @@
+use anyhow::Result;
+use turbo_tasks::Vc;
 
+use crate::parse::ParseResult;
+
+/// Designed after the renamer of esbuild.
+///
+/// This renamer renames non-top-level identifiers in parallel, and top-level
+/// identifiers in series.
+
+struct Renamer {}
+
+async fn rename_module(module: Vc<ParseResult>) -> Result<Vc<ParseResult>> {
+    match &*module.await? {
+        ParseResult::Ok {
+            program,
+            comments,
+            eval_context,
+            globals,
+            source_map,
+        } => {}
+        _ => module,
+    }
+}
