e166db54128dec3e369bc295af945ac8cc27461b
Tobias Koppers <tobias.koppers@googlemail.com>
2024-07-18 08:49:13 +0200

    fixup

--- crates/turbo-tasks-memory/src/task.rs
+++ crates/turbo-tasks-memory/src/task.rs
@@ -875,7 +875,7 @@ impl Task {
     ) {
         let mut state = self.full_state_mut();
         match state.state_type {
-            InProgress(state) if state.stale => {
+            InProgress(ref state) if state.stale => {
                 // We don't want to assign the output cell here
                 // as we want to avoid unnecessary updates
                 // TODO maybe this should be controlled by a heuristic
