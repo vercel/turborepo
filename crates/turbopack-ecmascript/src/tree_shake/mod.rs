45477548eb4215c771f0f4bdadb9bb7eb4751543
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-21 05:36:33 +0900

    strong

--- crates/turbopack-ecmascript/src/tree_shake/mod.rs
+++ crates/turbopack-ecmascript/src/tree_shake/mod.rs
@@ -177,11 +177,7 @@ impl Analyzer<'_> {
                     if let Some(declarator) = &state.declarator {
                         if declarator != item_id {
                             // A write also depends on the declaration.
-                            if item.side_effects {
-                                self.g.add_strong_deps(item_id, [declarator]);
-                            } else {
-                                self.g.add_weak_deps(item_id, [declarator]);
-                            }
+                            self.g.add_strong_deps(item_id, [declarator]);
                         }
                     }
                 }
