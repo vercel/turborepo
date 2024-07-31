d5e01cb8953a7c22c5cc61ce1c309c38f074cc15
Tobias Koppers <tobias.koppers@googlemail.com>
2024-07-31 11:52:18 +0200

    fix test case

--- crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/comptime/input/index.js
+++ crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/comptime/input/index.js
@@ -4,6 +4,7 @@ import { something2 } from "package/dep2.js";
 it("should not include a module that is side effect free and exports are not used due to static analysis", () => {
   if (true) {
     something2();
+    return;
   }
   something();
 });
