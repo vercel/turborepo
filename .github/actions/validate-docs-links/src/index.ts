ea03c44c7e105178dca7de6ad7f5d8be67e51edd
Anthony Shew <anthonyshew@gmail.com>
2024-08-07 10:37:37 -0600

    yuh

--- .github/actions/validate-docs-links/src/index.ts
+++ .github/actions/validate-docs-links/src/index.ts
@@ -180,7 +180,7 @@ function validateInternalLink(errors: Errors, href: string): void {
 
   // check if doc page exists, key is the url path without `/docs/`
   // e.g. `api/example`
-  console.lo(link);
+  console.log(link);
   console.log(documentMap.keys());
 
   const foundPage = documentMap.get(`../../..${link}`);
