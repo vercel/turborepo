ff5304331dda18bff3c692d2ca2c85ac76823c37
nicholaslyang <nicholas.yang@vercel.com>
2024-08-08 12:56:05 -0400

    Implement basic predicate filtering

--- crates/turborepo-lib/src/commands/query.rs
+++ crates/turborepo-lib/src/commands/query.rs
@@ -1,4 +1,6 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema, ServerError};
+use miette::{Diagnostic, Report, SourceSpan};
+use thiserror::Error;
 use turborepo_telemetry::events::command::CommandEventBuilder;
 
 use crate::{
@@ -8,6 +10,45 @@ use crate::{
     run::builder::RunBuilder,
     signal::SignalHandler,
 };
+
+#[derive(Debug, Diagnostic, Error)]
+#[error("{message}")]
+struct QueryError {
+    message: String,
+    #[source_code]
+    query: String,
+    #[label]
+    span: Option<SourceSpan>,
+    #[label]
+    span2: Option<SourceSpan>,
+    #[label]
+    span3: Option<SourceSpan>,
+}
+
+impl QueryError {
+    fn get_index_from_row_column(query: &str, row: usize, column: usize) -> usize {
+        let mut index = 0;
+        for line in query.lines().take(row + 1) {
+            index += line.len() + 1;
+        }
+        index + column
+    }
+    fn new(server_error: ServerError, query: String) -> Self {
+        let span: Option<SourceSpan> = server_error.locations.first().map(|location| {
+            let idx =
+                Self::get_index_from_row_column(query.as_ref(), location.line, location.column);
+            (idx, idx + 1).into()
+        });
+
+        QueryError {
+            message: server_error.message,
+            query,
+            span,
+            span2: None,
+            span3: None,
+        }
+    }
+}
 
 pub async fn run(
     mut base: CommandBase,
@@ -28,8 +69,15 @@ pub async fn run(
 
     let schema = Schema::new(Query::new(run), EmptyMutation, EmptySubscription);
 
-    let result = schema.execute(query).await;
-    println!("{}", serde_json::to_string_pretty(&result)?);
+    let result = schema.execute(&query).await;
+    if result.errors.is_empty() {
+        println!("{}", serde_json::to_string_pretty(&result)?);
+    } else {
+        for error in result.errors {
+            let error = QueryError::new(error, query.clone());
+            eprintln!("{:?}", Report::new(error));
+        }
+    }
 
     Ok(0)
 }
--- crates/turborepo-lib/src/query.rs
+++ crates/turborepo-lib/src/query.rs
@@ -32,6 +32,193 @@ struct Package {
     name: PackageName,
 }
 
+impl Package {
+    fn immediate_dependents_count(&self) -> usize {
+        self.run
+            .pkg_dep_graph()
+            .immediate_ancestors(&PackageNode::Workspace(self.name.clone()))
+            .map_or(0, |pkgs| pkgs.len())
+    }
+
+    fn immediate_dependencies_count(&self) -> usize {
+        self.run
+            .pkg_dep_graph()
+            .immediate_dependencies(&PackageNode::Workspace(self.name.clone()))
+            .map_or(0, |pkgs| pkgs.len())
+    }
+
+    fn dependent_count(&self) -> usize {
+        let node: PackageNode = PackageNode::Workspace(self.name.clone());
+
+        self.run.pkg_dep_graph().ancestors(&node).len()
+    }
+
+    fn dependency_count(&self) -> usize {
+        let node: PackageNode = PackageNode::Workspace(self.name.clone());
+
+        self.run.pkg_dep_graph().dependencies(&node).len()
+    }
+}
+
+#[derive(Enum, Copy, Clone, Eq, PartialEq, Debug)]
+enum PackageFields {
+    Name,
+    DependencyCount,
+    DependentCount,
+    ImmediateDependentCount,
+    ImmediateDependencyCount,
+}
+
+#[derive(InputObject)]
+struct FieldValuePair {
+    field: PackageFields,
+    value: Any,
+}
+
+/// Predicates are used to filter packages
+#[derive(InputObject)]
+struct PackagePredicate {
+    and: Option<Vec<PackagePredicate>>,
+    or: Option<Vec<PackagePredicate>>,
+    equal: Option<FieldValuePair>,
+    not_equal: Option<FieldValuePair>,
+    greater_than: Option<FieldValuePair>,
+    less_than: Option<FieldValuePair>,
+    not: Option<Box<PackagePredicate>>,
+}
+
+impl PackagePredicate {
+    fn check_equals(pkg: &Package, field: &PackageFields, value: &Any) -> bool {
+        match (field, &value.0) {
+            (PackageFields::Name, Value::String(name)) => pkg.name.as_ref() == name,
+            (PackageFields::DependencyCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.dependency_count() == n as usize
+            }
+            (PackageFields::DependentCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.dependent_count() == n as usize
+            }
+            (PackageFields::ImmediateDependentCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.immediate_dependents_count() == n as usize
+            }
+            (PackageFields::ImmediateDependencyCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.immediate_dependencies_count() == n as usize
+            }
+            _ => false,
+        }
+    }
+
+    fn check_greater_than(pkg: &Package, field: &PackageFields, value: &Any) -> bool {
+        match (field, &value.0) {
+            (PackageFields::DependencyCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.dependency_count() > n as usize
+            }
+            (PackageFields::DependentCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.dependent_count() > n as usize
+            }
+            (PackageFields::ImmediateDependentCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.immediate_dependents_count() > n as usize
+            }
+            (PackageFields::ImmediateDependencyCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.immediate_dependencies_count() > n as usize
+            }
+            _ => false,
+        }
+    }
+
+    fn check_less_than(pkg: &Package, field: &PackageFields, value: &Any) -> bool {
+        match (field, &value.0) {
+            (PackageFields::DependencyCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.dependency_count() < n as usize
+            }
+            (PackageFields::DependentCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.dependent_count() < n as usize
+            }
+            (PackageFields::ImmediateDependentCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.immediate_dependents_count() < n as usize
+            }
+            (PackageFields::ImmediateDependencyCount, Value::Number(n)) => {
+                let Some(n) = n.as_u64() else {
+                    return false;
+                };
+                pkg.immediate_dependencies_count() < n as usize
+            }
+            _ => false,
+        }
+    }
+
+    fn check(&self, pkg: &Package) -> bool {
+        let and = self
+            .and
+            .as_ref()
+            .map(|predicates| predicates.iter().all(|p| p.check(pkg)));
+        let or = self
+            .or
+            .as_ref()
+            .map(|predicates| predicates.iter().any(|p| p.check(pkg)));
+        let equal = self
+            .equal
+            .as_ref()
+            .map(|pair| Self::check_equals(pkg, &pair.field, &pair.value));
+        let not_equal = self
+            .not_equal
+            .as_ref()
+            .map(|pair| !Self::check_equals(pkg, &pair.field, &pair.value));
+
+        let greater_than = self
+            .greater_than
+            .as_ref()
+            .map(|pair| Self::check_greater_than(pkg, &pair.field, &pair.value));
+
+        let less_than = self
+            .less_than
+            .as_ref()
+            .map(|pair| Self::check_greater_than(pkg, &pair.field, &pair.value));
+        let not = self.not.as_ref().map(|predicate| !predicate.check(pkg));
+
+        and.into_iter()
+            .chain(or)
+            .chain(equal)
+            .chain(not_equal)
+            .chain(greater_than)
+            .chain(less_than)
+            .chain(not)
+            .all(|p| p)
+    }
+}
+
 #[Object]
 impl Query {
     async fn package(&self, name: String) -> Result<Package, Error> {
@@ -40,6 +227,31 @@ impl Query {
             run: self.run.clone(),
             name,
         })
+    }
+
+    async fn packages(&self, filter: Option<PackagePredicate>) -> Result<Vec<Package>, Error> {
+        let Some(filter) = filter else {
+            return Ok(self
+                .run
+                .pkg_dep_graph()
+                .packages()
+                .map(|(name, _)| Package {
+                    run: self.run.clone(),
+                    name: name.clone(),
+                })
+                .collect());
+        };
+
+        Ok(self
+            .run
+            .pkg_dep_graph()
+            .packages()
+            .map(|(name, _)| Package {
+                run: self.run.clone(),
+                name: name.clone(),
+            })
+            .filter(|pkg| filter.check(pkg))
+            .collect())
     }
 }
 
