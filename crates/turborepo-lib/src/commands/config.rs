cf18c3fa30230eaf712fb04da2c1042896212275
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2024-08-07 20:14:27 -0400

    stop using RelativeUnixPath for printing config

--- crates/turborepo-lib/src/commands/config.rs
+++ crates/turborepo-lib/src/commands/config.rs
@@ -1,6 +1,5 @@
 use camino::Utf8Path;
 use serde::Serialize;
-use turbopath::RelativeUnixPath;
 use turborepo_repository::{
     package_graph::PackageGraph, package_json::PackageJson, package_manager::PackageManager,
 };
@@ -26,7 +25,7 @@ struct ConfigOutput<'a> {
     env_mode: EnvMode,
     scm_base: &'a str,
     scm_head: &'a str,
-    cache_dir: &'a RelativeUnixPath,
+    cache_dir: &'a Utf8Path,
 }
 
 pub async fn run(base: CommandBase) -> Result<(), cli::Error> {
@@ -58,7 +57,7 @@ pub async fn run(base: CommandBase) -> Result<(), cli::Error> {
             env_mode: config.env_mode(),
             scm_base: config.scm_base(),
             scm_head: config.scm_head(),
-            cache_dir: RelativeUnixPath::new(config.cache_dir()).expect("cache_dir is valid utf-8"),
+            cache_dir: config.cache_dir(),
         })?
     );
     Ok(())
