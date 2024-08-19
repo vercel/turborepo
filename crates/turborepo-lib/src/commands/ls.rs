452a916b1c9307447ecb31de0471a125a504682f
Thomas Knickman <tom.knickman@vercel.com>
2024-08-19 17:10:07 -0400

    feat(ls): support output format (#9031)
    ### Description
    
    Support output format for `ls`
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->

--- crates/turborepo-lib/src/commands/ls.rs
+++ crates/turborepo-lib/src/commands/ls.rs
@@ -1,6 +1,7 @@
 //! A command for outputting info about packages and tasks in a turborepo.
 
 use miette::Diagnostic;
+use serde::Serialize;
 use thiserror::Error;
 use turbopath::AnchoredSystemPath;
 use turborepo_repository::{
@@ -12,7 +13,7 @@ use turborepo_ui::{color, cprint, cprintln, ColorConfig, BOLD, BOLD_GREEN, GREY}
 
 use crate::{
     cli,
-    cli::{Command, ExecutionArgs},
+    cli::{Command, ExecutionArgs, OutputFormat},
     commands::{run::get_signal, CommandBase},
     run::{builder::RunBuilder, Run},
     signal::SignalHandler,
@@ -24,17 +25,91 @@ pub enum Error {
     PackageNotFound { package: String },
 }
 
+#[derive(Serialize)]
+struct ItemsWithCount<T> {
+    count: usize,
+    items: Vec<T>,
+}
+
+#[derive(Clone, Serialize)]
+#[serde(into = "RepositoryDetailsDisplay<'a>")]
 struct RepositoryDetails<'a> {
     color_config: ColorConfig,
     package_manager: &'a PackageManager,
     packages: Vec<(&'a PackageName, &'a AnchoredSystemPath)>,
 }
 
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct RepositoryDetailsDisplay<'a> {
+    package_manager: &'a PackageManager,
+    packages: ItemsWithCount<PackageDetailDisplay>,
+}
+
+#[derive(Serialize)]
+struct PackageDetailDisplay {
+    name: String,
+    path: String,
+}
+
+impl<'a> From<RepositoryDetails<'a>> for RepositoryDetailsDisplay<'a> {
+    fn from(val: RepositoryDetails<'a>) -> Self {
+        RepositoryDetailsDisplay {
+            package_manager: val.package_manager,
+            packages: ItemsWithCount {
+                count: val.packages.len(),
+                items: val
+                    .packages
+                    .into_iter()
+                    .map(|(name, path)| PackageDetailDisplay {
+                        name: name.to_string(),
+                        path: path.to_string(),
+                    })
+                    .collect(),
+            },
+        }
+    }
+}
+
+#[derive(Clone, Serialize)]
+struct PackageTask<'a> {
+    name: &'a str,
+    command: &'a str,
+}
+
+#[derive(Clone, Serialize)]
+#[serde(into = "PackageDetailsDisplay<'a>")]
 struct PackageDetails<'a> {
+    #[serde(skip)]
     color_config: ColorConfig,
     name: &'a str,
-    tasks: Vec<(&'a str, &'a str)>,
+    tasks: Vec<PackageTask<'a>>,
     dependencies: Vec<&'a str>,
+}
+
+#[derive(Clone, Serialize)]
+struct PackageDetailsList<'a> {
+    packages: Vec<PackageDetails<'a>>,
+}
+
+#[derive(Serialize)]
+struct PackageDetailsDisplay<'a> {
+    name: &'a str,
+    tasks: ItemsWithCount<PackageTask<'a>>,
+    dependencies: Vec<&'a str>,
+}
+
+impl<'a> From<PackageDetails<'a>> for PackageDetailsDisplay<'a> {
+    fn from(val: PackageDetails<'a>) -> Self {
+        PackageDetailsDisplay {
+            name: val.name,
+            dependencies: val.dependencies,
+            tasks: ItemsWithCount {
+                count: val.tasks.len(),
+                items: val.tasks,
+            },
+        }
+    }
 }
 
 pub async fn run(
@@ -43,6 +118,7 @@ pub async fn run(
     telemetry: CommandEventBuilder,
     filter: Vec<String>,
     affected: bool,
+    output: Option<OutputFormat>,
 ) -> Result<(), cli::Error> {
     let signal = get_signal()?;
     let handler = SignalHandler::new(signal);
@@ -61,11 +137,26 @@ pub async fn run(
     let run = run_builder.build(&handler, telemetry).await?;
 
     if packages.is_empty() {
-        RepositoryDetails::new(&run).print()?;
+        RepositoryDetails::new(&run).print(output)?;
     } else {
-        for package in packages {
-            let package_details = PackageDetails::new(&run, &package)?;
-            package_details.print();
+        match output {
+            Some(OutputFormat::Json) => {
+                let mut package_details_list = PackageDetailsList { packages: vec![] };
+                //  collect all package details
+                for package in &packages {
+                    let package_details = PackageDetails::new(&run, package)?;
+                    package_details_list.packages.push(package_details);
+                }
+
+                let as_json = serde_json::to_string_pretty(&package_details_list)?;
+                println!("{}", as_json);
+            }
+            Some(OutputFormat::Pretty) | None => {
+                for package in packages {
+                    let package_details = PackageDetails::new(&run, &package)?;
+                    package_details.print();
+                }
+            }
         }
     }
 
@@ -99,20 +190,41 @@ impl<'a> RepositoryDetails<'a> {
             packages,
         }
     }
-    fn print(&self) -> Result<(), cli::Error> {
-        if self.packages.len() == 1 {
-            cprintln!(self.color_config, BOLD, "{} package\n", self.packages.len());
-        } else {
-            cprintln!(
-                self.color_config,
-                BOLD,
-                "{} packages\n",
-                self.packages.len()
-            );
-        }
+    fn pretty_print(&self) {
+        let package_copy = match self.packages.len() {
+            0 => "no packages",
+            1 => "package",
+            _ => "packages",
+        };
+
+        cprint!(
+            self.color_config,
+            BOLD,
+            "{} {} ",
+            self.packages.len(),
+            package_copy
+        );
+        cprintln!(self.color_config, GREY, "({})\n", self.package_manager);
 
         for (package_name, entry) in &self.packages {
             println!("  {} {}", package_name, GREY.apply_to(entry));
+        }
+    }
+
+    fn json_print(&self) -> Result<(), cli::Error> {
+        let as_json = serde_json::to_string_pretty(&self)?;
+        println!("{}", as_json);
+        Ok(())
+    }
+
+    fn print(&self, output: Option<OutputFormat>) -> Result<(), cli::Error> {
+        match output {
+            Some(OutputFormat::Json) => {
+                self.json_print()?;
+            }
+            Some(OutputFormat::Pretty) | None => {
+                self.pretty_print();
+            }
         }
 
         Ok(())
@@ -153,7 +265,7 @@ impl<'a> PackageDetails<'a> {
             tasks: package_json
                 .scripts
                 .iter()
-                .map(|(name, command)| (name.as_str(), command.as_str()))
+                .map(|(name, command)| PackageTask { name, command })
                 .collect(),
         })
     }
@@ -180,11 +292,11 @@ impl<'a> PackageDetails<'a> {
         } else {
             println!();
         }
-        for (name, command) in &self.tasks {
+        for task in &self.tasks {
             println!(
                 "  {}: {}",
-                name,
-                color!(self.color_config, GREY, "{}", command)
+                task.name,
+                color!(self.color_config, GREY, "{}", task.command)
             );
         }
         println!();
