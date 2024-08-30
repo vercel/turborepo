2f8ff7b9fef1e0f240abf7e937698a0d7768eaca
Anthony Shew <anthony.shew@vercel.com>
2024-08-30 11:19:30 -0600

    examples: Fix sourcemapping in kitchen-sink. (#9086)
    ## Description
    
    The TypeScript strategy on this example wouldn't take you to source. It
    does now!

--- examples/kitchen-sink/packages/ui/tsup.config.ts
+++ examples/kitchen-sink/packages/ui/tsup.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entry: ["./src/index.tsx"],
+  entry: ["./src/link/index.tsx", "./src/counter-button/index.tsx"],
   format: ["cjs", "esm"],
-  dts: true,
   external: ["react"],
   banner: {
     js: "'use client'",
