4cf7840879aa17ad4aa41981da81e999bfa7c9f5
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2024-09-09 17:39:09 -0400

    adds tests for framework inference

---
+++ packages/eslint-plugin-turbo/__tests__/lib/no-undeclared-env-vars/framework-inference/no-undeclared-env-vars.commonjs.test.ts
@@ -0,0 +1,61 @@
+import path from "node:path";
+import { RuleTester } from "eslint";
+import { RULES } from "../../../../lib/constants";
+import rule from "../../../../lib/rules/no-undeclared-env-vars";
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 2020 },
+});
+
+const cwd = path.join(
+  __dirname,
+  "../../../../__fixtures__/framework-inference"
+);
+const nextJsFilename = path.join(cwd, "/apps/nextjs/index.js");
+const viteFilename = path.join(cwd, "/apps/vite/index.js");
+const options = (extra: Record<string, unknown> = {}) => ({
+  options: [
+    {
+      cwd,
+      ...extra,
+    },
+  ],
+});
+
+ruleTester.run(RULES.noUndeclaredEnvVars, rule, {
+  valid: [
+    {
+      code: `const { NEXT_PUBLIC_ZILTOID } = process.env;`,
+      ...options(),
+      filename: nextJsFilename,
+    },
+    {
+      code: `const { VITE_THINGS } = process.env;`,
+      ...options(),
+      filename: viteFilename,
+    },
+  ],
+  invalid: [
+    {
+      code: `const { NEXT_PUBLIC_ZILTOID } = process.env;`,
+      ...options(),
+      filename: viteFilename,
+      errors: [
+        {
+          message:
+            "NEXT_PUBLIC_ZILTOID is not listed as a dependency in turbo.json",
+        },
+      ],
+    },
+    {
+      code: `const { VITE_THINGS } = process.env;`,
+      ...options(),
+      filename: nextJsFilename,
+      errors: [
+        {
+          message: "VITE_THINGS is not listed as a dependency in turbo.json",
+        },
+      ],
+    },
+  ],
+});
