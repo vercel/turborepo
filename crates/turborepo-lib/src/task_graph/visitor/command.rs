0c7177e9206f29764d2da9462bd28b17e8324d31
Chris Olszewski <chris.olszewski@vercel.com>
2024-10-16 10:15:23 -0400

    chore(visitor): move command construction to command factory

---
+++ crates/turborepo-lib/src/task_graph/visitor/command.rs
@@ -0,0 +1,78 @@
+use std::path::PathBuf;
+
+use turbopath::AbsoluteSystemPath;
+use turborepo_env::EnvironmentVariableMap;
+use turborepo_repository::package_graph::{PackageGraph, PackageName};
+
+use super::Error;
+use crate::{opts::RunOpts, process::Command, run::task_id::TaskId};
+
+#[derive(Debug)]
+pub struct CommandFactory<'a> {
+    repo_root: &'a AbsoluteSystemPath,
+    package_graph: &'a PackageGraph,
+    package_manager_binary: Result<PathBuf, which::Error>,
+    run_opts: &'a RunOpts,
+}
+
+impl<'a> CommandFactory<'a> {
+    pub fn new(
+        repo_root: &'a AbsoluteSystemPath,
+        package_graph: &'a PackageGraph,
+        run_opts: &'a RunOpts,
+    ) -> Self {
+        let package_manager_binary = which::which(package_graph.package_manager().command());
+        Self {
+            repo_root,
+            package_graph,
+            package_manager_binary,
+            run_opts,
+        }
+    }
+
+    pub fn command(
+        &self,
+        task_id: &TaskId,
+        environment: EnvironmentVariableMap,
+    ) -> Result<Option<Command>, Error> {
+        let workspace_info = self
+            .package_graph
+            .package_info(&PackageName::from(task_id.package()))
+            .ok_or_else(|| Error::MissingPackage {
+                package_name: task_id.package().into(),
+                task_id: task_id.clone().into_owned(),
+            })?;
+
+        // bail if the script doesn't exist or is empty
+        if workspace_info
+            .package_json
+            .scripts
+            .get(task_id.task())
+            .map_or(true, |script| script.is_empty())
+        {
+            return Ok(None);
+        }
+        let package_manager_binary = self.package_manager_binary.as_deref().map_err(|e| *e)?;
+        let mut cmd = Command::new(package_manager_binary);
+        let mut args = vec!["run".to_string(), task_id.task().to_string()];
+        if let Some(pass_through_args) = &self.run_opts.args_for_task(task_id) {
+            args.extend(
+                self.package_graph
+                    .package_manager()
+                    .arg_separator(pass_through_args.as_slice())
+                    .map(|s| s.to_string()),
+            );
+            args.extend(pass_through_args.iter().cloned());
+        }
+        cmd.args(args);
+
+        let package_dir = self.repo_root.resolve(workspace_info.package_path());
+        cmd.current_dir(package_dir);
+
+        // We clear the env before populating it with variables we expect
+        cmd.env_clear();
+        cmd.envs(environment.iter());
+
+        Ok(Some(cmd))
+    }
+}
