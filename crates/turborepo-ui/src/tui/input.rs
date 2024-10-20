003d405de2349b5256a9b5e6e262d79563709dee
Chris Olszewski <chris.olszewski@vercel.com>
2024-10-20 15:24:21 -0400

    fix(tui): generate stop event on ctrl c

--- crates/turborepo-ui/src/tui/input.rs
+++ crates/turborepo-ui/src/tui/input.rs
@@ -61,7 +61,8 @@ fn translate_key_event(options: InputOptions, key_event: KeyEvent) -> Option<Eve
     }
     match key_event.code {
         KeyCode::Char('c') if key_event.modifiers == crossterm::event::KeyModifiers::CONTROL => {
-            ctrl_c()
+            ctrl_c();
+            Some(Event::InternalStop)
         }
         KeyCode::Char('c') if options.has_selection => Some(Event::CopySelection),
         // Interactive branches
