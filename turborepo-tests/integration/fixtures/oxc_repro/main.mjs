0f263a6d41b56ff5a3aebf87b60a7ddb7131288f
nicholaslyang <nicholas.yang@vercel.com>
2024-10-21 12:42:56 -0400

    Remove unnecessary file

--- turborepo-tests/integration/fixtures/oxc_repro/main.mjs
+++
@@ -1,8 +0,0 @@
-import resolve from "oxc-resolver";
-
-console.log('resolving "./apps/web/nm/@repo/typescript-config/index.js"');
-console.log(
-  resolve.sync(".", "./apps/web/nm/@repo/typescript-config/index.js")
-);
-console.log('resolving "./apps/web/nm/@repo/index.js"');
-console.log(resolve.sync(".", "./apps/web/nm/@repo/index.js"));
