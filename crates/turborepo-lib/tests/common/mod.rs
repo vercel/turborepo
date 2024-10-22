cd54aaa3e003507ade475a201cb417a7fdae5c1a
nicholaslyang <nicholas.yang@vercel.com>
2024-10-22 14:14:17 -0400

    adding more tests

---
+++ crates/turborepo-lib/tests/common/mod.rs
@@ -0,0 +1,44 @@
+use std::{path::Path, process::Command};
+
+use camino::Utf8Path;
+
+pub fn setup_fixture(
+    fixture: &str,
+    package_manager: Option<&str>,
+    test_dir: &Path,
+) -> Result<(), anyhow::Error> {
+    let script_path = Utf8Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("../../turborepo-tests/helpers/setup_integration_test.sh");
+
+    Command::new("bash")
+        .arg("-c")
+        .arg(format!(
+            "{} {} {}",
+            script_path,
+            fixture,
+            package_manager.unwrap_or("npm@10.5.0")
+        ))
+        .current_dir(test_dir)
+        .spawn()?
+        .wait()?;
+
+    Ok(())
+}
+
+pub fn check_query(fixture: &str, queries: Vec<&str>) -> Result<(), anyhow::Error> {
+    let tempdir = tempfile::tempdir()?;
+    setup_fixture(fixture, None, tempdir.path())?;
+    for query in queries {
+        let output = assert_cmd::Command::cargo_bin("turbo")?
+            .arg("query")
+            .arg(query)
+            .current_dir(tempdir.path())
+            .output()?;
+
+        let stdout = String::from_utf8(output.stdout)?;
+        let query_output: serde_json::Value = serde_json::from_str(&stdout)?;
+        insta::assert_json_snapshot!(query_output);
+    }
+
+    Ok(())
+}
--- crates/turborepo/tests/query.rs
+++ crates/turborepo/tests/query.rs
@@ -1,52 +1,14 @@
-use std::{path::Path, process::Command};
+use crate::common::check_query;
 
-use camino::Utf8Path;
-
-fn setup_fixture(
-    fixture: &str,
-    package_manager: Option<&str>,
-    test_dir: &Path,
-) -> Result<(), anyhow::Error> {
-    let script_path = Utf8Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("../../turborepo-tests/helpers/setup_integration_test.sh");
-
-    Command::new("bash")
-        .arg("-c")
-        .arg(format!(
-            "{} {} {}",
-            script_path,
-            fixture,
-            package_manager.unwrap_or("npm@10.5.0")
-        ))
-        .current_dir(test_dir)
-        .spawn()?
-        .wait()?;
-
-    Ok(())
-}
-
-fn check_query(fixture: &str, query: &str) -> Result<(), anyhow::Error> {
-    let tempdir = tempfile::tempdir()?;
-    setup_fixture(fixture, None, tempdir.path())?;
-    let output = assert_cmd::Command::cargo_bin("turbo")?
-        .arg("query")
-        .arg(query)
-        .current_dir(tempdir.path())
-        .output()?;
-
-    let stdout = String::from_utf8(output.stdout)?;
-    let query_output: serde_json::Value = serde_json::from_str(&stdout)?;
-    insta::assert_json_snapshot!(query_output);
-
-    Ok(())
-}
+mod common;
 
 #[cfg(not(windows))]
 #[test]
 fn test_double_symlink() -> Result<(), anyhow::Error> {
     check_query(
         "oxc_repro",
-        "query {
+        vec![
+            "query {
                  file(path: \"./index.js\") {
                     path
                     dependencies {
@@ -55,6 +17,28 @@ fn test_double_symlink() -> Result<(), anyhow::Error> {
                     }
                  }
               }",
+        ],
     )?;
+    Ok(())
+}
+
+fn test_trace() -> Result<(), anyhow::Error> {
+    check_query(
+        "turbo_trace",
+        vec![
+            "query { file(path: \"main.ts\") { path } }",
+            "query { file(path: \"main.ts\") { path, dependencies { files { items { path } } } } }",
+            "query { file(path: \"button.tsx\") { path, dependencies { files { items { path } } } \
+             } }",
+            "query { file(path: \"circular.ts\") { path dependencies { files { items { path } } } \
+             } }",
+            "query { file(path: \"invalid.ts\") { path dependencies { files { items { path } } \
+             errors { items { message } } } } }",
+            "query { file(path: \"main.ts\") { path ast } }",
+            "query { file(path: \"main.ts\") { path dependencies(depth: 1) { files { items { path \
+             } } } } }",
+        ],
+    )?;
+
     Ok(())
 }
