22f9e24008652f60142fc3ae2725aad16cddad8e
nicholaslyang <nicholas.yang@vercel.com>
2024-10-22 15:48:02 -0400

    Adding new tests, cleaning up the format of the snapshot file output

--- crates/turborepo-lib/tests/common/mod.rs
+++ crates/turborepo-lib/tests/common/mod.rs
@@ -25,20 +25,30 @@ pub fn setup_fixture(
     Ok(())
 }
 
-pub fn check_query(fixture: &str, queries: Vec<&str>) -> Result<(), anyhow::Error> {
-    let tempdir = tempfile::tempdir()?;
-    setup_fixture(fixture, None, tempdir.path())?;
-    for query in queries {
-        let output = assert_cmd::Command::cargo_bin("turbo")?
-            .arg("query")
-            .arg(query)
-            .current_dir(tempdir.path())
-            .output()?;
+#[macro_export]
+macro_rules! check {
+    ($fixture:expr, $package_manager:expr, $($name:expr => $query:expr,)*) => {
+        {
+            let tempdir = tempfile::tempdir()?;
+            crate::common::setup_fixture($fixture, None, tempdir.path())?;
+            $(
+                let output = assert_cmd::Command::cargo_bin("turbo")?
+                    .arg("query")
+                    .arg($query)
+                    .current_dir(tempdir.path())
+                    .output()?;
 
-        let stdout = String::from_utf8(output.stdout)?;
-        let query_output: serde_json::Value = serde_json::from_str(&stdout)?;
-        insta::assert_json_snapshot!(query_output);
+                let stdout = String::from_utf8(output.stdout)?;
+                let query_output: serde_json::Value = serde_json::from_str(&stdout)?;
+                let test_name = format!(
+                    "{}_{}_{}_({})",
+                    module_path!(),
+                    $fixture,
+                    $name.replace(' ', "_"),
+                    $package_manager
+                );
+                insta::assert_json_snapshot!(test_name, query_output);
+            )*
+        }
     }
-
-    Ok(())
 }
--- crates/turborepo/tests/query.rs
+++ crates/turborepo/tests/query.rs
@@ -1,14 +1,12 @@
-use crate::common::check_query;
-
 mod common;
 
 #[cfg(not(windows))]
 #[test]
 fn test_double_symlink() -> Result<(), anyhow::Error> {
-    check_query(
+    check!(
         "oxc_repro",
-        vec![
-            "query {
+        "npm@10.5.0",
+        "get_dependencies" => "query {
                  file(path: \"./index.js\") {
                     path
                     dependencies {
@@ -17,28 +15,23 @@ fn test_double_symlink() -> Result<(), anyhow::Error> {
                     }
                  }
               }",
-        ],
-    )?;
+    );
     Ok(())
 }
 
+#[test]
 fn test_trace() -> Result<(), anyhow::Error> {
-    check_query(
+    check!(
         "turbo_trace",
-        vec![
-            "query { file(path: \"main.ts\") { path } }",
-            "query { file(path: \"main.ts\") { path, dependencies { files { items { path } } } } }",
-            "query { file(path: \"button.tsx\") { path, dependencies { files { items { path } } } \
-             } }",
-            "query { file(path: \"circular.ts\") { path dependencies { files { items { path } } } \
-             } }",
-            "query { file(path: \"invalid.ts\") { path dependencies { files { items { path } } \
-             errors { items { message } } } } }",
-            "query { file(path: \"main.ts\") { path ast } }",
-            "query { file(path: \"main.ts\") { path dependencies(depth: 1) { files { items { path \
-             } } } } }",
-        ],
-    )?;
+        "npm@10.5.0",
+        "get `main.ts`" => "query { file(path: \"main.ts\") { path } }",
+        "get `main.ts` with dependencies" => "query { file(path: \"main.ts\") { path, dependencies { files { items { path } } } } }",
+        "get `button.tsx` with dependencies" => "query { file(path: \"button.tsx\") { path, dependencies { files { items { path } } } } }",
+        "get `circular.ts` with dependencies" => "query { file(path: \"circular.ts\") { path dependencies { files { items { path } } } } }",
+        "get `invalid.ts` with dependencies" => "query { file(path: \"invalid.ts\") { path dependencies { files { items { path } } errors { items { message } } } } }",
+        "get `main.ts` with ast" => "query { file(path: \"main.ts\") { path ast } }",
+        "get `main.ts` with depth = 0" => "query { file(path: \"main.ts\") { path dependencies(depth: 1) { files { items { path } } } } }",
+    );
 
     Ok(())
 }
