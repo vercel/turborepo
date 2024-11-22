a6c94e90ef083bcf2960a638ffe858e7ff8bfee2
Anthony Shew <anthonyshew@gmail.com>
2024-11-22 14:32:53 -0700

    WIP

--- packages/eslint-config-turbo/index.js
+++ packages/eslint-config-turbo/index.js
@@ -1,13 +1,5 @@
 const plugin = require("eslint-plugin-turbo");
 
 module.exports = {
-  flat: {
-    plugins: {
-      turbo: plugin,
-    },
-    rules: {
-      "turbo/no-undeclared-env-vars": "error",
-    },
-  },
   extends: ["plugin:turbo/recommended"],
 };
