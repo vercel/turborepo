87c6ecdb98ec7ef1ce06149386f00125d891badd
Anthony Shew <anthonyshew@gmail.com>
2024-11-22 14:00:21 -0700

    WIP

--- examples/basic/apps/web/eslint.config.js
+++ examples/basic/apps/web/eslint.config.js
@@ -1,5 +1,4 @@
 import { config } from "@repo/eslint-config/next-js";
-import nextPlugin from "@next/eslint-plugin-next";
 
 /** @type {import("eslint").Linter.Config} */
 export default [...config];
