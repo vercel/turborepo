a7ae5e75f79ba097283b5c8f4d8779052039ce99
Anthony Shew <anthonyshew@gmail.com>
2024-11-22 14:26:24 -0700

    feat: Update eslint-config-turbo to support ESLint Flat Config.

--- packages/eslint-config-turbo/index.js
+++ packages/eslint-config-turbo/index.js
@@ -1,3 +1,13 @@
+const plugin = require("eslint-plugin-turbo");
+
 module.exports = {
+  flat: {
+    plugins: {
+      turbo: plugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "error",
+    },
+  },
   extends: ["plugin:turbo/recommended"],
 };
