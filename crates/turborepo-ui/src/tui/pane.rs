4e7a6954d9c5e02e102c636a8db8eac74e9eb170
Anthony Shew <anthonyshew@gmail.com>
2024-11-29 12:47:41 -0700

    chore(tui): Dim pane text helpers.

--- crates/turborepo-ui/src/tui/pane.rs
+++ crates/turborepo-ui/src/tui/pane.rs
@@ -1,5 +1,5 @@
 use ratatui::{
-    style::Style,
+    style::{Modifier, Style},
     text::Line,
     widgets::{Block, Widget},
 };
@@ -39,7 +39,7 @@ impl<'a, W> TerminalPane<'a, W> {
     }
 
     fn footer(&self) -> Line {
-        let build_message_vec = |footer_text: &str| -> String {
+        let build_message_vec = |footer_text: &str| -> Line {
             let mut messages = vec![footer_text];
 
             if !self.has_sidebar {
@@ -51,18 +51,18 @@ impl<'a, W> TerminalPane<'a, W> {
             }
 
             // Spaces are used to pad the footer text for aesthetics
-            format!("   {}", messages.join(", "))
+            let formatted_messages = format!("   {}", messages.join(", "));
+
+            Line::styled(
+                formatted_messages.to_string(),
+                Style::default().add_modifier(Modifier::DIM),
+            )
+            .left_aligned()
         };
 
         match self.section {
-            LayoutSections::Pane => {
-                let messages = build_message_vec(FOOTER_TEXT_ACTIVE);
-                Line::from(messages).left_aligned()
-            }
-            LayoutSections::TaskList => {
-                let messages = build_message_vec(FOOTER_TEXT_INACTIVE);
-                Line::from(messages).left_aligned()
-            }
+            LayoutSections::Pane => build_message_vec(FOOTER_TEXT_ACTIVE),
+            LayoutSections::TaskList => build_message_vec(FOOTER_TEXT_INACTIVE),
             LayoutSections::Search { results, .. } => {
                 Line::from(format!("/ {}", results.query())).left_aligned()
             }
