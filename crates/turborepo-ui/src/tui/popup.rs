8a0798170535ae56238521c248e07713a079ff55
Anthony Shew <anthonyshew@gmail.com>
2024-12-01 12:26:30 -0700

    WIP

--- crates/turborepo-ui/src/tui/popup.rs
+++ crates/turborepo-ui/src/tui/popup.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 use ratatui::{
     layout::{Constraint, Flex, Layout, Rect},
     text::Line,
@@ -14,22 +16,22 @@ const BIND_LIST_ITEMS: [&str; 11] = [
     "/ - Filter tasks to search term",
     "ESC - Clear filter",
     "i - Interact with task",
-    "CTRL+z - Stop interacting with task",
+    "Ctrl+z - Stop interacting with task",
     "c - Copy logs selection (Only when logs are selected)",
-    "CTRL+n - Scroll logs up",
-    "CTRL+p - Scroll logs down",
+    "Ctrl+n - Scroll logs up",
+    "Ctrl+p - Scroll logs down",
 ];
 
 pub fn popup_area(area: SizeInfo) -> Rect {
+    let screen_width = area.task_list_width() + area.pane_cols();
+    let screen_height = area.pane_rows();
+
     let popup_width = BIND_LIST_ITEMS
         .iter()
         .map(|s| s.len() + 4)
         .max()
         .unwrap_or(0) as u16;
-    let popup_height = (BIND_LIST_ITEMS.len() + 4) as u16;
-
-    let screen_width = area.task_list_width() + area.pane_cols();
-    let screen_height = area.pane_rows();
+    let popup_height = min((BIND_LIST_ITEMS.len() + 4) as u16, screen_height);
 
     let x = (screen_width - popup_width) / 2;
     let y = (screen_height - popup_height) / 2;
