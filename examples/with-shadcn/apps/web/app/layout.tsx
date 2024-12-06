e0eb261a79c8a620252a5895a2f357d211f46c0b
Futa Ogawa <ogawa0071@gmail.com>
2024-12-07 02:26:54 +0900

    Merge `examples/basic` to `examples/with-shadcn`

--- examples/with-shadcn/apps/web/app/layout.tsx
+++ examples/with-shadcn/apps/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@repo/ui/globals.css";
-import "@/app/globals.css";
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
