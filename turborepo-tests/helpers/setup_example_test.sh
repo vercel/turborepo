b69ec299320ce6e9975c513580fc2055d4f9c12d
Anthony Shew <anthonyshew@gmail.com>
2024-12-12 16:45:31 -0700

    WIP

--- turborepo-tests/helpers/setup_example_test.sh
+++ turborepo-tests/helpers/setup_example_test.sh
@@ -72,7 +72,7 @@ if grep -q ">>> FULL TURBO" ./tmp/run-1.txt; then
 fi
 
 # Simulating the user's second run and dumping logs to a file
-$turbo_command >./tmp/run-2.txt
+$turbo_command >tee >(./tmp/run-2.txt)
 
 # Make sure the user hits FULL TURBO on the second go
 if ! grep -q ">>> FULL TURBO" ./tmp/run-2.txt; then
