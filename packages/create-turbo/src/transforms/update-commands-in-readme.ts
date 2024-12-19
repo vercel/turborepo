870f012b5b67f96fa11aec0a8aafbfeaad287d3b
Anthony Shew <anthonyshew@gmail.com>
2024-12-19 16:08:46 -0700

    Fix tests.

--- packages/create-turbo/src/transforms/update-commands-in-readme.ts
+++ packages/create-turbo/src/transforms/update-commands-in-readme.ts
@@ -8,23 +8,38 @@ const meta = {
 };
 
 // an array of all the possible replacement strings.
-const PACKAGE_MANAGER_REPLACEMENTS = ['pnpm run', 'npm run', 'yarn run', 'bun run', 'pnpm', 'npm', 'yarn', 'bun'];
+const PACKAGE_MANAGER_REPLACEMENTS = [
+  "pnpm run",
+  "npm run",
+  "yarn run",
+  "bun run",
+  "pnpm",
+  "npm",
+  "yarn",
+  "bun",
+];
 
 export async function transform(args: TransformInput): TransformResult {
-  const { prompts, example } = args;
+  const { prompts } = args;
 
   const selectedPackageManager = prompts.packageManager;
   const readmeFilePath = path.join(prompts.root, "README.md");
   try {
     // Read the content of the file
-    let data = await fs.readFile(readmeFilePath, "utf8");
+    const data = await fs.readFile(readmeFilePath, "utf8");
+
+    if (!selectedPackageManager) {
+      throw new Error("A package manager was not selected.");
+    }
 
     // replace package manager
-    const updatedReadmeData = replacePackageManager(selectedPackageManager, data);    
+    const updatedReadmeData = replacePackageManager(
+      selectedPackageManager,
+      data
+    );
 
     // Write the updated content back to the file
     await fs.writeFile(readmeFilePath, updatedReadmeData, "utf8");
-
   } catch (err) {
     throw new TransformError("Unable to update README.md", {
       transform: meta.name,
@@ -34,16 +49,34 @@ export async function transform(args: TransformInput): TransformResult {
   return { result: "success", ...meta };
 }
 
-function replacePackageManager(packageManager: { name: string }, text: string): string {
+function replacePackageManager(
+  packageManager: { name: string },
+  text: string
+): string {
   // regex to search for a pattern enclosed in single backticks (` `), double backticks (`` ``) or
   // triple backticks (``` ```) considering there might be newlines in between backticks and commands.
-  const searchRegex = new RegExp(`\`\`\`[\\s\\S]*?\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join('|')})\\b[\\s\\S]*?\`\`\`|\`\`[\\s\\S]*?\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join('|')})\\b[\\s\\S]*?\`\`|\`[\\s\\S]*?\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join('|')})\\b[\\s\\S]*?\``, 'g');
+  const searchRegex = new RegExp(
+    `\`\`\`[\\s\\S]*?\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join(
+      "|"
+    )})\\b[\\s\\S]*?\`\`\`|\`\`[\\s\\S]*?\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join(
+      "|"
+    )})\\b[\\s\\S]*?\`\`|\`[\\s\\S]*?\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join(
+      "|"
+    )})\\b[\\s\\S]*?\``,
+    "g"
+  );
 
   // Replace all occurrences of regex with selectedPackageManager
   const finalText = text.replace(searchRegex, (match) => {
     // replacement regex => the regex required to replace the package manager.
-    const replacementRegex = new RegExp(`\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join('|')})\\b`, 'g');
-    const updatedText = match.replace(replacementRegex, `${packageManager.name} run`);
+    const replacementRegex = new RegExp(
+      `\\b(?:${PACKAGE_MANAGER_REPLACEMENTS.join("|")})\\b`,
+      "g"
+    );
+    const updatedText = match.replace(
+      replacementRegex,
+      `${packageManager.name} run`
+    );
     return updatedText;
   });
   return finalText;
