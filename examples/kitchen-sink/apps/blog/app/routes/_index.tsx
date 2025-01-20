78d255cdbaeca29c0c70ac93e94f03d81ed0dcbf
Anthony Shew <anthonyshew@gmail.com>
2025-01-20 16:16:35 -0700

    WIP

---
+++ examples/kitchen-sink/apps/blog/app/routes/_index.tsx
@@ -0,0 +1,19 @@
+import { CounterButton } from "@repo/ui/counter-button";
+import { Link } from "@repo/ui/link";
+
+export default function Index(): JSX.Element {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Blog <br />
+        <span>Kitchen Sink</span>
+      </h1>
+      <CounterButton />
+      <p className="description">
+        Built With <Link href="https://turbo.build/repo">Turborepo</Link>
+        {" & "}
+        <Link href="https://remix.run/">Remix</Link>
+      </p>
+    </div>
+  );
+}
