e7e3d10f203ed012be41d77bfb3fdb32b2acc3be
Anthony Shew <anthonyshew@gmail.com>
2025-01-22 17:15:44 -0700

    moar

--- examples/with-tailwind/apps/docs/app/page.tsx
+++ examples/with-tailwind/apps/docs/app/page.tsx
@@ -43,13 +43,13 @@ const LINKS = [
   },
 ];
 
-export default function Page(): JSX.Element {
+export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         <p className="fixed top-0 left-0 flex justify-center w-full px-4 pt-8 pb-6 border-b bg-gradient-to-b backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
           examples/with-tailwind -&nbsp;
-          <code className="font-mono font-bold">web</code>
+          <code className="font-mono font-bold">docs</code>
         </p>
         <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
