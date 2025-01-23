0adf45825c3023e386dd682359bdab0390a17884
Chris Olszewski <chris.olszewski@vercel.com>
2025-01-23 10:06:35 -0500

    feat(bun): parse bun lockfile

--- crates/turborepo-lockfiles/src/bun/de.rs
+++ crates/turborepo-lockfiles/src/bun/de.rs
@@ -1,1 +1,159 @@
+use std::collections::VecDeque;
 
+use serde::Deserialize;
+
+use super::{PackageEntry, PackageInfo};
+// Comment explaining entry schemas taken from bun.lock.zig
+// first index is resolution for each type of package
+// npm         -> [
+//                "name@version",
+//                registry (TODO: remove if default),
+//                INFO,
+//                integrity
+//                ]
+// symlink     -> [ "name@link:path", INFO ]
+// folder      -> [ "name@file:path", INFO ]
+// workspace   -> [ "name@workspace:path", INFO ]
+// tarball     -> [ "name@tarball", INFO ]
+// root        -> [ "name@root:", { bin, binDir } ]
+// git         -> [ "name@git+repo", INFO, .bun-tag string (TODO: remove this) ]
+// github      -> [ "name@github:user/repo", INFO, .bun-tag string (TODO: remove
+// this) ]
+impl<'de> Deserialize<'de> for PackageEntry {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        use serde::de;
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum Vals {
+            Str(String),
+            Info(Box<PackageInfo>),
+        }
+        let mut vals = VecDeque::<Vals>::deserialize(deserializer)?;
+        let key = vals
+            .pop_front()
+            .ok_or_else(|| de::Error::custom("expected package entry to not be empty"))?;
+        let Vals::Str(key) = key else {
+            return Err(de::Error::custom(
+                "expected first element in package to be string",
+            ));
+        };
+        let val_to_info = |val| match val {
+            Vals::Str(_) => None,
+            Vals::Info(package_info) => Some(*package_info),
+        };
+        // For workspace packages deps are second element, rest have them as third
+        // element
+        let info = vals
+            .pop_front()
+            .and_then(val_to_info)
+            .or_else(|| vals.pop_front().and_then(val_to_info));
+        Ok(Self {
+            key,
+            info,
+            // The rest are only necessary for serializing a lockfile and aren't needed until adding
+            // `prune` support
+            registry: None,
+            checksum: None,
+            root: None,
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{str::FromStr, sync::OnceLock};
+
+    use serde_json::json;
+    use test_case::test_case;
+
+    use super::*;
+    use crate::{bun::WorkspaceEntry, BunLockfile};
+
+    macro_rules! fixture {
+        ($name:ident, $kind:ty, $cons:expr) => {
+            fn $name() -> &'static $kind {
+                static ONCE: OnceLock<$kind> = OnceLock::new();
+                ONCE.get_or_init(|| $cons)
+            }
+        };
+    }
+
+    fixture!(
+        basic_workspace,
+        WorkspaceEntry,
+        WorkspaceEntry {
+            name: "bun-test".into(),
+            dev_dependencies: Some(
+                Some(("turbo".to_string(), "^2.3.3".to_string()))
+                    .into_iter()
+                    .collect()
+            ),
+            ..Default::default()
+        }
+    );
+
+    fixture!(
+        workspace_with_version,
+        WorkspaceEntry,
+        WorkspaceEntry {
+            name: "docs".into(),
+            version: Some("0.1.0".into()),
+            ..Default::default()
+        }
+    );
+
+    fixture!(
+        registry_pkg,
+        PackageEntry,
+        PackageEntry {
+            key: "is-odd@3.0.1".into(),
+            registry: None,
+            info: Some(PackageInfo {
+                dependencies: Some(("is-number".into(), "^6.0.0".into()))
+                    .into_iter()
+                    .collect(),
+                ..Default::default()
+            }),
+            checksum: None,
+            root: None,
+        }
+    );
+
+    fixture!(
+        workspace_pkg,
+        PackageEntry,
+        PackageEntry {
+            key: "docs".into(),
+            info: Some(PackageInfo {
+                dependencies: Some(("is-odd".into(), "3.0.1".into()))
+                    .into_iter()
+                    .collect(),
+                ..Default::default()
+            }),
+            registry: None,
+            checksum: None,
+            root: None,
+        }
+    );
+    #[test_case(json!({"name": "bun-test", "devDependencies": {"turbo": "^2.3.3"}}), basic_workspace() ; "basic")]
+    #[test_case(json!({"name": "docs", "version": "0.1.0"}), workspace_with_version() ; "with version")]
+    #[test_case(json!(["is-odd@3.0.1", "", {"dependencies": {"is-number": "^6.0.0"}}, "sha"]), registry_pkg() ; "registry package")]
+    #[test_case(json!(["docs", {"dependencies": {"is-odd": "3.0.1"}}]), workspace_pkg() ; "workspace package")]
+    fn test_deserialization<T: for<'a> Deserialize<'a> + PartialEq + std::fmt::Debug>(
+        input: serde_json::Value,
+        expected: &T,
+    ) {
+        let actual: T = serde_json::from_value(input).unwrap();
+        assert_eq!(&actual, expected);
+    }
+
+    #[test]
+    fn test_full_parse() {
+        let contents = include_str!("../../fixtures/basic-bun.lock");
+        let result = BunLockfile::from_str(contents);
+        assert!(result.is_ok(), "{}", result.unwrap_err());
+    }
+}
--- crates/turborepo-lockfiles/src/bun/mod.rs
+++ crates/turborepo-lockfiles/src/bun/mod.rs
@@ -1,32 +1,82 @@
-use std::{any::Any, str::FromStr};
+use std::{any::Any, collections::HashMap, str::FromStr};
+
+use biome_json_formatter::context::JsonFormatOptions;
+use biome_json_parser::JsonParserOptions;
+use id::PossibleKeyIter;
+use serde::Deserialize;
+use serde_json::Value;
 
 use crate::Lockfile;
 
 mod de;
+mod id;
 
-type Map<K, V> = std::collections::BTreeMap<K, V>;
+type Map<K, V> = std::collections::HashMap<K, V>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("Failed to strip commas: {0}")]
+    Utf8(#[from] std::str::Utf8Error),
+    #[error("Failed to strip commas: {0}")]
+    Format(#[from] biome_formatter::FormatError),
+    #[error("Failed to strip commas: {0}")]
+    Print(#[from] biome_formatter::PrintError),
     #[error("Turborepo cannot serialize Bun lockfiles.")]
     NotImplemented,
 }
 
-#[derive(Debug)]
-pub struct BunLockfile;
-
-impl BunLockfile {
-    pub fn from_bytes(input: &[u8]) -> Result<Self, super::Error> {
-        Ok(Self)
-    }
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BunLockfile {
+    #[allow(unused)]
+    lockfile_version: i32,
+    workspaces: Map<String, WorkspaceEntry>,
+    packages: Map<String, PackageEntry>,
 }
 
-impl FromStr for BunLockfile {
-    type Err = super::Error;
+#[derive(Debug, Deserialize, PartialEq, Default)]
+#[serde(rename_all = "camelCase")]
+struct WorkspaceEntry {
+    name: String,
+    version: Option<String>,
+    dependencies: Option<Map<String, String>>,
+    dev_dependencies: Option<Map<String, String>>,
+    optional_dependencies: Option<Map<String, String>>,
+    peer_dependencies: Option<Map<String, String>>,
+    optional_peers: Option<Vec<String>>,
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self)
-    }
+#[derive(Debug, PartialEq)]
+struct PackageEntry {
+    key: String,
+    registry: Option<String>,
+    // Present except for workspace & root deps
+    info: Option<PackageInfo>,
+    // Present on registry
+    checksum: Option<String>,
+    root: Option<RootInfo>,
+}
+
+#[derive(Debug, Deserialize, Default, PartialEq)]
+struct PackageInfo {
+    #[serde(default)]
+    dependencies: Map<String, String>,
+    #[serde(default)]
+    dev_dependencies: Map<String, String>,
+    #[serde(default)]
+    optional_dependencies: Map<String, String>,
+    #[serde(default)]
+    peer_dependencies: Map<String, String>,
+    // We do not care about the rest here
+    // the values here should be generic
+    #[serde(flatten)]
+    other: Map<String, Value>,
+}
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+struct RootInfo {
+    bin: Option<String>,
+    bin_dir: Option<String>,
 }
 
 impl Lockfile for BunLockfile {
@@ -37,7 +87,21 @@ impl Lockfile for BunLockfile {
         name: &str,
         version: &str,
     ) -> Result<Option<crate::Package>, crate::Error> {
-        unimplemented!()
+        let workspace_entry = self
+            .workspaces
+            .get(workspace_path)
+            .ok_or_else(|| crate::Error::MissingWorkspace(workspace_path.into()))?;
+        let workspace_name = &workspace_entry.name;
+        let workspace_key = format!("{workspace_name}/{name}");
+        if let Some((key, entry)) = self.package_entry(&workspace_key) {
+            let version = entry.version().to_string();
+            Ok(Some(crate::Package {
+                key: key.to_string(),
+                version,
+            }))
+        } else {
+            Ok(None)
+        }
     }
 
     #[tracing::instrument(skip(self))]
@@ -45,15 +109,30 @@ impl Lockfile for BunLockfile {
         &self,
         key: &str,
     ) -> Result<Option<std::collections::HashMap<String, String>>, crate::Error> {
-        unimplemented!()
+        let entry = self
+            .packages
+            .get(key)
+            .ok_or_else(|| crate::Error::MissingPackage(key.into()))?;
+
+        let mut deps = HashMap::new();
+        for (dependency, version) in entry.info.iter().flat_map(|info| info.all_dependencies()) {
+            let parent_key = format!("{key}/{dependency}");
+            let Some((dep_key, _)) = self.package_entry(&parent_key) else {
+                return Err(crate::Error::MissingPackage(dependency.to_string()));
+            };
+            deps.insert(dep_key.to_string(), version.to_string());
+        }
+
+        Ok(Some(deps))
     }
 
+    #[allow(unused)]
     fn subgraph(
         &self,
         workspace_packages: &[String],
         packages: &[String],
     ) -> Result<Box<dyn Lockfile>, super::Error> {
-        unimplemented!()
+        Err(crate::Error::Bun(Error::NotImplemented))
     }
 
     fn encode(&self) -> Result<Vec<u8>, crate::Error> {
@@ -68,10 +147,137 @@ impl Lockfile for BunLockfile {
     }
 
     fn turbo_version(&self) -> Option<String> {
-        unimplemented!()
+        let (_, entry) = self.package_entry("turbo")?;
+        Some(entry.version().to_owned())
     }
 
     fn human_name(&self, package: &crate::Package) -> Option<String> {
-        unimplemented!()
+        let entry = self.packages.get(&package.key)?;
+        Some(entry.key.clone())
+    }
+}
+
+impl BunLockfile {
+    pub fn from_bytes(input: &[u8]) -> Result<Self, super::Error> {
+        let s = std::str::from_utf8(input).map_err(Error::from)?;
+        Self::from_str(s)
+    }
+
+    // Given a specific key for a package, return the most specific key that is
+    // present in the lockfile
+    fn package_entry(&self, key: &str) -> Option<(&str, &PackageEntry)> {
+        let (key, entry) =
+            PossibleKeyIter::new(key).find_map(|k| self.packages.get_key_value(k))?;
+        Some((key, entry))
+    }
+}
+
+impl FromStr for BunLockfile {
+    type Err = super::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parsed_json = biome_json_parser::parse_json(
+            s,
+            JsonParserOptions::default().with_allow_trailing_commas(),
+        );
+        if parsed_json.has_errors() {
+            // TODO
+        }
+        let syntax_tree = parsed_json.syntax();
+        let format = biome_json_formatter::format_node(
+            JsonFormatOptions::default()
+                .with_trailing_commas(biome_json_formatter::context::TrailingCommas::None),
+            &syntax_tree,
+        )
+        .map_err(Error::from)?;
+        let strict_json = format.print().map_err(Error::from)?;
+        let this = serde_json::from_str(strict_json.as_code())?;
+        Ok(this)
+    }
+}
+impl PackageEntry {
+    // Extracts version from key
+    fn version(&self) -> &str {
+        self.key
+            .rsplit_once('@')
+            .map(|(_, version)| version)
+            .unwrap_or(&self.key)
+    }
+}
+
+impl PackageInfo {
+    pub fn all_dependencies(&self) -> impl Iterator<Item = (&str, &str)> {
+        [
+            self.dependencies.iter(),
+            self.dev_dependencies.iter(),
+            self.optional_dependencies.iter(),
+            self.peer_dependencies.iter(),
+        ]
+        .into_iter()
+        .flatten()
+        .map(|(k, v)| (k.as_str(), v.as_str()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use pretty_assertions::assert_eq;
+    use test_case::test_case;
+
+    use super::*;
+
+    const BASIC_LOCKFILE: &str = include_str!("../../fixtures/basic-bun.lock");
+
+    #[test_case("", "turbo", "^2.3.3", "turbo" ; "root")]
+    #[test_case("apps/docs", "is-odd", "3.0.1", "is-odd" ; "docs is odd")]
+    #[test_case("apps/web", "is-odd", "3.0.0", "web/is-odd" ; "web is odd")]
+    #[test_case("packages/ui", "node-plop/inquirer/rxjs/tslib", "^1.14.0", "node-plop/inquirer/rxjs/tslib" ; "full key")]
+    fn test_resolve_package(workspace: &str, name: &str, version: &str, expected: &str) {
+        let lockfile = BunLockfile::from_str(BASIC_LOCKFILE).unwrap();
+        let result = lockfile
+            .resolve_package(workspace, name, version)
+            .unwrap()
+            .unwrap();
+        assert_eq!(result.key, expected);
+    }
+
+    const TURBO_GEN_DEPS: &[&str] = [
+        "@turbo/gen/chalk",
+        "@turbo/gen/minimatch",
+        "@turbo/workspaces",
+        "commander",
+        "fs-extra",
+        "inquirer",
+        "node-plop",
+        "proxy-agent",
+        "ts-node",
+        "update-check",
+        "validate-npm-package-name",
+    ]
+    .as_slice();
+    const TURBO_GEN_CHALK_DEPS: &[&str] = [
+        "@turbo/gen/chalk/ansi-styles",
+        "@turbo/gen/chalk/escape-string-regexp",
+        "@turbo/gen/chalk/supports-color",
+    ]
+    .as_slice();
+    const CHALK_DEPS: &[&str] = ["ansi-styles", "supports-color"].as_slice();
+
+    #[test_case("@turbo/gen", TURBO_GEN_DEPS)]
+    #[test_case("@turbo/gen/chalk", TURBO_GEN_CHALK_DEPS)]
+    #[test_case("chalk", CHALK_DEPS)]
+    fn test_all_dependencies(key: &str, expected: &[&str]) {
+        let lockfile = BunLockfile::from_str(BASIC_LOCKFILE).unwrap();
+        let mut expected = expected.to_vec();
+        expected.sort();
+
+        let mut actual = lockfile
+            .all_dependencies(key)
+            .unwrap()
+            .unwrap()
+            .into_keys()
+            .collect::<Vec<_>>();
+        actual.sort();
+        assert_eq!(actual, expected);
     }
 }
