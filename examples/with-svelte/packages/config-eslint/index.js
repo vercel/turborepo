3e57a37c8fbf523b4670fd7829e2f153e46afe26
Anthony Shew <anthonyshew@gmail.com>
2025-01-27 11:11:02 -0700

    WIP

--- examples/with-svelte/packages/config-eslint/index.js
+++ examples/with-svelte/packages/config-eslint/index.js
@@ -1,31 +1,61 @@
-module.exports = {
-  root: true,
-  extends: [
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import parser from 'svelte-eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export const config = [
+  ...compat.extends(
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:svelte/recommended',
     'prettier',
     'turbo'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-    extraFileExtensions: ['.svelte']
+  ),
+  {
+    ignores: ['.svelte-kit/**']
   },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+
+      parserOptions: {
+        extraFileExtensions: ['.svelte']
+      }
+    }
   },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: parser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
       parserOptions: {
         parser: '@typescript-eslint/parser'
       }
     }
-  ]
-};
+  }
+];
