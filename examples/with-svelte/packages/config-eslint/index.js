6f28a20e1b7a0e45377f857482b8a22a9ea30d14
Anthony Shew <anthonyshew@gmail.com>
2025-01-27 11:40:06 -0700

    WIP

--- examples/with-svelte/packages/config-eslint/index.js
+++ examples/with-svelte/packages/config-eslint/index.js
@@ -1,61 +1,27 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import parser from 'svelte-eslint-parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import ts from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
-
-export const config = [
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended',
-    'prettier',
-    'turbo'
-  ),
+export const config = ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs['flat/recommended'],
   {
-    ignores: ['.svelte-kit/**']
-  },
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslint
-    },
-
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node
-      },
-
-      parser: tsParser,
-      ecmaVersion: 2020,
-      sourceType: 'module',
-
-      parserOptions: {
-        extraFileExtensions: ['.svelte']
       }
     }
   },
   {
     files: ['**/*.svelte'],
+    ignores: ['.svelte-kit/*'],
     languageOptions: {
-      parser: parser,
-      ecmaVersion: 5,
-      sourceType: 'script',
-
       parserOptions: {
-        parser: '@typescript-eslint/parser'
+        parser: ts.parser
       }
     }
   }
-];
+);
