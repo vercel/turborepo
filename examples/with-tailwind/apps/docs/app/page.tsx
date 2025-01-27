f67764bf83d95fe450a9fd639fd8ad44ac74deaa
kayumuzzaman <46846503+kayumuzzaman@users.noreply.github.com>
2025-01-28 00:20:10 +0600

    fix(example): remove conflict css property from with-tailwind example (#9816)
    Co-authored-by: Anthony Shew <anthony.shew@vercel.com>

--- examples/with-tailwind/apps/docs/app/page.tsx
+++ examples/with-tailwind/apps/docs/app/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-        <p className="fixed top-0 left-0 flex justify-center w-full px-4 pt-8 pb-6 border-b bg-gradient-to-b backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
+        <p className="fixed top-0 left-0 flex justify-center w-full px-4 pt-8 pb-6 border-b bg-gradient-to-b backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4">
           examples/with-tailwind -&nbsp;
           <code className="font-mono font-bold">docs</code>
         </p>
