c3127e0af0c96201ca6bd3234ff8d82ba3bc525d
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2025-01-31 14:48:51 -0500

    wip

--- packages/turbo-repository/scripts/build.sh
+++ packages/turbo-repository/scripts/build.sh
@@ -3,14 +3,23 @@
 user_provided_flags="$@"
 script_provided_flags="\
   --platform \
-  -p turborepo-napi \
-  --cargo-cwd ../../ \
-  --cargo-name turborepo_napi \
+  -p=turborepo-napi \
+  --cargo-cwd=../../ \
+  --cargo-name=turborepo_napi \
   native \
-  --js false \
+  --js=false \
 "
 
-node_modules/.bin/napi build "$user_provided_flags" "$script_provided_flags"
+for flag in $user_provided_flags; do
+  if [[ $flag == --target=* ]]; then
+    target=${flag#*=}
+    rustup target add "$target"
+  fi
+done
+
+flags="$script_provided_flags $user_provided_flags"
+echo $flags
+node_modules/.bin/napi build "$flags"
 
 # Unfortunately, when napi generates a .d.ts file, it doesn't match our formatting rules (it doesn't have semicolons).
 # Since there's now way to configure this from napi itself, so we need to run prettier on it after generating it.
