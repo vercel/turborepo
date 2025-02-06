3b9881a182c44892cdc67b3826bc5565c113d7d7
Chris Olszewski <chris.olszewski@vercel.com>
2025-02-06 10:18:23 -0500

    fix: Revert "chore(release): remove goreleaser" (#9908)
    Reverts vercel/turborepo#9905
    
    Something up with the actual publish step

---
+++ cli/scripts/npm-native-packages/npm-native-packages.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+
+// Map to node os and arch names.
+const nodeOsLookup = {
+  darwin: "darwin",
+  linux: "linux",
+  windows: "win32",
+};
+
+const nodeArchLookup = {
+  amd64: "x64",
+  arm64: "arm64",
+};
+
+const humanizedArchLookup = {
+  amd64: "64",
+  arm64: "arm64",
+};
+
+const template = require("./template/template.package.json");
+const os = process.argv[2];
+const arch = process.argv[3];
+const version = process.argv[4];
+
+template.name = `turbo-${os}-${humanizedArchLookup[arch]}`;
+template.description = `The ${os}-${humanizedArchLookup[arch]} binary for turbo, a monorepo build system.`;
+template.os = [nodeOsLookup[os]];
+template.cpu = [nodeArchLookup[arch]];
+template.version = version;
+
+const outputPath = path.join(__dirname, "build", template.name);
+fs.rmSync(outputPath, { recursive: true, force: true });
+fs.mkdirSync(path.join(outputPath, "bin"), { recursive: true });
+
+if (os === "windows") {
+  fs.copyFileSync(
+    path.join(__dirname, "template", "bin", "turbo"),
+    path.join(outputPath, "bin", "turbo")
+  );
+}
+fs.copyFileSync(
+  path.join(__dirname, "template", "README.md"),
+  path.join(outputPath, "README.md")
+);
+fs.writeFileSync(
+  path.join(outputPath, "package.json"),
+  JSON.stringify(template, null, 2)
+);
--- packages/turbo-releaser/src/types.ts
+++
@@ -1,8 +0,0 @@
-export type SupportedOS = "darwin" | "linux" | "windows";
-export type SupportedArch = "x64" | "arm64";
-export type HumanArch = "64" | "arm64";
-
-export interface Platform {
-  os: SupportedOS;
-  arch: SupportedArch;
-}
