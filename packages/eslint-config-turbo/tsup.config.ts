08eca1601fc2634431da064e647470f036fabd99
Anthony Shew <anthonyshew@gmail.com>
2025-02-14 12:44:51 -0700

    WIP

--- packages/eslint-config-turbo/tsup.config.ts
+++ packages/eslint-config-turbo/tsup.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, type Options } from "tsup";
 
+// eslint-disable-next-line import/no-default-export -- Default export needed
 export default defineConfig((options: Options) => ({
   entry: ["index.ts", "flat/index.ts"],
   clean: true,
