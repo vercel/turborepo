4cc60f89b87cda399559a7ce849b249086906ab8
Tyoneb <59046401+Tyoneb@users.noreply.github.com>
2025-03-05 15:30:26 +0100

    docs: fix JSDoc annotation for ESLint flat configs of basic example (#10089)
    ### Description
    
    This is a very minor change I noticed when copying the basic example.
    The JSDoc annotation for the ESLint flat configs is incorrect,
    indicating a config object instead of an array of config objects, which
    prevents the IDE from providing proper autocomplete.
    
    ### Testing Instructions
    
    The IDE autocomplete feature allows to very quickly validate the change.

--- examples/basic/packages/eslint-config/next.js
+++ examples/basic/packages/eslint-config/next.js
@@ -10,7 +10,7 @@ import { config as baseConfig } from "./base.js";
 /**
  * A custom ESLint configuration for libraries that use Next.js.
  *
- * @type {import("eslint").Linter.Config}
+ * @type {import("eslint").Linter.Config[]}
  * */
 export const nextJsConfig = [
   ...baseConfig,
