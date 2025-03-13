b7374c41bdb3cfa7b4d1327436e018490c2d32e3
Anthony Shew <anthonyshew@gmail.com>
2025-03-13 15:07:33 -0600

    WIP cec55

--- docs/site/components/search-dialog.tsx
+++ docs/site/components/search-dialog.tsx
@@ -4,7 +4,6 @@ import { gitHubRepoUrl } from "@/lib/constants";
 import algo from "algoliasearch/lite";
 import type { SharedProps } from "fumadocs-ui/components/dialog/search";
 import FumaSearchDialog from "fumadocs-ui/components/dialog/search-algolia";
-import { usePathname } from "next/navigation";
 
 const client = algo(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
@@ -14,8 +13,6 @@ const client = algo(
 const index = client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX!);
 
 export function SearchDialog(props: SharedProps): JSX.Element {
-  const path = usePathname();
-
   return (
     <FumaSearchDialog
       index={index}
@@ -23,7 +20,6 @@ export function SearchDialog(props: SharedProps): JSX.Element {
       links={[
         ["Home", "/"],
         ["Turborepo documentation", "/repo/docs"],
-        ["Turbopack documentation", "/pack/docs"],
         ["Blog", "/blog"],
         ["Changelog", `${gitHubRepoUrl}/releases`],
         ["Github", gitHubRepoUrl],
