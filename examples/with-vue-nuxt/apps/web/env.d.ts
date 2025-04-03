6a670f5d54ce8589b812c21e000422e208a99f6b
Milos Milosevic <highlander.mm@gmail.com>
2025-04-03 13:26:09 +0200

    fix(examples): fix build in fresh vue example project

--- examples/with-vue-nuxt/apps/web/env.d.ts
+++ examples/with-vue-nuxt/apps/web/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+
+  const component: DefineComponent<{}, {}, any>;
+
+  export default component;
+}
