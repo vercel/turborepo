2df9e084a59b33bb31a48bcb646e1ad43d514d35
Anthony Shew <anthony.shew@vercel.com>
2025-04-21 12:50:19 -0600

    docs: copy to markdown button (#10355)
    ### Description
    
    Provides a button for a docs page to be copied to the user's clipboard
    as markdown, presumably to paste into an LLM.
    
    ### Testing Instructions
    
    Try the button!
    
    https://turbo-site-git-shew-454de.vercel.sh/docs/getting-started/examples

--- docs/site/app/(sidebar)/docs/[[...slug]]/page.tsx
+++ docs/site/app/(sidebar)/docs/[[...slug]]/page.tsx
@@ -1,9 +1,11 @@
 import { notFound } from "next/navigation";
+import { readFileSync } from "fs";
 import type { Metadata } from "next/types";
 import { repoDocsPages } from "@/app/source";
 import { createMetadata } from "@/lib/create-metadata";
 import { mdxComponents } from "@/mdx-components";
 import { SystemEnvironmentVariablesHashHighlighter } from "./system-environment-variables-hash-highlighter";
+import { CopyToMarkdown } from "@/components/copy-to-markdown";
 
 export async function generateMetadata(props: {
   params: Promise<{ slug?: string[] }>;
@@ -36,14 +38,25 @@ export default async function Page(props: {
     notFound();
   }
 
+  const rawMarkdown = readFileSync(page.data._file.absolutePath)
+    .toString()
+    // Removes frontmatter
+    .replace(/^---\n(.*?\n)---\n/s, "")
+    // Removes import statements for components
+    .replace(/^import\s+{[^}]+}\s+from\s+['"]#\/[^'"]+['"];(\r?\n|$)/gm, "");
+
   const Mdx = page.data.body;
 
   return (
     <>
       <SystemEnvironmentVariablesHashHighlighter />
-      <h1 className="scroll-m-7 text-4xl font-semibold tracking-normal">
-        {page.data.title}
-      </h1>
+      <div className="flex justify-between gap-4">
+        <h1 className="scroll-m-7 text-4xl font-semibold tracking-normal">
+          {page.data.title}
+        </h1>
+
+        <CopyToMarkdown markdownContent={rawMarkdown} />
+      </div>
       <Mdx components={mdxComponents} />
     </>
   );
