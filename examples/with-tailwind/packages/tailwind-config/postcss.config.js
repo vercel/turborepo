e10a103dfdc0d03ab86553885d48c4e5bc43b1fc
Anthony Shew <anthony.shew@vercel.com>
2025-05-05 15:35:25 -0600

    docs(examples): drop JS config from Tailwind example (#10445)
    ### Description
    
    Found out that JS configurations aren't hip with the kids these days, so
    switching out the approach. The CSS variables are a bit contrived, but
    work great for showing off what we're needing to show off.

--- examples/with-tailwind/packages/tailwind-config/postcss.config.js
+++ examples/with-tailwind/packages/tailwind-config/postcss.config.js
@@ -1,3 +1,4 @@
+// Optional PostCSS configuration for applications that need it
 export const postcssConfig = {
   plugins: {
     "@tailwindcss/postcss": {},
--- examples/with-tailwind/packages/tailwind-config/tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "tailwindcss";
-
-const config: Omit<Config, "content"> = {
-  darkMode: "class",
-};
-export default config;
