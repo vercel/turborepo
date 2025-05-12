3a3d8b9fbdcaab5b6b699fbba54af64fa6c3a8f4
Anthony Shew <anthonyshew@gmail.com>
2025-05-11 23:03:20 -0600

    WIP 2e53d

--- packages/eslint-plugin-turbo/lib/rules/no-undeclared-env-vars.ts
+++ packages/eslint-plugin-turbo/lib/rules/no-undeclared-env-vars.ts
@@ -264,11 +264,10 @@ function create(context: RuleContextWithOptions): Rule.RuleListener {
 
   return {
     Program() {
+      // Reload project configuration so that changes show in the user's editor
       project.reload();
     },
     MemberExpression(node) {
-      // Reload project configuration so that changes show in the user's editor
-
       // we only care about complete process env declarations and non-computed keys
       if (isProcessEnv(node) || isImportMetaEnv(node)) {
         // we're doing something with process.env
