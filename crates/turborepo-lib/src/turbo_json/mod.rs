649a0dcf03328055249a4a6dc24f192f1f328990
Chris Olszewski <chris.olszewski@vercel.com>
2025-08-26 16:26:07 -0400

    chore(turbo_json): move validation logic to new module

--- crates/turborepo-lib/src/turbo_json/mod.rs
+++ crates/turborepo-lib/src/turbo_json/mod.rs
@@ -8,7 +8,6 @@ use std::{
 use biome_deserialize_macros::Deserializable;
 use camino::Utf8Path;
 use clap::ValueEnum;
-use miette::{NamedSource, SourceSpan};
 use serde::{Deserialize, Serialize};
 use turbopath::{AbsoluteSystemPath, RelativeUnixPath};
 use turborepo_errors::Spanned;
@@ -28,6 +27,7 @@ mod loader;
 pub mod parser;
 mod processed;
 mod raw;
+pub mod validator;
 
 pub use future_flags::FutureFlags;
 pub use loader::{TurboJsonLoader, TurboJsonReader};
@@ -35,8 +35,9 @@ pub use processed::ProcessedTaskDefinition;
 pub use raw::{
     RawPackageTurboJson, RawRemoteCacheOptions, RawRootTurboJson, RawTaskDefinition, RawTurboJson,
 };
+use validator::TurboJSONValidation;
 
-use crate::{boundaries::BoundariesConfig, config::UnnecessaryPackageTaskSyntaxError};
+use crate::boundaries::BoundariesConfig;
 
 const ENV_PIPELINE_DELIMITER: &str = "$";
 const TOPOLOGICAL_PIPELINE_DELIMITER: &str = "^";
@@ -466,72 +467,6 @@ impl TurboJson {
 
         with_tasks.push(Spanned::new(UnescapedString::from(with.to_string())))
     }
-}
-
-type TurboJSONValidation = fn(&TurboJson) -> Vec<Error>;
-
-pub fn validate_no_package_task_syntax(turbo_json: &TurboJson) -> Vec<Error> {
-    turbo_json
-        .tasks
-        .iter()
-        .filter(|(task_name, _)| task_name.is_package_task())
-        .map(|(task_name, entry)| {
-            let (span, text) = entry.span_and_text("turbo.json");
-            Error::UnnecessaryPackageTaskSyntax(Box::new(UnnecessaryPackageTaskSyntaxError {
-                actual: task_name.to_string(),
-                wanted: task_name.task().to_string(),
-                span,
-                text,
-            }))
-        })
-        .collect()
-}
-
-pub fn validate_extends(turbo_json: &TurboJson) -> Vec<Error> {
-    match turbo_json.extends.first() {
-        Some(package_name) if package_name != ROOT_PKG_NAME || turbo_json.extends.len() > 1 => {
-            let (span, text) = turbo_json.extends.span_and_text("turbo.json");
-            vec![Error::ExtendFromNonRoot { span, text }]
-        }
-        None => {
-            let path = turbo_json
-                .path
-                .as_ref()
-                .map_or("turbo.json", |p| p.as_ref());
-
-            let (span, text) = match turbo_json.text {
-                Some(ref text) => {
-                    let len = text.len();
-                    let span: SourceSpan = (0, len - 1).into();
-                    (Some(span), text.to_string())
-                }
-                None => (None, String::new()),
-            };
-
-            vec![Error::NoExtends {
-                span,
-                text: NamedSource::new(path, text),
-            }]
-        }
-        _ => vec![],
-    }
-}
-
-pub fn validate_with_has_no_topo(turbo_json: &TurboJson) -> Vec<Error> {
-    turbo_json
-        .tasks
-        .iter()
-        .flat_map(|(_, definition)| {
-            definition.with.iter().flatten().filter_map(|with_task| {
-                if with_task.starts_with(TOPOLOGICAL_PIPELINE_DELIMITER) {
-                    let (span, text) = with_task.span_and_text("turbo.json");
-                    Some(Error::InvalidTaskWith { span, text })
-                } else {
-                    None
-                }
-            })
-        })
-        .collect()
 }
 
 fn gather_env_vars(
@@ -1173,31 +1108,5 @@ mod tests {
             assert!(raw_config.global_dependencies.is_none());
             assert!(raw_config.future_flags.is_none());
         }
-    }
-
-    #[test]
-    fn test_validate_with_has_no_topo() {
-        let turbo_json = TurboJson {
-            tasks: Pipeline(
-                vec![(
-                    TaskName::from("dev"),
-                    Spanned::new(RawTaskDefinition {
-                        with: Some(vec![Spanned::new(UnescapedString::from("^proxy"))]),
-                        ..Default::default()
-                    }),
-                )]
-                .into_iter()
-                .collect(),
-            ),
-            ..Default::default()
-        };
-
-        let errs = validate_with_has_no_topo(&turbo_json);
-        assert_eq!(errs.len(), 1);
-        let error = &errs[0];
-        assert_eq!(
-            error.to_string(),
-            "`with` cannot use dependency relationships."
-        );
     }
 }
