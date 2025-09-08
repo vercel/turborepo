aca3830b260989885da4d5532b1ee2eb82089a83
Anthony Shew <anthonyshew@gmail.com>
2025-09-08 06:49:48 -0600

    fix: use newline for separator in misuse text

--- crates/turborepo-lib/src/cli/error.rs
+++ crates/turborepo-lib/src/cli/error.rs
@@ -4,12 +4,12 @@ use itertools::Itertools;
 use miette::Diagnostic;
 use thiserror::Error;
 use turborepo_repository::package_graph;
-use turborepo_signals::{listeners::get_signal, SignalHandler};
+use turborepo_signals::{SignalHandler, listeners::get_signal};
 use turborepo_telemetry::events::command::CommandEventBuilder;
-use turborepo_ui::{color, BOLD, GREY};
+use turborepo_ui::{BOLD, GREY, color};
 
 use crate::{
-    commands::{bin, generate, get_mfe_port, link, login, ls, prune, CommandBase},
+    commands::{CommandBase, bin, generate, get_mfe_port, link, login, ls, prune},
     daemon::DaemonError,
     query,
     rewrite_json::RewriteError,
