a24611fb64af47268772e2f45455498fdd3567f3
Anthony Shew <anthonyshew@gmail.com>
2025-09-30 21:52:03 -0700

    WIP 48526

--- examples/with-microfrontends/apps/docs/app/docs/layout.tsx
+++ examples/with-microfrontends/apps/docs/app/docs/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "../globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
 });
 
--- examples/with-microfrontends/apps/docs/app/docs/page.tsx
+++ examples/with-microfrontends/apps/docs/app/docs/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/docs/app/page.tsx</code>
+            Get started by editing <code>apps/docs/app/docs/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
