cbf5181510f55da8f908008ce7db53351545c766
Nathan M. <neodarksoulink@gmail.com>
2025-10-13 15:46:38 +0300

    refactor(examples): enhance `with-nestjs` (#8131) (#10964)
    ### Description
    
    Recently mentioned by @fzzv in his comment:
    https://github.com/vercel/turborepo/issues/8131#issuecomment-3393825635.
    
    This PR improves the global aspect and fixes some minor issues in the
    `with-nestjs` example.
    
    ### Changelogs
    
    - Fix `@repo/api` types exportation
    - Enhance `apps/wep/` app example (now retrieve links from server)
    - Fix `apps/api` Lint errors
    - Enhance the `README` file's documentation
    
    ### Testing
    
    > For the testing instructions, refer to the [integration PR testing
    instructions](https://github.com/vercel/turborepo/pull/8162)
    
    All the commands work as expected, notably the main ones, as we can see:
    
    > Build Command
    > <img width="1507" height="650" alt="Screenshot_14"
    src="https://github.com/user-attachments/assets/69996afc-a265-4341-9f0c-4108b6bebfa0"
    />
    
    > Test command
    > <img width="1277" height="717" alt="Screenshot_13"
    src="https://github.com/user-attachments/assets/d6a18a39-e399-46b0-9463-cbd0ca692f48"
    />
    
    ---
    
    > Resolve #10877
    > Resolve #10928

--- examples/with-nestjs/apps/api/src/main.ts
+++ examples/with-nestjs/apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 
