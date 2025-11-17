caee619c69e064bcc85870c4b6f3be0cf104f432
Anthony Shew <anthony.shew@vercel.com>
2025-11-17 10:59:53 -0700

    feat: Add Windows-specific env var to default passthroughs (#11126)
    ### Description
    
    In https://github.com/vercel/turborepo/issues/11114, dotnet on Windows
    wasn't working for an unexpected reason. It turned out that there's a
    `ProgramFiles(x86)` environment variable that's important to the setup.
    
    I did some digging and this is what I found:
    
    > On 64-bit Windows, 32-bit applications are typically installed in a
    separate directory to maintain compatibility and prevent conflicts with
    64-bit applications. This directory is designated by ProgramFiles(x86).
    
    This sounds like it fits into being a part of our default passthroughs,
    given its system-level nature.
    
    ### Testing Instructions
    
    CI

--- crates/turborepo-lib/src/task_hash.rs
+++ crates/turborepo-lib/src/task_hash.rs
@@ -463,6 +463,7 @@ impl<'a> TaskHasher<'a> {
                     "TEMP",
                     // Windows
                     "WINDIR",
+                    "ProgramFiles(x86)",
                     // VSCode IDE - https://github.com/microsoft/vscode-js-debug/blob/5b0f41dbe845d693a541c1fae30cec04c878216f/src/targets/node/nodeLauncherBase.ts#L320
                     "VSCODE_*",
                     "ELECTRON_RUN_AS_NODE",
