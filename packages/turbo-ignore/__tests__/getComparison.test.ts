84a9b18fca9112267228299dbd497b42ea552d23
Jatin Bisen <jatinnbisen@gmail.com>
2025-12-04 22:22:19 +0530

    Revert "fix: Command injection in `turbo-ignore` (#11154)"

--- packages/turbo-ignore/__tests__/getComparison.test.ts
+++ packages/turbo-ignore/__tests__/getComparison.test.ts
@@ -83,7 +83,7 @@ describe("getComparison()", () => {
 
   it("uses previousDeploy when running in Vercel CI with VERCEL_GIT_PREVIOUS_SHA", () => {
     const mockExec = jest
-      .spyOn(child_process, "execFileSync")
+      .spyOn(child_process, "execSync")
       .mockReturnValue("commit");
 
     process.env.VERCEL = "1";
@@ -108,7 +108,7 @@ describe("getComparison()", () => {
 
   it("uses fallback when running in Vercel CI with unreachable VERCEL_GIT_PREVIOUS_SHA", () => {
     const mockExec = jest
-      .spyOn(child_process, "execFileSync")
+      .spyOn(child_process, "execSync")
       .mockImplementation(() => {
         throw new Error("fatal: Not a valid object name mygitsha");
       });
@@ -137,7 +137,7 @@ describe("getComparison()", () => {
 
   it("returns null running in Vercel CI with unreachable VERCEL_GIT_PREVIOUS_SHA and no fallback", () => {
     const mockExec = jest
-      .spyOn(child_process, "execFileSync")
+      .spyOn(child_process, "execSync")
       .mockImplementation(() => {
         throw new Error("fatal: Not a valid object name mygitsha");
       });
@@ -158,7 +158,7 @@ describe("getComparison()", () => {
 
   it("modifies output when running in Vercel CI with VERCEL_GIT_PREVIOUS_SHA but no VERCEL_GIT_COMMIT_REF", () => {
     const mockExec = jest
-      .spyOn(child_process, "execFileSync")
+      .spyOn(child_process, "execSync")
       .mockReturnValue("commit");
 
     process.env.VERCEL = "1";
@@ -182,7 +182,7 @@ describe("getComparison()", () => {
 
   it("modifies output when running in Vercel CI with unreachable VERCEL_GIT_PREVIOUS_SHA and no VERCEL_GIT_COMMIT_REF", () => {
     const mockExec = jest
-      .spyOn(child_process, "execFileSync")
+      .spyOn(child_process, "execSync")
       .mockImplementation(() => {
         throw new Error("fatal: Not a valid object name mygitsha");
       });
