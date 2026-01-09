3eacd46a3f337918024764016d029cd5862edc4e
Anthony Shew <anthonyshew@gmail.com>
2026-01-08 17:44:00 -0700

    devtools

---
+++ docs/site/app/[lang]/devtools/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+import { DevtoolsClientComponent } from "./devtools-client";
+
+export const metadata: Metadata = {
+  title: "Turborepo Devtools",
+  description: "Visualize your Turborepo package and task graphs"
+};
+
+export default function DevtoolsPage() {
+  return <DevtoolsClientComponent />;
+}
