6a43d9d37dceb64af9df41bf3d75972afd3eac2c
Anthony Shew <anthonyshew@gmail.com>
2026-01-10 06:37:40 -0700

    WIP 4cb9c

--- docs/site/lib/openapi.ts
+++ docs/site/lib/openapi.ts
@@ -2,7 +2,6 @@ import { createOpenAPI } from "fumadocs-openapi/server";
 import spec from "./remote-cache-openapi.json";
 
 export const openapi = createOpenAPI({
-  // @ts-expect-error - fumadocs-openapi types are out of sync with runtime
   input: () => ({
     "remote-cache": spec
   })
