efaaca34861d79175afe92d170e3dec685bf13b5
Florian Salihovic <florian.salihovic@me.com>
2026-03-22 07:30:15 +0100

    fix(examples): Update of `with-svelte` template - updating dependencies - using the `.gitignore` file for ignore patterns in eslint config - using the eslint config by reference in `apps/docs`, `apps/web`, and `packages/ui`

--- examples/with-svelte/packages/ui/eslint.config.js
+++ examples/with-svelte/packages/ui/eslint.config.js
@@ -1,15 +1,3 @@
-import ts from 'typescript-eslint';
 import { config } from '@repo/eslint-config/index.js';
 
-export default [
-  ...config,
-  {
-    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.ts'],
-    ignores: ['.svelte-kit/**', 'dist/**'],
-    languageOptions: {
-      parserOptions: {
-        parser: ts.parser
-      }
-    }
-  }
-];
+export default [...config];
