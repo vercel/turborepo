47ec5113f51121ffffc942f969b65392c00e061b
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2026-08-18 20:45:25 +0200

    fix: Refine responsive showcase columns

--- apps/docs/app/[lang]/(home)/showcase/page.tsx
+++ apps/docs/app/[lang]/(home)/showcase/page.tsx
@@ -21,7 +21,7 @@ function Showcase() {
         </div>
       </div>
 
-      <div className="mb-8 px-0 min-h-screen sm:px-8 grid grid-cols-3 items-center gap-16 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 ">
+      <div className="mb-8 grid min-h-screen grid-cols-2 items-center gap-6 sm:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7">
         <Clients linked />
       </div>
     </main>
